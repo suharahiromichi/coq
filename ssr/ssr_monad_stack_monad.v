746c79d5b6a45029ba5aa9dc07a6a8257287238d
SUHARA Hiromichi <suharahiromichi@gmail.com>
2014-02-02 20:00:47 +0900

    モナドの定義を見直す。可換図式の証明を追加する。

--- ssr/ssr_monad_stack_monad.v
+++ ssr/ssr_monad_stack_monad.v
@@ -4,35 +4,45 @@
 (** @suharahiromichi 2014_02_01 *)
 
 Require Import ssreflect ssrbool ssrnat seq eqtype ssrfun.
+Require Import String.                      (* Error *)
 
 (** スタックモナド Stack Monad の定義 *)
-Definition Stack := seq nat -> nat * seq nat.
+Definition Stack (T : Type) :=
+  seq nat -> (T * seq nat).
 
 (** 主要な関数の定義  *)
-Definition ret : nat -> Stack :=
-  fun n : nat => fun q : seq nat => (n, q).
+Definition ret {T : Type} : T -> Stack T :=
+  fun n : T => fun q : seq nat => (n, q).
 
-Definition bind : Stack -> (nat -> Stack) -> Stack :=
-  fun s : Stack =>
-    fun c : nat -> Stack =>
+Definition bind {T S : Type} : Stack T -> (T -> Stack S) -> Stack S :=
+  fun s : Stack T =>
+    fun c : T -> Stack S =>
       fun q : seq nat =>
         let (n, q') := s q in c n q'.
 Infix ">>=" := bind (right associativity, at level 61).
 (** 右結合で、優先順位は = より強い。(=は70)  *)
 
-Definition bind2 : Stack -> Stack -> Stack :=
-  fun s1 : Stack =>
-    fun s2 : Stack =>
+Definition bind2 {T S : Type} : Stack T -> Stack S -> Stack S :=
+  fun s1 : Stack T =>
+    fun s2 : Stack S =>
       s1 >>= fun _ => s2.
 Infix ">>" := bind2 (right associativity, at level 61).
 
+Definition id_ {T : Type} (v : T) : T := v.
+
+Definition fmap_ {T S : Type} (f : T -> S) (mx : Stack T) : Stack S :=
+  mx >>= (fun x => ret (f x)).
+
+Definition join_ {T : Type} (s : Stack (Stack T)) : Stack T :=
+  s >>= id_.
+
 (** スタック処理の関数  *)
-Definition push : nat -> Stack :=
+Definition push : nat -> Stack nat :=
   fun n : nat =>
     fun q : seq nat =>
       (0, n :: q).
 
-Definition pop : Stack :=
+Definition pop : Stack nat :=
   fun q : seq nat =>
     match q with
       | n :: q' => (n, q')
@@ -40,12 +50,12 @@ Definition pop : Stack :=
     end.
 
 (** 空のスタックを作る。 *)
-Definition empty : Stack :=
+Definition empty : Stack nat :=
   fun _ => (0, [::]).
 
 (** 二項演算のいろいろな定義 *)
 (** 直接定義する方法 *)
-Definition binary_op2 (op : nat -> nat -> nat) : Stack :=
+Definition binary_op2 (op : nat -> nat -> nat) : Stack nat :=
   fun q : seq nat =>
     match q with
       | n :: m :: q' => (0, (op m n) :: q')
@@ -56,7 +66,7 @@ Definition SMinus2 := binary_op2 minus.
 Definition SMult2 := binary_op2 mult.
 
 (** push, pop を使う方法 *)
-Definition binary_op1 (op : nat -> nat -> nat) : Stack :=
+Definition binary_op1 (op : nat -> nat -> nat) : Stack nat :=
   fun q : seq nat =>
     let (n, q') := pop q in
     let (m, q'') := pop q' in
@@ -66,13 +76,13 @@ Definition SMinus1 := binary_op1 minus.
 Definition SMult1 := binary_op1 mult.
 
 (** bind, ret も使う方法 *)
-Definition binary_op (op : nat -> nat -> nat) : Stack :=
+Definition binary_op (op : nat -> nat -> nat) : Stack nat :=
   pop >>=
       fun n => pop >>=                      (* n は後にpushしたもの。 *)
                    fun m => push (op m n).  (* m は先にpushしたもの。 *)
-Definition SPlus : Stack := binary_op plus.
-Definition SMinus : Stack := binary_op minus.
-Definition SMult : Stack := binary_op mult.
+Definition SPlus : Stack nat := binary_op plus.
+Definition SMinus : Stack nat := binary_op minus.
+Definition SMult : Stack nat := binary_op mult.
 
 (** 実行例 *)
 (** 3 + (5 - 2) = 6 *)
@@ -101,12 +111,14 @@ Qed.
 
 (** モナド則を証明する。 *)
 Axiom functional_extensionality :
-  forall {f g : Stack},
+  forall {T : Type},
+  forall {f g : Stack T},
     (forall (q : seq nat), f q = g q) -> f = g.
 
 (** モナド則 その1 *)
 Theorem monad_1 :
-  forall (f : nat -> Stack) (n : nat),
+  forall (T S : Type),
+  forall (f : T -> Stack S) (n : T),
     ret n >>= f = f n.
 Proof.
   done.
@@ -114,10 +126,11 @@ Qed.
   
 (** モナド則 その2 *)
 Theorem monad_2 :
-  forall (s : Stack),
+  forall (T : Type),
+  forall (s : Stack T),
     s >>= ret = s.
 Proof.
-  move=> s.
+  move=> T s.
   apply: functional_extensionality.
   move=> q.
   rewrite /bind /ret.
@@ -127,11 +140,12 @@ Qed.
 
 (** モナド則 その3 *)
 Theorem monad_3 :
-  forall (f g : nat -> Stack) (s : Stack),
+  forall (T S R : Type),
+  forall (f : T -> Stack S) (g : S -> Stack R) (s : Stack T),
     (s >>= f) >>= g =
     s >>= (fun n => f n >>= g).
 Proof.
-  move=> f g s.
+  move=> T S R f g s.
   apply: functional_extensionality.
   move=> q.
   rewrite /bind.
@@ -139,32 +153,63 @@ Proof.
   done.
 Qed.
 
-(** 補足：SSReflect的には、
+(** 可換図式を証明する。 *)
+Theorem assoc_law :
+  forall (T : Type) (s : Stack (Stack (Stack T))),
+    join_ (join_ s) = join_ ((fmap_ join_) s).
+Proof.
+  move => T s.
+  rewrite /join_ /fmap_ /ret.
+  by rewrite !monad_3.
+Qed.
+
+Theorem unit_left_law :
+  forall (T : Type) (s : Stack (Stack T)),
+    join_ (ret s) = id_ s.
+Proof.
+  move => T s.
+  rewrite /join_.
+  by rewrite monad_1.
+Qed.
+
+Theorem unit_right_law :
+  forall (T : Type) (m : Stack (Stack T)),
+    join_ ((fmap_ ret) m) = id_ m.
+Proof.
+  move=> A m.
+  rewrite /join_ /fmap_ /id_.
+  by rewrite monad_3 monad_2.
+Qed.
+
+(** おまけ：SSReflect的には、
 functional_extensionality を使わずに、
 「=1」での成立をいうのがよいだろうか？ *)
 Theorem monad_1' :
-  forall (f : nat -> Stack) (n : nat),
+  forall (T S : Type),
+  forall (f : T -> Stack S) (n : T),
     ret n >>= f =1 f n.
 Proof.
   done.
 Qed.
 
 Theorem monad_2' :
-  forall (s : Stack),
+  forall (T : Type),
+  forall (s : Stack T),
     s >>= ret =1 s.
 Proof.
-  move=> s q.
+  move=> T s q.
   rewrite /bind /ret.
   destruct s.
   done.
 Qed.
 
 Theorem monad_3' :
-  forall (f g : nat -> Stack) (s : Stack),
+  forall (T S R : Type),
+  forall (f : T -> Stack S) (g : S -> Stack R) (s : Stack T),
     (s >>= f) >>= g =1
     s >>= (fun n => f n >>= g).
 Proof.
-  move=> f g s q.
+  move=> T S R f g s q.
   rewrite /bind.
   destruct s.
   done.
