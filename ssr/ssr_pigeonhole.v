94e7ff78c8e117b58a9b751bc1cfed3504323d4c
SUHARA Hiromichi <suharahiromichi@gmail.com>
2014-04-30 13:15:14 +0900

    Markdownでコメントをつけた。

--- ssr/ssr_pigeonhole.v
+++ ssr/ssr_pigeonhole.v
@@ -1,10 +1,15 @@
 (**
+SSReflectによる鳩の巣定理の証明
+======
+@suharahiromichi
+
 プログラミング Coq 自然数を扱う
 http://www.iij-ii.co.jp/lab/techdoc/coqt/coqt4.html
 
-鳩の巣原理の証明をSSReflectに移した。証明の内容はまったく同じ。
-@suharahiromichi
+鳩の巣原理の証明をSSReflectに移してみました。
+証明の内容はまったく同じなので、比べるとおもしろいとおもいます。
  *)
+
 Require Import ssreflect ssrbool ssrnat seq.
 
 Lemma lt_S_n : forall (n m : nat), S n < S m -> n < m.
@@ -26,31 +31,29 @@ Theorem pigeonhole : forall (xs : list nat),
                        size xs < foldr plus 0 xs ->
                        exists x : nat, InList nat x.+2 xs.
 Proof.
-  move=> xs. elim: xs.
+  elim.
   (* xs = [] の場合 *)
     by [].
   (* xs = x :: xs' の場合 *)
-    move=> /= a.
-    elim: a.
-      (* a = 0 の場合 *)
-      move => xs IHxs H.
-      apply lt_Snm_nm in H.
-      apply IHxs in H.
-      elim: H => x.
-      exists x. by constructor.
+  elim.
+    (* a = 0 の場合 *)
+    move=> xs IHxs H; apply lt_Snm_nm, IHxs in H.
+    by elim: H => x; exists x; constructor.
 
-    move=> /= a.
-    elim: a.
-      (* a = 1 の場合 *)
-      move=> /= H1 xs IHxs H.
-      apply lt_S_n in H.
-      apply IHxs in H.
-      elim: H => x.
-      exists x. by constructor.
+  elim.
+    (* a = 1 の場合 *)
+    move=> _ xs IHxs H; apply lt_S_n, IHxs in H.
+    by elim: H => x; exists x; constructor.
 
-    move=> /= a.
-      (* a >= 2 の場合 *)
-      exists a. by constructor.      
+  (* a >= 2 の場合 *)
+  move=> a.
+  by exists a; constructor.
 Qed.
 
-(* END *)
+(**
+注意：「ソフトウェアの基礎」の練習問題とは、異なるので注意してください。
+
+http://proofcafe.org/sf/Logic_J.
+ *)
+
+(* $Id: ssr_pigeonhole.v,v 1.20 2014/04/30 04:14:36 suhara Exp suhara $ *)
