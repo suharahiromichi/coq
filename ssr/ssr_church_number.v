bfb0bf9c5f3e88fa8b58cef67daa961ba4b448cc
SUHARA Hiromichi <suharahiromichi@gmail.com>
2014-05-12 22:46:39 +0900

    全面改稿した。

--- ssr/ssr_church_number.v
+++ ssr/ssr_church_number.v
@@ -1,5 +1,5 @@
 (**
-「リストは自分自身のfoldr関数として定義される」または、チャーチ数について（仮版）
+「リストは自分自身のfoldr関数として定義される」こと、または、チャーチ数について
 =========
 
 2014_05_11 @suharahiromichi
@@ -10,168 +10,194 @@
 
 TAPL（参考文献1.)には、p.47とp.275の2箇所に渡って、
 「リストは自分自身のfoldr関数として定義される」
-と書かれているが、前後の説明を読んでも（リストのチャーチ表現は理解できるが）、
-この一文の意味することはよくわからない。
+と書かれている。
+しかし、前後の説明を読んでもこの一文の意味することはよくわからなかった。
 
 また、別なところで、
-「（System Fは）fixに頼ることなく純粋な言語でソート関数のようなものが書ける…」
-と重要なことがさらりと書いてあったりもする（同 p.277）。
+「fixに頼ることなく純粋な言語でソート関数のようなものが書ける…」（同 p.277）
+と極めて重要そうなことが書いてある。
+これは、チャーチ数やリストのチャーチ表現がそのような性質を持つのであって、
+System Fはそれらの型付けをすることができる。と理解するべきなのだろう。
 
-以下では、このふたつが同じことを言っていることを示す。
-つまり、System Fの能力はチャーチ表現に基づいている、ということである。
+以上のことを納得するために、数やリストについて、
+
+1. Inductiveな定義
+2. チャーチ表現
+3. fold関数
+
+の関係を調べてみる。
+
+なお、チャーチ数のbuildとunbuildについては参考文献2.を参考にした。
+参考文献2.は、無限大のチャーチ数と不動点演算子の関係について論じているが、
+ここでは、任意の自然数（長さのリスト）に対して、
+Inductiveな定義とチャーチ表現の相互変換ができることを証明する。
 *)
 
 Require Import ssreflect ssrbool ssrnat.
-Require Import ssrfun fintype finset.
 
 (**
 # チャーチ数
-
-natとfoldnatを次のように定義する。
   *)
 
-Inductive nat : Type :=
+(**
+## Inductiveな定義
+*)
+Inductive Nat : Type :=
 | O
-| succ of nat.
+| S of Nat.
 
-Print nat_rect.
-(* 
-nat_rect = 
-fun (P : nat -> Type) (f : P O) (f0 : forall n : nat, P n -> P (succ n)) =>
-fix F (n : nat) : P n :=
-  match n as n0 return (P n0) with
-  | O => f
-  | succ n0 => f0 n0 (F n0)
-  end
-     : forall P : nat -> Type,
-       P O -> (forall n : nat, P n -> P (succ n)) -> forall n : nat, P n
+(**
+## チャーチ数
+*)
+Definition CNat := forall X, (X -> X) -> X -> X.
+
+Definition C0 : CNat := fun X => fun s : X -> X =>
+                                   fun z : X => z.
+Definition C1 : CNat := fun X => fun s : X -> X =>
+                                   fun z : X => s z.
+Definition C2 : CNat := fun X => fun s : X -> X =>
+                                   fun z : X => s (s z).
+
+(**
+## チャーチ数をInductiveな定義にする
+*)
+Definition build (c : CNat) : Nat :=
+  c Nat S O.
+
+Eval compute in build C2.
+(**
+= S (S O) : Nat
+
+buildは、チャーチ数cに、Sをfoldしているとみることもできる。
  *)
 
-Fixpoint foldnat (f : nat -> nat) (n : nat) : nat :=
+(**
+## Fold関数
+*)
+Fixpoint foldNat (X : Type) (s : X -> X) (z : X) (n : Nat) : X :=
   match n with
-    | O => O
-    | succ n' => f (foldnat f n')
+    | O => z
+    | S n' => s (foldNat X s z n')
   end.
 
-Check foldnat.
+Check foldNat.
+(**
+ : forall X : Type, (X -> X) -> X -> Nat -> X
+ *)
 
 (**
-foldnat の型は以下である。
-
-    foldnat : (nat -> nat) -> nat -> nat
-
-(nat -> nat) -> nat -> nat のnatを全称∀で抽象（？）したものが、
-チャーチ数（自然数のチャーチ表現）なのである。
-
-    CNat = ∀X. (X -> X) -> X -> X
+## Inductiveな定義をチャーチ数にする
 *)
+Definition unbuild (n : Nat) : CNat :=
+  fun X =>
+    fun s : X -> X =>
+      fun z : X => foldNat X s z n.
+
+Eval compute in unbuild (S (S O)).
+(**
+ = fun (X : Type) (s : X -> X) (z : X) => s (s z) : CNat
+ *)
 
 (**
-foldnat succ は何もしない関数になる。
+## buildとunbuildとで元に戻ることの証明
 *)
 
-Check foldnat succ.
-Eval compute in foldnat succ (succ (succ (succ O))).
+Theorem build_unbuild :
+  forall n : Nat, build(unbuild n) = n.
+Proof.
+  rewrite /build /unbuild.
+  elim.
+    by [].
+  by rewrite /=; move=> n0 ->.
+Qed.
 
 (**
 # リスト
-
-Listとfoldrを以下のように定義する。
 *)
 
-Inductive List (X : Type) : Type :=
-| nil
-| cons of X & List X.
+(**
+## Inductiveな定義
+*)
+Inductive ListNat : Type :=
+| Nil
+| Cons of nat & ListNat.
 
-Print List_rect.
-(* 
-List_rect = 
-fun (X : Type) (P : List X -> Type) (f : P (nil X))
-  (f0 : forall (x : X) (l : List X), P l -> P (cons X x l)) =>
-fix F (l : List X) : P l :=
-  match l as l0 return (P l0) with
-  | nil => f
-  | cons y l0 => f0 y l0 (F l0)
-  end
-     : forall (X : Type) (P : List X -> Type),
-       P (nil X) ->
-       (forall (x : X) (l : List X), P l -> P (cons X x l)) ->
-       forall l : List X, P l
+(**
+## チャーチ表現
+*)
+Definition CListNat := forall R, (nat -> R -> R) -> R -> R.
+
+Definition CNil : CListNat :=
+  fun R => fun c : nat -> R -> R =>
+             fun n : R => n.                (* [] *)
+Definition CL1 : CListNat :=
+  fun R => fun c : nat -> R -> R =>
+             fun n : R => c 1 n.            (* [1] *)
+Definition CL2 : CListNat :=
+  fun R => fun c : nat -> R -> R =>
+             fun n : R => c 1 (c 2 n).      (* [1,2] *)
+Definition CL3 : CListNat :=
+  fun R => fun c : nat -> R -> R =>
+             fun n : R => c 1 (c 2 (c 3 n)). (* [1,2,3] *)
+
+(**
+## チャーチ表現をInductiveな定義にする
+*)
+Definition build' (c : CListNat) : ListNat :=
+  c ListNat Cons Nil.
+
+Eval compute in build' CL2.
+(**
+= Cons 1 (Cons 2 Nil) : ListNat
+
+build'は、チャーチ表現のリストcに、Consをfoldrしているとみることもできる。
  *)
 
-Fixpoint foldr (X : Type) (f : X -> List X -> List X) (l : List X) : List X :=
+(**
+## Foldr関数
+*)
+Fixpoint foldr (R : Type) (c : nat -> R -> R) (n : R) (l : ListNat) : R :=
   match l with
-    | nil => nil X
-    | cons x l => f x (foldr X f l)
+    | Nil => n
+    | Cons x l' => c x (foldr R c n l')
   end.
 
 Check foldr.
-
 (**
-foldr の型は以下である。
-
-    foldr : (X -> List X -> List X) -> List X -> List X
-
-「List X」を全称∀で抽象したものが、チャーチ表現になる。
-
-    CList X = ∀R. (X -> X -> R) -> R -> R
-*)
-
-(**
-foldr cons は何もしない関数になる。
-*)
-
-Check foldr nat (cons nat).
-Eval compute in foldr nat (cons nat) (cons nat O (cons nat O (nil nat))).
-
-(* END *)
-
-(**
-以下は、再帰的構造ではないので、予備とする。
+ : forall R : Type, (nat -> R -> R) -> R -> ListNat -> R
  *)
 
 (**
-# 自然数のPair
-
-PairNatとfoldpairnatを以下のように定義する。
+## Inductiveな定義をチャーチ数にする
 *)
+Definition unbuild' (l : ListNat) : CListNat :=
+  fun R =>
+    fun c : nat -> R -> R =>
+      fun n : R => foldr R c n l.
 
-Inductive PairNat : Type :=
-  pairNat of nat & nat.
-
-Print PairNat_rect.
-(* 
-PairNat_rect = 
-fun (P : PairNat -> Type) (f : forall n n0 : nat, P (pairNat n n0))
-  (p : PairNat) =>
-match p as p0 return (P p0) with
-| pairNat x x0 => f x x0
-end
-     : forall P : PairNat -> Type,
-       (forall n n0 : nat, P (pairNat n n0)) -> forall p : PairNat, P p
+Eval compute in unbuild' (Cons 1 (Cons 2 Nil)).
+(**
+ = fun (R : Type) (c : nat -> R -> R) (n : R) => c 1 (c 2 n) : CListNat
  *)
-Fixpoint foldpairnat (f : nat -> nat -> PairNat) (x : PairNat) : PairNat :=
-  match x with
-    | pairNat a b => f a b
-  end.
-
-Check foldpairnat.
 
 (**
-foldpairnat の型は以下である。
-
-    foldpairnat : (nat -> nat -> PairNat) -> PairNat -> PairNat
-
-「PairNat」を全称∀で抽象したものが、チャーチ表現になる。これは間違い。
-
-    CPairNat = ∀X. (nat -> nat -> X) -> X -> X
+## buildとunbuildとで元に戻ることの証明
 *)
+
+Theorem build'_unbuild' :
+  forall l : ListNat, build'(unbuild' l) = l.
+Proof.
+  rewrite /build' /unbuild'.
+  elim.
+    by [].
+  by rewrite /=; move=> n l ->.
+Qed.
 
 (**
-foldpairnat pairNat は何もしない関数になる。
-*)
+# 参考文献
 
-Check foldpairnat pairNat. 
-Eval compute in foldpairnat pairNat (pairNat (succ O) O).
+1. Pierce、住井 監訳「型システム入門 プログラミング言語と型の理論」オーム社
+2. 酒井 「不動点演算子はチャーチ数での無限大?」 http://msakai.jp/d/?date=20100628
+*)
 
 (* END *)
