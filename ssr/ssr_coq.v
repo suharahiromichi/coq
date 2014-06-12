5b3a1602c0849dc91d813f0902261a5e87e63139
SUHARA Hiromichi <suharahiromichi@gmail.com>
2014-06-12 23:42:32 +0900

    SSReflectの例を独自の証明に置き換える。

--- ssr/ssr_coq.v
+++ ssr/ssr_coq.v
@@ -33,61 +33,24 @@ Qed.
 *)
 Require Import ssreflect ssrnat ssrbool eqtype.
 
-(* 前提の m <= n からしてboolである。 *)
-Theorem le_minn : forall m n, m <= n -> minn m n = m.
-Proof.
-  move=> n p.
-  move/minn_idPl.
-  by [].
-Qed.
-
-(**
-## 説明的な証明
-*)
 Definition min'' m n :=
-  (*  if m < n then m else n. *)
-  match m < n with
+  match m <= n with
     | true => m
     | false => n
   end.
-Definition max'' m n := if m < n then n else m.
-
-Lemma max'C {m n} : max'' m n = max'' n m.
-Proof.
-  by rewrite /max''; case ltngtP.
-Qed.
-
-Lemma max'E m n : max'' m n = m + (n - m).
-Proof.
-  rewrite /max'' addnC.
-  case: leqP => [/eqnP-> | /ltnW/subnK].
-  by [].
-  by [].
-Qed.
-
-Lemma max'_idPl {m n} : reflect (max'' m n = m) (m >= n).
-Proof.
-  rewrite -subn_eq0 -(eqn_add2l m) addn0 -max'E.
-  by apply: eqP.
-Qed.
-
-Lemma max'_idPr {m n} : reflect (max'' m n = n) (m <= n).
-Proof.
-  by rewrite max'C; apply: max'_idPl.
-Qed.
-
-Lemma min'_idPl {m n} : reflect (min'' m n = m) (m <= n).
-Proof.
-  rewrite (sameP max'_idPr eqP).
-  rewrite -(eqn_add2l m) eq_sym -addn_min_max eqn_add2r.
-  by apply: eqP.
-Qed.
 
 Theorem le_min'' : forall m n, m <= n -> min'' m n = m.
 Proof.
-  move=> n p.
-  move/min'_idPl.
-  by [].
+  move=> n p H.
+  rewrite /min''.
+  move/leP in H.
+  case: H.                                  (* H は Prop *)
+  + have Hnn : n <= n by apply/idP.         (* Hnn は bool *)
+    by rewrite Hnn.
+  + move=> m Hnm.
+    have Hnm1 : n <= m.+1
+      by apply leqW; apply/leP; apply Hnm.  (* Hnm1はbool *)
+    by rewrite Hnm1.
 Qed.
 
 (* END *)
