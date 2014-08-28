7cc01c3e86085b1541e566b2bdd7eaf766a51d30
SUHARA Hiromichi <suharahiromichi@gmail.com>
2014-08-28 23:51:10 +0900

    dfs_path

--- ssr/ssr_ais_6_1_4_rst_depth_first_search.v
+++ ssr/ssr_ais_6_1_4_rst_depth_first_search.v
@@ -30,5 +30,42 @@ Fixpoint dfs (n : nat) (a : seq T) (x : T) {struct n} :=
     if x \in a then a else foldl (dfs n') (x :: a) (e x)
   else a.
 
+Inductive dfs_path x y (a : seq T) : Prop :=
+  DfsPath p of (path grel x p) & (y = last x p) & [disjoint (x :: p) & a].
+
+(* 古い書き方 *)
+Inductive dfs_path' x y (a : seq T) : Prop :=
+  DfsPath' p : (path grel x p) -> (y = last x p) -> [disjoint (x :: p) & a] -> dfs_path' x y (a : seq T).
+
+(**
+dfs_path の意味：
+- ふたつの連続した要素からなるseq 「x :: p」 は、grel関係である。それらはグラフ上で隣接adjacentしている。
+- y は p の最後の要素である。
+- seq 「x :: p」は、seq a の要素ではない。disjoint
+*)
+
+Lemma dfsP : forall n x y (a : seq T),
+               #|T| <= #|a| + n ->
+                            y \notin a ->
+                            reflect (dfs_path x y a) (y \in dfs n a x).
+Proof.
+  elim=> [|n IHn] x y a Hn Hy /=.           (* elim by n *)
+  admit.
+  admit.
+Qed.
+
+Lemma max_card : forall (T : finType) (A : pred T), #|A| <= #|T|.
+Proof.
+  admit.
+Qed.
+
+(* last は、pの最後の要素、[::]ならx。 *)
+Lemma dfs_pathP : forall x y,
+                    reflect
+                      (exists2 p, (path grel x p) & (y = last x p))
+                      (y \in dfs #|T| [::] x).
+Proof.
+  admit.
+Qed.
 
 (* END *)
