2748f9048fcefc8ccdd88c3f939a81ccdd87dfd7
SUHARA Hiromichi <suharahiromichi@gmail.com>
2014-09-04 23:59:44 +0900

    コメントを追加した。

--- pnp/ssr_htt_fact.v
+++ pnp/ssr_htt_fact.v
@@ -1,14 +1,13 @@
 (* http://ilyasergey.net/pnp/ *)
 
-Module FACT.
-
 (** * 「Hoare Type Theory の基礎」から抜粋 *)
 (** * Elements of Hoare Type Theory *)
 
 Require Import ssreflect ssrbool ssrnat eqtype seq ssrfun.
 
 Add LoadPath "./../htt".
-Require Import prelude pred pcm unionmap heap heaptac stmod stsep stlog stlogR.  
+Require Import prelude pred pcm unionmap heap heaptac
+        stmod stsep stlog stlogR.  
 
 Set Implicit Arguments.
 Unset Strict Implicit.
@@ -27,7 +26,6 @@ Definition fact_inv (n acc : ptr) (N : nat) h : Prop :=
       (fact_pure n') * a' = fact_pure N].
 
 (** 階乗の加算器(accumulator)部分の証明  *)
-
 Definition fact_acc_tp n acc := 
   unit -> {N}, 
      STsep (fact_inv n acc N, 
@@ -37,30 +35,60 @@ Program Definition fact_acc (n acc : ptr): fact_acc_tp n acc :=
   Fix (fun (loop : fact_acc_tp n acc) (_ : unit) => 
     Do (a1 <-- !acc;
         n' <-- !n;
-        if n' == 0 then ret a1
-        else acc ::= a1 * n';;
-             n ::= n' - 1;;
-             loop tt)).
+        if n' == 0 then
+          ret a1
+        else
+          acc ::= a1 * n';;
+          n ::= n' - 1;;
+          loop tt)).
 Next Obligation. 
-  apply: ghR => i N. 
-  case=> n' [a'][->{i}] Hi _. 
-  heval. 
+  apply: ghR => i N.                        (* conseq を消す。 *)
+  case=> n' [a'] []. move=> -> Hi _.        (* case=> n' [a'][->{i}] Hi _.  *)
+
+  Search (verify _ _ _).
+  (* (x <-- !x; e x) を2個処理する。 *)
+  do 2! [apply: bnd_readR => /=].           (* heval. *)
+  
   case X: (n' == 0). 
+  
+  (* (n' == 0) = true *)  
   Search _ (verify _ _ _) (ret _).
-  - apply: val_ret => /= _. 
-    move/eqP: X => Z; subst n'.
-    split; first by exists 0, a' => //.
-      by rewrite mul1n in Hi.
+  (* (ret v) を変形する。 *)
+  - apply: val_ret => /= _.
+
+    (* fact_inv n acc N (n :-> n' \+ acc :-> a') /\ a' = fact_pure N
+       を証明する。 *)
+    move/eqP : X => Z; subst n'.            (* 前提のXを n' = 0 にする。 *)
+    rewrite mul1n in Hi.
+    split.
+    + exists 0. exists a'. rewrite /= mul1n. by [].
+    + by [].
+
+  (* (n' == 0) = false *)
+  (* 代入文の並びなら、heval でよい。 *)
   - heval.
+    
+    Check (gh_ex N).
+    (* (loop tt) を (Do loop tt) にするだけのように見えるが、
+     ({N}, STsep (_, _)) を (STbin _) に、Nを追い出している。 *)
+    Check (loop tt).
+    Check (Do loop tt).
     apply: (gh_ex N). 
+
+    (* Do loop tt からループ不変式を取り出す。 *)
     apply: val_doR => // _.
-    exists (n'-1), (a' * n'); split => //=. 
-    rewrite -Hi => {Hi}; rewrite [a' * _]mulnC mulnA [_ * n']mulnC.
-      by case: n' X => //= n' _; rewrite subn1 -pred_Sn. 
+
+    (* fact_inv n acc N (n :-> (n' - 1) \+ acc :-> (a' * n'))
+       を証明する。 *)
+    rewrite /fact_inv. exists (n' - 1). exists (a' * n').
+    split => //=. 
+    rewrite -Hi=> {Hi}.
+    rewrite [a' * _]mulnC mulnA [_ * n']mulnC.
+    case: n' X => //= n' _.
+    by rewrite subn1 -pred_Sn. 
 Qed.
 
 (** 全体の証明  *)
-
 Definition fact_tp N :=
   STsep ([Pred h | h = Unit], 
          [vfun res h => res = fact_pure N /\ h = Unit]).
@@ -74,13 +102,36 @@ Program Definition fact (N : nat) : fact_tp N :=
       ret res).
 Next Obligation.
   rewrite /conseq => /=.
-  move=> _ ->.
-  heval=> n; heval=> acc; rewrite joinC unitR.
-  apply: bnd_seq => /=; apply: (gh_ex N); apply: val_doR => //.
-  - by exists N, 1; rewrite muln1.
-  - by move=> _ _ [[n'][a'][->] _ ->] _; heval.
-Qed.
+  move=> i ->.                              (* rewriteは、 i = Unit  *)
+  heval=> n.                                (* n <-- alloc N をheapに。 *)
+  heval=> acc.                              (* acc <-- alloc 1 をheapに。 *)
+  (* x \+ y を y \+ x *)
+  (* x \+ Unit を x *)
+  rewrite joinC unitR.
 
-End FACT.
+  Search _ (verify _ _ _).
+  (* (x <-- e1; e2 x) を変形する。 *)
+  apply: bnd_seq => /=.
+
+  (* (fact_acc n acc tt) を (Do fact_acc n acc tt)  *)
+  apply: (gh_ex N).
+
+  (* (Do fact_acc n acc tt) *)
+  apply: val_doR => //.
+
+  (* fact_inv n acc N (n :-> N \+ acc :-> 1) の証明 *)
+  - by exists N, 1; rewrite muln1.
+  (*  *)
+  - move=> x m.
+    case.
+    case=> n'.
+    case=> a'.
+    case=> H1 _ H2 _.
+    (* move=> x m [] [n'] [a'] [] H1 _ H2 _.
+       または
+       move=> x m [[n'] [a'] [H1] _ H2 _]. *)
+    rewrite H1 H2.
+    by heval.                               (* e1;; e2;; ret _ *)
+Qed.
 
 (* END *)
