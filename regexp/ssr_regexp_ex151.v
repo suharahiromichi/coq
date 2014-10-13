19bda9cd94b655191fd4fc4889046527cb36496a
SUHARA Hiromichi <suharahiromichi@gmail.com>
2014-10-13 15:05:39 +0900

    説明を追加した。

--- regexp/ssr_regexp_ex151.v
+++ regexp/ssr_regexp_ex151.v
@@ -20,6 +20,51 @@ Import Prenex Implicits.
 Tukuba Coq Users' Grup 「Coqによる定理証明」
 坂口さん著「反復定理で遊ぼう」 1.5.1 から。
 *)
+
+Check pumping.
+(**
+(0) i,j,kは自然数、x,y,z,u,v,wは語である。a,b,cは文字である。
+(1) Lは与えられた語sが正規表現であるかを判定する。例1.5.1の「lang a b」が Lに代入される。
+(2) "++" は語の連結。
+(3) 反復補題：Lは正規言語 (reguar L) ->
+    (∃k, ∀x,y,z, k≦|y| /\ L(xyz) /\ (∃u,v,w, y = uvw -> v≠ε -> ∀i, L(xu(v^i)wz)))
+(4) 反復補題は「正規言語なら」というかたちになっている。逆は成立するとは限らない。
+(5) pumpingは(3)の反復補題の対偶である。
+
+ : forall (char : finType) (L : automata.word char -> Prop),
+       (forall k : nat,
+        exists x y z : seq char,
+          k <= size y /\
+          L (x ++ y ++ z) /\
+          (forall u v w : seq char,
+           y = u ++ v ++ w ->
+           ~~ nilp v ->
+           exists i : nat, L (x ++ u ++ rep v i ++ w ++ z) -> False)) ->
+       ~ regular L
+*)
+(**
+反復補題は、
+正規言語 -> DFAがある -> 状態は有限である -> 状態を繰り返しても同じところに戻る
+ということを言っている。
+
+また、(3)の外側の「->」の右は、「状態を繰り返しても。。」を厳密にいうものである。
+DFTが語を受理して状態p0からpeに遷移すること δ'(p0, xyz) = pe を「p0 ---δ'xyz---> pe」と書く。
+
+xyzを受理するDFAを考える。
+p0 ------------------------------------δ'xyz---------------------------------> pe
+
+p0 ----δ'x----> px --------------------δ'y-------------------> py ----δ'z----> pe
+
+pxからpyの途中で、p'を2回通過することは必ずある。その前後の語（空でない）をvとする。
+y=uvw。
+p0 ----δ'x----> px ----δ'u----> p' ----δ'v----> p' ----δ'u---> py ----δ'z----> pe
+
+xu(v^i)wzのように、vの語をi回にすると、p'--->p'をi-1回だけ余計に繰り返すことになる。
+                                +---------------+
+                                V   i回繰り返す  |
+p0 ----δ'x----> px ----δ'u----> p' ----δ'v----> p' ----δ'u---> py ----δ'z----> pe
+*)
+
 Variable char : finType.
 
 Lemma nseq_eq_cat (x : char) n xs ys :
@@ -32,10 +77,11 @@ Proof.
     + by move=> a xs ys [] H; subst a; case/IH => {2}<- {2}<-. (* 2番め出現でrewriteする。 *)
 Qed.
 
-Definition lang (a b : char) : word char -> Prop := (* 1.5.1 例: {a^n b^n | n : nat}  の言語 *)
+(** 1.5.1 例: {a^n b^n | n : nat} の言語 *)
+Definition lang (a b : char) : word char -> Prop :=
   fun (s : word char) => exists (n : nat), s = nseq n a ++ nseq n b.
 
-(* 一部を冗長に書き直している。 *)
+(** lang が正規言語でないことを証明する *) (* 一部を冗長に書き直している。 *)
 Lemma anbn_non_regular (a b : char) :
   a != b -> ~ regular (lang a b).
 Proof.
@@ -49,8 +95,10 @@ Proof.
   - by [].
   - exists k. by [].
   - move=> u v w H0.
-    move: H0 (H0).
-    do 2 case/nseq_eq_cat => <-.
+    move: H0 (H0).                          (* 前提をふたつにコピーする。 *)
+    (* do 2 case/nseq_eq_cat => <-. *)
+    case/nseq_eq_cat => <-.
+    case/nseq_eq_cat => <-.
     move=> <-.
     move: {u v w} (size u) (size v) (size w) => u v w.
     rewrite !cat_nseq_eq addnA.
