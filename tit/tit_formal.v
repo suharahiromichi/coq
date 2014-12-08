766ca3794e5ce64e99f2bd3d1d50eead84b0db38
SUHARA Hiromichi <suharahiromichi@gmail.com>
2014-12-08 22:09:32 +0900

    証明をみなおす。

--- tit/tit_formal.v
+++ tit/tit_formal.v
@@ -1,95 +1,129 @@
+(**
+Coqでの形式化された不完全性定理の証明
+========
+
+2014/12/08 @suharahiromichi
+
+この文章のソースコードは以下にあります。
+
+https://github.com/suharahiromichi/coq/blob/master/tit/tit_formal.v
+ *)
+
+(**
+# はじめに
+
+菊池誠さん著「不完全性定理」共立出版 2014 の 7.4節と7.5節の形式化された
+第一不完全性定理と第二不完全性定理の証明をCoq SSReflectに写してみた。
+
+原則として説明は省くので、必要に応じてオリジナルを参照してください。
+*)
+
 Require Import ssreflect ssrbool eqtype ssrnat.
+
+(**
+# 準備
+
+## 古典論理の公理
+
+全体に、排中律や二重否定除去を使用するので、古典論理をインポートする。
+*)
+
 Require Import Coq.Logic.Decidable.
+Require Import Coq.Logic.Classical.
+Check classic.                              (* forall P : Prop, P \/ ~ P *)
+Check not_not.                              (* forall P : Prop, ~ ~ P -> P *)
 
-Axiom excluded_middle : forall φ : Prop, φ \/ ~ φ.
+(**
+## 命題論理の公理
 
-Lemma Ax1 (φ ψ : Prop) : φ -> (ψ -> φ).
+2.3節で導入された「命題論理の公理」をCoqの枠組みの中で証明しておく。
+*)
+
+Lemma Ax1 {φ ψ : Prop} : φ -> (ψ -> φ).
 Proof.
   move=> Hpai Hpsi.
   by apply: Hpai.
 Qed.
 
-Lemma Ax2 (φ ψ ρ : Prop) : (φ -> (ψ -> ρ)) -> ((φ -> ψ) -> (φ -> ρ)).
+Lemma Ax2 {φ ψ ρ : Prop} : (φ -> (ψ -> ρ)) -> ((φ -> ψ) -> (φ -> ρ)).
 Proof.
   move=> H1 H2 Hpai.
   apply: H1.
   - by apply: Hpai.
-  - by apply: H2; apply Hpai.
+  - by apply H2, Hpai.
 Qed.
 
-Lemma Ax3 (φ ψ : Prop) : (~ φ -> ~ ψ) -> (ψ -> φ).
+Lemma Ax3 {φ ψ : Prop} : (~ φ -> ~ ψ) -> (ψ -> φ).
 Proof.
-  move=> H.
-  Check contrapositive.
-  apply contrapositive.                     (* rewrite /decidable. *)
-  - by apply: excluded_middle.
-  - by apply: H.
+  apply contrapositive.
+  - by apply: classic.
 Qed.
 
+(**
+対偶については、他の組み合わせも証明しておく。
+ *)
 Lemma Ax3' (φ ψ : Prop) : (φ -> ψ) -> (~ ψ -> ~ φ).
 Proof.
-  move=> H Hnpsi Hpai.
-  apply Hnpsi.
-  apply H.
-  by apply Hpai.
+  move=> H Hnpai Hpsi.
+  by apply Hnpai, H, Hpsi.
 Qed.
 
 Lemma Ax3'' (φ ψ : Prop) : (φ -> ~ ψ) -> (ψ -> ~ φ).
 Proof.
   move=> H Hpsi Hpai.
-  apply H in Hpai.
-  by [].
+  by apply H in Hpai.
 Qed.
 
 Lemma Ax3''' (φ ψ : Prop) : (~ φ -> ψ) -> (~ ψ -> φ).
 Proof.
   move=> H.
-  Check (Ax3 φ (~ ψ)).
-  by [].
-
-
-  admit.
+  apply: Ax3 => Hnpai Hnpsi.
+  by apply Hnpsi, H, Hnpai.
 Qed.
 
+(*
 Variable model theory : Set.
 Variable T : theory.
 Variable v : model.
 Variable assertion : theory -> Prop -> Prop.
 Variable models : model -> Prop -> Prop.
-Variable enc : Prop -> nat.
 Notation "T ⊦ φ" := (assertion T φ) (at level 100, no associativity).
 Notation "v ⊧ φ" := (models v φ) (at level 100, no associativity).
-Notation "⌜ φ ⌝" := (enc φ) (at level 9, no associativity).
+*)
 
+(**
+## 可導性条件
+
+7.4節で導入された可導性条件を公理として定義する。
+本資料ではここを形式化の出発点とする。
+
+また、公理と定理は T ⊦ φ の形なので、Tは省略する。
+*)
+Variable enc : Prop -> nat.
+Notation "⌜ φ ⌝" := (enc φ) (at level 9, no associativity).
 Variable PrT : nat -> Prop.
 
-(* D1 を除いて T ⊦ φ の形なので、Tは取り去ってしまう。 *)
-
-(* φをΣ1文とすると、Σ1完全性から、φ -> PrT ⌜φ⌝ となる(定理7.4.4)。 *)
-Axiom D1 : forall (φ : Prop), φ -> PrT ⌜φ⌝. (* 定理7.4.4 *)
-Axiom D2 : forall (φ ψ : Prop), (PrT ⌜φ -> ψ⌝) -> PrT ⌜φ⌝ -> PrT ⌜ψ⌝.
-Axiom D3 : forall (φ : Prop), PrT ⌜φ⌝ -> PrT ⌜(PrT ⌜φ⌝)⌝.
+(* φをΣ1文とすると、Σ1完全性から、T ⊦ φ -> PrT ⌜φ⌝ となる(定理7.4.4)。 *)
+Axiom D1 : forall {φ : Prop}, φ -> PrT ⌜φ⌝. (* 定理7.4.4 *)
+Axiom D2 : forall {φ ψ : Prop}, (PrT ⌜φ -> ψ⌝) -> PrT ⌜φ⌝ -> PrT ⌜ψ⌝.
+Axiom D3 : forall {φ : Prop}, PrT ⌜φ⌝ -> PrT ⌜(PrT ⌜φ⌝)⌝.
 
 Lemma L7_4_3 (φ ψ : Prop) : (φ -> ψ) -> PrT ⌜φ⌝ -> PrT ⌜ψ⌝.
 Proof.
   move=> H.
-  apply: D2.
-  apply: D1.
-  by apply H.
+  by apply D2, D1, H.
 Qed.
 
-Definition Con := ~ PrT ⌜0 = 1⌝.
-
 Lemma L7_5_3 (φ : Prop) :
-                    (PrT ⌜φ⌝ -> PrT ⌜~ φ⌝) -> (PrT ⌜φ⌝ -> PrT ⌜0 = 1⌝).
+  (PrT ⌜φ⌝ -> PrT ⌜~ φ⌝) -> (PrT ⌜φ⌝ -> PrT ⌜0 = 1⌝).
 Proof.
-  Check (Ax2 (PrT ⌜φ⌝) (PrT ⌜~ φ⌝) (PrT ⌜0 = 1⌝)).
+  Check (@Ax2 (PrT ⌜~ φ⌝) (PrT ⌜0 = 1⌝)).
   apply: Ax2 => H.
   apply: D2.
   have Hcontra : φ -> (~ φ -> 0 = 1) by [].
   apply L7_4_3 in Hcontra.
   apply Hcontra.
-  apply H.
+  by apply H.
 Qed.
 
 Lemma L7_5_4 (φ : Prop) :
@@ -101,88 +135,58 @@ Proof.
   have Hcontra : ~ φ -> (φ -> 0 = 1) by [].
   apply L7_4_3 in Hcontra.
   apply Hcontra.
-  apply H.
+  by apply H.
 Qed.
+
+(**
+# 第一不完全性定理
+
+## ゲーデル文
+ *)
 
 Variable σ : Prop.
 Axiom G : σ <-> ~ PrT ⌜σ⌝.
+Definition Con := ~ PrT ⌜0 = 1⌝.
 
+(**
+## 第一不完全性定理 (1)
+*)
 Theorem T7_5_5_1 : Con -> ~ PrT ⌜σ⌝.
 Proof.
-  rewrite /Con.
-  apply: Ax3'.
-  apply: L7_5_3 => H.
-  Check (D3 σ).
-  apply D3 in H.
-  move: H.
   have G' : PrT ⌜σ⌝ -> ~ σ.
   - move=> G2 Gsigma.
     apply G in Gsigma.
     apply Gsigma.
-      by apply: G2.
-  apply: L7_4_3.
-  by apply: G'.
+    by apply: G2.
+  apply: Ax3'.
+  apply: L7_5_3.
+  move/D3.
+  by apply L7_4_3, G'.
 Qed.
 
-
-Axiom classic : forall P:Prop,  ~ ~ P -> P.
-
+(**
+## 第一不完全性定理 (2)
+*)
 Theorem T7_5_5_2 :
   (PrT ⌜(PrT ⌜σ⌝)⌝ -> PrT ⌜σ⌝) /\ Con -> ~ PrT ⌜~ σ⌝.
 Proof.
-  rewrite /Con.
   case=> H.
   apply Ax3 => H1.
-  apply classic in H1.
-  move=> H2.
-  apply H2.
-  move {H2}.
-
-  have G' : (PrT ⌜~ σ⌝ -> PrT ⌜σ ⌝) -> (PrT ⌜~ σ⌝ -> PrT ⌜0 = 1⌝).
-  by apply L7_5_4.
-  
-  apply G'.
-  - move=> G2.
-    apply H.
-    apply D3.
-
-    have GG : ~ σ -> PrT ⌜σ ⌝.
-    apply Ax3'''.
-    move=> GGG.
-    apply G in GGG.
-    by [].
-
-    Check L7_4_3 (~ σ) (PrT ⌜σ⌝).
-    apply (L7_4_3 (~ σ) (PrT ⌜σ⌝)) in GG.
-    apply H.
-    apply GG.
-    apply H1.
-    by [].
+  apply not_not in H1.
+  - apply.
+    apply (L7_5_4 σ).
+    + move=> H2.
+      have G' : ~ σ -> PrT ⌜σ ⌝ by apply Ax3''' => /G.
+      apply (L7_4_3 (~ σ) (PrT ⌜σ⌝)) in G'.
+      apply H, G'.
+      by apply H1.
+    + by apply H1.
+  - by apply classic.
 Qed.
 
-
-  have G' : (PrT ⌜PrT ⌜σ ⌝ ⌝ -> PrT ⌜σ ⌝) -> (PrT ⌜~ σ⌝ -> PrT ⌜σ⌝).
-  Check L7_4_3 (~ σ) σ.
-  move=> G1.
-  apply (L7_4_3 (~ σ) σ).
-
-  apply G' in H.
-
-  apply L7_5_4 in H.
-
-
-  have G' : PrT ⌜~ σ⌝ -> PrT ⌜σ⌝.
-    - apply L7_4_3.
-      move=> H3.
-      apply G.
-      move=> G2.
-      apply H3.
-      apply G.
-      move=> G3.
-      apply H2.
-  move: G' H1.
-  by apply L7_5_4.
-Qed.
+(**
+# 第二不完全性定理
+ *)
 
 Lemma encoding (φ ψ : Prop) :
   (φ <-> ψ) -> (PrT ⌜φ⌝ -> PrT ⌜ψ⌝).
@@ -190,38 +194,30 @@ Proof.
   move=> H H0.
   case H.
   move=> H1 H2.
-  apply D1 in H1.
-  apply (D2 φ ψ) in H1.
+  apply D1, (@D2 φ ψ) in H1.
   - by apply H1.
-    + apply (D2 φ ψ) in H1.
-      * by apply H0.
-      * by apply H0.
+  - by apply (@D2 φ ψ) in H1; apply H0.
 Qed.    
 
 Lemma L7_5_8 :  Con <-> σ.
 Proof.
   split.
+  (* -> *)
   - move=> H.
     apply G.
     by apply T7_5_5_1 in H.
-  - move=> H.
-    apply G in H.
-    move: H.
+  (* <- *)
+  - move/G.
     apply Ax3'.
-    apply L7_4_3.
-    by [].
+    by apply L7_4_3.
 Qed.
-
 
 Theorem T7_5_13 : Con -> ~ PrT ⌜Con⌝.
 Proof.
-  apply Ax3' => H.
-  Check encoding  Con σ L7_5_8.
-  apply (encoding  Con σ L7_5_8) in H.
-  move: H.
-  apply Ax3 => H.
-  apply T7_5_5_1.
-  by [].
+  apply Ax3'.
+  move/(encoding  Con σ L7_5_8).
+  apply Ax3.
+  by apply T7_5_5_1.
 Qed.
 
 (* END *)
