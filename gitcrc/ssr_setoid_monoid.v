ca14096d164dc4c9b74b2793ee9a41ebcab61899
SUHARA Hiromichi <suharahiromichi@gmail.com>
2015-01-02 23:14:33 +0900

    Powerを追加した。

--- gitcrc/ssr_setoid_monoid.v
+++ gitcrc/ssr_setoid_monoid.v
@@ -62,22 +62,31 @@ Notation "x == y :> S" := (equal (s:=S) x y) (* (@equal S x y) でもよい。 *
 Notation "x == y" := (x == y :> _)          (* equql ではだめ。 *)
                        (at level 70, no associativity).
 
-Section Example_1.                          (* 補足説明 *)
-  Check Setoid_of eq : Setoid.
-  Check Setoid_of (@eq nat) : Setoid.
+Section Example_1.
+  Definition eq2 {A : Type} (x y : A * A) : Prop :=
+    x.1 = y.1 /\ x.2 = y.2.
   
-  Program Definition eq_Setoid (A: Type) : Setoid := Setoid_of (@eq A).
-  (* 証明は要求されないで、定義が完了した。 *)
-
-  Definition U : Setoid := eq_Setoid nat.
-  (* Variables x y : @carrier U : Type. *)
-  Variables x y : U : Type.
-  (* Check @equal U x y : Prop. *)
-  Check equal x y : Prop.
-  Check x == y :> U : Prop.
+  Program Definition eq2_Setoid {A : Type} : Setoid := Setoid_of (@eq2 A).
+  Next Obligation.
+    by rewrite /eq2.
+  Qed.
+  Next Obligation.
+    move: H.
+    rewrite /eq2; case.
+    by split.
+  Qed.
+  Next Obligation.
+    rewrite /eq2.
+    case: H.
+    case: H0.
+    rewrite /=.
+    split; by subst.
+  Qed.
+  
+  Variables x y : @eq2_Setoid nat : Type.
+  Variables x' y' : @eq2_Setoid nat : Type.
   Check x == y : Prop.
 End Example_1.
-
 
 (**
 # Setoid上の関数
@@ -101,9 +110,9 @@ Properについて：
 型Xでequalなら型Yでもequalとすること（同値関係の保存性）を基にして、
 rewriteによる書き換えができるようになる。 *)
 
-Record Binop (X : Setoid) : Type :=
+Structure Binop (X : Setoid) : Type :=
   {
-    binop :> @carrier X -> @carrier X -> @carrier X;
+    binop :> X -> X -> X;
     prf_Binop :> Proper ((==) ==> (==) ==> (==)) binop
   (* ∀ x y x' y', x == x' -> y == y' -> x ・ y == x' ・ y' を意味する。*)
   }.
@@ -112,18 +121,41 @@ Notation makeBinop op := {| binop := op |}. (* binopを与える。 *)
 
 Section Example_2.
   (* Example_1 の続き。 *)
-  Variables x x' : U.
-  Variables y y' : U.
-
-  Check Binop U.
-  Program Definition test : Binop U := {| binop := addn |}.
+  Definition plus2 (x y : nat * nat) : (nat * nat) :=
+    (fst x + fst y, snd x + snd y).
   
-  Goal x == x' -> y == y' -> test x y == test x' y'.
+  Program Definition plus2_Binop : Binop eq2_Setoid := {| binop := plus2 |}.
+  Next Obligation.
+    (* Proper (eq2 ==> eq2 ==> eq2) plus2 *)
+    rewrite /Proper /respectful.
+    rewrite /eq2 /plus2.
+    move=> x y H1 x' y' H2.
+    case: H1.
+    case: H2.
+    rewrite /= => H1 H2 H3 H4.
+    split.
+    by rewrite H1 H3.
+    by rewrite H2 H4.
+  Qed.
+  
+  Variables x x' : @eq2_Setoid nat : Type.
+  Variables y y' : @eq2_Setoid nat : Type.
+
+  (* ********** *)
+  (* Properの例 *)
+  (* ********** *)
+  Check binop plus2_Binop x y : eq2_Setoid.
+  Check       plus2_Binop x y : eq2_Setoid.
+  Check       plus2 x y : nat * nat.
+  Goal x == x' -> y == y' -> (plus2_Binop x y) == (plus2_Binop x' y').
   Proof.
     intros Hxx' Hyy'.
+    admit.
+(*
     rewrite Hxx'.
     rewrite Hyy'.
     reflexivity.
+*)
   Qed.
 End Example_2.
 
@@ -145,9 +177,39 @@ Notation makeMonoid s unit op :=
     monoid_binop := op                      (* binopを与える。 *)
   |}.
 
-Check Monoid.
-Program Definition Mult : Monoid :=
-  makeMonoid U 0%nat test.
+Section Example_3.
+  Program Definition Mult : Monoid :=
+    makeMonoid (@eq2_Setoid nat) (0, 0) plus2_Binop.
+  Next Obligation.
+      by rewrite /eq2 /plus2 /=.
+  Qed.
+  Next Obligation.
+      by rewrite /eq2 /plus2 //=.
+  Qed.
+End Example_3.
+
+
+(**
+# Power
+*)
+(* *************** *)
+(* ad-hoc 多相の例 *)
+(* *************** *)
+Record Monoid' {A : Setoid} (dot : Binop A) (one : A) : Type :=
+  {
+    prf_Monoid_idl' : forall x, dot one x == x;
+    prf_Monoid_idr' : forall x, dot x one == x
+  }.
+
+Fixpoint power `{@Monoid' A dot one} (a : A) (n : nat) :=
+  match n with
+    | 0%nat => one
+    | S p => dot a (power a p)
+  end.
+
+(**
+# Group
+ *)
 
 (**
 # 参考：
