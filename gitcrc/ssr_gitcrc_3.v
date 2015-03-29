8bf931249a74b7327a516c0b967378afb8e4e446
SUHARA Hiromichi <suharahiromichi@gmail.com>
2015-03-29 18:12:48 +0900

    route_equiv_equivb のための補題をマージした。

--- gitcrc/ssr_gitcrc_3.v
+++ gitcrc/ssr_gitcrc_3.v
@@ -4,9 +4,10 @@ A Gentle Introduction to Type Classes and Relations in Coq
 Chapter 3. Lost in Manhattan
 を SSReflectで書いてみた。
 
-@suharahiromichi
+3.7 Deciding Route Equivalence
+のEx1'の証明ができることを目標とする。
 
-SSReflectなので、route_eqb が中心となり =r== で表す。
+@suharahiromichi
 *)
 
 Require Import ssreflect ssrbool ssrfun eqtype ssrnat seq path div choice.
@@ -15,6 +16,7 @@ Require Import fintype tuple finfun bigop prime ssralg poly ssrnum ssrint rat.
 Set Implicit Arguments.
 Unset Strict Implicit.
 Unset Printing Implicit Defensive.
+
 Local Open Scope ring_scope.
 
 (**
@@ -34,18 +36,33 @@ Record Point : Type :=
 
 Definition Point_O := Build_Point 0 0.
 
+(**
+3.3 Route Semantics
+ *)
 Definition translate (dx dy : int) (P : Point) :=
   Build_Point (Point_x P + dx) (Point_y P + dy).
 
 (** Equality test  between Points *)
-
 Definition Point_eqb (P P': Point) : bool :=
   (Point_x P == Point_x P') &&
   (Point_y P == Point_y P').
 
-(* Prove the correctness of Point_eqb *)
+(*  move P r follows the route r starting from P *)
+Fixpoint move (r:route) (P:Point) : Point :=
+  match r with
+    | nil => P
+    | North :: r' => move r' (translate 0 1 P)
+    | East :: r' => move r' (translate 1 0 P) 
+    | South :: r' => move r' (translate 0 (-1) P)
+    | West :: r' => move r' (translate (-1) 0 P)
+  end.
 
-Lemma Point_eqb_correct :
+(* We consider that two routes are "equivalent" if they define
+  the same moves. For instance, the routes
+  East::North::West::South::East::nil and East::nil are equivalent *)
+
+
+Lemma Point_eqb_correct' :
   forall p p', Point_eqb p p' <-> p = p'.
 Proof.
   case=> x y.                               (* by p *)
@@ -66,47 +83,68 @@ Lemma PointEqP p p' : reflect (p = p') (Point_eqb p p').
 Proof.
   apply: (@iffP (Point_eqb p p')).
   - by apply: idP.
-  - by rewrite (Point_eqb_correct p p').
-  - by rewrite (Point_eqb_correct p p').
+  - by rewrite (Point_eqb_correct' p p').
+  - by rewrite (Point_eqb_correct' p p').
 Qed.
 
-(**
-3.3 Route Semantics
-*)
-(*  move P r follows the route r starting from P *)
+(* Point_eqb_correct の別の証明 *)
+(* バニラCoqの andb_true_iff だが、andP を直接使えばよい。 *)
+Lemma andb_true_iff :
+  forall b1 b2 : bool, b1 && b2 = true <-> b1 = true /\ b2 = true.
+Proof.
+  move=> b1 b2.
+  split => H.
+  by apply/andP.
+  by apply/andP.
+Qed.
+(* notu *)
 
-Fixpoint move (r : route) (P : Point) : Point :=
- match r with
- | nil => P
- | North :: r' => move r' (translate 0 1 P)
- | East :: r' => move r' (translate 1 0 P) 
- | South :: r' => move r' (translate 0 (-1) P)
- | West :: r' => move r' (translate (-1) 0 P)
- end.
+Lemma int_eq_bool (a b : int) :
+  (a == b) = true <-> a = b.
+Proof.
+  admit.
+Qed.
 
-(* We consider that two routes are "equivalent" if they define
-  the same moves. For instance, the routes
-  East::North::West::South::East::nil and East::nil are equivalent *)
+Lemma int_eq_boolP (a b : int) :
+  reflect (a = b) (a == b).
+Proof.
+  admit.
+Qed.
 
-(* これは、SSReflect の rel ではない。 *)
-Definition route_equiv (r r' : route) :=
-  forall (P : Point), (move r P) = (move  r' P).
-Check route_equiv : route -> route -> Prop.
-Infix "=r=" := route_equiv (at level 70):type_scope.
+(* Prove the correctness of Point_eqb *)
+Lemma Point_eqb_correct :
+  forall p p', Point_eqb p p' = true <-> p = p'.
+Proof.
+  move=> p p'.
+  elim: p; elim: p'; split.
+  - rewrite /Point_eqb //=.
+    move/andP.
+    elim.
+    move/int_eq_boolP => H1.
+    move/int_eq_boolP => H2.
+      by rewrite H1; rewrite H2.
+  - elim.
+    rewrite /Point_eqb //=.
+    apply/andP.
+    split.
+      by apply/int_eq_boolP.
+      by apply/int_eq_boolP.
+Qed.      
+
+(* *************************** *)
 
 (* Point_O が固定であることに注意。ここでは、このboolを中心に使っていく。 *)
-Definition route_eqb (r r' : route) :=
-  Point_eqb (move r Point_O) (move  r' Point_O).
-Check route_eqb : route -> route -> bool.
-Check route_eqb : rel route.
-Infix "=r==" := route_eqb (at level 70):type_scope. (* ！注意！ *)
+Definition route_eqb r r' : bool :=
+  Point_eqb (move r Point_O) (move r' Point_O).
+Infix "=r==" := route_eqb (at level 70):type_scope.
 
-(* route_eqb (=r==) を使う場合。 *)
-Example Ex1' : East::North::West::South::East::nil =r== East::nil.
+Example Ex1'' : East::North::West::South::East::nil =r== East::nil.
 Proof.
+  apply Point_eqb_correct.
   by [].
 Qed.
 
+(* *************************** *)
 (**
 3.4 On Route Equivalence 
 *)
@@ -155,22 +193,11 @@ Proof.
       * by apply: H2.
 Qed.
 
-(* ここは、=r= を使う。 *)
-Lemma route_cons : forall r r' d, r =r= r' -> d::r =r= d::r'.
-Proof. 
-  move=> r r' d H.
-  rewrite /route_equiv in H.
-  rewrite /route_equiv.
-  move=> P.
-  case: P => Point_x0 Point_y0.
-  case: d => //=.
-Qed.
-
 (* route_eqb (=r==) を使う場合。 *)
 Example Ex2' : South::East::North::West::South::East::nil =r== South::East::nil.
 Proof.
   (* apply: route_cons. *)
-  by apply: Ex1'.
+  by apply: Ex1''.
 Qed.
 
 Example Ex3' : forall r, North::East::South::West::r =r== r.
@@ -186,26 +213,93 @@ Proof.
   by [].
 Qed.
 
+(* ****************** *)
+(* ****************** *)
+(* route_equiv と route_eqb の reflect を証明する *)
+(* ****************** *)
+(* ****************** *)
+
+(* これは、SSReflect の rel ではない。 *)
+Definition route_equiv (r r' : route) :=
+  forall (P : Point), (move r P) = (move  r' P).
+Check route_equiv : route -> route -> Prop.
+Infix "=r=" := route_equiv (at level 70):type_scope.
+
+
+
 (**
-3.5 Proper Functions
 3.6 Some Other instances of Proper
-
-Properによる、reflexivityとrewriteを拡張する。
-Vanilla Coqを使うため省略する。
-coq_gitcrc_3_lost_in_ny.v
 *)
-
-(**
-3.7 Deciding Route Equivalence
-
-ここでは、Prop版とbool版のReflectを証明する。
-*)
-
-Lemma route_equiv_equivb (r r' : route) :
-  route_equiv r r' <-> route_eqb r r' = true.
+Lemma route_compose :
+  forall r r' P, move (r ++ r') P = move r' (move r P).
 Proof.
-  (* coq_gitcrc_3_lost_in_ny.v *)
-  admit.                                    (* 3.6を参照 *)
+  induction r as [|d s IHs]; simpl;
+  [auto | destruct d; intros;rewrite IHs;auto].
+Qed.
+
+(*
+Example Ex3 : forall r, North::East::South::West::r =r= r.
+Proof.
+  intros r P;
+    destruct P; simpl.
+  unfold route_equiv, translate; simpl;
+    do 2 f_equal; ring.
+Qed.
+*)
+
+
+
+Lemma translate_comm :
+  forall dx dy dx' dy' P,
+    translate dx dy (translate dx' dy' P) = translate dx' dy' (translate dx dy P).
+Proof.
+  admit.
+Qed.
+
+Lemma move_translate :
+  forall r P dx dy,
+    move r (translate dx dy P) = translate dx dy (move r P).
+Proof.
+  induction r as [|a r]; simpl; [reflexivity|].  
+  destruct a;simpl; intros;rewrite <- IHr;rewrite  (translate_comm); auto.
+Qed.
+
+Lemma move_comm :
+  forall r r' P,
+    move r (move r' P) =  move r' (move r P).
+Proof.
+  induction r as [| a r']; [reflexivity|].
+  simpl; destruct a;
+  intros; repeat rewrite move_translate; rewrite IHr'; auto.
+Qed.
+
+Lemma app_comm : forall r r', r++r' =r=  r'++r.
+Proof.
+  intros r r' P; repeat rewrite route_compose; apply move_comm.
+Qed.
+
+(** the following lemma  will be used for deciding route equivalence *)
+Lemma route_equiv_Origin :
+  forall r r', r =r= r' <-> move r Point_O  = move r' Point_O .
+Proof.
+  admit.
+Qed.
+
+(**  ... we can now prove route_eqb's  correctness *)
+Lemma route_equiv_equivb :
+  forall r r',
+    route_equiv r r' <-> route_eqb r r' = true.
+Proof.
+  intros r r'; rewrite route_equiv_Origin;
+  unfold route_eqb; rewrite Point_eqb_correct; tauto.
+Qed.
+
+Ltac route_eq_tac := rewrite route_equiv_equivb; reflexivity.
+
+(** another proof of Ex1, using computation  *)
+Example Ex1' : East::North::West::South::East::nil =r= East::nil.
+Proof.
+  route_eq_tac.
 Qed.
 
 Lemma route_equivP (r r' : route) :
@@ -217,7 +311,6 @@ Proof.
   - by apply (route_equiv_equivb r r').
 Qed.
 
-(* route_eqb (=r=) を使う場合。 *)
 Example Ex1 : East::North::West::South::East::nil =r= East::nil.
 Proof.
   apply/route_equivP.
@@ -225,8 +318,10 @@ Proof.
 Qed.
 
 (* Ex5 は、=r= による rewrite を使うので、この範囲では解けない。 *)
-Example Ex5' : forall r r',  r ++ North::East::South::West::r' =r== r ++ r'.
+Example Ex5' : forall r r',  r =r= r' -> r ++ North::East::South::West::r' =r= r ++ r'.
 Proof.
+  move=> r r' H.
+  (* rewrite H. *)
   admit.
 Qed.
 
