3b1ac71c44c112e9ae9fe9185ba2661d20462b3b
SUHARA Hiromichi <suharahiromichi@gmail.com>
2015-06-05 22:19:50 +0900

    first commit

---
+++ iii/ssr_isort_2.v
@@ -0,0 +1,168 @@
+(**
+プログラミング Coq 証明駆動開発入門(1)
+http://www.iij-ii.co.jp/lab/techdoc/coqt/coqt8.html
+
+をSSReflectに書き直した。
+Permutation は SSReflect の相当の補題を使っているため、
+証明の詳細は原著と異なることに注意してください。
+*)
+
+(* ************************************************** *)
+(* nat と ≦ より一般的な、eqType と R で証明を試みる。 *)
+(* ************************************************** *)
+
+Require Import ssreflect ssrfun ssrbool eqtype ssrnat seq.
+
+Set Implicit Arguments.
+Unset Strict Implicit.
+Unset Printing Implicit Defensive.
+Set Print All.
+
+Section isort.
+  Variable T : eqType.
+  Variable R : rel T.
+
+  (* Permutation, seq.v *)
+  Check perm_eq (1::2::3::nil) (2::1::3::nil).
+  Eval compute in perm_eq (1::2::3::nil) (2::1::3::nil). (* true *)
+  Eval compute in perm_eq nil nil.                       (* true *)
+
+  (* ソート処理の定義 *)
+  Fixpoint insert (a : T) (l : seq T) : seq T :=
+    match l with
+      | nil => a :: nil
+      | x :: xs => if R a x then a :: l else x :: insert a xs
+    end.
+
+  Fixpoint insertion_sort (l : seq T) : seq T :=
+    match l with
+      | nil => nil
+      | x :: xs => insert x (insertion_sort xs)
+    end.
+
+  (* 証明 *)
+  Lemma perm_iff : forall (m n : seq T),
+                   (forall l, perm_eq m l = perm_eq n l) <-> perm_eq m n.
+  Proof.
+    move=> m n.
+    split=> H.
+    - by rewrite H.
+    - by apply/perm_eqlP.
+  Qed.
+
+  Lemma perm_swap : forall (l l' : seq T) (x a : T),
+                      perm_eq [:: x, a & l] l' = perm_eq [:: a, x & l] l'.
+  Proof.
+    move=> l l' x a.
+    apply perm_iff => //.
+    rewrite -[[:: x, a & l]]cat1s -[[:: a & l]]cat1s.
+    apply/perm_eqlP.
+      by apply (perm_catCA [:: x] [:: a] l).
+  Qed.
+
+  Lemma insert_perm : forall (l : seq T) (x : T),
+                        perm_eq (x::l) (insert x l).
+  Proof.
+    elim=> [_ // | a l H x //=].
+    case: (R x a) => [//= |].
+    apply perm_iff => // l'.
+    rewrite perm_swap => //.
+    apply perm_iff => //.
+    by rewrite perm_cons.
+  Qed.
+
+  Theorem isort_permutation : forall (l : seq T),
+                                perm_eq l (insertion_sort l).
+  Proof.
+    elim=> [// | a l H //=].
+    apply perm_eq_trans with (a :: insertion_sort l).
+    + by rewrite perm_cons.
+    + by apply insert_perm.
+  Qed.
+
+  Inductive LocallySorted : seq T -> Prop :=
+  | LSorted_nil : LocallySorted nil
+  | LSorted_cons1 : forall a : T, LocallySorted (a :: nil)
+  | LSorted_consn : forall (a b : T) (l : seq T),
+                      LocallySorted (b :: l) ->
+                      R a b -> LocallySorted (a :: b :: l).
+
+  Lemma insert_sorted : forall (a : T) (l : seq T),
+                          LocallySorted l -> LocallySorted (insert a l).
+  Proof.
+    move=> a.
+    elim=> [H //= | a0 l IHl H //=].
+    - by apply LSorted_cons1.
+    - case Heqb : (R a a0).                 (* remember *)
+      + apply LSorted_consn.
+        * by apply H.
+        * by rewrite Heqb.
+      + inversion H.
+        * apply LSorted_consn.
+          apply LSorted_cons1.
+          admit.
+        * subst; simpl; simpl in *.
+          elim H' : (R a b).
+           - apply LSorted_consn.
+             rewrite H' in IHl.
+               by apply IHl.                (* apply H2. *)
+           - admit.
+           - apply LSorted_consn.
+             rewrite H' in IHl.
+               by apply IHl.                (* apply H2. *)
+           - by [].                         (* apply H3. *)
+  Qed.
+  
+  Theorem isort_sorted : forall (l : seq T),
+                           LocallySorted (insertion_sort l).
+  Proof.
+    elim=> [| a l H //=].
+    - by apply LSorted_nil.
+    - by apply insert_sorted.
+  Qed.
+
+End isort.
+
+Eval compute in insert leq 1 nil.                      (* [:: 1] *)
+Eval compute in insert leq 5 [:: 1; 4; 2; 9; 3].       (* [:: 1; 4; 2; 5; 9; 3] *)
+Eval compute in insertion_sort leq [:: 2; 4; 1; 5; 3]. (* [:: 1; 2; 3; 4; 5] *)
+
+Check leq : nat -> nat -> bool.
+Check leq : nat -> nat -> Prop.
+Check leq : rel nat : Type.
+Check le : nat -> nat -> Prop.
+Fail Check le : nat -> nat -> bool.
+Fail Check le : rel nat.                    (* rel : Type -> Type *)
+Check LocallySorted leq (1::2::3::nil) : Prop.
+Fail Check LocallySorted le (1::2::3::nil) : Prop.
+
+Require Import path.
+
+(* Sorted, path.v *)
+Check sorted : forall T : eqType, rel T -> seq T -> bool.
+Check leq : nat -> nat -> bool.
+Check leq : nat -> nat -> Prop.
+Check leq : rel nat : Type.
+Check le : nat -> nat -> Prop.
+Fail Check le : nat -> nat -> bool.
+Fail Check le : rel nat.                    (* rel : Type -> Type *)
+
+Check sorted ltn (1::2::3::nil).
+Check sorted leq (1::2::3::nil).
+Eval compute in sorted leq (1::2::3::nil). (* true *)
+Eval compute in sorted leq (3::nil).       (* true *)
+Eval compute in sorted leq nil.            (* true *)
+Eval compute in sorted leq (2::1::3::nil). (* false *)
+
+(* END *)
+
+(*
+Lemma leb_complete_conv : forall (T : eqType) (R : rel T) (n m : T),
+                            R m n = false -> R n m.
+Proof.
+  move=> n m.
+  move/negbT.
+  by rewrite -ltnNge.
+Qed.
+*)
+
