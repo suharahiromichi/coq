44b3855cfc640b6b669193c65998ab7902895caf
SUHARA Hiromichi <suharahiromichi@gmail.com>
2015-06-13 00:02:09 +0900

    first commit

---
+++ iii/ssr_coqt6.v
@@ -0,0 +1,124 @@
+(**
+プログラミング Coq --- 証明ができない！ こんなとき
+
+http://www.iij-ii.co.jp/lab/techdoc/coqt/coqt6.html
+をSSReflectに書き直した。
+*)
+
+Require Import ssreflect ssrfun ssrbool eqtype ssrnat seq.
+
+Set Implicit Arguments.
+Unset Strict Implicit.
+Unset Printing Implicit Defensive.
+Set Print All.
+
+
+Inductive InList (A : Type) (a : A) : seq A -> Prop :=
+  | headIL : forall xs, InList a (a::xs)   (* 1 *)
+  | consIL : forall x xs, InList a xs -> InList a (x::xs).   (* 2 *)
+
+Goal forall (A : Type) (a : A) (l : seq A),
+       (forall (x y : A), x = y \/ x <> y) -> InList a l \/ ~ InList a l.
+Proof.
+  move=> A a l H.
+  elim: l.
+  - right=> H0.
+      by inversion H0.
+  - move=> a0 l IHl.
+    elim: IHl => H1.
+    + left.
+        by apply consIL.
+    + elim: (H a a0) => H2.
+      * left.
+        rewrite H2.
+          by apply headIL.
+      * right.
+        move=> H3.
+          by inversion H3.
+Qed.
+
+Theorem fold_symmetric :
+  forall (A:Type) (f:A -> A -> A),
+    (forall x y z:A, f x (f y z) = f (f x y) z) ->
+    (forall x y:A, f x y = f y x) ->
+    forall (a0:A) (l:list A), foldl f a0 l = foldr f a0 l.
+destruct l.
+ reflexivity.
+
+ simpl.
+ generalize a, a0.
+ induction l.
+  intros.
+  simpl.
+  apply H0.
+
+  simpl.
+  intros.
+  rewrite H.
+  replace (f (f a3 a2) a1) with (f a3 (f a2 a1)).
+  apply IHl.
+  apply H.
+Qed.
+
+
+
+Theorem fold_symmetric' :
+  forall (A : Type) (f : A -> A -> A),
+    (forall x y z : A, f x (f y z) = f (f x y) z) ->
+    (forall x y : A, f x y = f y x) ->
+    forall (a0 : A) (l : list A), foldl f a0 l = foldr f a0 l.
+Proof.
+  move=> A f H H0 a0.
+  elim.
+  - by [].
+  - simpl.
+    move=> a l.
+    elim: l a0.
+    + intros.
+      simpl.
+      by [].
+    + move=> a1 l IH1 a2 H2.
+      replace (f (f a2 a) a1) with (f a2 (f a a1)).
+      simpl.
+      Check IH1 (f a a1).
+      admit.                                (* XXXXX *)
+      apply H.
+Qed.
+
+Theorem problem8 : forall (n : nat),
+                     (exists m, n = 2 * m) \/ (exists m, n = 2 * m + 1).
+Proof.
+  elim=> [|n IHn].
+  - left.
+    by exists 0.
+  - elim: IHn.
+    + right.
+      elim: H => x H.
+      exists x.
+      by rewrite H addnC.
+    + elim=> x H.
+      left.
+      exists x.+1.
+      rewrite addnC in H.
+      by rewrite mulnSr addnC H.
+Qed.
+
+Theorem problem9 : forall (A : Type) (l m : seq A) (a : A),
+                     InList a (l ++ m) -> InList a l \/ InList a m.
+Proof.
+  move=> A l m a.
+  elim: l => [H| a0 l IHl H].
+  - right.
+    by apply H.
+  - inversion H.
+    + subst.
+      left.
+      by apply: headIL.
+    + elim: (IHl H1).
+      * left.
+          by apply: consIL.
+      * right.
+        by apply H3.
+Qed.
+
+(* END *)
