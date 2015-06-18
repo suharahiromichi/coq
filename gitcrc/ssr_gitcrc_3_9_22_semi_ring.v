c2586081053b29dbcbcd4c57384655236883bea3
SUHARA Hiromichi <suharahiromichi@gmail.com>
2015-06-18 21:29:28 +0900

    コメントを修正した。

--- gitcrc/ssr_gitcrc_3_9_22_semi_ring.v
+++ gitcrc/ssr_gitcrc_3_9_22_semi_ring.v
@@ -30,13 +30,17 @@ Section mat.                                (* matrices. *)
             (zero one : A) 
             (plus mult minus : A -> A -> A)
             (sym : A -> A).
-  Notation "0" := zero.  Notation "1" := one.
+  
+  Notation "0" := zero.
+  Notation "1" := one.
   Notation "x + y" := (plus x y).  
   Notation "x * y " := (mult x y).
   
-  Structure M2 : Type := {c00 : A;  c01 : A;
-                          c10 : A;  c11 : A}.
-  
+  Structure M2 : Type :=
+    {
+      c00 : A; c01 : A;
+      c10 : A; c11 : A
+    }.
   
   Definition Zero2 : M2 := Build_M2 0 0 0 0.
   Definition Id2 : M2 := Build_M2 1 0 0 1.
@@ -54,11 +58,11 @@ Section mat.                                (* matrices. *)
               (c10 m + c10 m')
               (c11 m + c11 m').
   
-  Lemma M2_eq_intros :                      (* not used *)
-    forall m m': M2, c00 m = c00 m' ->
-                     c01 m = c01 m' ->
-                     c10 m = c10 m' ->
-                     c11 m = c11 m' -> m = m'.
+  Lemma M2_eq_intros :
+    forall m m' : M2,
+      c00 m = c00 m' -> c01 m = c01 m' ->
+      c10 m = c10 m' -> c11 m = c11 m' ->
+      m = m'.
   Proof.
     elim=> m00 m01 m10 m11 /=.
     elim=> m'00 m'01 m'10 m'11 /=.
