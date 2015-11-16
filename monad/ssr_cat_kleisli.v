6c166a04386c7a18cbf1c76c1ddf9b81a0abd3bc
SUHARA Hiromichi <suharahiromichi@gmail.com>
2015-11-16 22:05:54 +0900

    first commit

---
+++ monad/ssr_cat_kleisli.v
@@ -0,0 +1,91 @@
+(**
+Kleisli クライスリ圏
+ *)
+
+Require Import ssreflect ssrfun ssrbool eqtype ssrnat seq.
+
+Set Implicit Arguments.
+Unset Strict Implicit.
+Unset Printing Implicit Defensive.
+
+Section Categories.
+
+  Class Setoid :=
+    {
+      carrier : Type;
+      equiv : carrier -> carrier -> Prop
+    }.
+  Coercion carrier : Setoid >-> Sortclass.
+  Notation "x === z" := (equiv x z) (at level 70).
+  
+  Class Category :=
+    {
+      (* 対象の定義 *)
+      Obj : Type;                           (* Category -> Type *)
+      
+      (* 射の定義 *)      
+      Mor : Obj -> Obj -> Setoid;           (* Coersion が有効になる。 *)
+      
+      (* 恒等射の定義 *)
+      idC : forall {A : Obj}, Mor A A;
+      
+      (* 射の合成の定義 *)
+      composeC : forall {A B C : Obj}, Mor B C -> Mor A B -> Mor A C;
+      
+      (* 単位律の定義 *)
+      left_identity : forall (A B : Obj) (f : Mor A B), composeC idC f === f;
+      right_identity : forall (A B : Obj) (f : Mor A B), composeC f idC === f;
+      
+      (* 結合律の定義 *)
+      associativity : forall (A B C D : Obj)
+                             (f : Mor A B) (g : Mor B C) (h : Mor C D),
+                        composeC (composeC h g) f === composeC h (composeC g f)
+    }.
+
+  Coercion Obj : Category >-> Sortclass.
+  Coercion Mor : Category >-> Funclass.
+  
+  Class Kleisli (C : Category) (T : C -> C) :=
+    {
+      pure: forall {X : C}, C X (T X);
+      bind: forall {X Y : C}, C X (T Y) -> C (T X) (T Y)
+    }.
+End Categories.
+
+Section Monad.
+  
+  Instance function (X Y : Type): Setoid :=
+  {
+    carrier := X -> Y;
+    equiv f g := forall x, f x = g x
+  }.
+  
+  Instance Types : Category :=
+  {
+    Obj := Type;
+    Mor X Y := function X Y;
+    idC X x := x;
+    composeC X Y Z g f x := g (f x)
+  }.
+  Proof.
+    - by [].
+    - by [].
+    - by [].      
+  Defined.
+  
+  About option.              (* Coq.Init.Datatypes.option *)
+  Check Some : forall A : Type, A -> option A.
+  Check @None : forall A : Type, option A.
+  
+  Instance Maybe : @Kleisli Types option :=
+    {
+      pure X x := Some x;
+      bind X Y f m :=
+        match m with
+          | Some x => f x
+          | _ => None
+        end
+    }.
+End Monad.
+
+(* END *)
