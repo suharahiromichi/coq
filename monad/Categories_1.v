cc02ecba167cb1404a051daf08f1ee3b6f5098c9
SUHARA Hiromichi <suharahiromichi@gmail.com>
2015-12-05 18:27:21 +0900

    SSReflectをImportした。

--- monad/Categories_1.v
+++ monad/Categories_1.v
@@ -2,75 +2,95 @@
 (* Steve Awodey's book on category theory *)
 (******************************************************************************)
 (* Chapter 1.3: Categories                                                    *)
-(******************************************************************************)
+(***`***************************************************************************)
 (* これをもとに改変。Instance ... Proper を使うようにした。 *)
 (* @suharahiromichi *)
 
+Require Import ssreflect ssrfun ssrbool eqtype ssrnat seq.
+Require Import finset fintype.
 
+Set Implicit Arguments.
+Unset Strict Implicit.
+Unset Printing Implicit Defensive.
+
+Require Import Notations.
 Require Import Morphisms.
 Require Import Coq.Setoids.Setoid.
 
-Reserved Notation "x ---> y" (at level 51, left associativity).
+(*
+Reserved Notation "x ~> y" (at level 51, left associativity).
+*)
 Reserved Notation "x \\o y" (at level 51, left associativity).
 Reserved Notation "x === y" (at level 71, left associativity).
 
 Generalizable Variables a b c d e.
 
-Class Category (Ob : Type) (Hom : Ob -> Ob -> Type) :=
+Class Category (Obj : Type) (Hom : Obj -> Obj -> Type) :=
   {
-    hom := Hom where "a ---> b" := (hom a b);
-    ob  := Ob;
-    id   : forall {a : Ob}, a ---> a;
-    comp : forall {a b c : Ob}, a ---> b -> b ---> c -> a ---> c  where "f \\o g" := (comp f g);
-    eqv  : forall {a b : Ob}, a ---> b -> a ---> b -> Prop        where "f === g" := (eqv f g);
+    hom := Hom where "a ~> b" := (hom a b);
+    ob  := Obj;
+    id   : forall {a : Obj}, a ~> a;
+    comp : forall {a b c : Obj}, a ~> b -> b ~> c -> a ~> c where "f \\o g" := (comp f g);
+    eqv  : forall {a b : Obj}, a ~> b -> a ~> b -> Prop     where "f === g" := (eqv f g);
     
-    eqv_equivalence : forall {a b : Ob}, Equivalence (@eqv a b);
-    comp_respects   : forall {a b c : Ob}, Proper (@eqv a b ==> @eqv b c ==> @eqv a c) comp;
+    eqv_equivalence : forall {a b : Obj}, Equivalence (@eqv a b);
+    comp_respects   : forall {a b c : Obj}, Proper (@eqv a b ==> @eqv b c ==> @eqv a c) comp;
     
-    left_identity   : forall `(f : a ---> b), @id a \\o f === f;
-    right_identity  : forall `(f : a ---> b), f \\o @id b === f;
-    associativity   : forall `(f : a ---> b) `(g : b ---> c) `(h : c ---> d),
-                        (f \\o g) \\o h === f \\o (g \\o h)
+    left_identity   : forall `(f : a ~> b), id \\o f === f;
+    right_identity  : forall `(f : a ~> b), f \\o id === f;
+    associativity   : forall `(f : a ~> b) `(g : b ~> c) `(h : c ~> d),
+                        f \\o g \\o h === f \\o (g \\o h)
 }.
 Coercion ob : Category >-> Sortclass.
 
-Notation "a ---> b"       := (hom a b).
-Notation "f === g"        := (eqv f g).
-Notation "f \\o g"        := (comp f g).
+Notation "a ~> b"       := (hom a b).
+Notation "f === g"      := (eqv f g).
+Notation "f \\o g"      := (comp f g).
 (* Notation "a ~~{ C }~~> b" := (@hom _ _ C a b). *)
 
+Generalizable Variables Obj Hom.
 
-Generalizable Variables Ob Hom.
-
-Instance route_equiv_Equiv `(C : Category Ob Hom) (a b :Ob) :
-  Equivalence (@eqv Ob Hom C a b).
+ (* eqv が、Reflexive と Symmetric と Transitive とを満たす。 *)
+ Instance category_eqv_Equiv `(C : Category Obj Hom) (a b : Obj) :
+  Equivalence (@eqv Obj Hom C a b).
 Proof.
-  now apply eqv_equivalence.
+  by apply eqv_equivalence.
 Qed.
 
-Instance cons_route_Proper `(C : Category Ob Hom) (a b c :Ob) :
-  Proper (@eqv Ob Hom C a b ==> @eqv Ob Hom C b c ==> @eqv Ob Hom C a c) (comp).
+(* comp は eqv について固有関数である。 *)
+Instance category_comp_Proper `(C : Category Obj Hom) (a b c : Obj) :
+  Proper (@eqv Obj Hom C a b ==> @eqv Obj Hom C b c ==> @eqv Obj Hom C a c) (comp).
 Proof.
-  now apply comp_respects.
+  by apply comp_respects.
 Qed.
 
-Lemma juggle1 : forall `{C:Category} `(f : a ---> b) `(g : b ---> c) `(h : c ---> d) `(k : d ---> e),
-                  (((f \\o g) \\o h) \\o k) === (f \\o (g \\o h) \\o k).
+(* 可換性についての定理を証明する。 *)
+Lemma juggle1 : forall `{C : Category}
+                       `(f : a ~> b) `(g : b ~> c) `(h : c ~> d) `(k : d ~> e),
+                  f \\o g \\o h \\o k === f \\o (g \\o h) \\o k.
 Proof.
   intros.
   Check associativity f g h.
-  setoid_rewrite <- associativity.
+  rewrite <- associativity.
   reflexivity.
 Defined.
 
-Lemma juggle2 : forall `{C:Category}`(f:a ---> b)`(g:b ---> c)`(h:c ---> d)`(k:d ---> e),
-                  (f \\o (g \\o (h \\o k))) === (f \\o (g \\o h) \\o k).
-  intros; repeat setoid_rewrite <- associativity. reflexivity.
+Lemma juggle2 : forall `{C : Category}
+                       `(f : a ~> b) `(g : b ~> c) `(h : c ~> d) `(k : d ~> e),
+                  f \\o (g \\o (h \\o k)) === f \\o (g \\o h) \\o k.
+Proof.
+  intros.
+  do ! rewrite <- associativity.
+  reflexivity.
 Defined.
 
-Lemma juggle3 : forall `{C:Category}`(f:a ---> b)`(g:b ---> c)`(h:c ---> d)`(k:d ---> e),
-                  ((f \\o g) \\o (h \\o k)) === (f \\o (g \\o h) \\o k).
-  intros; repeat setoid_rewrite <- associativity. reflexivity.
+Lemma juggle3 : forall `{C : Category}
+                       `(f : a ~> b) `(g : b ~> c) `(h : c ~> d) `(k : d ~> e),
+                  f \\o g \\o (h \\o k) === f \\o (g \\o h) \\o k.
+Proof.
+  intros.
+  do ! rewrite <- associativity.
+  reflexivity.
 Defined.
 
 (* END *)
