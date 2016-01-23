a74855b2d331efaf61cd2f95c7d6d1e08ded7ed9
SUHARA Hiromichi <suharahiromichi@gmail.com>
2016-01-24 00:05:40 +0900

    SSReflect で証明した。

--- sf/coq_bin_norm.v
+++ sf/coq_bin_norm.v
@@ -1,3 +1,11 @@
+Require Import ssreflect ssrfun ssrbool eqtype ssrnat.
+
+Set Implicit Arguments.
+Unset Strict Implicit.
+Unset Printing Implicit Defensive.
+
+Open Scope coq_nat_scope.
+
 Inductive bin :=
 | o : bin -> bin
 | i : bin -> bin
@@ -7,7 +15,7 @@ Check (i (i (o z))).
 
 Fixpoint binnat (b : bin) : nat :=          (* f *)
   match b with
-  | z => O
+  | z => 0
   | o b => 2 * (binnat b)
   | i b => 2 * (binnat b) + 1
   end.
@@ -28,8 +36,8 @@ Compute bininc (i (i (i z))).
 
 Fixpoint natbin (n : nat) : bin :=          (* g *)
   match n with
-  | O => z
-  | S n => bininc (natbin n)
+  | 0 => z
+  | n.+1 => bininc (natbin n)
   end.
 
 (* 直接的な正規化 *)
@@ -37,7 +45,7 @@ Fixpoint normalize (b : bin) : bin :=
   match b with
   | o b =>
     match (binnat b) with
-    | O => z
+    | 0 => z
     | _ => o (normalize b)
     end
   | i b => i (normalize b)
@@ -47,18 +55,35 @@ Fixpoint normalize (b : bin) : bin :=
 Compute normalize (o (i (i (o (o z))))).
 Compute natbin (binnat (o (i (i (o (o z)))))).
 
-Lemma hodai1 n :
-  natbin (S n + S n) = o (natbin (S n)).
+Lemma test' n : n + n.+2 = 2 * n.+1.
 Proof.
+  Search (2 * _).
   simpl.
-  admit.
+  rewrite NPeano.Nat.add_0_r.
+  by [].
+Qed.
+
+Lemma hodai1 n :
+  natbin (2 * n.+2) = o (natbin n.+2).
+Proof.
+  elim: n.+1.
+  - by [].
+  - move=> n' H /=.
+    Search (_ + 0).
+    rewrite NPeano.Nat.add_0_r.
+    rewrite test'.
+    by rewrite H /=.
 Qed.
 
 Lemma hodai2 n :
-  natbin (S n + S n + 1) = i (natbin (S n)).
+  natbin (2 * n.+2 + 1) = i (natbin n.+2).
 Proof.
-  simpl.
-  admit.
+  elim: n.+1.
+  - by [].
+  - move=> n' H /=.
+    rewrite NPeano.Nat.add_0_r.
+    rewrite test'.
+    by rewrite H /=.
 Qed.
 
 (* natを経由する正規化と、直接的な正規化が、同じ結果になることを証明する。 *)
@@ -75,9 +100,7 @@ Proof.
       induction n.
       * simpl.
         reflexivity.
-      * rewrite plus_0_r.
-        rewrite hodai1.
-        reflexivity.
+      * by apply hodai1.
   - simpl.      
     rewrite <- IHb.
     case (binnat b).
@@ -87,9 +110,7 @@ Proof.
       induction n.
       * simpl.
         reflexivity.
-      * rewrite plus_0_r.
-        rewrite hodai2.
-        reflexivity.
+      * by apply hodai2.
   - simpl.
     reflexivity.
 Qed.
