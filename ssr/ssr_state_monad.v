1cd8740098554dfc69f91e1fe19240748a848abd
SUHARA Hiromichi <suharahiromichi@gmail.com>
2016-03-19 23:58:51 +0900

    get, put の引数の型を Section のなかのVariableで定義するようにした。 Sectionの外では、@get nat のように型を指定する必要がある。

--- ssr/ssr_state_monad.v
+++ ssr/ssr_state_monad.v
@@ -2,59 +2,64 @@
 (** その前半部分 State Monad *)
 (* @suharahiromichi 2014_01_13 *)
 
-Require Import ssreflect ssrbool ssrnat seq eqtype.
-Require Import ssrfun.
+From mathcomp Require Import ssreflect ssrbool ssrnat seq eqtype.
+From mathcomp Require Import ssrfun.
 Require Import String.                      (* Error *)
 Require Import Program.                     (* Program *)
+
+Set Implicit Arguments.
+Unset Strict Implicit.
+Unset Printing Implicit Defensive.
 
 Inductive Tree (a : Set) : Set :=
 | Leaf : a -> Tree a
 | Node : Tree a -> Tree a -> Tree a.
 
-Implicit Arguments Leaf [[a]].
-Implicit Arguments Node [[a]].
-
 Fixpoint relabel' (a : Set) (t : Tree a) (s : nat) : Tree nat * nat
   :=
     match t with
       | Leaf _ =>
-        (Leaf s, 1 + s)
+        (Leaf s, s.+1)
       | Node l r =>
-        let (l , s ) := relabel' a l s in
-        let (r , s ) := relabel' a r s in
+        let (l , s ) := relabel' l s in
+        let (r , s ) := relabel' r s in
         (Node l r , s )
    end.
 
-Definition s := nat.
+Section state_monad.
 
+Variable st : Set.
+  
 Definition State (a : Set) : Type :=
-  s -> a * s.
+  st -> a * st.
 
 Definition ret {a : Set} : a -> State a := fun x s => (x , s).
 
 Definition bind {a b : Set} : State a -> (a -> State b) -> State b
   := fun c1 c2 s1 =>
        let (x , s2 ) := c1 s1 in c2 x s2 .
-Infix ">>=" := bind (right associativity, at level 71).
 
 Definition bind2 {a b : Set} : State a -> State b -> State b
   := fun p1 p2 =>
-       p1 >>= fun _ => p2.
-Infix ">>>" := bind2 (right associativity, at level 71).
+       bind p1 (fun _ => p2).               (* p1 >>= (fun _ => p2) *)
 
-Definition get : State s :=
+Definition get : State st :=
   fun s => (s, s).
 
-Definition put : s -> State unit :=
+Definition put : st -> State unit :=
   fun s _ => (tt, s).
 
-Fixpoint relabel {a : Set} (t : Tree a) : State (Tree nat)
+End state_monad.
+Infix ">>=" := bind (right associativity, at level 71).
+Infix ">>>" := bind2 (right associativity, at level 71).
+
+Fixpoint relabel {a : Set} (t : Tree a) : State nat (Tree nat)
   :=
     match t with
       | Leaf _ =>
-        get >>=
-            fun n => put (S n) >>>
-                         ret (Leaf n)
+        @get nat >>=
+            fun n => @put nat n.+1 >>>
+                          ret (Leaf n)
       | Node l r =>
         relabel l >>=
         fun l => relabel r >>=
