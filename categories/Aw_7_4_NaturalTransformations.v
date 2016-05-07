5f533d7c4316f5b5894bc79523d22d50e611c0b2
SUHARA Hiromichi <suharahiromichi@gmail.com>
2016-05-07 17:55:30 +0900

    説明を追加した。

--- categories/Aw_7_4_NaturalTransformations.v
+++ categories/Aw_7_4_NaturalTransformations.v
@@ -14,20 +14,28 @@ Require Import Aw_1_5_Isomorphisms.
 (* Chapter 7.4: Natural Transformations                                        *)
 (*******************************************************************************)
 
-(* Definition 7.6 *)
-
 Generalizable Variable A B.
-Class NaturalTransformation `{C1 : Category} `{C2 : Category} {Fobj1 Fobj2 : C1 -> C2}
-      (F1 : @Functor C1 _ _ C2 _ _ Fobj1) (F2 : @Functor C1 _ _ C2 _ _ Fobj2) :=
+
+(* Definition 7.6 *)
+(* 自然変換 *)
+(* 圏CとD、関手F:C→D と G:C→D が与えられたとき、
+   自然変換 ϑ : F→G は以下を満たす。 *)
+
+Class NaturalTransformation `{C : Category} `{D : Category} {Fobj1 Fobj2 : C -> D}
+      (F : @Functor C _ _ D _ _ Fobj1) (G : @Functor C _ _ D _ _ Fobj2) :=
   {
-    nt_component : forall c : C1, (Fobj1 c) ~~{C2}~~> (Fobj2 c);
+    (* Dの射 ϑc : F c → G c が存在する。Dの射ϑcは、cにおけるϑの成分という。 *)
+    nt_component : forall c : C, (Fobj1 c) ~~{D}~~> (Fobj2 c);
+    
+    (* ϑc' \\o F f === G f \\o ϑc が成立する。可換になる。 *)
     nt_commutes  : forall `(f : A ~> B),
-                     F2 \ f \\o (nt_component A) === (nt_component B) \\o F1 \ f
+                     (nt_component B) \\o F \ f === G \ f \\o (nt_component A)
   }.
 Notation "F ~~~> G" := (@NaturalTransformation _ _ _ _ _ _ _ _ F G) : category_scope.
 Coercion nt_component : NaturalTransformation >-> Funclass.
 
 (* the identity natural transformation *)
+(* 恒等自然変換 ϑ : F→F *)
 Program Instance nt_id `{C : Category} `{D : Category} {Fobj}
            (F : @Functor C _ _ D _ _ Fobj) : NaturalTransformation F F.
 Obligation 1.
@@ -38,42 +46,50 @@ Proof.
   Check (fun c => @iid _ _ _ (F c)).  
   Check (@Build_NaturalTransformation _ _ _ _ _ _ _ _ F F (fun c => @iid _ _ _ (F c))).
   apply (@Build_NaturalTransformation _ _ _ _ _ _ _ _ F F (fun c => @iid _ _ _ (F c))).
+  move=> A B f.
+(*
   setoid_rewrite left_identity.
   setoid_rewrite right_identity.
+*)
+  rewrite [iid \\o _]left_identity.         (* 左辺 *)
+  rewrite [_ \\o iid]right_identity.        (* 右辺 *)
   reflexivity.
 Defined.
 Obligation 2.
 Proof.
-  setoid_rewrite left_identity.
-  setoid_rewrite right_identity.
+  rewrite [iid \\o _]left_identity.
+  rewrite [_ \\o iid]right_identity.
   reflexivity.
 Defined.  
 
-Program Instance nt_comp `{C : Category}`{D : Category}
-  {Fobj}{F : @Functor C _ _ D _ _ Fobj}
-  {Gobj}{G : @Functor C _ _ D _ _ Gobj}
-  {Hobj}{H : @Functor C _ _ D _ _ Hobj}
-  (nt_f_g : F ~~~> G) (nt_g_h : G ~~~> H) : F ~~~> H.
+(* 自然変換は結合律を満たす。 *)
+Program Instance nt_comp `{C : Category} `{D : Category}
+        {Fobj} {F : @Functor C _ _ D _ _ Fobj}
+        {Gobj} {G : @Functor C _ _ D _ _ Gobj}
+        {Hobj} {H : @Functor C _ _ D _ _ Hobj}
+        (nt_f_g : F ~~~> G) (nt_g_h : G ~~~> H) : F ~~~> H.
 Obligation 1.
 Proof.
   Check (fun c => nt_g_h c \\o nt_f_g c) : (forall c : C, Fobj c ~~{ D }~~> Hobj c).
   apply (@Build_NaturalTransformation _ _ _ _ _ _ _ _ F H (fun c => nt_g_h c \\o nt_f_g c)).
-  set (@nt_commutes _ _ C _ _ D _ _ F G nt_f_g) as comm1.
-  set (@nt_commutes _ _ C _ _ D _ _ G H nt_g_h) as comm2.
-  intros.
-  setoid_rewrite associativity.
-  setoid_rewrite <- comm1.
-  setoid_rewrite <- associativity.
-  setoid_rewrite comm2.
+  move=> A B f.
+  have comm1 := @nt_commutes _ _ C _ _ D _ _ F G nt_f_g.
+  have comm2 := @nt_commutes _ _ C _ _ D _ _ G H nt_g_h.
+  rewrite associativity.
+  rewrite comm1.
+  rewrite -associativity.
+  rewrite comm2.
+  rewrite associativity.
   reflexivity.
 Defined.
 Obligation 2.
-  set (@nt_commutes _ _ C _ _ D _ _ F G nt_f_g) as comm1.
-  set (@nt_commutes _ _ C _ _ D _ _ G H nt_g_h) as comm2.
-  setoid_rewrite associativity.
-  setoid_rewrite <- comm1.
-  setoid_rewrite <- associativity.
-  setoid_rewrite comm2.
+  have comm1 := @nt_commutes _ _ C _ _ D _ _ F G nt_f_g.
+  have comm2 := @nt_commutes _ _ C _ _ D _ _ G H nt_g_h.
+  rewrite associativity.
+  rewrite comm1.
+  rewrite -associativity.
+  rewrite comm2.
+  rewrite associativity.
   reflexivity.
 Defined.
 
