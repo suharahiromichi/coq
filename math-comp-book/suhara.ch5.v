32e28a153d91206a45120eeee2a1a4a4258c757a
SUHARA Hiromichi <suharahiromichi@gmail.com>
2016-08-29 19:37:00 +0900

    定義 (Notation) を整理した。

--- math-comp-book/suhara.ch5.v
+++ math-comp-book/suhara.ch5.v
@@ -9,13 +9,17 @@ Fixpoint iota m n :=
     [::].
 Compute iota 3 5.                           (* = [:: 3; 4; 5; 6; 7] *)
 
-Reserved Notation "\sum_ ( m <= i < n ) F"
+Reserved Notation "\sum''_ ( m <= i < n ) F"
          (at level 41, F at level 41, i, m, n at level 50,
-          format "'[' \sum_ ( m  <=  i  <  n ) '/  '  F ']'").
+          format "'[' \sum''_ ( m  <=  i  <  n ) '/  '  F ']'").
 
-Reserved Notation "\sum_ ( i <- r | P ) F"
-         (at level 41, F at level 41, i, r at level 50,
-          format "'[' \sum_ ( i  <-  r  |  P ) '/  '  F ']'").
+Reserved Notation "\sum'_ ( m <= i < n | P ) F"
+         (at level 41, F at level 41, i, m, n at level 50,
+          format "'[' \sum'_ ( m  <=  i  <  n | P ) '/  '  F ']'").
+
+Reserved Notation "\sum'_ ( m <= i < n ) F"
+         (at level 41, F at level 41, i, m, n at level 50,
+          format "'[' \sum'_ ( m  <=  i  <  n ) '/  '  F ']'").
 
 Reserved Notation "\big [ op / idx ]_ ( i <- r | P ) F"
          (at level 36, F at level 36, op, idx at level 10, i, r at level 50,
@@ -29,23 +33,32 @@ Reserved Notation "\big [ op / idx ]_ ( m <= i < n | P ) F"
          (at level 36, F at level 36, op, idx at level 10, m, i, n at level 50,
           format "'[' \big [ op / idx ]_ ( m  <=  i  <  n  |  P )  F ']'").
 
+Reserved Notation "\big [ op / idx ]_ ( m <= i < n ) F"
+         (at level 36, F at level 36, op, idx at level 10, m, i, n at level 50,
+          format "'[' \big [ op / idx ]_ ( m  <=  i  <  n )  F ']'").
+
+Reserved Notation "\sum_ ( m <= i < n | P ) F"
+         (at level 41, F at level 41, i, m, n at level 50,
+          format "'[' \sum_ ( m  <=  i  <  n | P ) '/  '  F ']'").
+
+Reserved Notation "\sum_ ( m <= i < n ) F"
+         (at level 41, F at level 41, i, m, n at level 50,
+          format "'[' \sum_ ( m  <=  i  <  n ) '/  '  F ']'").
 
 (* 1.7 Iterators in mathematics *)
 (* もっとも簡単な定義 *)
-(*
-Notation "\sum_ ( m <= i < n ) F" :=
+
+Notation "\sum''_ ( m <= i < n ) F" :=
   (foldr (fun i a => F + a) 0 (iota m (n - m))).
-*)
 
 (* 5.8 The generic theory of “big” operators *)
-(* index_iota を使った例 *)
-(*
+
 Definition index_iota m n :=                (* U(i=m, i<n)、n は含まない！ *)
   iota m (n - m).
 
 Compute index_iota 3 5.                     (* = [:: 3; 4] = U(i=3, i<5) ... *)
 
-Definition bigop {R I : Type}
+Definition bigop' {R I : Type}
            (idx : R)                        (* 初期値 *)
            (op : R -> R -> R)               (* big-opに対するsmall-op *)
            (r : seq I)                      (* リスト *)
@@ -54,27 +67,15 @@ Definition bigop {R I : Type}
   R :=                                      (* 結果 *)
   foldr (fun (i : I) (x : R) => if P i then op (F i) x else x) idx r.
 
-Notation "\big [ op / idx ]_ ( i <- r | P ) F" :=
-  (bigop idx op r (fun i => P%B) (fun i => F)) : big_scope.
-
-Notation "\big [ op / idx ]_ ( i <- r ) F" :=
-  (bigop idx op r (fun i => true) (fun i => F)) : big_scope.
-
-Notation "\big [ op / idx ]_ ( m <= i < n | P ) F" :=
-  (bigop idx op (index_iota m n) (fun i : nat => P%B) (fun i => F)) : big_scope.
-
-Notation "\big [ op / idx ]_ ( m <= i < n ) F" :=
-  (bigop idx op (index_iota m n) (fun i : nat => true) (fun i => F)) : big_scope.
-
 Local Notation "+%N" :=
   addn (at level 0, only parsing).
 
-Notation "\sum_ ( m <= i < n | P ) F":=
-  (\big[+%N/0%N]_(m <= i < n | P) F%N) : nat_scope.
+Notation "\sum'_ ( m <= i < n | P ) F":=
+  (bigop' 0%N +%N (index_iota m n) P F%N) : nat_scope.
 
-Notation "\sum_ ( m <= i < n ) F":=
-  (\big[+%N/0%N]_(m <= i < n) F%N) : nat_scope.
-*)
+Notation "\sum'_ ( m <= i < n ) F":=
+  (bigop' 0%N +%N (index_iota m n) true F%N) : nat_scope.
+
 
 (* 5.9 Stable notations for big operators *)
 (* To solve these problems 以降を抜粋する。 *)
@@ -93,22 +94,32 @@ Definition applybig {R I} (body : @bigbody R I) acc :=
 Definition bigop {R I} idx r (body : I -> @bigbody R I) :=
   foldr (applybig \o body) idx r.
 
-Notation "\big [ op / idx ]_ ( i <- r | P ) F":=
-  (bigop idx r (fun i => BigBody i op P%B F)) : big_scope.
+Notation "\big [ op / idx ]_ ( i <- r | P ) F" :=
+  (bigop idx r (fun i => BigBody i op P F)) : big_scope.
 
-Notation "\big [ op / idx ]_ ( i <- r ) F":=
+Notation "\big [ op / idx ]_ ( i <- r ) F" :=
   (bigop idx r (fun i => BigBody i op true F)) : big_scope.
 
-(* \sum_ の定義は上と同じ。 *)
+Notation "\big [ op / idx ]_ ( m <= i < n | P ) F" :=
+  (\big[op/idx]_(i <- index_iota m n | P) F) : big_scope.
+
+Notation "\big [ op / idx ]_ ( m <= i < n ) F" :=
+  (\big[op/idx]_(i <- index_iota m n) F) : big_scope.
+
 Local Notation "+%N" :=
   addn (at level 0, only parsing).
 
-Notation "\sum_ ( i <- r | P ) F":=
-  (\big[+%N/0%N]_(i <- r | P%B) F%N) : nat_scope.
+Notation "\sum_ ( m <= i < n | P ) F":=
+  (\big[+%N/0%N]_(m <= i < n | P) F%N) : nat_scope.
+(*
+  (bigop 0%N (index_iota m n) (fun i => BigBody i addn P F%N)) : nat_scope.
+*)
 
-Notation "\sum_ ( i <- r ) F":=
-  (\big[+%N/0%N]_(i <- r) F%N) : nat_scope.
-
+Notation "\sum_ ( m <= i < n ) F":=
+  (\big[+%N/0%N]_(m <= i < n) F%N) : nat_scope.
+(*
+  (bigop 0%N (index_iota m n) (fun i => BigBody i addn true F%N)) : nat_scope.
+*)
 
 (* *********** *)
 (* テストと証明 *)
@@ -117,27 +128,31 @@ Notation "\sum_ ( i <- r ) F":=
 Eval compute in \sum_(1 <= i < 5) (i * 2 - 1). (* = 16 : nat *)
 Eval compute in \sum_(1 <= i < 5) i.           (* = 10 : nat *)
 
-Check big_nat_recr.                         (* この証明を与える。 *)
-(*
-  forall (R : Type) (idx : R) (op : Monoid.law idx)  (n m : nat) (F : nat -> R),
+Lemma big_nat_recr {R : Type} (idx : R) (op : Monoid.law idx)  (n m : nat) (F : nat -> R) :
     m <= n ->
     \big[op/idx]_(m <= i < n.+1) F i =
     op (\big[op/idx]_(m <= i < n) F i) (F n).
- *)
-
-Check big_mkcond.                           (* この証明を与える。 *)
 (*
-  forall (R : Type) (idx : R) (op : Monoid.law idx) 
-         (I : Type) (r : seq I) (P : pred I) (F : I -> R),
-    \big[op/idx]_(i <- r | P i) F i =
-    \big[op/idx]_(i <- r) (if P i then F i else idx).
- *)
+    bigop idx (index_iota m n.+1) (fun i => @BigBody R nat i op true (F i)) =
+    op (bigop idx (index_iota m n) (fun i => BigBody i op true (F i))) (F n).
+*)
+Proof.
+Admitted.
+
+Lemma big_mkcond {I R : Type} (idx : R) (op : Monoid.law idx)
+      (r : seq I) (P : pred I) (F : I -> R) :
+  \big[op/idx]_(i <- r | P i) F i =
+  \big[op/idx]_(i <- r) (if P i then F i else idx).
+ (*
+  bigop idx r (fun i => @BigBody R I i op (P i) (F i)) =
+  bigop idx r (fun i => @BigBody R I i op true (if P i then F i else idx)).
+*)
+Proof.
+Admitted.
 
 Lemma sum_odd_3 : \sum_(0 <= i < 3.*2 | odd i) i = 3^2.
 Proof.
-  rewrite big_mkcond big_nat_recr //=.
-  rewrite unlock /=.
-  ring.
+  by rewrite big_mkcond big_nat_recr //=.
 Qed.
 
 Lemma test : \sum_(0 <= i < 6) i= (\sum_(0 <= i < 5) i) + 5.
@@ -150,8 +165,9 @@ Qed.
 
 Lemma sum_odd n : \sum_(0 <= i < n.*2 | odd i) i = n^2.
 Proof.
-  elim: n => [|n IHn]; first by rewrite unlock.
-  rewrite doubleS big_mkcond 2?big_nat_recr // -big_mkcond /=.
+  elim: n => [|n IHn].
+  - done.
+  - rewrite doubleS big_mkcond 2?big_nat_recr // -big_mkcond /=.
     rewrite {}IHn odd_double /= addn0 -addnn -!mulnn; ring.
 Qed.
 
