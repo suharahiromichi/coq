a906dd393cd6aa83bf883c0273883867c2c7f3a7
SUHARA Hiromichi <suharahiromichi@gmail.com>
2016-08-31 23:48:14 +0900

    カノニカルの説明を追加した。

--- math-comp-book/suhara.ch6-tuples.v
+++ math-comp-book/suhara.ch6-tuples.v
@@ -5,16 +5,25 @@ From mathcomp Require Import all_ssreflect.
 
 Structure tuple_of n T :=
   Tuple {
-      tval :> seq T;
+      tval :> seq T;                        (* コアーション *)
       _ : size tval == n
     }.
 
 Notation "n .-tuple T" := (tuple_of n T)
                             (at level 2, format "n .-tuple T") : type_scope.
 
-Lemma size_tuple T n (t : n.-tuple T) : size t = n.
+Lemma Hsize : size [::1;1;1] == 3. Proof. done. Qed.
+Check Tuple 3 nat [::1;1;1] Hsize : 3.-tuple nat.
+Check Tuple 3 nat [::1;1;1] Hsize : seq nat. (* コアーション による。 *)
+Check size (Tuple 3 nat [::1;1;1] Hsize) : nat. (* seq nat のサイズ *)
+
+(* n.-tuple T 型の値のサイズは、n である。 *)
+Lemma size_tuple n T (t : n.-tuple T) : size t = n.
 Proof.
-  case: t => s i.
+  case: t => s i /=.
+  Check (Tuple n T s i) : n.-tuple T.
+  Check (Tuple n T s i) : seq T.            (* コアーション による。 *)
+  Check size (Tuple n T s i).               (* seq T のサイズ *)
   by apply/eqP.
 Qed.
 
@@ -30,32 +39,39 @@ Proof.
     by rewrite map_rev revK size_map.
 Qed.
 
-
 (* Unification debugging toolkit *)
 Notation "X (*...*)" :=
-  (let x := X in let y := _ in x)   (at level 100, format "X  (*...*)").
+  (let x := X in let y := _ in x)
+    (at level 100, format "X  (*...*)").
 Notation "[LHS ’of’ equation ]" :=
   (let LHS := _ in
-   let _infer_LHS := equation : LHS = _ in LHS)   (at level 4).
+   let _infer_LHS := equation : LHS = _ in LHS)
+    (at level 4).
 Notation "[unify X ’with’ Y ]" :=
   (let unification := erefl _ : X = Y in True).
 
+
 Section CanonicalTuples.
   Variables (n : nat) (A B : Type).
+
+  (* tuple 全体に対する操作を tuple の tval に対する操作とみなすことができる。 *)
+  (* rev_tuple ............. rev_tupleP *)
 
   Lemma rev_tupleP (t : n.-tuple A) : size (rev t) == n.
   Proof.
       by rewrite size_rev size_tuple.
   Qed.
-  Canonical rev_tuple (t : n.-tuple A) :=
-    Tuple n A (rev t) (rev_tupleP t).
+(* Definition rev_tuple (t : n.-tuple A) := Tuple n A (rev t) (rev_tupleP t). *)
+  Canonical rev_tuple (t : n.-tuple A) := Tuple n A (rev t) (rev_tupleP t).
+  Print Canonical Projections.         (* rev <- tval ( rev_tuple ) *)
   
   Lemma map_tupleP (f: A -> B) (t: n.-tuple A) : size (map f t) == n.
   Proof.
       by rewrite size_map size_tuple.
   Qed.
-  Canonical map_tuple (f: A -> B) (t: n.-tuple A) :=
-    Tuple n B (map f t) (map_tupleP f t).
+(* Definition map_tuple (f: A -> B) (t: n.-tuple A) := Tuple n B (map f t) (map_tupleP f t). *)
+  Canonical map_tuple (f: A -> B) (t: n.-tuple A) := Tuple n B (map f t) (map_tupleP f t).
+  Print Canonical Projections.              (* map <- tval ( map_tuple ) *)
   
   Lemma cons_tupleP (t : n.-tuple A) x : size (x :: t) == n.+1.
   Proof.
@@ -65,13 +81,32 @@ Section CanonicalTuples.
     Tuple n.+1 A (x :: t) (cons_tupleP t x).
 End CanonicalTuples.
 
+Check size_tuple : forall (n : nat_eqType) (T : Type) (t : n.-tupleT), size t = n.
 
 (* 証明 *)
 
 Example just_tuple n (t : n.-tuple nat) :
   size (rev [seq 2 * x | x <- rev t]) = size t.
 Proof.
-    by rewrite !size_tuple.
+  Check size_tuple n nat t.
+  rewrite (size_tuple n nat t).
+  
+  Check (size_tuple n nat
+                    (rev_tuple n nat
+                               (map_tuple n nat nat [eta muln 2] (rev_tuple n nat t)))).
+  rewrite (size_tuple n nat
+                      (rev_tuple n nat
+                                 (map_tuple n nat nat [eta muln 2] (rev_tuple n nat t)))).
+  reflexivity.
+
+  Restart.
+  rewrite size_tuple.
+  (* rev_tuple と map_tuple のカノニカルをつかう。 *)
+  rewrite size_tuple. (* カノニカルをDefinitionにすると、ふたつめの rewrite がエラーになる。 *)  
+  reflexivity.
+
+  Restart.
+  by rewrite 2!size_tuple.
 Qed.
 
 (*  6.2 n-tuples, a sub-type of sequences *)
@@ -106,4 +141,42 @@ Canonical tuple_eqType (n : nat_eqType) (T : eqType) :=
 
 (* 6.2.1 The sub-type kit *)
 
-(* END *)
+Canonical tuple_subType := Eval hnf in [subType for tval].
+
+Variables (s : seq nat) (t : 3.-tuple nat).
+
+Hypothesis size3s : size s == 3.
+
+Let t1 : 3.-tuple nat := Sub s size3s.
+Let s2 := if insub s is Some t then val (t : 3.-tuple nat) else nil.
+Let t3 := insubd t s.                       (* : 3.-tuple nat *)
+
+Definition pred T := T -> bool.
+
+Section SubTypeKit.
+  Variables (T : Type) (P : pred T).
+  
+  Structure subType : Type :=
+    SubType {
+        sub_sort :> Type;
+        val : sub_sort -> T;
+        Sub : forall x, P x -> sub_sort;
+        (* elimination rule for sub_sort *)
+        _: forall K (_ : forall x Px, K (@Sub x Px)) u, K u;
+        _: forall x Px, val (@Sub x Px) = x
+      }.
+
+  Notation "[ ’subType’ ’for’ v ]" :=
+    (SubType _ v _
+             (fun K K_S u => let (x, Px) as u return K u := u in K_S x Px)
+             (fun x px => erefl x)).
+
+End SubTypeKit.
+  
+(* 6.2.2 A note on boolean Σ-types *)
+
+Theorem eq_irrelevance (T : eqType) (x y : T) : forall e1 e2 : x = y, e1 = e2.
+Proof.
+Admitted.
+
+ (* END *)
