f2a0ca4a6e284aca1e540f1082710e83f2662184
SUHARA Hiromichi <suharahiromichi@gmail.com>
2016-10-07 20:15:51 +0900

    説明を追加した。

--- math-comp-book/suhara.ch6-finfun.v
+++ math-comp-book/suhara.ch6-finfun.v
@@ -6,30 +6,44 @@ From mathcomp Require Import all_ssreflect.
 Section FinFunDef.
   Variables (aT : finType) (rT : Type).
   
-  Inductive finfun : Type := Finfun of #|aT|.-tuple rT.
+  Inductive finfun_type : Type := Finfun of #|aT|.-tuple rT.
+  (* テキストは finfun だが、finfun_type に変更する。 *)
   
   Definition fgraph f := let: Finfun t := f in t.
+  (* Finfun 構造体から tupple を取り出す。 *)
   
   Canonical finfun_subType := Eval hnf in [newType for fgraph].
   Eval hnf in [newType for fgraph].         (* [subType for fgraph] *)
-  
 End FinFunDef.
 
-Notation "{ ’ffun’ fT }" := (finfun fT).
+(* Notation "{ 'ffun' aT -> rT }" := (finfun_type aT rT). *)
+(* Notation "{ 'ffun' fT }" := (finfun_type (Phant fT)). *)
 
-Definition fun_of_fin aT rT f x := tnth (@fgraph aT rT f) (enum_rank x).
-(* Coercion fun_of_fin : finfun >-> FunClass. *)
-(* Definition finfun aT rT f := @Finfun aT rT (codom_tuple f). *)
+(* 有限ドメイン関数から通常の関数に変換する。 *)
+Check fgraph : forall (aT : finType) (rT : Type), (finfun_type aT rT) -> #|aT|.-tuple rT.
+Check @enum_rank : forall T : finType, T -> 'I_ _.
+Check tnth : forall (n : nat) (T : Type), n.-tuple T -> 'I_n -> T.
 
-Notation "[’ffun’x : aT => F ]" := (finfun (fun x : aT => F)).
+Definition fun_of_fin (aT : finType) (rT : Type) (f : finfun_type aT rT) : (aT -> rT) :=
+  fun (x : aT) => tnth (@fgraph aT rT f) (enum_rank x).
+Coercion fun_of_fin : finfun_type >-> Funclass.
 
-Check [ffun i : 'I_4 => i + 2] : {ffun ordinal_finType 4 -> nat}.
+(* 通常の関数から有限ドメイン関数に変換する。 *)
+Check codom_tuple : forall (aT : finType) (rT : Type), (aT -> rT) -> #|aT|.-tuple rT.
+
+Definition finfun (aT : finType) (rT : Type) (f : aT -> rT) : (finfun_type aT rT) :=
+  @Finfun aT rT (codom_tuple f).
+
+Notation "[ 'ffun' x : aT => F ]" := (finfun _ _ (fun x : aT => F)).
+
+Check [ffun i : 'I_4 => i + 2] : finfun_type (ordinal_finType 4) nat.
+Check {ffun ordinal_finType 4 -> nat}.
 
 (* eqType の継承 *)
-Definition finfun_eqMixin aT (rT : eqType) :=
-  Eval hnf in [eqMixin of finfun aT rT by <:].
-Canonical finfun_eqType aT (rT : eqType) :=
-  Eval hnf in EqType (finfun aT rT) (finfun_eqMixin aT rT).
+Definition finfun_eqMixin (aT : finType) (rT : eqType) :=
+  Eval hnf in [eqMixin of finfun_type aT rT by <:].
+Canonical finfun_eqType (aT : finType) (rT : eqType) :=
+  Eval hnf in EqType (finfun_type aT rT) (finfun_eqMixin aT rT).
 
 (* codomain も有限な場合 *)
 
@@ -46,16 +60,28 @@ Definition tuple_enum (T : finType) n : seq (n.-tuple T) :=
 Lemma enumP T n : Finite.axiom (tuple_enum T n).
 *)
 
-(* finfun としての tuple *)
+(* 有限型としての としての 有限tuple (有限型を要素とするtuple) *)
 Definition tuple_finMixin (n : nat_eqType) (T : finType) :=
   Eval hnf in FinMixin (@FinTuple.enumP n T).
 Canonical tuple_finType (n : nat_eqType) (T : finType) :=
   Eval hnf in FinType (n.-tuple T) (tuple_finMixin n T).
 
+(* 有限型としてのfinfun *)
+Unset Printing Implicit.    (* Unset : implicitな引数を表示しない。 D:しない。 A:する。*)
+Unset Printing Coercions.   (* Unset : コアーションを表示しない     D:しない。 A:する。*)
+Unset Printing Notations. (*   Set : Notation を使って表示する。  D:する。  A:しない。*) 
+Unset Printing Universe.  (* Unset : 高階のTypeを表示しない。     D:しない。 A:- *)
+
+(* 上で定義した funfun_type ではうまくいかない。 *)
 Definition finfun_finMixin (aT rT : finType)  :=
-  [finMixin of (finfun_type aT rT) by <:].
+  [finMixin of (finfun.finfun_type aT rT) by <:].
 Canonical finfun_finType (aT rT : finType)  :=
-  Eval hnf in FinType (finfun_type aT rT) (finfun_finMixin aT rT).
+  Eval hnf in FinType (finfun.finfun_type aT rT) (finfun_finMixin aT rT).
+
+(* [finMixin of T] の T は、a subCountType structure over a type
+   that has a finType structure. *)
+(* [finMixin of T by <:] == a finType structure for T, when T has a subType   *)
+(*                   structure over an existing finType.                      *)
 
 Lemma card_ffun (aT rT : finType) : #|{ffun aT -> rT}| = #|rT| ^ #|aT|.
 Proof.
