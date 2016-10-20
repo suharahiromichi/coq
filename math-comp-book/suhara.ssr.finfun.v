5c8903a9527d32dc0cc5aaedc819b7d28720428a
SUHARA Hiromichi <suharahiromichi@gmail.com>
2016-10-21 06:41:49 +0900

    追記した。

--- math-comp-book/suhara.ssr.finfun.v
+++ math-comp-book/suhara.ssr.finfun.v
@@ -4,6 +4,13 @@ Set Implicit Arguments.
 Unset Strict Implicit.
 Unset Printing Implicit Defensive.
 
+Unset Printing Implicit. (* Unset : implicitな引数を表示しない。 D:しない。 A:する。*)
+Unset Printing Coercions. (* Unset : コアーションを表示しない     D:しない。 A:する。*)
+Set   Printing Notations. (*   Set : Notation を使って表示する。  D:する。  A:しない。*) 
+Unset Printing Universe.  (* Unset : 高階のTypeを表示しない。     D:しない。 A:- *)
+Print Graph.              (* Print Coercions. *)
+Print Canonical Projections.
+
 Section Def.
   Variables (aT : finType) (rT : Type).
   
@@ -17,7 +24,8 @@ End Def.
 Notation "{ 'ffun' fT }" := (finfun_of (Phant fT))
   (at level 0, format "{ 'ffun'  '[hv' fT ']' }") : type_scope.
 
-(*
+(*****************************************************************************)
+
 Local Notation fun_of_fin_def :=
   (fun aT rT f x => tnth (@fgraph aT rT f) (enum_rank x)).
 Local Notation finfun_def := (fun aT rT f => @Finfun aT rT (codom_tuple f)).
@@ -35,7 +43,8 @@ Notation "[ 'ffun' x => F ]" := [ffun x : _ => F]
 
 Notation "[ 'ffun' => F ]" := [ffun : _ => F]
   (at level 0, format "[ 'ffun' =>  F ]") : fun_scope.
-*)
+
+Check [ffun i :'I_4 => i + 2] :{ffun 'I_4 -> nat}.
 
 (*****************************************************************************)
 
@@ -57,27 +66,34 @@ Section EqTheory.
   Definition finfun_eqMixin :=
     Eval hnf in [eqMixin of finfun_type aT rT by <:].
   Canonical finfun_eqType := Eval hnf in EqType _ finfun_eqMixin.
+(*
   Canonical finfun_of_eqType := Eval hnf in [eqType of fT].
+*)
 End EqTheory.
 
 Definition finfun_choiceMixin aT (rT : choiceType) :=
   [choiceMixin of finfun_type aT rT by <:].
 Canonical finfun_choiceType aT rT :=
   Eval hnf in ChoiceType _ (finfun_choiceMixin aT rT).
+(*
 Canonical finfun_of_choiceType (aT : finType) (rT : choiceType) :=
   Eval hnf in [choiceType of {ffun aT -> rT}].
+*)
 
 Definition finfun_countMixin aT (rT : countType) :=
   [countMixin of finfun_type aT rT by <:].
 Canonical finfun_countType aT (rT : countType) :=
   Eval hnf in CountType _ (finfun_countMixin aT rT).
+(*
 Canonical finfun_of_countType (aT : finType) (rT : countType) :=
   Eval hnf in [countType of {ffun aT -> rT}].
+*)
 Canonical finfun_subCountType aT (rT : countType) :=
   Eval hnf in [subCountType of finfun_type aT rT].
+(*
 Canonical finfun_of_subCountType (aT : finType) (rT : countType) :=
   Eval hnf in [subCountType of {ffun aT -> rT}].
-
+*)
 (*****************************************************************************)
 
 Section FinTheory.
@@ -86,7 +102,7 @@ Section FinTheory.
   Notation ffT := (finfun_type aT rT).
   Implicit Types (D : pred aT) (R : pred rT) (F : aT -> pred rT).
   
-  Definition finfun_finMixin := [finMixin of ffT by <:].
+  Definition finfun_finMixin := [finMixin of ffT by <:]. (* finfun_subCountType が要る。 *)
   Canonical finfun_finType := Eval hnf in FinType ffT finfun_finMixin.
   Canonical finfun_subFinType := Eval hnf in [subFinType of ffT].
   Canonical finfun_of_finType := Eval hnf in [finType of fT for finfun_finType].
