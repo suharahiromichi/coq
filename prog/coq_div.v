c71653165c52b0b6ae67a74bdfdd165f08daaab3
SUHARA Hiromichi <suharahiromichi@gmail.com>
2017-01-31 00:08:47 +0900

    {measure n} をつけた場合

--- prog/coq_div.v
+++ prog/coq_div.v
@@ -8,7 +8,35 @@ Require Import Program.
 
 Set Implicit Arguments.
 
-Program Fixpoint div2 (n : nat) :
+Program Fixpoint div2 (n : nat) {measure n} :
+  { x : nat | n = 2 * x \/ n = 2 * x + 1 } :=
+  match n with
+  | S (S p) => S (div2 p)
+  | _ => O
+  end.
+Obligation 2.
+Proof.
+  remember (div2 _ _).
+  destruct s as [n H]; simpl.
+  omega.
+Defined.
+Obligation 3.
+Proof.
+  destruct n as [| n].
+  - now left.                               (* n = 0 *)
+  - destruct n as [| n].
+    + now right.                            (* n = 1 *)
+    + induction (H n).
+      reflexivity.                          (* n >= 2 *)
+      
+  Restart.
+  destruct n as [| n]; try auto.
+  destruct n as [| n]; try auto.
+  induction (H n); auto.
+Defined.
+
+(* measure なしの場合 *)
+Program Fixpoint div2' (n : nat) :
   { x : nat | n = 2 * x \/ n = 2 * x + 1 } :=
   match n with
   | S (S p) => S (div2 p)
