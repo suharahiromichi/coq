a195f449e3c02baf6cd88fcc6a8fafdb3feac4d7
SUHARA Hiromichi <suharahiromichi@gmail.com>
2017-01-31 22:09:57 +0900

    div2 n * 2 の定義と証明

--- prog/coq_div.v
+++ prog/coq_div.v
@@ -4,7 +4,9 @@
 Require Import Omega.
 Require Import List.
 Require Import Arith.
+Require Import Arith.Even.
 Require Import Program.
+Require Import Cpdt.CpdtTactics Cpdt.Coinductive.
 
 Set Implicit Arguments.
 
@@ -81,5 +83,53 @@ Proof.
   destruct n as [| n]; try auto.
   induction (H n); auto.
 Defined.
+
+(* Even *)
+
+Lemma even_2 x : even (x * 2).
+Proof.
+Admitted.
+
+Lemma odd_2 x : odd (x + x) -> False.
+Proof.
+Admitted.
+
+Lemma even_2_1 x : even (x + x + 1) -> False.
+Proof.
+Admitted.
+
+Hint Resolve even_2.
+
+Program Definition div2_2 (n : nat) :
+  { m : nat | even m } :=
+  div2 n * 2.
+(* Obligation なし。 *)
+
+Program Definition div2_2' (n : nat) :
+  { m : nat | (even n -> n = m) /\ (odd n -> n - 1 = m) } :=
+  div2 n * 2.
+Obligation 1.
+Proof.
+  remember (div2 _).
+  remember (_ * 2).
+  destruct s as [m H].
+  destruct H as [H1 | H2]; simpl in *.
+  - split.
+    + omega.
+    + clear Heqs.
+      rewrite <- plus_n_O in H1.
+      intro H.
+      rewrite H1 in H.
+      now apply odd_2 in H.
+  - split.
+    + clear Heqs.
+      rewrite <- plus_n_O in H2.
+      intro H.
+      rewrite H2 in H.
+      now apply even_2_1 in H.
+    + omega.
+Defined.
+
+(* div2 の末尾再帰版をつくる。 *)
 
 (* END *)
