f99ec04c7c3f01215fe3d82f1cfd23f818b64a89
SUHARA Hiromichi <suharahiromichi@gmail.com>
2017-02-08 00:11:46 +0900

    first commit

---
+++ prog/coq_nth.v
@@ -0,0 +1,104 @@
+Require Import Omega.
+Require Import List.
+Require Import Arith.
+Require Import Program.
+
+Set Implicit Arguments.
+
+Section Nth.
+  Variable A : Type.
+  
+  Fixpoint nth' (l : list A) (n : nat) (default : A) {struct l} : A :=
+    match n with
+    | 0 => match l with
+           | [] => default
+           | x :: _ => x
+           end
+    | S m => match l with
+             | [] => default
+             | _ :: t => nth' t m default
+             end
+    end.
+  
+  Lemma len__S_len (x : A) (l : list A) : length (x :: l) = S (length l).
+  Proof.
+    now simpl.
+  Qed.
+  
+  Program Fixpoint safe_nth (l : list A)
+          (n : nat | n < length l)
+          {measure (length l)} : A :=
+    match n with
+    | 0 => match l with
+           | [] => !
+           | x :: l' => x
+           end
+    | S m => match l with
+             | [] => !
+             | x' :: l' => safe_nth l' m
+             end
+    end.
+  Obligations.
+  Obligation 1.
+  Proof.
+    now inversion H.
+  Defined.
+  Obligation 2.
+  Proof.
+    now inversion H.
+  Defined.
+  Obligation 3.
+  Proof.
+    rewrite len__S_len in H.
+    omega.
+  Defined.
+End Nth.
+
+Definition ll := [1;2;3].
+Definition n_of_ll : { n : nat | n < length ll}.
+Proof.
+  exists 2.
+  simpl.
+  omega.
+Defined.
+
+Compute safe_nth nat ll n_of_ll.            (* 3 *)
+
+Extraction safe_nth.
+
+(* **** *)
+(* 応用 *)
+(* **** *)
+
+Definition sorted' (al : list nat) :=       (* := での定義！ *)
+  forall (i j : nat)
+         (Hi : {n : nat | n < length al})
+         (Hj : {n : nat | n < length al}),
+    i < j < length al ->
+    safe_nth nat al Hi <= safe_nth nat al Hj.
+
+Goal sorted' [1;2;3].
+Proof.
+  unfold sorted'.
+  intros.
+  case Hi as [xn Hn].
+  case Hj as [xm Hm].
+  simpl.
+  (* 
+   Goal : 
+   safe_nth nat [1; 2; 3] (exist (fun n : nat => n < 3) xn Hn) <=
+   safe_nth nat [1; 2; 3] (exist (fun n : nat => n < 3) xm Hm)
+   *)
+  Admitted.
+
+Definition sorted''' (al: list nat) :=
+  forall i j, i < j < length al -> nth i al 0 <= nth j al 0.
+
+Goal sorted''' [1;2;3].
+Proof.
+  unfold sorted'''.
+  intros.
+  (* Goal : nth i [1; 2; 3] 0 <= nth j [1; 2; 3] 0 *)
+  Admitted.
+
+(* END *)
