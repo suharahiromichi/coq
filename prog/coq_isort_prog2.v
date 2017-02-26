3f922edcf2af959c49fd3eb75bef12e2f25ec705
SUHARA Hiromichi <suharahiromichi@gmail.com>
2017-02-26 10:33:31 +0900

    実行例を追加する。

--- prog/coq_isort_prog2.v
+++ prog/coq_isort_prog2.v
@@ -19,6 +19,7 @@ Require Import List.
 Require Import Arith.
 Require Import Sorting.Permutation.
 Require Import Sorting.Sorted.
+Require Import Program.
 
 (** * 準備 *)
 
@@ -37,7 +38,30 @@ Hint Resolve LSorted_nil LSorted_cons1 LSorted_consn : sort.
   
 Hint Resolve lt_le_weak : lt_le.
 
-(** * insert の証明 *)
+(** * insert の定義と証明 *)
+
+(**
+最初に挿入関数 [insert] を証明付きで定義します。定義自体は最初の参考文献とおなじです。
+リスト [l] に [a] を挿入した結果が関数の値 [l'] になるので、
+これらをソートのために使う場合は次の関係を満たす必要があります。
+
+(1) [a] と [l] を結合(cons)したものと、[l'] が Permutation の関係にあること。
+
+(2) [l] がソートされているなら、[l'] もソートされていること。
+
+のふたつです。これに加えて
+
+(3) [a] と リスト [l] と [l'] との関係として、
+
+(3.1) [a] は [l'] の先頭である（[a]が[l]の先頭に置かれる場合）、または、
+
+(3.2) [l] と [l'] の先頭が同じ（[a]が[l]の途中以降に挿入される場合）
+
+を追加しておきます。以上を連言でつないでいます。
+
+関数の定義では、サブタイプコアーションが働くので、insert の再帰呼び出しでは、
+証明の部分を無視して、リストだけを返す関数として使うことができます。
+*)
 
 Program Fixpoint insert (a : nat) (l : list nat) {struct l} : 
   {l' : list nat | Permutation (a :: l) l' /\
@@ -73,11 +97,21 @@ Proof.
           now auto with sort lt_le.
     + now auto.
 Defined.
-        
+
 (**
 rewrite では、通常の [=] ではなく、
 Permutation に対する rewrite が行われていることに注意してください。
 
+ゴール [Permutation (a :: x :: l') (x :: x0)] に対して、
+[[
+perm_swap : forall (A : Type) (x y : A) (l : list A),
+       Permutation (y :: x :: l) (x :: y :: l)
+]]
+でrewriteすることで、ゴールのPermutationの左側が書き換えられ、
+
+[Permutation (x :: a :: l') (x :: x0)]
+
+が得られます。
 それができるのは、Import している [Sorting/Permutation.v] のなかで、
 [Instance Permutation_Equivalence] が定義されているからですが、
 このあたりについては、
@@ -94,8 +128,18 @@ A Gentle Introduction to Type Classes and Relations in Coq
 [apply LSorted_consn] と [apply lt_le] が実行されています。
  *)
 
+(**
+実行してみます。証明の部分を取り除いて値だけを取り出すために、
+[proj1_sig] の演算子 [`] を使います。
+*)
 
-(** OCaml のコードを生成してみます。 *)
+Compute ` (insert 1 [2; 3]).             (** ==> [1; 2; 3]  *)
+
+Compute ` (insert 2 [1; 3]).             (** ==> [1; 2; 3]  *)
+
+(**
+OCaml のコードを生成してみます。証明の部分のコードは含まれていません。
+ *)
 
 Extraction insert.
 (**
@@ -112,7 +156,21 @@ let rec insert a = function
 *)
 
 
-(** * Insertion Sort の証明  *)
+(** * Insertion Sort の定義と証明  *)
+
+(**
+最後にソート関数 isort を証明付きで定義します。定義自体は最初の参考文献とおなじです。
+また、その正しさの証明も同じで、ソート前の引数 [l] と、ソート結果の関数値 [l'] に対して、
+
+(1) [l] と [l'] が Permutation の関係であること。
+
+(2) [l'] がソートされていること。
+
+のふたつで、それらを連言でつないでいます。
+
+関数の定義では、サブタイプコアーションが働くので、isort の再帰呼び出しのみならず、
+下位処理として呼び出す insert もリストだけを返す関数として使うことができます。
+  *)
 
 Program Fixpoint isort l {struct l} :  
   {l' : list nat | Permutation l l' /\ LocallySorted le l'} := 
@@ -136,7 +194,17 @@ Proof.
   - destruct a0; now auto.
 Defined.
 
-(** OCaml のコードを生成してみます。 *)
+(**
+実行してみます。証明の部分を取り除いて値だけを取り出すために、
+[proj1_sig] の演算子 [`] を使います。
+*)
+
+Compute ` (isort [0; 2; 1; 3]).             (** ==> [0; 1; 2; 3]  *)
+
+
+(**
+OCaml のコードを生成してみます。証明の部分のコードは含まれていません。
+ *)
 
 Extraction isort.
 (**
