97bd8cec464823b402f69491e2c7a157192f0b42
SUHARA Hiromichi <suharahiromichi@gmail.com>
2017-09-17 19:43:18 +0900

    追記した。

--- math-comp-book/suhara.ch6.matome.v
+++ math-comp-book/suhara.ch6.matome.v
@@ -1,8 +1,22 @@
 (* まとめのまとめ。
 
+Mathcomp では、型をつぎのように構成している。
+(eqType) ライプニッツのeqとブールのeqのリフレクションできる型。eqMixin
+
+(choiceType) choice opereator (find) が存在すること。
+
+(countType) countable n番め要素が(あれば)一意に決まること。
+
+(finType) 型の要素の列挙(enum)が、eqTypeの意味でユニークである型。finMixin
+(Tuple) サイズの決まったseqだが、finType である。choiceType, countType でもある。
+(finfun) 定義域(引数)がTupleである関数
+(finset) 要素かどうか決める関数がfinfunである集合
+
+
 Mathcomp で理解するべきなのは、Propとbooleanのリフレクションである。
 
 (eqP) eqType では、 == と =
+(allP) seq では、(∀x, x \in s -> a x) と (all a s).
 (forallP) finType では、[forall x, P] と (forall x, P)
 (ffunP) finFun では、=1 と = (関数どうしの等しさ)
 (setP) finSet では、=i と = (集合どうしの等しさ)
