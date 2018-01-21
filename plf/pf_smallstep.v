b32db761251e2bdeb886dcced36811e3515f452c
SUHARA Hiromichi <suharahiromichi@gmail.com>
2018-01-21 19:04:42 +0900

    first commit

---
+++ plf/pf_smallstep.v
@@ -0,0 +1,18 @@
+(* ProofCafe SF/PLF Support Page *)
+(* Smallstep.v *)
+
+(* これは、ProofCafe で、Software Fundation, Program Langu  *)
+(* ご注意： *)
+
+Require Import Coq.Arith.Arith.
+Require Import Coq.Arith.EqNat.
+Require Import Coq.omega.Omega.
+Require Import Coq.Lists.List.
+Import ListNotations.
+Require Import Maps.
+Require Import Imp. 
+Require Import Smallstep.
+
+
+
+(* END *)
