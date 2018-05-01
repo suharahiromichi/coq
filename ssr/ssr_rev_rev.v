68b3035e3a0f0c183305b1f733e0dcec4540af12
SUHARA Hiromichi <suharahiromichi@gmail.com>
2018-05-02 01:38:33 +0900

    空行を減らした。

--- ssr/ssr_rev_rev.v
+++ ssr/ssr_rev_rev.v
@@ -18,11 +18,10 @@ Section Rev.
   Variable T : Type.                        (* 任意のT型のデータについて、 *)
 
   (** # 二種類のreverseの定義 *)
-  
   (** ## rcons を使った定義 *)
   Definition rcons l (a : T) := l ++ [:: a]. (* リストの末尾に要素を置く関数 *)
   
-  Fixpoint rev1 (l : seq T) : seq T :=      (* rconsを使った、よくある定義 *)
+  Fixpoint rev1 (l : seq T) : seq T :=
     match l with
     | nil    => nil
     | h :: t => rcons (rev1 t) h
@@ -38,7 +37,6 @@ Section Rev.
   Definition rev2 (l : seq T) : seq T := catrev l [::].
   
   (** # 補題（予備定理）を証明する。  *)
-  
   (** ## rev1 に関する補題を証明する。  *)
   Lemma rcons_rev1 (x : T) (l : seq T) : rev1 (rcons l x) = x :: (rev1 l).
   Proof.
@@ -81,7 +79,6 @@ Section Rev.
   Qed.
   
   (** # 対合(involutive)であることを証明する。 *)
-  
   (** ## rev1 が対合であることを証明する。 *)
   Theorem rev1_involutive (l : seq T) : rev1 (rev1 l) = l.
   Proof.
@@ -112,45 +109,27 @@ Section Rev.
   Qed.
 End Rev.
 
-(*
-# Coq/SSReflect/MathComp の参考文献
+(**
+# 参考文献
 
 - 本家
-
 https://math-comp.github.io/math-comp/
 
-
 - 萩原学、アフェルト・レナルド 「Coq/SSReflect/MathComp」 森北出版 2018
-
 （SSReflect本。最近出版された。おすすめ。数学の定理の証明がテーマである。）
 
-
-- Georges Gonthier, Assia Mahboubi, Enrico Tassi,
-
-"A Small Scale Reflection Extension for the Coq system"
-
-https://hal.inria.fr/inria-00258384v17/document
-
-(GMT。SSReflect拡張部分のリファレンスマニュアル)
-
-
-- Ilya Sergey,
-
-"Programs and Proofs"
-
+- Ilya Sergey, "Programs and Proofs"
 http://ilyasergey.net/pnp/
-
 （PnP。プログラムの証明をテーマにしている。）
 
-
-- Assia Mahboubi, Enrico Tassi,
-
-"Mathematical Components"
-
+- Assia Mahboubi, Enrico Tassi, "Mathematical Components"
 https://math-comp.github.io/mcb/
-
 （MCB。MathCompライブラリのしくみの説明が詳しい。）
 
+- Georges Gonthier, Assia Mahboubi, Enrico Tassi,
+"A Small Scale Reflection Extension for the Coq system"
+https://hal.inria.fr/inria-00258384v17/document
+（GMT。SSReflect拡張部分のリファレンスマニュアル。）
  *)
 
 (* END *)
