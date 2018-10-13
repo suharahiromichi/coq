5c5239db10b0ba76c0c1a021acca5b18b83e7301
SUHARA Hiromichi <suhara@falla.vbox>
2018-10-14 08:28:48 +0900

    first commit

---
+++ plf/pf_stlcprop.v
@@ -0,0 +1,45 @@
+(** ProofCafe SF/PLF Support Page *)
+(** StlcProp.v *)
+
+(**
+これは、「Software Foundations, Vol.2 Programming Language Foundations」 の
+勉強会（ProofCafe）のサポートページです。復習などに利用しててください。
+
+本ファイルの実行は、本ファイルを pfl ディレクトリの中に混ぜて置くか、
+pfl のオリジナルの適当なファイルの適当な場所にcopy&pasteすることで可能です。
+ *)
+
+(** ご注意：
+
+1. 実際の勉強会は、本ファイルではなく、オリジナルのファイルを直接編集・実行しておこないます。
+
+2. 本ファイルには、演習問題の答えは含まれません。
+
+*)
+
+Require Import Coq.Arith.Arith.
+Require Import Maps.
+Require Import Imp. 
+Require Import Smallstep.
+Require Import Types.
+Require Import Stlc.
+Require Import StlcProp.
+
+(* Module *)
+Import STLC.
+Export STLC.
+
+(* ################################################################# *)
+(** ProofCafe ##80 2018/10/20 *)
+
+(**
+目次
+
+*)
+
+(**
+概要
+
+*)
+
+(* END *)
