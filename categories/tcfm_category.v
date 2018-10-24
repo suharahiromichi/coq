f7fb4b918558fc14389114813c7af7036bf5a133
SUHARA Hiromichi <suhara@falla.vbox>
2018-10-24 21:14:12 +0900

    シングルトンのインスタンスを作る。

--- categories/tcfm_category.v
+++ categories/tcfm_category.v
@@ -1,9 +1,11 @@
-(* "Type Classes for Mathematics in Type Theory" の Category の定義 *)
+(* TCfM から圏論の定義の部分を抜き出す。 *)
+(* "Type Classes for Mathematics in Type Theory" *)
 
-Global Generalizable All Variables.
+(* Global Generalizable All Variables. *)
+
+Generalizable Variables O x y.
 
 Require Import Relations.
-(* Require Import Setoid. *)
 Require Import Morphisms.                   (* Proper *)
 
 Class Equiv A := equiv : relation A.
@@ -21,18 +23,47 @@ Notation "A \o B" := (comp A B) (at level 40, left associativity).
 
 Class Setoid A {Ae : Equiv A} : Prop := setoid_eq :> Equivalence (@equiv A Ae).
 
-Notation "(=)" := equiv.
-
 Class Category (O : Type) `{!Arrows O} `{forall x y : O, Equiv (x --> y)}
       `{!CatId O} `{!CatComp O} : Prop :=
   {
     arrow_equiv :> forall x y, Setoid (x --> y);
-    
+    comp_proper :> forall x y z, Proper (equiv ==> equiv ==> equiv) (@comp _ _ _ x y z);
     comp_assoc w x y z (a : w --> x) (b : x --> y) (c : y --> z) :
       c \o (b \o a) = (c \o b) \o a;
-    comp_proper :> forall x y z, Proper ((=) ==> (=) ==> (=)) (@comp _ _ _ x y z);
-    id_l `(a : x --> y) : cat_id y \o a = a;
-    id_r `(a : x --> y) : a \o cat_id x = a;
+    id_l `(a : x --> y) : cat_id _ \o a = a;
+    id_r `(a : x --> y) : a \o cat_id _ = a;
   }.
+
+
+Require Import Omega.
+Require Import Coq.Logic.ProofIrrelevance.
+
+(* *********** *)
+(* シングルトン *)
+(* *********** *)
+
+Definition O0 : Type := unit.
+Definition A0 : Arrows O0 := fun (x y : unit) => nat.
+Definition E0 (x y : unit) : Equiv (x --> y) := fun (m n : nat) => m = n.
+Definition I0 : CatId O0 := fun (_ : unit) => 0.
+Definition C0 : CatComp O0 := fun (_ _ _ : unit) (m n : nat) => m + n.
+
+Check @Category O0 A0 E0 I0 C0.
+Program Instance SPLUS : @Category O0 A0 E0 I0 C0.
+Obligation 1.
+Proof.
+  unfold Setoid, equiv, E0.
+  split.
+  + now unfold Reflexive.
+  + now unfold Symmetric.
+  + unfold Transitive.
+    intros x' y' z H1 H2.
+    now rewrite H1, H2.
+Qed.
+Obligation 2.
+Proof.
+  unfold comp, C0.
+  now apply plus_assoc.
+Qed.
 
 (* END *)
