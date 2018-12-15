06853db1bf58e4589922341a1f12e7bed5b60528
SUHARA Hiromichi <suhara@falla.vbox>
2018-12-15 14:25:38 +0900

    説明を追加した。

--- plf/pf_stlcprop.v
+++ plf/pf_stlcprop.v
@@ -61,10 +61,7 @@ Check preservation : forall (t t' : tm) (T : ty),
 Check soundness : forall (t t' : tm) (T : ty),
     empty |- t \in T -> t ==>* t' -> ~ stuck t'.
 
-(* **** *)
-(* 補題 *)
-(* **** *)
-
+(** 1. *)
 (** 正準形、標準型 [TAPL 補題9.3.4] *)
 (** 進行性 Progress を証明するための補題である。 *)
 (** 項の型がBoolで、項が値なら、その項はtrueかfalse(Bool値)である。 *)
@@ -74,28 +71,76 @@ Check canonical_forms_bool :
 Check canonical_forms_fun : forall (t : tm) (T1 T2 : ty),
     empty |- t \in TArrow T1 T2 ->
                       value t -> exists (x : id) (u : tm), t = tabs x T1 u.
-(* 証明は、どちらも HVal : value t を destruct で場合分けする。 *)
+(** 証明は、どちらも HVal : value t を destruct で場合分けする。 *)
 
+(** 2. *)
+(** 進行性 Progress [TAPL 定理9.3.5] *)
+(** t が閉じた、正しく型付けされた項であるなら、Stackしない(行き詰まらない)。あるいは、
+    t は値であるか、ステップすることができる。 *)
+
+(** 値であるとは： *)
+Check value : tm -> Prop.
+Print value.
+(*
+Inductive value : tm -> Prop :=
+    v_abs : forall (x : id) (T : ty) (t : tm), value (tabs x T t)
+  | v_true : value ttrue
+  | v_false : value tfalse
+ *)
+
+(** ステップできるとは： *)
+Check step : tm -> tm -> Prop.       (* 2項がステップの関係である。 *)
+Print step.
+(*
+Inductive step : tm -> tm -> Prop :=
+    ST_AppAbs : forall (x : id) (T : ty) (t12 v2 : tm),
+                value v2 -> tapp (tabs x T t12) v2 ==> [x := v2] t12
+  | ST_App1 : forall t1 t1' t2 : tm, t1 ==> t1' -> tapp t1 t2 ==> tapp t1' t2
+  | ST_App2 : forall v1 t2 t2' : tm,
+              value v1 -> t2 ==> t2' -> tapp v1 t2 ==> tapp v1 t2'
+  | ST_IfTrue : forall t1 t2 : tm, tif ttrue t1 t2 ==> t1
+  | ST_IfFalse : forall t1 t2 : tm, tif tfalse t1 t2 ==> t2
+  | ST_If : forall t1 t1' t2 t3 : tm, t1 ==> t1' -> tif t1 t2 t3 ==> tif t1' t2 t3
+ *)
+Check forall t, exists t', step t t' : Prop. (* t がステップできる。 *)
+
+(** 補足説明：t がステップできない場合、正規形 normal form と呼ぶ。
+    正規形と値は異なるかもしれない。保存性の定理を参照のこと。 *)
+Check forall t, normal_form step t : Prop.
+
+(** 証明の概要：
+    empty |- t \in T 、つまり has_type の定義による帰納法で証明する。
+    - t が、変数であるとき、変数は空コンテキストから型付できないので矛盾である。
+    - t が、True、False または 関数抽象 [λx:T'.t1] の場合は、値なので自明である。
+    - t が、関数適用のとき、 t = t1 t2 とすると、帰納法の仮定からt1は値かステップする。
+      + t1 が値であるなら、t2は帰納法の仮定から値かステップできる。
+         * t2 が値なら、t1も値なので、[ST_AppAbs]でステップできる。
+         * t2 がステップできるなら、t もステップできる。
+      + t1 がステップできるなら、t もステップできる。
+    - t が、IF式のとき、t = IF t1 THEN t2 ELSE t3 とすると、t1 は値かステップできる。
+      + t1 が値なら、それはBool型なので、真ならt2、偽ならt3 にステップできる。
+      + t1 がステップできるなら、t もステップできる。
+ *)
+
+
+(** 3. 代入補題 *)
 Notation "m '&' { a --> x }" := (update m a x) (at level 20).
 (* update Gamma x U |- t を Gamma & {x-->U} と書けるようにする。 *)
-
-
 
 (** 代入補題、置換補題 [TAPL 補題9.3.8] *)
 (** U型の自由変数xを含む項tの型がTのとき、xにU型の値vを代入しても、[x:=v]tはT型である。 *)
 Check substitution_preserves_typing : forall Gamma x U t v T,
     Gamma & {x-->U} |- t \in T -> empty |- v \in U -> Gamma |- [x:=v]t \in T.
 (** 補足説明： *)
-(** ふたつめの前提の φ|-v:U は、項vに自由変数が含まれないことを前提とすることを意味する。
-    これは、Stcl の 置換 Substituion の節の以下を反映している。
-    
-（引用）
-技術的注釈: 置換は、もし、つまり他の項の変数を置換する項が、 それ自身
-に自由変数を含むときを考えると、 定義がよりトリッキーなものになります。
-ここで興味があるのは閉じた項についてのstep関係の定義のみなので、そのさ
-らなる複雑さは避けることができます。
-（引用終）
- *)
+
+(** One technical subtlety in the statement of the lemma is that we
+    assume v has type U in the empty context — in other words, we
+    assume v is closed.
+
+    空な文脈でvは型Uであることを前提とする - 言い換えると、vは閉じてい
+    る（自由変数を持たない）ことを前提とする。
+    …これは、Stcl の 置換 Substituion の節の techincal note を反映し
+    ている。*)
 
 (** TAPL などでは、より一般的に定義されている。 *)
 Check forall Gamma x U t v T,
