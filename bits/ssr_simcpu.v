92addef71eea527a77b0640f21067303d77cdf9a
SUHARA Hiromichi <suhara@falla.vbox>
2019-01-19 21:59:15 +0900

    内容を整理した。

--- bits/ssr_simcpu.v
+++ bits/ssr_simcpu.v
@@ -9,16 +9,11 @@ Import Prenex Implicits.
   
 Section simcpu.
 
-  Lemma idblP {n : nat} (i : 'I_n) : i.*2 < n.*2.
-  Proof.
-    rewrite -!mul2n ltn_pmul2l.
-    - apply ltn_ord.
-    - done.
-  Qed.
+  Variable N : nat.                         (* word-lenght *)
+  Axiom HN : 0 < N.                         (* 0 ではない。 *)
   
-  Definition idbl {n : nat} (i : 'I_n) : 'I_n.*2 :=
-    Ordinal (idblP i).
-
+  Check leq_add : forall m1 m2 n1 n2 : nat, m1 <= n1 -> m2 <= n2 -> m1 + m2 <= n1 + n2.
+  
   Lemma ltn_add m1 m2 n1 n2 : m1 < n1 -> m2 < n2 -> m1 + m2 < n1 + n2.
   Proof.
     move=> lt_mn1 lt_mn2.
@@ -27,74 +22,6 @@ Section simcpu.
     - by rewrite ltn_add2l.
     - by rewrite ltn_add2r.
   Qed.
-  
-  Lemma iaddP {n : nat} (i j : 'I_n) : i + j < n.*2.
-  Proof.
-    rewrite -addnn.
-    apply ltn_add.
-    - apply ltn_ord.
-    - apply ltn_ord.
-  Qed.
-  
-  Definition iadd {n : nat} (i j : 'I_n) : 'I_n.*2 :=
-    Ordinal (iaddP i j).
-  
-  Lemma imodP {n : nat} (i : nat) : 0 < n -> i %% n < n.
-      by apply ltn_pmod.
-  Qed.
-  
-  Definition imod {n : nat} (i : nat) (H : 0 < n) : 'I_n :=
-    Ordinal (imodP i H).
-
-  Lemma iadd2P {n : nat} (i j : 'I_n) : 0 < n -> (i + j) %% n < n.
-  Proof.
-    by apply ltn_pmod.
-  Qed.
-  
-  Definition iadd2 {n : nat} (i j : 'I_n) (H : 0 < n) : 'I_n :=
-    Ordinal (iadd2P i j H).
-
-  Lemma iOp2P {n : nat} (i j : 'I_n) op : 0 < n -> (op i j) %% n < n.
-  Proof.
-    by apply ltn_pmod.
-  Qed.
-  
-  Definition iOp2 {n : nat} (i j : 'I_n) op (H : 0 < n) : 'I_n :=
-    Ordinal (iOp2P i j op H).
-
-  Lemma test : forall n i , n - i < n - 0 -> n - i < n.
-  Proof.
-    move=> n i H.
-    rewrite (subn0 n) in H.
-    done.
-  Qed.
-  
-  Lemma iNegP' {n : nat} (i : 'I_n) : 0 < n -> 0 < i -> n - i < n - 0.
-  Proof.
-    move=> Hn Hi.
-      by apply ltn_sub2l.
-  Qed.
-  
-  Definition ineg (n i : nat) :=
-    if i == 0 then 0 else n - i.
-  
-  Lemma iNegP {n : nat} (i : 'I_n) : 0 < n -> ineg n i < n.
-  Proof.
-    move=> Hn.
-    rewrite /ineg.
-    case H : ((nat_of_ord i) == 0).
-    - done.
-    - apply test.
-      apply ltn_sub2l.
-      + done.
-      + move/eqP in H.
-        Search _ (_ <> 0).
-        apply PeanoNat.Nat.neq_0_lt_0 in H.
-          by move/ltP in H.
-  Qed.
-  
-  Definition iNeg {n : nat} (i : 'I_n) (H : 0 < n) : 'I_n :=
-    Ordinal (iNegP i H).
   
   Lemma leq_ltn_add m1 m2 n1 n2 : m1 <= n1 -> m2 < n2 -> m1 + m2 < n1 + n2.
   Proof.
@@ -105,48 +32,102 @@ Section simcpu.
     - by rewrite leq_add2r.
   Qed.
   
-  Lemma test4 (i j n : nat) : 0 < n -> i < n -> j < n -> i * n + j < n * n.
+  (* 加算 ADD *)
+  Lemma imodP (i : nat) : i %% N < N.
+    apply ltn_pmod.
+    by apply HN.
+  Qed.
+  
+  Definition iadd (i j : 'I_N) : 'I_N := Ordinal (imodP (i + j)).
+  
+  (* 加算 ADD2 notused *)
+  Lemma iadd2P (i j : 'I_N) : i + j < N.*2.
   Proof.
-    move=> Hn Hi Hj.
-    Check prednK Hn.
-    rewrite -{2}(prednK Hn).
+    rewrite -addnn.
+    apply ltn_add.
+    - apply ltn_ord.
+    - apply ltn_ord.
+  Qed.
+  
+  Definition iadd2 (i j : 'I_N) : 'I_N.*2 := Ordinal (iadd2P i j).
+  
+  (* 2の補数 NEG *)
+  Definition nneg (i : nat) := if i == 0 then 0 else N - i.
+  (*  
+  Lemma test : forall n i , n - i < n - 0 -> n - i < n.
+  Proof.
+    move=> n i H.
+    rewrite (subn0 n) in H.
+    done.
+  Qed.
+ *)
+  
+  Lemma inegP (i : 'I_N) : nneg i < N.
+  Proof.
+    rewrite /nneg.
+    case H : ((nat_of_ord i) == 0).
+    - now apply HN.
+    - have H' : N - i < N - 0 -> N - i < N by rewrite (subn0 N).
+      apply H'.
+      apply ltn_sub2l.
+      + apply HN.
+      + move/eqP in H.
+        apply PeanoNat.Nat.neq_0_lt_0 in H.
+          by move/ltP in H.
+  Qed.
+  
+  Definition ineg (i : 'I_N) : 'I_N := Ordinal (inegP i).
+
+  (* 語の連結 JOIN *)
+  Definition njoin (i j : nat) := i * N + j.
+  
+  Lemma njoinP (i j : nat) : i < N -> j < N -> njoin i j < N * N.
+  Proof.
+    move=> Hi Hj.
+    rewrite /njoin.
+    rewrite -{2}(prednK HN).
     rewrite mulSnr.
     rewrite leq_ltn_add.
     - done.
     - rewrite leq_pmul2r.
-      + rewrite -(leq_add2r 1 i n.-1).
+      + rewrite -(leq_add2r 1 i N.-1).
         rewrite addn1.
-        rewrite [n.-1 + 1]addn1.
+        rewrite [N.-1 + 1]addn1.
         rewrite prednK.
         * done.
-        * done.
-      + done.
+        * by apply HN.
+      + by apply HN.
       + done.
   Qed.
-
-  Lemma test3 (i j m n : nat) : 0 < m -> 0 < n -> i < m -> j < n -> i * n + j < m * n.
+  
+  Lemma ijoinP (i j : 'I_N) : njoin i j < N * N.
   Proof.
-    move=> Hm Hn Hi Hj.
-  Admitted.
+    by apply (njoinP (ltn_ord i) (ltn_ord j)).
+  Qed.
+
+  Definition ijoin (i j : 'I_N) : 'I_(N * N) := Ordinal (ijoinP i j).
   
 End simcpu.
 
-(* たんに double する関数になっている。 *)
-Extraction idbl.
+Extraction iadd.  
 (*
-let idbl _ _ _ _ n i =
-  double (nat_of_ord n i)
+let iadd n i j =
+  modn (addn (nat_of_ord n i) (nat_of_ord n j)) n
 *)
-Extraction nat_of_ord.
-(* 
-let nat_of_ord _ i =
-  i
+Extraction ineg.
+(*
+  let ineg n i =
+  nneg n (nat_of_ord n i)
  *)
-Extraction double.
-Extraction iadd.
-Extraction imod.
-Extraction iadd2.
-Extraction iOp2.
-Extraction iNeg.
+Extraction njoin.
+(* 
+let njoin n i j =
+  addn (muln i n) j
+ *)
+Extraction ijoin.
+(* 
+let ijoin n i j =
+  njoin n (nat_of_ord n i) (nat_of_ord n j)
+ *)
 
 (* END *)
