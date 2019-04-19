1a833c6391fe34cb200403e4e9eb6f2e4de68027
SUHARA Hiromichi <suhara@falla.vbox>
2019-04-19 20:57:11 +0900

    Section を定義する。

--- ssr/ssr_mockbird_3.v
+++ ssr/ssr_mockbird_3.v
@@ -5,7 +5,7 @@ Inductive bird : Set :=
 
 (* ******* *)
 
-Theorem thm1_ssr :
+Theorem thm1_ssr' :
   (forall (A B : bird),
     exists (C : bird), forall (x : bird), app A (app B x) = app C x) ->
   (exists (M : bird), forall (x : bird), app M x = app x x) ->
@@ -13,43 +13,61 @@ Theorem thm1_ssr :
     exists (x : bird), app P x = x).
 Proof.
   move=> Hc Hm P.
-  case: Hm => [M Hm'].      (* Hm' は ものまね鳥 M についての命題。 *)
+  case: Hm => [M Hm'].       (* Hm' は ものまね鳥M についての命題。 *)
   case: (Hc P M) => [PM Hc']. (* Hc' は P と M を合成した、鳥PMについての命題。 *)
-  exists (app M PM).
-  rewrite Hc'.            (* 左辺のP と Mから合成鳥PMを得る。 *)
+  exists (app M PM).          (* 鳥P は、鳥MPMが好き、とする。 *)
+  rewrite Hc'.                (* 左辺のP と Mから合成鳥PMを得る。 *)
   rewrite Hm'.            (* 右辺のM PM からものまね鳥 M M を得る。 *)
-  by [].
+    by [].
 Qed.
 
 (* ******* *)
 
-Variable M : bird.                          (* ものまね鳥 *)
-Variable P : bird.                          (* 題意から求めたい鳥 *)
+Section Birds.
+  
+  Variable M : bird.              (* ものまね鳥 *)
+  Variable P : bird.              (* 誰かを好きな鳥、誰かを求めたい *)
 
-(* ものまね鳥の定義 *)
-Hypothesis mock : forall (x : bird), app M x = app x x.
+  (* ものまね鳥の定義 *)
+  Hypothesis mock : forall (x : bird), app M x = app x x. (*  *)
 
-(* 鳥の合成の定義（合成の作り方） *)
-Hypothesis comp : forall (A B x : bird), app A (app B x) = app (app A B) x.
+  (* 鳥の合成の定義（合成の作り方） *)
+  Hypothesis comp : forall (A B x : bird), app A (app B x) = app (app A B) x. (*  *)
 
-Goal exists (x : bird), app P x = x.
-Proof.
-  move: comp => Hc.
-  move: mock => Hm.
-  move: (Hc P M) => {Hc} Hc.
-  exists (app M (app P M)).
-  rewrite Hc.
-  rewrite Hm.
-    by [].
-Qed.
+  Goal exists (x : bird), app P x = x.
+  Proof.
+    move: comp => Hc.
+    move: mock => Hm.
+    move: (Hc P M) => {Hc} Hc.
+    exists (app M (app P M)).
+    rewrite Hc.
+    rewrite Hm.
+      by [].
+  Qed.
 
-(* さらにまとめる。 *)
+  (* さらにまとめる。 *)
 
-Goal exists (x : bird), app P x = x.
-Proof.
-  move: (comp P M) => Hc.
-  exists (app M (app P M)).
-  by rewrite Hc mock.
-Qed.
+  Lemma thm1_ssr : exists (x : bird), app P x = x.
+  Proof.
+    move: (comp P M) => Hc.
+    exists (app M (app P M)).               (* M (P M) *)
+      by rewrite Hc mock.
+  Qed.
+  
+(* M (P M) は、P の不動点 fixed point である、という。 *)
+
+End Birds.
+
+
+Check thm1_ssr'
+  : (forall A B : bird, exists C : bird, forall x : bird, app A (app B x) = app C x) ->
+    (exists M : bird, forall x : bird, app M x = app x x) ->
+    forall P : bird, exists x : bird, app P x = x.
+
+Check thm1_ssr
+  : forall M P : bird,
+    (forall x : bird, app M x = app x x) ->
+    (forall A B x : bird, app A (app B x) = app (app A B) x) ->
+    exists x : bird, app P x = x.
 
 (* END *)
