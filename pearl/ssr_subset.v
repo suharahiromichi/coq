7b3501df64f15a6829bf927c76b80733f9d58e49
SUHARA Hiromichi <suhara@falla.vbox>
2019-05-02 09:43:01 +0900

    説明を追加した。

--- pearl/ssr_subset.v
+++ pearl/ssr_subset.v
@@ -1,5 +1,5 @@
 (**
-Mathcomp の subset について
+Mathcomp の subset について (Proof Pearl ##1)
 ======
 2019/05/01
 
@@ -11,12 +11,41 @@ https://github.com/suharahiromichi/coq/blob/master/pearl/ssr_subset.v
  *)
 
 (**
+# 説明
 
-# はじめに
+Mathcomp では、``∈`` を示す `\in` は広く使われるために、``ssrbool.v`` で導入されますが、
+部分集合 ``⊆`` を示す、``\subset`` は、fintype.v で導入されています。
+
+要素がすべて列挙できる有限型 ``T : finType`` の上で、そのT の enumを全集合とすることで、
+ひと通りの集合演算が定義できるようになるからです。
+
+``T : finType`` のとき、booleanな関数 ``P : T -> bool`` を集合と同一視して、
+``P x`` を ``x \in P`` と同一視します。
+
+
+すると、その上で、集合 A と B の積を
+``x ∈ A ∩ B`` <-> `` (λx. A x ∧ B x) x`` 
+
+P の補集合を
+``x ∈ P^c`` <-> `` (λx. ¬ P x)`` 
+
+と定義できます。
+
+T の enum に対してPでフィルタすることで、Pの要素がすべて求められるので、
+その数を数えるとPの濃度を求めることができます。
+
+
+表記法 Notation については、文献[1.]の表5.1などを参照してください。
+
+
+また、``P : T -> bool`` を ``P : pred T`` と書くこともありますが、
+これは表記のみの違いです。
+
+
 
 集合Aが集合Bの部分集合 ``A ⊆ B`` であるとは、A の任意の要素が B の要素であることです。
 ``\subset`` 演算子が定義されている Mathcomp の finset.v のコメントには、
-そのように記載されています。
+実際、そのように記載されています。
 
 ``A \subset B == all x \in A satisfy x \in B.`` ..................... (1)
 
@@ -28,16 +57,20 @@ https://github.com/suharahiromichi/coq/blob/master/pearl/ssr_subset.v
 
 となっていて、これは、
 「論理式 A を満たし、論理式 B を満たさない要素の数が 0 である」という意味です。
-すなわち、
-``| P |``、``&&``を論理和、``~~``を論理否定とすると、``|A && ~~ B| = 0``
-
-Mathcomp では、xが集合の要素であること``x ∈ P`` と、
-xが論理式Pを満たすこと ``P x``を同一視するのですが、
-このことは、別の機会に説明します。
-
 
 (1)と(2)は同値のはずですが、それを証明する補題がなかった（見つけられなかった）ので、
 証明してみました。
+
+*)
+
+(**
+# コード例
+
+最初に、通常の Prop 型論理式について同値であることを証明します。
+
+ ``s1 ⊆ s2 <-> ∀x. x ⊆ s1 -> x ⊆ s2``
+
+その後、（Mathcomp らしく）boolean な論理式について真偽値が一致することを証明します。
 *)
 
 From mathcomp Require Import all_ssreflect.
@@ -47,7 +80,7 @@ Section Test.
   Variable T : finType.
 
 (**
-# 通常の論理式での証明
+## Prop な論理式での証明
  *)
   
   Lemma subsetP (s1 s2 : pred T) :
@@ -72,7 +105,7 @@ Section Test.
   Qed.
   
 (**
-# boolean な論理式での証明
+## boolean な論理式での証明
  *)
 
   Lemma subsetE (s1 s2 : pred T) :
@@ -91,7 +124,19 @@ Section Test.
   Qed.
 
 (**
-# おまけ (直接 boolean の式を証明する)
+# 最初に使った箇所
+
+単一化の証明 http://fetburner.hatenablog.com/entry/2015/12/06/224619
+
+Unify.v を Mathcomp への移植するときに必要になりました。移植例：
+
+https://github.com/suharahiromichi/coq/blob/master/unify/ssr_unify_bool_3.v
+*)
+
+(**
+# おまけ
+
+## 直接 boolean の式を証明する
  *)
   
   Lemma subsetE' (s1 s2 : pred T) : 
@@ -118,5 +163,12 @@ Section Test.
   Qed.
   
 End Test.
+
+(**
+# 参考文献
+
+[1.] 萩原学 アフェルト・レナルド 「Coq/SSReflect/MathCompによる定理証明」 森北出版
+
+*)
 
 (* END *)
