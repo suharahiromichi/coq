432420248c4ee34f8fb040d10bd6bd25ab8d0a3c
SUHARA Hiromichi <suhara@falla.vbox>
2019-06-23 01:41:35 +0900

    Let の証明の完了。

--- secd/ssr_msecd.v
+++ secd/ssr_msecd.v
@@ -1039,19 +1039,36 @@ Section Compiler.
         * by apply: MSECD_SS_Bool.
         * by apply: RTC_MSECD_SS_Reflexivity.
           
-    - move=> o' d1 d2 m n H1 IH1 H2 IH2 c H d' He.
-      exists (mNat (m + n)).
+    - admit.                                (* add *)
+    - admit.                                (* sub *)
+    - admit.                                (* mul *)
+    - admit.                                (* eq *)
+    - admit.                                (* var *)
+      
+    - move=> o' d1 d2 m n H1 IH1 H2 IH2 k H.
+      inversion H; subst=> e He.
+      case: (IH1 c1 H4 e He) => mv1 [Hc1 H1'].
+      have He2 : Compiler_SS_env (m :: o') (mv1 :: e)
+        by apply: Compiler_SS_env_cons.
+      case: (IH2 c2 H6 (mv1 :: e) He2) => mv2 [Hc2 H2'].
+      exists mv2.
       split.
-      + by apply: Compiler_SS_val_Nat.
-      + inversion H; subst=> s k.
-        case: (IH1 c1 H4 d' He) => v1 [Hv11 Hv12].
-        case: (IH2 c2 H6 d' He) => v2 [Hv21 Hv22].
-        apply: RTC_MSECD_SS_Transitivity.
-        * admit.
-        * apply: (RTC_MSECD_SS_Transitivity).
-          ** admit.
-          ** by apply: RTC_MSECD_SS_Reflexivity.
+      - done.
+      - move=> s k.
+        rewrite -catA.
+        eapply AppendSS.
+        + by apply: H1' => //.
+        + apply: (RTC_MSECD_SS_Transitivity _ _ _).
+          * by apply: MSECD_SS_Let.
+          * rewrite -/cat -catA.            (* fold cat *)
+            eapply AppendSS.
+            ** by apply: H2' => //.
+            ** apply: (RTC_MSECD_SS_Transitivity _ _ _).
+               *** by apply: MSECD_SS_EndLet.
+               *** by apply: RTC_MSECD_SS_Reflexivity.
   Admitted.
+  
+
 
   Theorem CorrectnessSS' o d v :
     MML_dB_NS o d v ->
