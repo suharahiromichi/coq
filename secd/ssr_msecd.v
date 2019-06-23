f6a3de8b841cd1997cb417f7f444985945bee673
SUHARA Hiromichi <suhara@falla.vbox>
2019-06-23 21:58:27 +0900

    dB_translation_NS_correctness の途中

--- secd/ssr_msecd.v
+++ secd/ssr_msecd.v
@@ -630,40 +630,6 @@ Section MiniMLdB.
    *)
 
   
-  Theorem dB_translation_NS_correctness g e v :
-    MML_NS g e v ->
-    forall o, dB_translation_NS_env g o ->
-              forall d, dB_translation_NS (mkctx g) e d ->
-                        exists vd, dB_translation_NS_val v vd /\ MML_dB_NS o d vd.
-  Proof.
-    elim.
-    - move=> g' n o He d H.
-      exists (vNat n).
-      inversion H; subst.
-      split.
-      * by apply: dB_translation_NS_val_Nat.
-      * by apply: MML_dB_NS_Nat.
-    - move=> g' b o He d H.
-      exists (vBool b).
-      inversion H; subst.
-      split.
-      * by apply: dB_translation_NS_val_Bool.
-      * by apply: MML_dB_NS_Bool.
-    - move=> g' e1 e2 m n H1 IH1 H2 IH2 o He d H.
-      inversion H; subst.
-      case: (IH1 o He d1 H5) => d1' [H11 H12].
-      case: (IH2 o He d2 H7) => d2' [H21 H22].
-      exists (vNat (m + n)).
-      split.
-      + by apply: dB_translation_NS_val_Nat.
-      + apply: MML_dB_NS_Plus.
-        * inversion H11; subst.
-          inversion H5; subst.
-
-        * admit.
-        * admit.
-  Admitted.
-  
   Theorem dB_translation_NS_correctness' g e v :
     MML_NS g e v ->
     forall o, dB_translation_NS_env g o ->
@@ -837,6 +803,122 @@ Section MiniMLdB.
        *)
 
       Admitted.
+
+  Theorem dB_translation_NS_correctness g e v :
+    MML_NS g e v ->
+    forall o, dB_translation_NS_env g o ->
+              forall d, dB_translation_NS (mkctx g) e d ->
+                        exists vd, dB_translation_NS_val v vd /\ MML_dB_NS o d vd.
+  Proof.
+    elim.
+    (* Nat *)
+    - move=> g' n o He d H.
+      exists (vNat n).
+      inversion H; subst.
+      split.
+      * by apply: dB_translation_NS_val_Nat.
+      * by apply: MML_dB_NS_Nat.
+
+    (* Bool *)
+    - move=> g' b o He d H.
+      exists (vBool b).
+      inversion H; subst.
+      split.
+      * by apply: dB_translation_NS_val_Bool.
+      * by apply: MML_dB_NS_Bool.
+
+    (* Plus *)
+    - move=> g' d1 d2 m n H1 IH1 H2 IH2 o He d H.
+      inversion H; subst.
+      case: (IH1 o He d0 H5) => d1' [H11 H12].
+      inversion H11; subst.
+      case: (IH2 o He d3 H7) => d2' [H21 H22].
+      inversion H21; subst.
+      exists (vNat (m + n)).
+      split.
+      + by apply: dB_translation_NS_val_Nat.
+      + apply: MML_dB_NS_Plus.
+        * by apply: H12.
+        * by apply: H22.
+
+    (* Minus *)
+    - move=> g' d1 d2 m n H1 IH1 H2 IH2 o He d H.
+      inversion H; subst.
+      case: (IH1 o He d0 H5) => d1' [H11 H12].
+      inversion H11; subst.
+      case: (IH2 o He d3 H7) => d2' [H21 H22].
+      inversion H21; subst.
+      exists (vNat (m - n)).
+      split.
+      + by apply: dB_translation_NS_val_Nat.
+      + apply: MML_dB_NS_Minus.
+        * by apply: H12.
+        * by apply: H22.
+
+    (* Times *)
+    - move=> g' d1 d2 m n H1 IH1 H2 IH2 o He d H.
+      inversion H; subst.
+      case: (IH1 o He d0 H5) => d1' [H11 H12].
+      inversion H11; subst.
+      case: (IH2 o He d3 H7) => d2' [H21 H22].
+      inversion H21; subst.
+      exists (vNat (m * n)).
+      split.
+      + by apply: dB_translation_NS_val_Nat.
+      + apply: MML_dB_NS_Times.
+        * by apply: H12.
+        * by apply: H22.
+
+    (* Eq *)
+    - move=> g' d1 d2 m n H1 IH1 H2 IH2 o He d H.
+      by inversion H.
+
+    (* Var *)
+    - move=> g' x o He d H.
+      inversion H; subst.
+      inversion He; subst.
+      * admit.
+      * admit.
+
+    (* Let *)
+    - move=> g' x e1 e2 v1 v2 H1 IH1 H2 IH2 o He d H.
+      inversion H.
+      admit.
+
+    (* If true *)
+    - move=> g' e1 e2 e3 v2 H1 IH1 H2 IH2 o He d H.
+      (* v2 は then 節 *)
+      inversion H; subst.
+      case: (IH1 o He d1 H6) => v1' [H11 H12].
+      inversion H11; subst.
+      case: (IH2 o He d2 H8) => v2' [H21 H22].
+      exists v2'.
+      split.
+      + by apply: H21.
+      + apply: MML_dB_NS_Iftrue.
+        * inversion H12; subst => //.
+          ** by apply: MML_dB_NS_Eq.
+          ** by apply: MML_dB_NS_Var.
+        * by apply: H22.
+          
+    (* If false *)
+    - move=> g' e1 e2 e3 v3 H1 IH1 H3 IH3 o He d H.
+      (* v3 は else 節 *)
+      inversion H; subst.
+      case: (IH1 o He d1 H6) => v1' [H11 H12].
+      inversion H11; subst.
+      case: (IH3 o He d3 H9) => v3' [H31 H32].
+      exists v3'.
+      split.
+      + by apply: H31.
+      + apply: MML_dB_NS_Iffalse.
+        * inversion H12; subst => //.
+          ** by apply: MML_dB_NS_Eq.
+          ** by apply: MML_dB_NS_Var.
+        * by apply: H32.
+          
+  Admitted.
+  
 
 End MiniMLdB.
 
