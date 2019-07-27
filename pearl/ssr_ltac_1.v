21d7d9ffdddcaa8179ee9cb9dbf5df233a4df565
SUHARA Hiromichi <suhara@falla.vbox>
2019-07-27 16:14:55 +0900

    説明を追加した。

--- pearl/ssr_ltac_1.v
+++ pearl/ssr_ltac_1.v
@@ -1,5 +1,5 @@
 (**
-Mathcomp で便利な Ltac 定義 (Proof Pearl ##4)
+Mathcomp の「不等式問題」を解決する
 ======
 2019/05/06
 
@@ -11,9 +11,45 @@ https://github.com/suharahiromichi/coq/blob/master/pearl/ssr_ltac_1.v
  *)
 
 (**
+OCaml 4.07.1, Coq 8.9.0, MathComp 1.9.0
+ *)
+
+(**
 # 説明
 
+不等式について。Standard Coqの場合は、``a <> b`` は、
+``~ (a = b)`` の表記（構文糖衣）にすぎません。また、二重否定もあまり出てきません。
+
+これに対して、Mathcomp は、``a != b``  が ``~~ (a == b)`` であるのはともかくとして、
+そもそもの、``a == b`` 自体が、``(a == b) = true`` の ``= true``
+を略した形（コアーション）で、``(a == b) <> false`` もあり、という具合に
+百花繚乱の状態です。二重否定も普通に出現することも事態を複雑にます。
+
+これを、Mathcompの「不等式問題」と呼ばれています（提唱者は私です）。
+
+Mathcomp ライブラリには、これらを解消するための補題
+（ビューまたrewriteで使う）
+が用意されてるわけですが、覚えることも、探すことも大変です。
+
+そこで、もっとも簡単かかたちに変換するタクティクを作ってみました。
 *)
+
+(**
+# コード例
+
+タクティクスの定義に難しい点はありません。ゴール部と前提部に対して
+繰り返しえ実行するようにしています。
+
+Prop型の否定 ``~`` の出現にも対応してますが、
+また、Ltac の定義のなかではコアーションが使えないため、
+is_true を明示する必要があります。
+
+
+``Definition is_true b := (b = true)``
+
+でした。
+*)
+
 
 From mathcomp Require Import all_ssreflect.
 
@@ -50,92 +86,118 @@ Ltac find_neg :=
   repeat find_neg_hypo;
   repeat find_neg_goal.
 
+(**
+# 補足説明
 
+Ltac の定義のなかで何をしているか見てみましょう。
+以下の表だけで、便利につかえるかもしれせんね。
+
+
+## 前提が...なら
+
+| 前   | タクティク | 後 |
+
+|:-------------------|:-----------------|:---------------|
+
+| (a == b) = true    | なももしない      | a == b         |
+
+| (a != b) = true    | なにもしない      | a != b         |
+
+| (a == b) <> true   | move/negP        | a != b         |
+
+| (a != b) <> true   | move/negP        | ~~ (a != b)    |
+
+| (a == b) = false   | move/negbT       | a != b         |
+
+| (a != b) = false   | move/negbT       | ~~ (a != b)    |
+
+| (a == b) <> false  | move/negpF       | a == b         |
+
+| (a != b) <> false  | move/negbF       | a != b         |
+
+| ~ (a == b)         | move/negP        | a != b         |
+                
+| ~ (a != b)         | move/negP        | ~~ (a != b)    |
+
+| (a == b) == true   | rewrite eqb_id   | a == b         |
+
+| (a != b) == true   | rewrite eqb_id   | a != b         |
+
+| (a == b) == false  | rewrite eqbF_n   | a != b         |
+
+| (a != b) == false  | rewrite eqbF_n   | ~~ (a != b)    |
+
+| ~~ ~~ (a == b)     | rewrite negbK    | a == b         |
+
+| ~~ (a != b)        | rewrite negbK    | (a == b) = true |
+
+ *)
+
+(**
+## ゴールが...なら
+
+| 前   | タクティク | 後 |
+
+|:-------------------|:-----------------|:---------------|
+| (a == b) = true    | なにもしない      | a == b         |
+
+| (a != b) = true    | なにもしない      | a != b         |
+
+| (a == b) <> true   | apply/negP       | a != b         |
+
+| (a != b) <> true   | apply/negP       | ~~ (a != b)    |
+
+| (a == b) = false   | apply/negbTE     | a != b         |
+
+| (a != b) = false   | apply/negbTE     | ~~ (a != b)    |
+
+| (a == b) <> false  | apply/Bool.not_false_iff_true | a == b |
+
+| (a != b) <> false  | apply/Bool.not_false_iff_true | a != b |
+
+| ~ (a == b)         | apply/negP       | a != b         |
+                
+| ~ (a != b)         | apply/negP       | ~~ (a != b)    |
+
+| (a == b) == true   | rewrite eqb_id   | a == b         |
+
+| (a != b) == true   | rewrite eqb_id   | a != b         |
+
+| (a == b) == false  | rewrite eqbF_n   | a != b         |
+
+| (a != b) == false  | rewrite eqbF_n   | ~~ (a != b)    |
+
+| ~~ ~~ (a == b)     | rewrite negbK    | a == b         |
+
+| ~~ (a != b)        | rewrite negbK    | (a == b) = true |
+
+*)
+
+
+(**
+# 実行例
+
+以下に実行例を示します。
+
+Standard Coqのように、ゴールはできるだけ前提に上げて（introして）
+から、このタクティクを実行してください。
+
+Mathcomp のタクティクのようにゴールのスタックトップに適用する、
+というわけではありません。
+*)
 
 Section Negative.
-
-(** 全体のテスト *)
 
   Goal forall (a b : nat), ~ (~~ ~~ (a == b) = false) -> (~~ (a == b)) <> true.
   Proof.
     move=> a b H1.
-      by find_neg.
-  Qed.
-  
-    
-(** find_neg_goal のテスト *)
-
-  Goal forall (a b : nat), (a == b) -> (a == b) = true.
-  Proof.
-    move=> a b H1.
-    find_neg_goal.
-    done.    
-  Qed.
-
-  Goal forall (a b : nat), (a != b) -> (a == b) <> true.
-  Proof.
-    move=> a b H1.
-    find_neg_goal.
-    done.    
-  Qed.
-
-  Goal forall (a b : nat), (a != b) -> ~((a == b) = true).
-  Proof.
-    move=> a b H1.
-    find_neg_goal.
-    done.    
-  Qed.
-  
-  Goal forall (a b : nat), (a != b) -> (a == b) = false.
-  Proof.
-    move=> a b H1.
-    find_neg_goal.
-    done.    
-  Qed.
-  
-  Goal forall (a b : nat), (a == b) -> (a == b) <> false.
-  Proof.
-    move=> a b H1.
-    find_neg_goal.
-    done.    
-  Qed.
-
-  Goal forall (a b : nat), (a == b) -> ~ ((a == b) = false).
-  Proof.
-    move=> a b H1.
-    find_neg_goal.
-    done.    
-  Qed.
-
-  Goal forall (a b : nat), (a != b) -> ~ (a == b).
-  Proof.
-    move=> a b H1.
-    find_neg_goal.
-    done.    
-  Qed.
-  
-  Goal forall (a b : nat), (a == b) -> (a == b) == true.
-  Proof.
-    move=> a b H1.
-    find_neg_goal.
-    done.
-  Qed.
-
-  Goal forall (a b : nat), (a != b) -> (a == b) == false.
-  Proof.
-    move=> a b H1.
-    find_neg_goal.
-    done.
-  Qed.
-
-  Goal forall (a b : nat), (a == b) -> ~~ ~~ (a == b).
-  Proof.
-    move=> a b H1.
-    find_neg_goal.
+    find_neg.
     done.
   Qed.
   
-(** find_neg_hypo のテスト *)
+(**
+前提部 find_neg_hypo の単体のテスト
+ *)
 
   Goal forall (a b : nat), (a == b) = true -> (a == b).
   Proof.
@@ -204,6 +266,80 @@ Section Negative.
   Proof.
     move=> a b H1.
     find_neg_hypo.
+    done.
+  Qed.
+  
+(**
+ゴール部 find_neg_goal の単体のテスト
+ *)
+
+  Goal forall (a b : nat), (a == b) -> (a == b) = true.
+  Proof.
+    move=> a b H1.
+    find_neg_goal.
+    done.    
+  Qed.
+
+  Goal forall (a b : nat), (a != b) -> (a == b) <> true.
+  Proof.
+    move=> a b H1.
+    find_neg_goal.
+    done.    
+  Qed.
+
+  Goal forall (a b : nat), (a != b) -> ~((a == b) = true).
+  Proof.
+    move=> a b H1.
+    find_neg_goal.
+    done.    
+  Qed.
+  
+  Goal forall (a b : nat), (a != b) -> (a == b) = false.
+  Proof.
+    move=> a b H1.
+    find_neg_goal.
+    done.    
+  Qed.
+  
+  Goal forall (a b : nat), (a == b) -> (a == b) <> false.
+  Proof.
+    move=> a b H1.
+    find_neg_goal.
+    done.    
+  Qed.
+
+  Goal forall (a b : nat), (a == b) -> ~ ((a == b) = false).
+  Proof.
+    move=> a b H1.
+    find_neg_goal.
+    done.    
+  Qed.
+
+  Goal forall (a b : nat), (a != b) -> ~ (a == b).
+  Proof.
+    move=> a b H1.
+    find_neg_goal.
+    done.    
+  Qed.
+  
+  Goal forall (a b : nat), (a == b) -> (a == b) == true.
+  Proof.
+    move=> a b H1.
+    find_neg_goal.
+    done.
+  Qed.
+
+  Goal forall (a b : nat), (a != b) -> (a == b) == false.
+  Proof.
+    move=> a b H1.
+    find_neg_goal.
+    done.
+  Qed.
+
+  Goal forall (a b : nat), (a == b) -> ~~ ~~ (a == b).
+  Proof.
+    move=> a b H1.
+    find_neg_goal.
     done.
   Qed.
   
