297a01827badf86dc95701c215f3a8630efcbe05
SUHARA Hiromichi <suhara@falla.vbox>
2019-08-30 23:32:05 +0900

    説明を追加した。

--- csm/csm_3_15_magma.v
+++ csm/csm_3_15_magma.v
@@ -20,7 +20,7 @@ Set Print All.
 (**
 ## 例1: 集合Mと、M上の二項演算*の組 [(M, * )] Magma の形式化
 *)
-Record magma : Type :=
+Record magma : Type :=                      (* 型クラス *)
   Magma {
       carrier : Type;
       operator : carrier -> carrier -> carrier
@@ -29,44 +29,88 @@ Record magma : Type :=
 Check magma : Type.
 Check Magma : forall carrier : Type, (carrier -> carrier -> carrier) -> magma.
 
+(** ****************** *)
 (** Magma [(Prop, /\)] *)
-Definition prop_and_magma := @Magma Prop and.
+(** ****************** *)
+Check and.                             (* Prop -> Prop -> Prop *)
+Definition prop_and_magma := @Magma Prop and. (* 型インスタンス *)
 Canonical prop_and_magma.
 Print Canonical Projections.                (* カノニカルの表示 *)
 (** [Prop <- carrier ( prop_and_magma )] *)
 
 Print prop_and_magma.
 (** [{| carrier := Prop; operator := and |} : magma] *)
-Compute carrier prop_and_magma.             (* Prop *)
-Compute @operator prop_and_magma.           (* and *)
+Compute carrier prop_and_magma.      (* 台 Prop を取り出す。 *)
+Compute @operator prop_and_magma.    (* オペレータ and を取り出す。 *)
 
 Lemma PropMagmaFalse (x y : carrier prop_and_magma) :
+  operator x False -> y.
+Proof.
+  rewrite /=; by case.
+Qed.
+
+(** これは Canonical 宣言が必要である。 *)
+Lemma PropMagmaFalse' (x y : Prop) :
+  operator x False -> y.
+Proof.
+  rewrite /=; by case.
+Qed.
+
+Lemma PropMagmaFalse'' (x y : carrier prop_and_magma) :
   @operator prop_and_magma x False -> y.
 Proof.
   rewrite /=; by case.
 Qed.
 
+(** 同じことを Magma を使わずに定義する。 *)
+Lemma PropFalse (x y : Prop) : and x False -> y.
+Proof.
+    by case.
+Qed.
+
+(** **************** *)
 (** Magma [(nat, +)] *)
-Definition nat_plus_magma := @Magma nat plus.
+(** **************** *)
+Definition nat_plus_magma := @Magma nat plus. (* 型インスタンス *)
 Canonical nat_plus_magma.
 Print Canonical Projections.                (* カノニカルの表示 *)
 (** [nat <- carrier ( nat_plus_magma )]  *)
 
 Print nat_plus_magma.
 (** [{| carrier := nat; operator := Init.Nat.add |} : magma] *)
-Compute carrier nat_plus_magma.             (* nat *)
-Compute @operator nat_plus_magma.           (* plus *)
+Compute carrier nat_plus_magma.     (* 台 nat を取り出す。 *)
+Compute @operator nat_plus_magma.   (* オペレータ plus を取り出す。 *)
 
 Lemma NatMagmaPlus (x y : carrier nat_plus_magma) :
+  operator x y = x + y.
+Proof.
+  rewrite /=; by [].
+Qed.
+
+(** これは Canonical 宣言が必要である。 *)
+Lemma NatMagmaPlus' (x y : nat) :
+  operator x y = x + y.
+Proof.
+  rewrite /=; by [].
+Qed.
+
+Lemma NatMagmaPlus'' (x y : carrier nat_plus_magma) :
   @operator nat_plus_magma x y = x + y.
 Proof.
   rewrite /=; by [].
 Qed.
 
+(** 同じことを Magma を使わずに定義する。 *)
+Lemma NatPlus (x y : nat) :
+  plus x y = x + y.
+Proof.
+  done.
+Qed.
+
 (**
 ## 例2: 代数構造の階層、半群の例
 *)
-Record semigroup : Type :=
+Record semigroup : Type :=                  (* 型クラス *)
   Semigroup {
       scarrier : magma;
       assoc : forall a b c : carrier scarrier,
@@ -82,10 +126,13 @@ carrier の型引数が省略されている。
 ]]
 *)
 
+(** *************** *)
+(** 半群 [(nat, +)] *)
+(** *************** *)
 Check addnA : associative addn.
 Check addnA 1 2 3 : 1 + (2 + 3) = 1 + 2 + 3.
 
-Definition nat_plus_semigroup := @Semigroup nat_plus_magma addnA.
+Definition nat_plus_semigroup := @Semigroup nat_plus_magma addnA. (* 型インスタンス *)
 Canonical nat_plus_semigroup.
 Print Canonical Projections.                (* カノニカルの表示 *)
 (** [nat_plus_magma <- scarrier ( nat_plus_semigroup )] *)
@@ -130,9 +177,13 @@ Section TEST2.
   Compute carrier nat_plus_magma.           (* nat *)
   Check @operator nat_plus_magma : nat -> nat -> nat.
   
-  Check @operator nat_plus_magma a b.
-  
-  Check a ^^ b.
+  Check @operator nat_plus_magma a b : carrier nat_plus_magma.
+  Check operator a b : carrier nat_plus_magma.
+  Check a ^^ b  : carrier nat_plus_magma.
+
+  Check @operator nat_plus_magma a b : nat.
+  Check operator a b : nat.
+  Check a ^^ b  : nat.
   
   Lemma natPlusExample2 (x y z : nat) :
     x ^^ (y ^^ z) = (x ^^ y) ^^ z.
@@ -143,7 +194,6 @@ End TEST2.
 
 (** [Canonical nat_plus_magma] は必要。 *)
 (** [Canonical nat_plus_semigroup]  は必要。 *)
-
 Lemma natPlusExample3 (x y z : nat) :
   x ^^ (y ^^ z) = (x ^^ y) ^^ z.
 Proof.
