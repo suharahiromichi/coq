1b0c86131c496c6a0ef08741f7decdd5a030de5d
SUHARA Hiromichi <suhara@falla.vbox>
2019-09-16 16:04:14 +0900

    first commit

---
+++ csm/csm_ex_3_no_answer.v
@@ -0,0 +1,34 @@
+(**
+Coq/SSReflect/MathComp による定理証明
+
+第3章 演習問題と追加の問題、回答付き
+
+======
+
+2018_09_16 @suharahiromichi
+ *)
+
+From mathcomp Require Import all_ssreflect.
+
+Set Implicit Arguments.
+Unset Strict Implicit.
+Unset Printing Implicit Defensive.
+
+(** 問 3.1  *)
+
+(** 問 3.2 *)
+
+(** 問 3.3 *)
+
+(** 問 3.4 *)
+
+(** 問 3.5 追加の問題 *)
+(** 与えられた玉の列に対する赤玉の数を示す述語 num_of_red を Inductive により定義せよ。 *)
+(** 例: 命題 num_of_red (cons 白玉 (cons 赤玉 nil)) 1 は、真となる。 *)
+
+(** 問 3.6 追加の問題 *)
+(** 問 3.4 で定義した 関数 赤数え の結果と、問 3.5 で定義した 述語
+    num_of_red の命題が必要十分条件であることを定理として示せ。
+    このとき、num_of_red の定義を見直してもよい。 *)
+
+(* END *)
