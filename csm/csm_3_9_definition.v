744eb61a56c96b72c360486a2227b5bfafc4c5d9
SUHARA Hiromichi <suhara@falla.vbox>
2019-09-21 12:09:01 +0900

    説明を追加した。

--- csm/csm_3_9_definition.v
+++ csm/csm_3_9_definition.v
@@ -15,6 +15,7 @@ ProofCafe 名古屋 補足資料
 From mathcomp Require Import all_ssreflect.
 Require Import Ascii String.
 Open Scope string_scope.
+(* Set Printing All. *)
 
 (**
 # 3.9.1 Definition
@@ -23,18 +24,56 @@ Open Scope string_scope.
 (**
 # 3.9.2 Lemma ... Qed
 
+テキスト p.101 の「補足：QedのかわりにDefinedを使うと別の機能になる」の説明は判り難い。
+
+Defind と Qed の違いは、Unfold できるかできないかの違いである。これを以下に説明する。
+*)
+
+(**
 Lemma/Defined による定義（QedでなくDefinedで終わる）についての補足説明
 
-Ordinal型（0から4までの5個の自然数からなる型）の値の定義が解り易く、実用的だろう。
+なお、「:=」で定義するのは Definition のみである。
+Lemma や Theorem, Corollay, Fact, Proposition, Remark は使えない。
+
+有限順序数 Ordinal型 (p.146) の値の定義が解り易く、実用的だろう。
+（0から4までの5個の自然数からなる型）
 
 'I_5型の、0〜4 をそれぞれ定義してみた。
  *)
 
-Definition p0 := @Ordinal 5 0 is_true_true.
+(**
+0 < 5 の証明を与える。
+*)
+Lemma lt_0_5 : 0 < 5. done. Qed.          (* ここは Defined でなくてよい。 *)
+Definition p0 := Ordinal lt_0_5.
+(* Definition p0 := @Ordinal 5 0 lt_0_5. *)
+
+(**
+by (done) で、1 < 5 などの証明が行われる。
+ *)
 Definition p1 : 'I_5. Proof. by apply: (@Ordinal 5 1). Defined.
 Lemma      p2 : 'I_5. Proof. by apply: (@Ordinal 5 2). Defined.
 Definition p3 : 'I_5. Proof. by apply: (@Ordinal 5 3). Qed.
 Lemma      p4 : 'I_5. Proof. by apply: (@Ordinal 5 4). Qed.
+
+(**
+補足説明
+
+0 < 5 の証明である lt_0_5 は、is_true_true に等しい。
+これは、0 < 5 を計算すると、true になることから。
+ *)
+Print lt_0_5.                               (* is_true_true *)
+Check is_true_true : 0 < 5.
+Check is_true_true : true = true.
+
+(**
+lt_0_5 の代わりに is_true_true を与えてもよい。
+*)
+Definition p0' := @Ordinal 5 0 is_true_true.
+Compute 0 + p0'.                            (* = 0 : nat *)
+(**
+補足説明終わり。
+*)
 
 (**
 自然数のサブタイプなので、自然数と加算して自然数が得られるはずである。
@@ -77,16 +116,10 @@ Print p3.            (* = Ordinal (n:=5) (m:=3) is_true_true : 'I_5 *)
 Print p4.            (* = Ordinal (n:=5) (m:=4) is_true_true : 'I_5 *)
 
 (**
-なお、「:=」で定義する場合は、
-Lemma や Theorem, Corollay, Fact, Proposition, Remark
-は使えない。
-*)
-
-(**
-証明のなかで simpl を実行してみるとよく判る。Coq Reference Manual の
-Controlling the reduction strategies and the conversion algorithm より。
-
+Coq Reference Manual の 
+Controlling the reduction strategies and the conversion algorithm
 https://coq.inria.fr/refman/proof-engine/vernacular-commands.html#controlling-the-reduction-strategies-and-the-conversion-algorithm
+より。
 
 - 「:=」やDefined で定義したqualidを transparent と呼ぶ。unfold（δ簡約）できる。
 
