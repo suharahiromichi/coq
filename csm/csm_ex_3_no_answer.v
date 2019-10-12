783796df87cd5ad831e387ff6de73503475ccec3
SUHARA Hiromichi <suhara@falla.vbox>
2019-10-12 17:49:31 +0900

    追記した。

--- csm/csm_ex_3_no_answer.v
+++ csm/csm_ex_3_no_answer.v
@@ -37,11 +37,11 @@ Unset Printing Implicit Defensive.
     the "Check" command. *)
 
 
-(** 問 3.4 *)
-(** ボールの列に含まれる赤玉の数を数える関数を定義します。*)
+(** いくつかの方法で、ボールの列に含まれる赤玉の数を数える関数を定義します。*)
 (** Define a function that counts the number of rouge balls in a
     sequenceq of balls in several ways. *)
 
+(** 問 3.4 *)
 (** Fixpint コマンドを使って、型 balls -> nat を持つ関数 count_rouges
     を定義してください。以下に動作例を示します。 *)
 (** そして、Compute コマンドを使って確認してください。 *)
@@ -167,5 +167,99 @@ Qed.
 (** なお、今回は証明は単純になりませんが、count_rouge_xxx の定義が簡単になります。 *)
 (** Note that this time the proof will not be simple,
     but the definition of count_rouge will be easy. *)
+
+(** 問 3a.1 (1) *)
+(** 赤玉と白玉のふたつの要素からなるball型をInductiveを使って定
+    義してください。 *)
+(** Exercise 3a.1 (1) *)
+(** Define the type "ball" that consists of two elements, red (rouge)
+    and white (blanc). *)
+
+(** 3.1 と同じ。 *)
+(** Same as ex. 3.1 *)
+
+
+(** 問 3a.1 (2) *)
+(** ball型をeqTypeのインスタンス、ball_eqType として定義してみましょう。 *)
+(** Exercise 3a.1 (2) *)
+(** Define the ball type as an instance of eqType, ball_eqType. *)
+
+(** 最初に、ball型どうしのbool形の等式 eqBall を定義します。 *)
+(** First, define a bool equation between ball types. *)
+
+(** 次に、eqBall が Standard Coqの等式 eq （=) と同値であるこを証明しましょう。
+    reflect は、以下の意味の述語で、ssrbool.v で定義されています。
+    
+    eq が証明可能 <-> eqBall が true
+    eq の否定が証明可能 <-> eqBall が false
+*)
+(** Next, let's prove that eqBall is equivalent to the Standard Coq equation (=).
+    reflect is a predicate with the following meaning, defined in ssrbool.v.
+    
+    eq can be proved <-> eqBall is true
+    eq negation can be proved <-> eqBall is false
+*)
+
+(** 最後に、ball_eqType を定義します。 *)
+(** Finally, define ball_eqType. *)
+
+(** eq_op (==) が使えることを確認します。 *)
+(** Confirm that you can use eq_op (==) *)
+
+(** 問 3a.2 *)
+(** ボールの列の型をlist型(またはseq型)を使って定義してください。 *)
+(** Exercise 3a.2 *)
+(** Using the "list" (or "seq") type, Dinefine the type of sequence of balls. *)
+
+(** seq形も、eqType のインスタンスなので、eq_op (==) が使えます。 *)
+(** The seq form is also an instance of eqType, so you can use eq_op (==) *)
+
+
+(** 問 3a.3 (略) *)
+(** Exercise 3.3 (omitted) *)
+
+
+(** 問 3a.4 *)
+(** Fixpint コマンドを使って、型 balls -> nat を持つ関数 count_rouges
+    を定義してください。 *)
+(** Exercise 3.4 *)
+(** Using "Fixpoint" command, Define the function "count_rouges" which
+    has the "balls -> nat" type. *)
+
+(** 3.4 と同じ。 *)
+(** Same as ex. 3.4 *)
+
+
+(** 問 3a.5 *)
+(** count_rougs_with_map を map と sumn を使って定義してください。
+    rougeを1、blancを0に置き換えた自然数列を求め、その総和を求めます。 *)
+(** Exercise 3a.5 *)
+(** Define the function using map and sumn. The natural number
+    sequence is obtained by replacing the rouge of the ball sequence
+    with 1 and blanc with 0. Then calculate the sum. *)
+(** Example
+    [:: rouge; blanc; rouge] ==> [:: 1; 0; 1] ==> 1 + 0 + 1 = 2 *)
+
+(** count_rouge と count_rouge_with_map の結果が一致することを証明して
+    ください。 *)
+(** Prove that the results count_rouge and count_rouge_with_map are
+    consistent. *)
+
+
+(** 問 3a.6 *)
+(** count_rougs_with_fil を filter と size を使って定義してください。
+    rougeだけを残したボールの列を求め、そのサイズを求めます。 *)
+(** Exercise 3a.6 *)
+(** Define the function using filter and size. Create a sequence of
+    balls leaving only rouge and calculate its size. *)
+(** Example
+    [:: rouge; blanc; rouge] ==> [:: rouge; rouge] ==> size is 2 *)
+
+(** count_rouge と count_rouge_with_fil の結果が一致することを証明して
+    ください。 *)
+(** Prove that the results count_rouge and count_rouge_with_fil are
+    consistent. *)
+
+
 
 (* END *)
