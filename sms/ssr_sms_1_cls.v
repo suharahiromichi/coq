d33ec0fcdfe435ea912a399349c2d2bbfcc01e91
SUHARA Hiromichi <suhara@falla.vbox>
2019-10-14 12:59:45 +0900

    push のコアーションと型のチェックを追加する。

--- sms/ssr_sms_1_cls.v
+++ sms/ssr_sms_1_cls.v
@@ -14,17 +14,19 @@ Inductive type : Set :=
 | tb
 .
 
-Inductive data : Set :=                     (* v *)
-| dn (n : nat)
-| db (b : bool)
+Inductive value : Set :=                    (* v *)
+| vn (n : nat)
+| vb (b : bool)
 .
+Coercion vn : nat >-> value.
+Coercion vb : bool >-> value.
 
 Inductive inst : Set :=                     (* c *)
 | iseq (cs : seq inst)
 | idrop
 | idup
 | iswap
-| ipush (ty : type) (d : data)
+| ipush (ty : type) (v : value)
 | iloop (cs : seq inst)
 | idip (cs : seq inst)
 | iadd
@@ -32,31 +34,31 @@ Inductive inst : Set :=                     (* c *)
 | imul
 | ieq
 | ineq
-| iret (d : data)                           (* for idip *)
+| iret (v : value)                           (* for idip *)
 .
 
 Definition cstack := seq inst.              (* c :: cs *)
-Definition vstack := seq data.              (* v :: vs *)
+Definition vstack := seq value.             (* v :: vs *)
 Definition env := (vstack * cstack)%type.   (* e *)
 
 Definition sample1 :=
   [::
-     ipush tn (dn 1);
-     ipush tn (dn 2);
+     ipush tn 1;
+     ipush tn 2;
      iadd;
      idrop
   ].
 
 Definition fact :=
   [::
-     ipush tn (dn 1);
-     ipush tn (dn 4);
+     ipush tn 1;
+     ipush tn 4;
      idup;
      ineq;
      iloop [::
               idup;
               idip [:: imul];
-              ipush tn (dn 1);
+              ipush tn 1;
               iswap;
               isub;
               idup;
@@ -66,27 +68,28 @@ Definition fact :=
   ].
 
 Inductive step : relation env :=            (* env -> env -> Prop *)
-| stepseq  vs cs1 cs2 cs : cs1 ++ cs2 = cs ->
+| stepseq vs cs1 cs2 cs : cs1 ++ cs2 = cs ->
                            step (vs, iseq cs1 :: cs2)           (vs, cs)
 | stepdrop v vs cs :       step (v :: vs, idrop :: cs)          (vs, cs)
-| stepdup  v vs cs :       step (v :: vs, idup :: cs)           (v :: v :: vs, cs)
+| stepdup v vs cs :        step (v :: vs, idup :: cs)           (v :: v :: vs, cs)
 | stepswap v1 v2 vs cs :   step (v2 :: v1 :: vs, iswap :: cs)   (v1 :: v2 :: vs, cs)
-| steppush ty v vs cs:     step (vs, ipush ty v :: cs)          (v :: vs, cs)
+| steppush_n n vs cs:      step (vs, ipush tn (vn n) :: cs)     ((vn n) :: vs, cs)
+| steppush_b b vs cs:      step (vs, ipush tb (vb b) :: cs)     ((vb b) :: vs, cs)
 | steploop_tt vs cs1 cs2 cs : cs1 ++ cs2 = cs ->
-                           step (db true :: vs, iloop cs1 :: cs2) (vs, cs)
-| steploop_ff vs cs1 cs2 : step (db false :: vs, iloop cs1 :: cs2) (vs, cs2)
+                           step (vb true :: vs, iloop cs1 :: cs2) (vs, cs)
+| steploop_ff vs cs1 cs2 : step (vb false :: vs, iloop cs1 :: cs2) (vs, cs2)
 | stepdip v vs1 vs2 cs1 cs2 cs : cs1 ++ [:: iret v] ++ cs2 = cs ->
                            step (v :: vs1, idip cs1 :: cs2) (vs2, cs)
 | stepadd v1 v2 v3 vs cs : v1 + v2 = v3 ->
-                           step (dn v2 :: dn v1 :: vs, iadd :: cs) (dn v3 :: vs, cs)
+                           step (vn v2 :: vn v1 :: vs, iadd :: cs) (vn v3 :: vs, cs)
 | stepsub v1 v2 v3 vs cs : v1 - v2 = v3 ->
-                           step (dn v2 :: dn v1 :: vs, isub :: cs) (dn v3 :: vs, cs)
+                           step (vn v2 :: vn v1 :: vs, isub :: cs) (vn v3 :: vs, cs)
 | stepmul v1 v2 v3 vs cs : v1 * v2 = v3 ->
-                           step (dn v2 :: dn v1 :: vs, imul :: cs) (dn v3 :: vs, cs)
-| stepeq_tt vs cs :        step (dn 0 :: vs, ieq :: cs)     (db true :: vs, cs)
-| stepeq_ff v vs cs :      step (dn v.+1 :: vs, ieq :: cs)  (db false :: vs, cs)
-| stepneq_tt v vs cs :     step (dn v.+1 :: vs, ineq :: cs) (db true :: vs, cs)
-| stepneq_ff vs cs :       step (dn 0 :: vs, ineq :: cs)    (db false :: vs, cs)
+                           step (vn v2 :: vn v1 :: vs, imul :: cs) (vn v3 :: vs, cs)
+| stepeq_tt vs cs :        step (vn 0 :: vs, ieq :: cs)     (vb true :: vs, cs)
+| stepeq_ff v vs cs :      step (vn v.+1 :: vs, ieq :: cs)  (vb false :: vs, cs)
+| stepneq_tt v vs cs :     step (vn v.+1 :: vs, ineq :: cs) (vb true :: vs, cs)
+| stepneq_ff vs cs :       step (vn 0 :: vs, ineq :: cs)    (vb false :: vs, cs)
 | stepret v vs cs:         step (vs, iret v :: cs)          (v :: vs, cs)
 .
 
@@ -143,11 +146,18 @@ Proof.
     left.
     exists ([:: v2, v1 & vs], cs).
       by apply: stepswap.
-  (* push *) (* 型のチェックを入れること。 *)
+  (* push *)
   - move=> ty v cs.
-    left.
-    exists ([:: v & vs], cs).
-      by apply: steppush.
+    case: ty; case: v; try i_none.  (* nat か bool で場合分けする。 *)
+    (* nat の組み合わせ と bool の組み合わせ以外を try i_none で片付ける。 *)
+    + move=> n.
+      left.
+      exists ([:: vn n & vs], cs).
+        by apply: steppush_n.
+    + move=> b.
+      left.
+      exists ([:: vb b & vs], cs).
+        by apply: steppush_b.
   (* loop *)
   - case: vs => [cs | v vs cs1 cs2]; first i_none.
     case: v => [n | b]; first i_none. (* nat か bool で場合分けする。 *)
@@ -170,39 +180,39 @@ Proof.
     case: v1; case: v2 => n1 n2; try i_none. (* nat か bool で場合分けする。 *)
     (* nat と nat の組み合わせ以外を try i_none で片付ける。 *)
     left.
-    exists (dn (n1 + n2) :: vs, cs).
+    exists (vn (n1 + n2) :: vs, cs).
       by apply: stepadd.
   (* sub *)
   - case: vs => [cs | v1 vs cs]; first i_none.
     case: vs => [| v2 vs]; first i_none.
     case: v1; case: v2 => n1 n2; try i_none.
     left.
-    exists (dn (n1 - n2) :: vs, cs).
+    exists (vn (n1 - n2) :: vs, cs).
       by apply: stepsub.
   (* mul *)
   - case: vs => [cs | v1 vs cs]; first i_none.
     case: vs => [| v2 vs]; first i_none.
     case: v1; case: v2 => n1 n2; try i_none.
     left.
-    exists (dn (n1 * n2) :: vs, cs).
+    exists (vn (n1 * n2) :: vs, cs).
       by apply: stepmul.
   (* eq *)
   - case: vs => [cs | v vs cs]; first i_none.
     case: v => n; try i_none.       (* nat か bool で場合分けする。 *)
     left.
     case: n.                        (* 0 か 非0 で場合分けする。 *)
-    + exists ([:: db true & vs], cs).
+    + exists ([:: vb true & vs], cs).
         by apply: stepeq_tt.
-    + exists ([:: db false & vs], cs).
+    + exists ([:: vb false & vs], cs).
         by apply: stepeq_ff.
   (* neq *)
   - case: vs => [cs | v vs cs]; first i_none.
     case: v => n; try i_none.
     left.
     case: n.
-    + exists ([:: db false & vs], cs).
+    + exists ([:: vb false & vs], cs).
         by apply: stepneq_ff.
-    + exists ([:: db true & vs], cs).
+    + exists ([:: vb true & vs], cs).
         by apply: stepneq_tt.
         
   (* ret 追加 *)
