cf3d20291489ec3eb599e9921b89af07beddeaf7
SUHARA Hiromichi <suhara@falla.vbox>
2019-11-28 23:35:00 +0900

    証明を完成させた。

--- ssr/ssr_palindrome.v
+++ ssr/ssr_palindrome.v
@@ -6,28 +6,30 @@ Unset Strict Implicit.
 
 Section Pal.
 
-  Inductive palindrome { X : Type } : seq X -> Prop :=
+  Variable X : Type.
+  
+  Inductive palindrome : seq X -> Prop :=
   | palindrome_nil : palindrome [::]
   | palindrome_x : forall (x : X), palindrome [:: x]
   | palindrome_xs : forall (x : X) (l : seq X),
       palindrome l -> palindrome (x :: l ++ [:: x]).
   Hint Constructors palindrome.
   
-  Theorem pal_rev { X : Type } (l : seq X) : palindrome l -> l = rev l.
+  Theorem pal_rev (l : seq X) : palindrome l -> l = rev l.
   Proof.
     elim=> //= x l' H IHpalindrome.
     rewrite -cat_cons -cat1s !rev_cat.
       by rewrite -IHpalindrome.
   Qed.
   
-  Inductive seq_alt { X : Type } : seq X -> Prop :=
+  Inductive seq_alt : seq X -> Prop :=
   | seq_alt_base : seq_alt [::]
   | seq_alt_x : forall (x : X), seq_alt [:: x]
   | seq_alt_xy : forall (x y : X)
                         (l : seq X), seq_alt l -> seq_alt (x :: l ++ [:: y]).
   Hint Constructors seq_alt.
   
-  Lemma seq_alt_cons { X : Type } (x : X) (l : seq X) : seq_alt l -> seq_alt (x::l).
+  Lemma seq_alt_cons (x : X) (l : seq X) : seq_alt l -> seq_alt (x::l).
   Proof.
     elim=> //= [y |x' y l' H IHseq_alt].
     - have -> : [:: x ; y] = x :: [::] ++ [:: y] by [].
@@ -40,14 +42,14 @@ Section Pal.
         by do 2!apply: seq_alt_xy.
   Qed.
 
-  Lemma seq_alt_correct { X : Type } : forall (l : seq X), seq_alt l.
+  Lemma seq_alt_correct : forall (l : seq X), seq_alt l.
   Proof.
     induction l.
     - apply seq_alt_base.
     - apply seq_alt_cons. apply IHl.
   Qed.
   
-  Lemma alt_induction { X : Type } (P : seq X -> Prop) :
+  Lemma alt_induction (P : seq X -> Prop) :
     P [::] ->
     (forall (x : X), P [:: x]) ->
     (forall (l : seq X), P l -> forall (x y : X), P (x :: l ++ [:: y])) ->
@@ -58,77 +60,63 @@ Section Pal.
       by apply: Hss.
   Qed.
   
-  Lemma cat_injective {X : Type} (s t : seq X) x : s ++ [:: x] = t ++ [:: x] -> s = t.
+  Lemma rev_one (x : X) : rev [:: x] = [:: x].
   Proof.
-    elim: s; elim: t => //=.
-    - move=> a l IH1 IH2.
-      inversion IH2; subst; clear IH2.
-  Admitted.
-  
-
-  (* sf/Lists.v *)
-  Lemma snoc_rev {X : Type} (x : X) (l : seq X) : rev (rcons l x) = x :: (rev l).
-  Proof.
-    elim: l => //= a l IHl.
-    rewrite rev_cons IHl /=.
-    by rewrite -rev_cons.
-  Qed.
-  
-  Lemma rev_injective { X : Type } (l1 l2 : seq X) : rev l1 = rev l2 -> l1 = l2.
-  Proof.
-    move=> H.
-    rewrite -(revK l1).
-    rewrite H.
-      by rewrite (revK l2).
-  Qed.
-  
-  Theorem rev_pal' { X : Type } (l : seq X) : l = rev l -> palindrome l.
-  Proof.
-    elim/(@alt_induction X) : l => //= l IH x y H.
-    have H1 : x :: l ++ [:: y] = (x :: l) ++ [:: y] by [].
-    rewrite H1 in H.
-    rewrite rev_cat in H.
-    injection H as Heq1.
-    rewrite -Heq1.
-    apply: palindrome_xs.
-    apply: IH.
-    rewrite -Heq1 in H.
-    rewrite -[x :: l]cat1s in H.
-    rewrite rev_cat in H.
-    have H2 : rev [:: x] = [:: x] by [].
-    rewrite H2 in H.
-    symmetry in H.
-    Check (@cat_injective X (rev l) l x).
-    move/(@cat_injective X (rev l) l x) in H.
     done.
   Qed.
   
 (*
-  Theorem rev_pal' { X : Type } : forall (l : seq X), l = rev l -> palindrome l.
+  Lemma app_injective (l1 l2 s : seq X) : l1 ++ s = l2 ++ s -> l1 = l2.
   Proof.
-  intros l H. induction l using alt_induction.
-  - (* Base case 1; empty seq is palindrome by definition. *)
-    apply palindrome_nil.
-  - (* Base case 2; singleton seq is palindrome by definition. *)
-    apply palindrome_x.
-  - (* Inductive step (and interesting case.) *)
-
-    (* Milk the reverse property to show that we end and begin with the same thing. *)
-    assert (Hi1 : x::l ++ [y] = (x::l) ++ [y]). reflexivity. rewrite Hi1 in H.
-    rewrite rev_app_distr in H. simpl in H. injection H as Heq1.
-
-    (* Do the same again; This time, we also find that rev l = l. *)
-    apply rev_injective in H. repeat (rewrite rev_app_distr in H). simpl in H. injection H as Heq2.
-
-    (* Use the induction hypothesis to deduce that l is a palindrome. *)
-    rewrite rev_involutive in H. symmetry in H. apply IHl in H.
-
-    (* We know we start and end with the same thing; Use the third way to construct a palindrome.
-       Since l is a palindrome, we are done. *)
-    rewrite Heq1. apply palindrome_xs. apply H.
-Qed.
- *)
-
+  Admitted.                                 (*  *)
+  (* これは証明できなかったので *)
+  Theorem rev_pal' (l : seq X) : l = rev l -> palindrome l.
+  Proof.
+    elim/alt_induction : l => //= l IH x y H.
+    rewrite -cat_cons rev_cat /= in H.
+    inversion H.                 (* injection H as H1 *)
+    (* H1 : x = y *)
+    (* H2 : l ++ [:: y] = rev (y :: l) *)
+    rewrite -H1 -[x :: l]cat1s rev_cat rev_one in H2.
+    rewrite -H1.
+    apply: palindrome_xs.
+    apply: IH.
+    move/(@app_injective l (rev l) [:: x]) in H2. (* XXX *)
+    done.
+  Abort.
+*)  
+  
+  (* revK (= rev_injective) の特別なかたちとして、次を用意した。 *)
+  Lemma rev_injective' (l1 l2 : seq X) : l1 = rev l2 -> rev l1 = l2.
+  Proof.
+    move=> H.
+    rewrite -(revK l2).
+      by rewrite -H.
+  Qed.
+  
+  Theorem rev_pal' (l : seq X) : l = rev l -> palindrome l.
+  Proof.
+    elim/alt_induction : l => //= l IH x y H.
+    rewrite -cat_cons rev_cat /= in H.
+    (* H : x :: l ++ [:: y] = y :: rev (x :: l) *)
+    inversion H.
+    (* H1 : x = y *)
+    (* H2 : l ++ [:: y] = rev (y :: l) *)
+    
+    rewrite -H1.
+    
+    rewrite -H1 -[x :: l]cat1s in H2.
+    move/rev_injective' in H2.
+    rewrite cat1s rev_cat rev_one cat_cons in H2.
+    (* H2 : x :: [::] ++ rev l = x :: l *)
+    inversion H2.
+    (* H3 : revl = l *)
+    
+    apply: palindrome_xs.
+    rewrite H3.
+      by apply: IH.
+  Qed.
+  
 End Pal.
 
 (* END *)
