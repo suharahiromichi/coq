b6369d3ef8f9b27d146de3d61d845b40f4d34440
SUHARA Hiromichi <suhara@falla.vbox>
2019-12-15 18:49:59 +0900

    説明を追加した。

--- csm/csm_4_2_eqtype.v
+++ csm/csm_4_2_eqtype.v
@@ -45,25 +45,75 @@ Definition string_eqMixin := @EqMixin string string_eqb string_eqP.
 Canonical string_eqType := EqType string string_eqMixin.
 
 (**
-## eqP と eqE
+# generic theory (ジェネリックな定理)
+
+eqType に対して使える定理
  *)
 
 (**
-## eq_refl と eq_sym
+## eqP (= と == に間を変換する)
  *)
+Check @eqP : forall (T : eqType) (x y : T), reflect (x = y) (x == y).
+
+Lemma test : forall (x y : nat), x == y -> x + y == y + y.
+Proof. move=> x y. by move=> /eqP ->. Qed.
+
+Lemma test4 : forall (n m : nat), if n == m then true else true.
+Proof. move=> n m.
+       case: eqP => H.                      (* ifP でもよい。 *)
+       (* H : n = m *)
+       + done.
+       + done.
+Qed.
 
 (**
-## contraXX
+## eq_refl と eq_sym (== の 反射性と対称性の補題)
  *)
+Check eq_refl : forall (T : eqType) (x : T), x == x.
+Check eq_sym  : forall (T : eqType) (x y : T), (x == y) = (y == x).
+
+(**
+## contraXX (== に関する対偶)
+
+直観主義論理では、一般に対偶は成り立たないが、否定の側がboolなら成立する。
+最後のふたつを覚えておけばよいかも。
+ *)
+Check contraTeq : forall (T1 : eqType) (b : bool) (x y : T1),
+    (x != y -> ~~ b) -> b -> x = y.
+Check contraNeq : forall (T1 : eqType) (b : bool) (x y : T1),
+    (x != y -> b) -> ~~ b -> x = y.
+Check contraFeq : forall (T1 : eqType) (b : bool) (x y : T1),
+    (x != y -> b) -> b = false -> x = y.
+Check contraTneq : forall (T1 : eqType) (b : bool) (x y : T1),
+    (x = y -> ~~ b) -> b -> x != y.
+Check contraNneq : forall (T1 : eqType) (b : bool) (x y : T1),
+    (x = y -> b) -> ~~ b -> x != y.
+Check contraFneq : forall (T1 : eqType) (b : bool) (x y : T1),
+    (x = y -> b) -> b = false -> x != y.
+Check contra_eqN : forall (T1 : eqType) (b : bool) (x y : T1),
+    (b -> x != y) -> x = y -> ~~ b.
+Check contra_eqF : forall (T1 : eqType) (b : bool) (x y : T1),
+    (b -> x != y) -> x = y -> b = false.
+Check contra_eqT : forall (T1 : eqType) (b : bool) (x y : T1),
+    (~~ b -> x != y) -> x = y -> b.
+
+Check contra_eq : forall (T1 T2 : eqType) (z1 z2 : T2) (x1 x2 : T1),
+       (x1 != x2 -> z1 != z2) -> z1 = z2 -> x1 = x2.
+Check contra_neq : forall (T1 T2 : eqType) (z1 z2 : T2) (x1 x2 : T1),
+       (x1 = x2 -> z1 = z2) -> z1 != z2 -> x1 != x2.
 
 (**
 ## eq_irrelevance
+
+https://github.com/suharahiromichi/coq/blob/master/pearl/ssr_axiom_free.v
  *)
+Check eq_irrelevance  : forall (T : eqType) (x y : T) (e1 e2 : x = y), e1 = e2.
+
 
 (**
 # pcancel や cancel から eqType を定義する：
 
-coq/math-comp-book/suhara.ch7-windrose-2.v も参照のこと。
+https://github.com/suharahiromichi/coq/blob/master/math-comp-book/suhara.ch7-windrose-2.v
 *)
 
 Inductive balle :=
