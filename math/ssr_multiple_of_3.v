e80f8dd061ca65224ec3b652010fcd28b6be19e5
SUHARA Hiromichi <suhara@falla.vbox>
2020-04-10 22:28:10 +0900

    補題を作成した。

--- math/ssr_multiple_of_3.v
+++ math/ssr_multiple_of_3.v
@@ -1,9 +1,114 @@
+(**
+10進各桁の和が3の倍数なら、3の倍数であることの証明
+========================
+
+@suharahiromichi
+
+2020/04/10
+ *)
+
 From mathcomp Require Import all_ssreflect.
 
 Set Implicit Arguments.
 Unset Strict Implicit.
 Unset Printing Implicit Defensive.
 (* Set Print All. *)
+
+(**
+# 証明
+*)
+
+Variable x : nat -> nat.
+
+(**
+## 補題： 9x, 99x, 999x は3の倍数である。
+ *)
+
+Lemma gt_exp m n : 0 < m -> 0 < m^n.
+Proof.
+  move=> H.
+  elim: n => // n IHn.
+  rewrite expnS.
+  rewrite -{1}(muln0 m).
+    by rewrite ltn_pmul2l.
+Qed.
+
+Lemma dvdn3_99 n : 3 %| (10^n - 1).
+Proof.
+  elim: n => //.
+  move=> n IHn.
+  rewrite expnS.
+  have {1}-> : 10 = 9 + 1 by [].
+  rewrite mulnDl.
+  rewrite mul1n.
+  rewrite -addnBA.
+  - rewrite dvdn_addr.
+    + done.
+    + by rewrite dvdn_mulr.
+  - by apply: gt_exp.
+Qed.
+
+Lemma dvdn3_99x n : 3 %| (10^n - 1) * (x n).
+Proof.
+  rewrite dvdn_mulr //.
+    by apply: dvdn3_99.
+Qed.
+
+(**
+## 補題 : 9x + 99y + 999z は3の倍数である。
+ *)
+
+Lemma dvdn3_s99 (n : nat) : 3 %| \sum_(1 <= i < n.+2)(10^i - 1).
+Proof.
+  elim: n => [| n IHn].
+  - by rewrite big_nat1.
+  - rewrite big_nat_recr.
+    rewrite dvdn_addl.
+    + done.
+    + apply: dvdn3_99.
+    + done.
+Qed.
+
+Lemma dvdn3_s99x (n : nat) : 3 %| \sum_(1 <= i < n.+2)((10^i - 1) * (x i)).
+Proof.
+  Admitted.
+
+(**
+## 補題 : 10x + 100y + 1000z = 9x + 99y + 999z + x + y + z
+*)
+
+Lemma l_100__99_1 (n : nat) : 10 ^ n = 10 ^ n - 1 + 1.
+Proof.
+  rewrite addn1 subn1 prednK //.
+    by apply: gt_exp.
+Qed.
+
+Lemma l_100x__99x_x (i : nat) : 10^i * (x i) = (10^i - 1) * (x i) + (x i).
+Proof.
+  rewrite -{3}[(x i)]mul1n.
+  rewrite -mulnDl.
+    by rewrite -l_100__99_1.
+Qed.
+
+Lemma s100x__s99x_x (n : nat) :
+  \sum_(1 <= i < n)(10^i * (x i)) =
+  \sum_(1 <= i < n)((10^i - 1) * (x i)) + \sum_(1 <= i < n)(x i).
+Proof.
+  Admitted.
+
+(**
+## 定理
+ *)
+Goal forall (n : nat), (3 %| \sum_(1 <= i < n.+2)(10^i * (x i))) =
+                       (3 %| \sum_(1 <= i < n.+2)(x i)).
+Proof.
+  move=> n.
+  rewrite s100x__s99x_x.
+  rewrite dvdn_addr //.
+    by apply: dvdn3_s99x.
+Qed.
+
+(*
 
 Lemma test100 x : (3 %| x) = (3 %| 100 * x).
 Proof.
@@ -64,7 +169,7 @@ Proof.
     by rewrite ltn_pmul2l.
 Qed.
 
-Lemma div3_99 n : 3 %| (10^n - 1).
+Lemma dvdn3_99 n : 3 %| (10^n - 1).
 Proof.
   elim: n => //.
   move=> n IHn.
@@ -90,7 +195,7 @@ Goal forall x n, (3 %| x) = (3 %| 10^n * x).
 Proof.
   move=> x n.
   have -> : 10^n = (10^n - 1) + 1 by apply: test.
-    by rewrite mulnDl mul1n dvdn_addr // dvdn_mulr // div3_99.
+    by rewrite mulnDl mul1n dvdn_addr // dvdn_mulr // dvdn3_99.
 Qed.
 
 Goal forall x, (3 %| x) = (3 %| 10 * x).
@@ -102,3 +207,5 @@ Proof.
     by rewrite dvdn_mulr.
 Qed.
 
+ *)
+  
