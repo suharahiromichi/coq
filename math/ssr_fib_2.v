99bd276b9d45f9651e55007aac793b56af0271ca
SUHARA Hiromichi <suhara@falla.vbox>
2020-05-16 20:21:24 +0900

    性質3

--- math/ssr_fib_2.v
+++ math/ssr_fib_2.v
@@ -33,8 +33,14 @@ Section Fib_2.
     rewrite addn_gt0.
       by apply/orP/or_intror.
   Qed.
-  
-  (* ここでは、帰納法で解く。 *)
+
+(**
+## 性質1
+
+```Ｆ1 ＋ Ｆ2 ＋ … ＋ Ｆn ＝ Ｆn+2 － 1```
+
+ここでは、帰納法で解く。
+ *)
   Lemma lemma1' (n : nat) :
     \sum_(0 <= i < n.+1)(fib i) = fib n.+2 - 1.
   Proof.  
@@ -47,7 +53,12 @@ Section Fib_2.
       congr (_ - _).
         by rewrite addnC.
   Qed.
-  
+
+(**
+## 性質2
+
+```Ｆ1 × Ｆ1 ＋ Ｆ2 × Ｆ2 ＋ … ＋ Ｆn × Ｆn ＝ Ｆn × Ｆn+1```
+*)
   Lemma lemma2 (n : nat) :
     \sum_(0 <= i < n.+1)(fib i * fib i) = fib n * fib n.+1.
   Proof.
@@ -60,5 +71,29 @@ Section Fib_2.
       rewrite mulnC.
         by congr (_ * _).
   Qed.
+
+(**
+## 性質3
+
+```Ｆ1 ＋ Ｆ3 ＋ Ｆ5 ＋ … ＋ Ｆ2n-1 ＝ Ｆ2n```
+*)  
+  Lemma lemma3 (n : nat) :
+    \sum_(0 <= i < n)(fib i.*2.+1) = fib n.*2.
+  Proof.
+    elim: n.
+    - by rewrite big_nil.
+    - move=> n IHn.
+      have -> : n.+1.*2 = n.*2.+2.
+      + rewrite -addn1 -!muln2.
+          by ssromega.
+      + rewrite fib_n.                      (* 右辺 *)
+        rewrite l_last; last done.          (* 左辺 *)
+        rewrite IHn.
+          by congr (_ + _).
+  Qed.
+  
+    
+
+End Fib_2.
   
 (* END *)
