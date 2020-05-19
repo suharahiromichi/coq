9479b045dabfc20914f338f984d3d53b658c766d
SUHARA Hiromichi <suhara@falla.vbox>
2020-05-19 19:23:08 +0900

    gcd_ind

--- coq_gcd.v
+++ coq_gcd.v
@@ -57,7 +57,13 @@ Proof.
   Search well_founded.
   exact lt_wf.
 Defined.
-Check gcd_ind.
+Check gcd_ind
+  : forall P : nat -> nat -> nat -> Prop,
+    (forall m n : nat, m = 0 -> P 0 n n) ->
+    (forall m n m' : nat,
+        m = S m' ->
+        P (mod_safe n m') m (gcd (mod_safe n m') m) ->
+        P (S m') n (gcd (mod_safe n m') m)) -> forall m n : nat, P m n (gcd m n).
 
 (* divides m n は、 m が n を割り切る。 *)
 Inductive divides (m : nat) : nat -> Prop :=
