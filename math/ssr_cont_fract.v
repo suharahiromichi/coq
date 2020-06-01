c942397a4101af892d618e3f1be7ba967c96e48e
SUHARA Hiromichi <suhara@falla.vbox>
2020-06-01 22:42:27 +0900

    内容を整理した。

--- math/ssr_cont_fract.v
+++ math/ssr_cont_fract.v
@@ -1,9 +1,4 @@
-(**
-連分数 Continued fraction
-*)
-
 From mathcomp Require Import all_ssreflect.
-From mathcomp Require Import all_algebra.
 Require Import ssromega.
 Require Import FunInd.                      (* Functional Scheme *)
 Require Import Recdef.                      (* Function *)
@@ -18,12 +13,16 @@ Unset Strict Implicit.
 Unset Printing Implicit Defensive.
 (* Set Print All. *)
 
+(**
+# 連分数 Continued fraction
+ *)
 Section CF.
-
-  Program Fixpoint f2cf'' (n m : nat) {wf lt m} : (seq nat) :=
+  
+  (* notu *)
+  Program Fixpoint f2cf'p (n m : nat) {wf lt m} : (seq nat) :=
     match m with
     | 0 => [::]
-    | _ => (n %/ m) :: f2cf'' m (n %% m)
+    | _ => (n %/ m) :: f2cf'p m (n %% m)
     end.
   Obligation 1.
   Proof.
@@ -31,7 +30,7 @@ Section CF.
     move/Lt.neq_0_lt in H.
     apply/ltP/H.
   Qed.
-  Compute f2cf'' 36 11.                     (* [:: 3; 3; 1; 2] *)
+  Compute f2cf'p 36 11.                     (* [:: 3; 3; 1; 2] *)
   
   Function f2cf' (n m : nat) {wf lt m} : (seq nat) :=
     match m with
@@ -44,7 +43,7 @@ Section CF.
         by rewrite ltn_mod.
     - by apply: lt_wf.
   Defined.                                (* Defined が必要である。 *)
-  (* f2fc'_ind が定義されるので、functional inducntion が可能になる。 *)
+  (* functional inducntion が可能になる。 *)
 
   Definition f2cf (p : (nat * nat)) : (seq nat) := f2cf' p.1 p.2.
   Compute f2cf (36, 11).
@@ -52,10 +51,11 @@ Section CF.
   Fixpoint cf2f (sa : seq nat)  : (nat * nat) :=
     match sa with
     | a :: sa' =>
-      let (p1, p2) := cf2f sa' in (a * p1 + p2, p1)
+      (a * (cf2f sa').1 + (cf2f sa').2, (cf2f sa').1)
+    (* let (p1, p2) := cf2f sa' in (a * p1 + p2, p1) *)
     | [::] => (1, 0)
     end.
-  
+
   Compute cf2f [:: 3; 3; 1; 2].             (* (36, 11) *)
   Compute cf2f [:: 0; 1; 1; 1; 1; 1; 1].
   Compute cf2f [:: 1; 2; 2; 2; 2; 2; 2].  
@@ -81,21 +81,63 @@ Section CF.
   Admitted.                                 (* OK *)
 
 (**
-continuant polynomial
+cf2f (f2cf p) = p は証明できない（pが簡約される）ので、
+f2cf (cf2f s) = s を証明する。
+*)  
+  Lemma f2cfE (n m : nat) : f2cf' n m = (n %/ m) :: f2cf' m (n %% m).
+  Proof.
+  Admitted.                                 (* あきらめ *)
 
-Concrete Mathematics
-6.7 CONTINUANTS
-では K_n(・) と表記されている。
+  Search _ ((_ * _ + _) %/ _). 
+  Lemma test1 n m r : (n * m + r) %/ m = n.
+  Proof.
+    rewrite divnMDl.
+    (* n + r %/ m = n *)
+  Admitted.                                 (* あきらめ *)
+
+  Search _ ((_ * _ + _) %% _).   
+  Lemma test2 n m r : (n * m + r) %% m = r.
+  Proof.
+    rewrite modnMDl.
+    (* r %% m = r *)
+  Admitted.                                 (* あきらめ *)
+  
+  Goal forall s, f2cf (cf2f s) = s.
+  Proof.
+    elim.
+    - done.
+    - move=> n s IHs.
+      simpl.
+      rewrite /f2cf /=.
+      rewrite f2cfE /=.
+      rewrite test1.
+      rewrite test2.
+      rewrite /f2cf in IHs.
+      rewrite IHs.
+      done.
+  Qed.
+End CF.
+
+(**
+# continuant polynomial
 
 平方根の連分数とペル方程式
 有澤健治
 https://leo.aichi-u.ac.jp/~keisoken/research/books/book51/book51.pdf
+では H(・) と表記されている。前に追加する。リストによる定義。
+
+Concrete Mathematics
+6.7 CONTINUANTS
+では K(・) と表記されている。後ろに追加する。revによる定義。
  *)
-  Program Fixpoint GaussH' (s : seq nat) {measure (size s)} : nat :=
+Section CP.
+
+  (* notu *)
+  Program Fixpoint GaussHp (s : seq nat) {measure (size s)} : nat :=
     match s with
     | [::] => 1
     | [:: n] => n
-    | n0 :: n1 :: s' => n0 * GaussH' (n1 :: s') + GaussH' s'
+    | n0 :: n1 :: s' => n0 * GaussHp (n1 :: s') + GaussHp s'
     end.
   Obligation 2.
   Proof.
@@ -123,87 +165,47 @@ https://leo.aichi-u.ac.jp/~keisoken/research/books/book51/book51.pdf
   Compute GaussH [:: 3; 1; 2].              (* 11 *)
   Compute cf2f [:: 3; 3; 1; 2].             (* (36, 11) *)
   
-(*
-  letが入ると証明がむつかしくなる。
-*)  
-  Lemma cf2fE n sa :
-    cf2f (n :: sa) =
-    (let (p1, p2) := cf2f sa in (n * p1 + p2, p1)).
+(**
+## 連分数とcontinuant
+*)
+  Lemma cf2fE n0 n1 s :
+    (cf2f [:: n0, n1 & s]).1 = n0 * (cf2f (n1 :: s)).1 + (cf2f s).1.
   Proof.
-    simpl.
     done.
   Qed.
-
-  Goal forall s, let: (n, _) := cf2f s in GaussH s = n.
+  
+  Goal forall s, (cf2f s).1 = GaussH s.
   Proof.
     move=> s.
     functional induction (GaussH s).
     - done.
     - by rewrite /cf2f muln1 addn0.
-    - admit.
-  Admitted.                                 (* OK *)
-  
-  
-  Fixpoint cf2f' (sa : seq nat)  : (nat * nat) :=
-    match sa with
-    | a :: sa' => (a * (cf2f' sa').1 + (cf2f' sa').2, (cf2f' sa').1)
-    | [::] => (1, 0)
-    end.
-
-  Compute cf2f' [:: 3; 3; 1; 2].             (* (36, 11) *)
-  Compute cf2f' [:: 0; 1; 1; 1; 1; 1; 1].
-  Compute cf2f' [:: 1; 2; 2; 2; 2; 2; 2].  
-  
-  Lemma cf2fE' n0 n1 s :
-    (cf2f' [:: n0, n1 & s]).1 = n0 * (cf2f' (n1 :: s)).1 + (cf2f' s).1.
-  Proof.
-    done.
-  Qed.
-  
-  Goal forall s, (cf2f' s).1 = GaussH s.
-  Proof.
-    move=> s.
-    functional induction (GaussH s).
-    - done.
-    - by rewrite /cf2f' muln1 addn0.
     - rewrite -IHn -IHn0.
-      rewrite -cf2fE'.
+      rewrite -cf2fE.
       done.
   Qed.
-
-  Goal forall n s, (cf2f' (n :: s)).2 = GaussH s.
+  
+  Goal forall n s, (cf2f (n :: s)).2 = GaussH s.
   Proof.
     move=> n s.
     functional induction (GaussH s).
     - done.
-    - by rewrite /cf2f' muln1 addn0.
+    - by rewrite /cf2f muln1 addn0.
     - rewrite -IHn0 -IHn1.
-      rewrite -cf2fE'.
+      rewrite -cf2fE.
       done.
   Qed.
   
 (**
-Concrete Mathematics, 6.7 CONTINUANTS (6.128)
-
-黄金比はフィボナッチ数に等しい。
+## continuant の性質
 *)
-  Compute GaussH [::].                      (* 1 = fib 1 *)
-  Compute GaussH [:: 1].                    (* 1 = fib 2 *)
-  Compute GaussH [:: 1; 1].                 (* 2 = fib 3 *)
-  Compute GaussH [:: 1; 1; 1].              (* 3 = fib 4 *)
-  Compute GaussH (nseq 3 1).                (* 3 = fib 4 *)
 
-  Function fib (n : nat) : nat :=
-    match n with
-    | 0 => 0
-    | 1 => 1
-    | (m.+1 as pn).+1 => fib m + fib pn (* fib n.-2 + fib n.-1 *)
-    end.
+  Compute GaussH [:: 1;2;3;4;5].                       (* 225 *)
+  Compute GaussH [:: 5;4;3;2;1].                       (* 225 *)
+  Compute 5 * GaussH [:: 1;2;3;4] + GaussH [:: 1;2;3]. (* 225 *)
 
-  Lemma fibE n : fib n.+2 = fib n + fib n.+1.
-  Proof.
-    done.
-  Qed.
+  Compute (GaussH [:: 1;2;3] * GaussH [:: 4;5]) + (GaussH [:: 1;2] * GaussH [:: 5]).
+  (* 225 *)
 
   Lemma GaussHE (n0 n1 : nat) (s : seq nat) :
     GaussH (n0 :: n1 :: s) = n0 * GaussH (n1 :: s) + GaussH s.
@@ -213,23 +215,6 @@ Concrete Mathematics, 6.7 CONTINUANTS (6.128)
     - done.
     - Admitted.                             (* あきらめ *)
   
-  Goal forall n, GaussH (nseq n 1) = fib n.+1.
-  Proof.
-    move=> n.
-    functional induction (fib n).
-    - done.
-    - done.
-    - rewrite fibE -IHn0 -IHn1.
-      rewrite [nseq _.+2 1]/=.
-      rewrite GaussHE mul1n.
-      rewrite [nseq _.+1 1]/=.
-        by rewrite addnC.
-  Qed.
-  
-  Compute GaussH [:: 1;2;3;4;5].            (* 225 *)
-  Compute (GaussH [:: 1;2;3] * GaussH [:: 4;5]) + (GaussH [:: 1;2] * GaussH [:: 5]).
-  (* 225 *)
-
   Lemma GaussHEr (n0 n1 : nat) (s : seq nat) :
     GaussH (rcons (rcons s n1) n0) = n0 * GaussH (rcons s n1) + GaussH s.
   Proof.
@@ -264,68 +249,40 @@ Concrete Mathematics, 6.7 CONTINUANTS (6.128)
       rewrite IHn IHn0.
       done.
   Qed.
-  
+
 (**
-cf2f (f2cf p) = p は証明できない（pが簡約される）ので、
-f2cf (cf2f s) = s を証明する。
-*)  
-  Lemma f2cfE (n m : nat) : f2cf' n m = (n %/ m) :: f2cf' m (n %% m).
-  Proof.
-  Admitted.
+## 黄金比はフィボナッチ数に等しい。
+*)
+  Compute GaussH [::].                      (* 1 = fib 1 *)
+  Compute GaussH [:: 1].                    (* 1 = fib 2 *)
+  Compute GaussH [:: 1; 1].                 (* 2 = fib 3 *)
+  Compute GaussH [:: 1; 1; 1].              (* 3 = fib 4 *)
+  Compute GaussH (nseq 3 1).                (* 3 = fib 4 *)
 
-  Search _ ((_ * _ + _) %/ _). 
-  Lemma test1 n m r : (n * m + r) %/ m = n.
-  Proof.
-    rewrite divnMDl.
-    (* n + r %/ m = n *)
-  Admitted.
-
-  Search _ ((_ * _ + _) %% _).   
-  Lemma test2 n m r : (n * m + r) %% m = r.
-  Proof.
-    rewrite modnMDl.
-    (* r %% m = r *)
-  Admitted.
+  Function fib (n : nat) : nat :=
+    match n with
+    | 0 => 0
+    | 1 => 1
+    | (m.+1 as pn).+1 => fib m + fib pn (* fib n.-2 + fib n.-1 *)
+    end.
   
-  Goal forall s, f2cf (cf2f' s) = s.
+  Lemma fibE n : fib n.+2 = fib n + fib n.+1.
   Proof.
-    elim.
-    - done.
-    - move=> n s IHs.
-      simpl.
-      rewrite /f2cf /=.
-      rewrite f2cfE /=.
-      rewrite test1.
-      rewrite test2.
-      rewrite /f2cf in IHs.
-      rewrite IHs.
-      done.
+    done.
   Qed.
-
-End CF.
-
-Extraction f2cf'.
-(* 
-let rec f2cf' n = function
-| O -> Nil
-| S n0 -> Cons ((divn n (S n0)), (f2cf' (S n0) (modn n (S n0))))
- *)
-
-Extraction cf2f.
-(* 
-let rec cf2f = function
-| Nil -> Pair ((S O), O)
-| Cons (a, sa') -> let Pair (p1, p2) = cf2f sa' in Pair ((addn (muln a p1) p2), p1)
- *)
-
-Extraction GaussH.
-(* 
-let rec gaussH = function
-| Nil -> S O
-| Cons (n0, l) ->
-  (match l with
-   | Nil -> n0
-   | Cons (n1, s') -> addn (muln n0 (gaussH (Cons (n1, s')))) (gaussH s'))
- *)
+  
+  Goal forall n, GaussH (nseq n 1) = fib n.+1.
+  Proof.
+    move=> n.
+    functional induction (fib n).
+    - done.
+    - done.
+    - rewrite fibE -IHn0 -IHn1.
+      rewrite [nseq _.+2 1]/=.
+      rewrite GaussHE mul1n.
+      rewrite [nseq _.+1 1]/=.
+        by rewrite addnC.
+  Qed.
+End CP.
 
 (* END *)
