5da2a4f47c4b36b464044877d7b3e2921253e996
SUHARA Hiromichi <suhara@falla.vbox>
2020-06-18 20:12:15 +0900

    first commit

---
+++ math/ssr_multiset_coef_rising_fact.v
@@ -0,0 +1,52 @@
+From mathcomp Require Import all_ssreflect.
+Require Import ssromega.
+Require Import ssr_multiset_coefficient.
+Require Import ssr_rising_fact.
+
+Set Implicit Arguments.
+Unset Strict Implicit.
+Unset Printing Implicit Defensive.
+(* Set Print All. *)
+
+Notation "n ^^ m" := (rising_factorial n m)
+(at level 30, right associativity) : nat_scope.
+
+Notation "''H' ( n , m )" := (multiset (n, m))
+(at level 8, format "''H' ( n ,  m )") : nat_scope.
+
+Section RFACT_MC.
+  
+  Lemma msc_small (m : nat) : 0 < m -> 'H(0, m) = 0.
+  Proof.
+    by case: m.
+  Qed.
+  
+  Lemma msc_rfact (n m : nat) : 'H(n, m) * m`! = n ^^ m.
+  Proof.
+    elim: m n => [| m IHm] n.
+    - by rewrite msc0 mul1n.
+    - rewrite factS mulnA ['H(n, m.+1) * m.+1]mulnC.
+      rewrite -mul_msc_diag -mulnA.
+        by rewrite IHm.
+  Qed.
+  
+  Lemma msc_rfactd n m : 'H(n, m) = (n ^^ m) %/ m`!.
+  Proof.
+      by rewrite -msc_rfact mulnK ?fact_gt0.
+  Qed.
+  
+  (* 別証明 *)
+  (* ffact に変換して証明する。 *)
+  
+  Lemma msc_rfactd' n m : 'H(n, m) = (n ^^ m) %/ m`!.  
+  Proof.
+    move: n m => [[| m] | n m] .
+    - done.                                 (* n,m = 0,0 *)
+    - rewrite msc_small; last done.         (* n,m = 0,m.+1 *)
+      rewrite rfact0n /=.
+        by rewrite div0n.
+    - by rewrite rfact_ffact msc_ffactd.    (* n,m = n.+1,m+1 *)
+  Qed.
+End RFACT_MC.
+
+(* END *)
