8c1695b22645c36b245ebbee2e1898cba164c7cc
SUHARA Hiromichi <suhara@falla.vbox>
2020-06-25 21:03:35 +0900

    関数の命名をMathCompに合わせる。

--- ssr/ssr_rev_rev.v
+++ ssr/ssr_rev_rev.v
@@ -2,12 +2,13 @@
 Coq/SSReflect/MathComp による証明の例
 
 2018_07_13 OSC名古屋2018 [ProofCafe proofcafe.connpass.com]
+2020_06_27 ProofCafe #100
 
 線形リストを反転(reverse)するプログラムについて：
 
-[(1)] 二種類のプログラムが同じ結果になることを証明する。
+(1) 二種類のプログラムが同じ結果になることを証明する。
 
-[(2)] 2回実行するともとに戻ることを証明する。
+(2) 2回実行するともとに戻ることを証明する。
 *)
 From mathcomp Require Import all_ssreflect.
 
@@ -22,25 +23,26 @@ Section Rev.
   (** * 二種類のリスト反転のプログラムの定義 *)
   (** ** rcons を使ったプログラム *)
   (* リストの末尾に要素を置く関数を使う。 *)
-  Definition rcons l (a : T) := l ++ [:: a].
-  Fixpoint rev1 (l : seq T) : seq T :=
+  Definition rcons' l (a : T) := l ++ [:: a].
+  
+  Fixpoint reverse (l : seq T) : seq T :=
     match l with
     | nil    => nil
-    | h :: t => rcons (rev1 t) h
+    | h :: t => rcons' (reverse t) h
     end.
   
   (** ** 末尾再帰を使ったプログラム *)
-  Fixpoint revsub (l m : seq T) : seq T :=
+  Fixpoint catrev (l m : seq T) : seq T :=
     match l with
     | [::] => m
-    | x :: l => revsub l (x :: m)
+    | x :: l => catrev l (x :: m)
     end.
-  Definition rev2 (l : seq T) : seq T := revsub l [::].
+  Definition rev (l : seq T) : seq T := catrev l [::].
   
-  (** ** 二種類のプログラムが同じであることの証明 *)
-  (* revsubの第2引数がappendのときの補題 *)
-  Lemma l_rev2_cat_r (l m n : seq T) :
-    revsub l (m ++ n) = revsub l m ++ n.
+  (** (1) 二種類のプログラムが同じであることの証明 *)
+  (* catrevの第2引数がappendのときの補題 *)
+  Lemma l_rev_cat_r (l m n : seq T) :
+    catrev l (m ++ n) = catrev l m ++ n.
   Proof.
     elim: l m => [| x l IHl m] /=.
     - done.
@@ -49,19 +51,19 @@ Section Rev.
       done.
   Qed.
   
-  Theorem rev1_rev2 (l : seq T) : rev1 l = rev2 l.
+  Theorem reverse_rev (l : seq T) : reverse l = rev l.
   Proof.
-    rewrite /rev2.
+    rewrite /rev.
     elim: l => [| a l IHl] /=.
     - done.
-    - rewrite IHl /rcons.
-      rewrite -l_rev2_cat_r.
+    - rewrite IHl /rcons'.
+      rewrite -l_rev_cat_r.
       done.
   Qed.
   
-  (** * 2回実行するともとに戻ることを証明 *)
-  (** ** rev1 について証明 *)
-  Lemma rcons_rev1 (x : T) (l : seq T) : rev1 (rcons l x) = x :: (rev1 l).
+  (** (2) 2回実行するともとに戻ることを証明 *)
+  (** ** reverse について証明 *)
+  Lemma rcons_reverse (x : T) (l : seq T) : reverse (rcons' l x) = x :: (reverse l).
   Proof.
     elim: l => [| x' l IHl] /=.
     - done.
@@ -69,42 +71,42 @@ Section Rev.
       done.
   Qed.
   
-  Theorem rev1_involutive (l : seq T) : rev1 (rev1 l) = l.
+  Theorem reverse_involutive (l : seq T) : reverse (reverse l) = l.
   Proof.
     elim: l => [| n l IHl] /=.
     - done.
-    - rewrite rcons_rev1.
+    - rewrite rcons_reverse.
       rewrite IHl.
       done.
   Qed.
   
-  (** ** rev2 について証明。rev1を経由する例 *)
+  (** ** rev について証明。reverseを経由する例 *)
   (* すでにある定理を再利用する。 *)
-  Theorem rev2_involutive (l : seq T) : rev2 (rev2 l) = l.
+  Theorem rev_involutive (l : seq T) : rev (rev l) = l.
   Proof.
-    rewrite -!rev1_rev2.
-    apply: rev1_involutive.
+    rewrite -!reverse_rev.
+    apply: reverse_involutive.
   Qed.
   
-  (** ** rev2 について証明。直接証明する例 *)
-  (* revsubの第1引数がappendのときの補題 *)
-  Lemma l_rev2_cat_l (l m n : seq T) :
-    revsub (l ++ m) n = revsub m [::] ++ revsub l n.
+  (** ** rev について証明。直接証明する例 *)
+  (* catrevの第1引数がappendのときの補題 *)
+  Lemma l_rev_cat_l (l m n : seq T) :
+    catrev (l ++ m) n = catrev m [::] ++ catrev l n.
   Proof.
     elim: l n => [n | a l IHl n] /=.
-    - rewrite -l_rev2_cat_r.
+    - rewrite -l_rev_cat_r.
       done.
     - rewrite IHl.
       done.
   Qed.
   
-  Theorem rev2_involutive' (l : seq T) : rev2 (rev2 l) = l.
+  Theorem rev_involutive' (l : seq T) : rev (rev l) = l.
   Proof.
-    rewrite /rev2.
+    rewrite /rev.
     elim: l => [| a l IHl] /=.
     - done.
-    - rewrite (l_rev2_cat_r l [::] [:: a]).
-      rewrite (l_rev2_cat_l (revsub l [::]) [:: a] [::]).
+    - rewrite (l_rev_cat_r l [::] [:: a]).
+      rewrite (l_rev_cat_l (catrev l [::]) [:: a] [::]).
       rewrite IHl.
       done.
   Qed.
@@ -129,14 +131,10 @@ Mathcomp/Coq などの定理証明器（定理証明支援システム）の使�
 プログラムや数学の問題の証明のしかたを勉強する会です。
 あらかじめ指定したテキストをもとに、実際に証明をしながら進めていきます。
 
-第3土曜日の午後、名古屋市内で開催します。
+第4土曜日の午後、名古屋市内、または、オンラインで開催します。
 参加費は無料です。できるだけPCを持参してください。
 
 [https://proofcafe.connpass.com] 開催案内
-
-##ProofCafe
-
-[https://twitter.com/ProofCafe] (bot)
 *)
 
 (**
