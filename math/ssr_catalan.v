48216b4132f57b99c22783ae493e2fb7f0b2b8cb
SUHARA Hiromichi <suhara@falla.vbox>
2020-06-28 15:44:13 +0900

    証明を整理した。

--- math/ssr_catalan.v
+++ math/ssr_catalan.v
@@ -1,88 +1,116 @@
+(**
+カタラン数をCoqで語らん
+========================
+
+@suharahiromichi
+
+2020/06/28
+*)
+
+(**
+# はじめに
+
+Coq/MathComp には binomial.v というパッケージがあって、
+二項係数（順列・組合せの「組合せ」）の定義と
+基本的ないくつかの補題が証明されています。これを使ってなにか証明してみましょう。
+
+二項係数を使って定義される、カタラン数 (Catalan Number) というものがあるります。
+nが自然数のとき、以下で定義されます。
+
+```math
+
+c_n =
+\frac{1}{n + 1}\dbinom{2 n}{n}
+```
+
+これの意味については、「カタラン数を語らん」としているサイト（文献[1]）に譲ります。
+
+ここでは、n番めのカタラン数について、
+
+```math
+
+c_n = \dbinom{2 n}{n} - \dbinom{2 n}{n - 1}、ただし 0 \lt n
+```
+
+が成り立つことを証明したい思います。実際には、上記ふたつの式を整理した、
+
+```math
+n \dbinom{2 n}{n} = (n + 1) \dbinom{2 n}{n - 1}、ただし 0 \lt n
+
+```
+
+を証明します。
+*)
+
+(**
+以下では、``n``、``m`` を（0以上の）自然数とします。また、MathCompの表記にあわせて、
+二項係数 $\dbinom{n}{m}$ を ``C(n, m)`` で、
+``n | m`` で「m は n で割り切れる」を示す（除数のほうが前）ことにします。
+*)
+
 From mathcomp Require Import all_ssreflect.
 Require Import ssromega.
-Require Import Recdef.                      (* Function *)
-Require Import Wf_nat.                      (* wf *)
-Require Import Program.Wf.                  (* Program wf *)
-
-Require Import ssr_multiset_coefficient.
 
 Set Implicit Arguments.
 Unset Strict Implicit.
 Unset Printing Implicit Defensive.
-(* Set Print All. *)
 
 (**
-# Catalan Number カタラン数
+# 補題
 *)
-
-Section DEFINE.
-
-  Definition catalan n := 'C(n.*2, n) %/ n.+1.
-  
-  Compute catalan 0.                          (* 1 *)
-  Compute catalan 1.                          (* 1 *)
-  Compute catalan 2.                          (* 2 *)
-  Compute catalan 3.                          (* 5 *)
-  
-  Fixpoint catalan_rec n {struct n} :=
-    match n with
-    | 0 => 1
-    | n'.+1 => \sum_(0 <= i < n'.+1)(catalan_rec (n' - (i + n')) * catalan_rec (n' - i))
-    end.
-  
-  Compute catalan_rec 0.
-  Compute catalan_rec 1.
-  Compute catalan_rec 2.
-
-End DEFINE.
-
 Section LEMMAS.
 
-  Lemma test n : 0 < n -> 0 < n.*2.
+(**
+# ``n! = n * (n - 1)!`` 、ただし ``0 < n``
+
+階乗の定義から自明ですが、MathComp では ``(n + 1) = (n + 1) * n!``
+でしか証明されていないので、証明しておきます。
+*)  
+  Lemma fact_pred n : 0 < n -> n`! = n * n.-1`!.
   Proof.
-    by rewrite double_gt0.
-  Qed.
-  
-  Lemma test2 n : n.*2 - n = n.
-  Proof.
-    rewrite -addnn.
-    ssromega.
-  Qed.
-  
-  Check factS : forall n : nat, n.+1`! = n.+1 * n`!.
-  Lemma test3 n : 0 < n -> n`! = n * n.-1`!.
-  Proof.
-    move=> H.
-    have Hf :(n.-1.+1)`! = n.-1.+1 * (n.-1)`! by apply: factS.
-    rewrite prednK in Hf; last done.
+    move=> Hn.
+    have H := factS n.-1.
+    rewrite prednK in H; last done.
     done.
   Qed.
   
-  Lemma test4 n : n.*2 - n.+1 = n.-1.
+(**
+## ``n! * m! | (n + m)!``
+
+さらに、階乗についての補題を証明します。
+ちょっと直観に反する補題ですが、二項係数の定義から導くことができます。
+また、便利です。
+*)  
+  Lemma divn_fact_mul_add_fact (n m p : nat) : n + m = p -> n`! * m`! %| p`!.
   Proof.
-    rewrite -addnn.
-    ssromega.
-  Qed.
-  
+    move=> <-.
+    have H : 'C(n + m, m) * (n`! * m`!) = (n + m)`!.
+    - rewrite -(@bin_fact (n + m) m); last by rewrite leq_addl.
+      rewrite -[n + m - m]addnBA; last by rewrite leqnn.
+        by rewrite subnn addn0 [m`! * n`!]mulnC.
+    - rewrite -H.
+      rewrite -{1}[n`! * m`!]mul1n.
+        by apply: dvdn_mul.
+  Qed.  
+
   Lemma test5 n : 0 < n -> (n * n.-1`! * n`!) %| n.*2`!.
   Proof.
     move=> H.
-    rewrite -test3; last done.
-    rewrite -addnn.
-      by rewrite divn_fact_mul_add_fact.
+    rewrite -fact_pred; last done.
+    rewrite divn_fact_mul_add_fact; first done.
+      (* n + n = n.*2 *)
+      by rewrite -addnn.
   Qed.
 
-  Lemma test0 n m p : n + m = p -> n`! * m`! %| p`!.
-  Proof.
-    move=> <-.
-    by apply: divn_fact_mul_add_fact.
-  Qed.
-  
   Lemma test6 n : 0 < n -> n.+1 * n`! * n.-1`! %| n.*2`!.
   Proof.
     move=> H.
-    rewrite -[n.+1 * n`!]test3; last done.
-    apply: test0.
+    rewrite -[n.+1 * n`!]fact_pred; last done.
+    apply: divn_fact_mul_add_fact.
+    (* n.+1 + n.-1 = n.*2 *)
+    have -> : n.+1 + n.-1 = n + n by ssromega.
+    by rewrite -addnn.
+(*
     rewrite -subn1.
     rewrite addnBA; last done.
     rewrite -addn1.
@@ -91,7 +119,7 @@ Section LEMMAS.
     rewrite subn1.
     Search _.+1.-1.
     rewrite -[(n + n).+1.-1]pred_Sn.
-    rewrite addnn.
+  *)
     done.
   Qed.
 
@@ -116,39 +144,71 @@ Section LEMMAS.
 方針
 左辺と右辺それぞれに、
 *)
-    Check muln_divA : forall d m n : nat, d %| n -> m * (n %/ d) = (m * n) %/ d.
+  Check muln_divA : forall d m n : nat, d %| n -> m * (n %/ d) = (m * n) %/ d.
 (**
 で、(N * X) %/ (N * Y) のかたちにする。
 *)    
-    Check divnMl : forall p m d : nat, 0 < p -> (p * m) %/ (p * d) = m %/ d.
+  Check divnMl : forall p m d : nat, 0 < p -> (p * m) %/ (p * d) = m %/ d.
 (**
 で、それを消す。
 *)    
 
-  Goal forall n, 0 < n -> n * 'C(n.*2, n) = n.+1 * 'C(n.*2, n.+1).
+ Theorem catalan_rel n : 0 < n -> n * 'C(n.*2, n) = n.+1 * 'C(n.*2, n.+1).
   Proof.
-    move=> n Hn.
-
+    move=> Hn.
+    
     (* LHS *)
-    rewrite bin_factd; last by rewrite test.
-    rewrite test2.
-    rewrite {1}[in n`! * n`!]test3; last done.
+    rewrite bin_factd; last by rewrite double_gt0.
+    have -> : n.*2 - n = n by rewrite -addnn; ssromega.
+    rewrite {1}[in n`! * n`!]fact_pred; last done.
     (* (n * X) %/ (n * Y) にする。 *)
     rewrite muln_divA; last by rewrite test5.
     rewrite -mulnA divnMl; last done.
     rewrite [(n.-1)`! * n`!]mulnC.
     
     (* RHS *)
-    rewrite bin_factd; last by rewrite test.
-    rewrite test4.
+    rewrite bin_factd; last by rewrite double_gt0.
+    have -> : n.*2 - n.+1 = n.-1 by rewrite -addnn; ssromega.
     rewrite factS.
     (* (n.+1 * X) %/ (n.+1 * Y) にする。 *)
     rewrite muln_divA; last by rewrite test6.
     rewrite -mulnA divnMl; last done.
-    
     done.
   Qed.
 
 End LEMMAS.
+
+(**
+# 文献
+
+[1] カタラン数を語らん、https://www.google.com/search?&q="カタラン数を語らん"
+*)
   
+(**
+# おまけ
+
+Catalan Number カタラン数
+*)
+
+Section DEFINE.
+
+  Definition catalan n := 'C(n.*2, n) %/ n.+1.
+  
+  Compute catalan 0.                          (* 1 *)
+  Compute catalan 1.                          (* 1 *)
+  Compute catalan 2.                          (* 2 *)
+  Compute catalan 3.                          (* 5 *)
+  
+  Fixpoint catalan_rec n {struct n} :=
+    match n with
+    | 0 => 1
+    | n'.+1 => \sum_(0 <= i < n'.+1)(catalan_rec (n' - (i + n')) * catalan_rec (n' - i))
+    end.
+  
+  Compute catalan_rec 0.
+  Compute catalan_rec 1.
+  Compute catalan_rec 2.
+
+End DEFINE.
+
 (* END *)
