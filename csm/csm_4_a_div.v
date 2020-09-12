559056353be777dd4dff3c43dfa043a5b3bc634e
SUHARA Hiromichi <suhara@falla.vbox>
2020-09-12 10:08:31 +0900

    説明を見直した。

--- csm/csm_4_a_div.v
+++ csm/csm_4_a_div.v
@@ -290,6 +290,16 @@ Section GCDLCM.
 End GCDLCM.  
 
 (**
+名古屋大学の講義で GCD の証明が取り上げられています。
+ここで GCD は、MathCompとは別の定義の、清礎帰納法で定義しています。
+
+この演習問題 gcd_divides は、MathCompの補題 dvdn_gcdl と dvdn_gcdr、
+gcd_max は、dvdn_gcd に対応します。
+
+[https://www.math.nagoya-u.ac.jp/~garrigue/lecture/2019_SS/ssrcoq5.pdf]
+*)
+
+(**
 # 互いに素 (coprime, relatively prime)
 *)
 (**
@@ -395,6 +405,10 @@ Section Modulo.
 ### 合同式の加算
 
 「合同な要素を足しても、合同関係は崩れない。」
+
+$$ m = n \pmod{d} \Longrightarrow 
+   p = q \pmod{d} \Longrightarrow 
+   m + p = n + q \pmod{d} $$
  *)  
   Lemma m_addn m n p q d  :
     m = n %[mod d] -> p = q %[mod d] -> m + p = n + q %[mod d].
@@ -418,6 +432,10 @@ Section Modulo.
 ### 合同式の乗算
 
 「掛け算もうまくいく。」
+
+$$ m = n \pmod{d} \Longrightarrow 
+   p = q \pmod{d} \Longrightarrow 
+   m p = n q \pmod{d} $$
  *)  
   Lemma m_muln m n p q d  :
     m = n %[mod d] -> p = q %[mod d] -> m * p = n * q %[mod d].
@@ -445,6 +463,8 @@ Section Modulo.
 ### 合同式のべき乗
 
 「掛け算の性質を繰り返し適用すると、べき乗することもできる。」
+
+$$ p = q \pmod{d} \Longrightarrow p^{m} = q^{m} \pmod{d} $$
  *)  
   Lemma m_exprn p q m d :
     p = q %[mod d] -> p^m = q^m %[mod d].
@@ -461,8 +481,9 @@ Section Modulo.
 (**
 ### 合同式の除算（式(4.37)）
 
-p と d が「互いに素の場合には、合同関係のもとでも割り算ができる。」
+「(p と d が)互いに素の場合には、(dを法とする)合同関係のもとでも(pによる)割り算ができる。」
 
+$$ m p = n p \pmod{d} \Longleftrightarrow m = n \pmod{d}, 但し p \perp d $$
 
 説明：まず、[1]の式(4.37) の→を証明します。
 拡張したGCDを使用するので [3] の chinese_modl 補題の証明、および、
@@ -471,36 +492,39 @@ p と d が「互いに素の場合には、合同関係のもとでも割り算
   Lemma m_divn_d_1 m n p d :
     coprime p d -> m * p = n * p %[mod d] -> m = n %[mod d].
   Proof.
-    move/eqP.                                 (* gcdn p d = 1 *)
+    rewrite /coprime => /eqP.               (* 前提を gcdn p d = 1 *)
+    
     (* p = 0 と 0 < p に場合分けする。 *)
     case: (posnP p).
     
-    (* p = 0 の場合、d = 1 でもある。 *)
+    (* p = 0 の場合 *)
     - move=> ->.                            (* p = 0 で書き換える。 *)
-      rewrite gcd0n.
+      rewrite gcd0n.                        (* 前提から d = 1 でもある。 *)
       move=> ->.                            (* d = 1 で書き換える。 *)
         by rewrite !modn1.
         
     (* 0 < p の場合 *)
-  - move=> p_gt0 Hco.       (* 0 < p 条件と、coprime条件をpopする。 *)
-    case: (@egcdnP p d p_gt0).
-    rewrite Hco.
-    move=> p' d' Hdef _ H.
-    (* 不定方程式 ``Hdef : p' * p = d' * d + 1`` を展開した状態である。 *)
+    - move=> p_gt0 Hco.     (* 0 < p 条件と、coprime条件を intro する。 *)
+      Check @egcdnP p d p_gt0 : egcdn_spec p d (egcdn p d).
+      case: (@egcdnP p d p_gt0). (* 拡張ユーグリッドの互除法の定義を前提に積む。 *)
+      (* egcdn_spec は inductive に定義されているので、条件がひとつでも case が要る。 *)
+      rewrite Hco.
+      move=> p' d' Hdef _ H.
+      (* ``Hdef : p' * p = d' * d + 1`` は不定方程式を展開した状態である。 *)
     
-    (* H の 両辺に p' をかける。 p' が ``1 / p`` のような働きをする。 *)
-    Check @m_muln' (m * p) (n * p) p' d
-      : m * p = n * p %[mod d] -> m * p * p' = n * p * p' %[mod d].
-    move/(@m_muln' (m * p) (n * p) p' d) in H.
-    
-    (* 不定方程式 ``Hdef`` を H に代入して整理する。  *)
-    rewrite -2!mulnA -[p * p']mulnC in H.
-    rewrite Hdef in H.
-      by rewrite 2!mulnDr 2!muln1 2!mulnA 2!modnMDl in H.
+      (* H の 両辺に p' をかける。 p' が p の逆数のような働きをする。 *)
+      Check @m_muln' (m * p) (n * p) p' d
+        : m * p = n * p %[mod d] -> m * p * p' = n * p * p' %[mod d].
+      move/(@m_muln' (m * p) (n * p) p' d) in H.
+      
+      (* 不定方程式 ``Hdef`` を H に代入して整理する。 *)
+      rewrite -2!mulnA -[p * p']mulnC in H.
+      rewrite Hdef in H.
+        by rewrite 2!mulnDr 2!muln1 2!mulnA 2!modnMDl in H.
   Qed.
   
 (**
-[1]の式(4.37) の←は、掛け算の合同の公式から明らかです。
+説明：[1]の式(4.37) の←は、掛け算の合同の公式から明らかです。後の証明では不使用です。
 *)
   Lemma m_divn_d' m n p d :
     coprime p d -> (m * p = n * p %[mod d] <-> m = n %[mod d]).
@@ -526,52 +550,42 @@ MathCompらしく、bool値の同値で証明しておきます。
 (**
 ### 合同式の除算（式(4.38)、法を割る）
 
-「合同関係で割り算をするもうひとつの方法は、
-法とする自身も他の数と同じように割ることである。。。。
+「合同関係で割り算をするもうひとつの方法は、法とする自身も他の数と同じように割ることである。
 これは、modの分配則だけに依存している。」
+
+$$ m d1 = n d1 \pmod{d2 d1} \Longleftrightarrow m = n \pmod{d2}, 但し 0 \lt d1 $$
  *)  
   Lemma m_divn_dp m n d1 d2 :
     0 < d1 -> (m * d1 == n * d1 %[mod d2 * d1]) = (m == n %[mod d2]).
   Proof.
     move=> Hd1.
-    rewrite -[RHS](eqn_pmul2r Hd1).
-    rewrite 2!(muln_modl Hd1).
-    done.
+    rewrite -[RHS](eqn_pmul2r Hd1). (* 右辺の両辺にd1を掛ける。d1≠0なので可能である。 *)
+    
+    (* d1による）剰余の分配則を適用する。これも d1≠0なので可能である。 *)
+    Check @muln_modl d1 : forall m d : nat, 0 < d1 -> m %% d * d1 = (m * d1) %% (d * d1).
+      by rewrite 2!(muln_modl Hd1).
   Qed.
-
+  
 (**
 ### 最大公約数を法とする合同式（式(4.41)）
 
+$$ m = n \pmod{lcm(d1,d2)} \Longleftrightarrow \\
+   m = n \pmod{d1} \ かつ\ m = n \pmod{d2} $$
+
 説明：まず、最大公約数とdivisibleの関係を使いやすい補題にしておきます。
  *)
-  Check dvdn_lcm
-    : forall d1 d2 m : nat, (lcmn d1 d2 %| m) = (d1 %| m) && (d2 %| m).
-  
   Lemma lcmn_dvdn d1 d2 m : lcmn d1 d2 %| m -> d1 %| m.
   Proof.
-    Check dvdn_lcm d1 d2 m.
+    Check dvdn_lcm d1 d2 m : (lcmn d1 d2 %| m) = (d1 %| m) && (d2 %| m).
     rewrite dvdn_lcm => /andP.
       by case.
   Qed.
-
+  
   Lemma dvdn_lcmn d1 d2 m : d1 %| m -> d2 %| m -> lcmn d1 d2 %| m.
   Proof.
-    Check dvdn_lcm d1 d2 m.
     rewrite dvdn_lcm => H1 H2.
     apply/andP.
       by split.
-  Qed.
-  
-(**
-``eqn_mod_dvd : (m == n %[mod d]) = (d %| (m - n))`` の補題を使って証明する
-ために、``n <= m`` と m < n`` で場合分けします。このとき、
-後者の条件を ``m <= n`` に変形することで、場合分け後の証明を共通にすることができます。
-その変形をする補題を証明しておきます。
-*)
-  Lemma le_m_n m n : (n <= m) = false -> m <= n.
-  Proof.
-    move=> Hmn.
-      by ssromega.
   Qed.
   
 (**
@@ -580,16 +594,29 @@ MathCompらしく、bool値の同値で証明しておきます。
   Lemma m_divn_lcmn_1_1_1 m n d1 d2 :
     n <= m -> m = n %[mod lcmn d1 d2] -> m = n %[mod d1].
   Proof.
-    move=> Hnm H.
-    apply/eqP.
     Check eqn_mod_dvd
       : forall d m n : nat, n <= m -> (m == n %[mod d]) = (d %| m - n).
+    
+    move=> Hnm /eqP H.
+    rewrite eqn_mod_dvd in H; last done.
+    move/lcmn_dvdn in H.
+    
+    apply/eqP.
     rewrite eqn_mod_dvd; last done.
-    move/eqP in H.
-      rewrite eqn_mod_dvd in H; last done.
-        by move/lcmn_dvdn in H.
+    done.
   Qed.
-
+  
+(**
+自然数の減算 ``m - n`` を含む証明の場合、``n <= m`` と ``m < n`` で場合分けします。
+しかし ``m < n -> m <= n`` なので、後者の条件を ``m <= n`` に変形できれば、
+同じ補題が使えるわけです。その変形をする補題を証明しておきます。
+*)
+  Lemma le_m_n m n : (n <= m) = false -> m <= n.
+  Proof.
+    move=> Hmn.
+      by ssromega.
+  Qed.
+  
 (**
 証明の中核となる部分です。場合分けしてそれぞれに共通部分を適用します。
 *)
@@ -601,7 +628,7 @@ MathCompらしく、bool値の同値で証明しておきます。
     (* n <= m である場合 *)
     - by apply: m_divn_lcmn_1_1_1 H.
       
-    (* m <= n である場合 *)
+    (* m < n である場合 *)
     - move/le_m_n in Hnm.       (* n > m を m <= n にする。 *)
       apply/esym.               (* 合同式の左辺と右辺を入れ替える。 *)
       move/esym in H.           (* 合同式の左辺と右辺を入れ替える。 *)
@@ -615,7 +642,7 @@ MathCompらしく、bool値の同値で証明しておきます。
     m = n %[mod lcmn d1 d2] -> m = n %[mod d1] /\ m = n %[mod d2].
   Proof.
     split.
-    Check @m_divn_lcm_1_1 m n d1 d2.
+    Check @m_divn_lcm_1_1 m n d1 d2 : m = n %[mod lcmn d1 d2] -> m = n %[mod d1].
     - by apply: m_divn_lcm_1_1 H.
     - rewrite lcmnC in H.
         by apply: m_divn_lcm_1_1 H.
@@ -627,13 +654,14 @@ MathCompらしく、bool値の同値で証明しておきます。
   Lemma m_divn_lcm_2_1 m n d1 d2 :
     n <= m -> m = n %[mod d1] -> m = n %[mod d2] -> m = n %[mod lcmn d1 d2].
   Proof.
-    move=> Hnm H1 H2.
-    apply/eqP.
-    rewrite eqn_mod_dvd; last done.
-    move/eqP in H1.
-    move/eqP in H2.
+    move=> Hnm /eqP H1 /eqP H2.
     rewrite eqn_mod_dvd in H1; last done.
     rewrite eqn_mod_dvd in H2; last done.
+    
+    apply/eqP.
+    rewrite eqn_mod_dvd; last done.
+    
+    Check dvdn_lcmn : forall d1 d2 m : nat, d1 %| m -> d2 %| m -> lcmn d1 d2 %| m.
       by apply: dvdn_lcmn.
   Qed.
   
@@ -645,12 +673,16 @@ MathCompらしく、bool値の同値で証明しておきます。
   Proof.
     move=> H1 H2.
     case Hnm : (n <= m).
-    - by apply: m_divn_lcm_2_1.
-    - move/le_m_n in Hnm.       (* n > m を m <= n にする。 *)
-      apply/esym.               (* 合同式の左辺と右辺を入れ替える。 *)
-      move/esym in H1.          (* 合同式の左辺と右辺を入れ替える。 *)
-      move/esym in H2.          (* 合同式の左辺と右辺を入れ替える。 *)
-        by apply: m_divn_lcm_2_1.
+    (* n <= m の場合 *)
+    - by apply: m_divn_lcm_2_1.       (* 先の補題がそのまま使える。 *)
+      
+    (* m < n の場合 *)      
+    - move/le_m_n in Hnm.               (* n > m を m <= n にする。 *)
+      (* ゴールと条件の合同式の左辺と右辺を入れ替える。 *)
+      apply/esym.
+      move/esym in H1.
+      move/esym in H2.
+        by apply: m_divn_lcm_2_1.     (* 先の補題が使えるようになった。 *)
   Qed.
   
 (**
@@ -671,6 +703,9 @@ MathCompらしく、bool値の同値で証明しておきます。
 (**
 ### 中国人の剰余定理の特別な場合（式(4.42)）
 
+$$ m = n \pmod{d1 d2} \Longleftrightarrow \\
+   m = n \pmod{d1}\ かつ\ m = n \pmod{d2}, 但し d1 \perp d2 $$
+
 説明： 互いに素なら、LCMは積であることを証明します。
 補題 ``muln_lcm_gcd m n : lcmn m n * gcdn m n = m * n`` を使います。
 *)  
@@ -678,8 +713,7 @@ MathCompらしく、bool値の同値で証明しておきます。
   Proof.
     rewrite /coprime.
     move=> /eqP Hco.
-    Check muln_lcm_gcd :
-      forall m n : nat, lcmn m n * gcdn m n = m * n.
+    Check muln_lcm_gcd : forall m n : nat, lcmn m n * gcdn m n = m * n.
       by rewrite -muln_lcm_gcd Hco muln1.
   Qed.
 
@@ -698,8 +732,7 @@ MathCompらしく、bool値の同値で証明しておきます。
   Qed.
   
 (**
-より一般的な証明は、``div.v`` の ``chinese_mod`` を参照してください。
-ただし導き方は異なります。
+中国人の剰余定理のより一般的な証明は、``div.v`` の ``chinese_mod`` を参照してください。
  *)
 End Modulo.
 
