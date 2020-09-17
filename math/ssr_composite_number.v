8a9a6ed3a43ede095736f4aa7a7ccd9d0126963b
SUHARA Hiromichi <suhara@falla.vbox>
2020-09-17 22:30:44 +0900

    証明を見直す。「１を越える」に統一する。

--- math/ssr_composite_number.v
+++ math/ssr_composite_number.v
@@ -1,17 +1,29 @@
 (**
+メルセンヌ数の基本定理の証明
+==================
 
-======
+2020_9_19 @suharahiromichi
+*)
 
-2020_8_22 @suharahiromichi
+(**
+# はじめに
+
+「pが素数のとき、メルセンヌ数 $M_{p}$ も素数である」という、
+メルセンヌ数の基本定理を証明します。
+この命題は、「逆」が成り立たないことで有名ですが、
+実際の証明では対偶を証明することが簡単です。
+
+
+ソースコードは以下にあります。
+
+https://github.com/suharahiromichi/coq/blob/master/math/ssr_composite_number.v
  *)
 
 From mathcomp Require Import all_ssreflect.
-From mathcomp Require Import bigop matrix.
 
 Require Import ssromega.
 (**
-[https://github.com/suharahiromichi/coq/blob/master/common/ssromega.v]
-
+https://github.com/suharahiromichi/coq/blob/master/common/ssromega.v
 
 もダウンロードして同じディレクトリに置いて、coqc ssromega.v を実行し、
 ssromega.vo ができていることを確認してください。
@@ -27,20 +39,19 @@ Unset Printing Implicit Defensive.
  自然数 n が合成数なら、メルセンヌ数 $ M_{n} = 2^{n} - 1 $
 も合成数であることを証明してください。
 
-実際には、nが、2以上の任意の2自然数に積であるとき（すなわち合成数であるとき）、
+実際には、nが、1を越える任意の2自然数に積であるとき（すなわち合成数であるとき）、
 $ 2^{n} -1 $ もふたつの自然数の積（合成数）であることを証明します。
 
 また、合成数の定義は、「それより小さい、ふたつの正の整数の積で表される整数」
-ですが、ここでは、「ふたつの 2以上の自然数 の積で表される自然数」としています。
+ですが、ここでは、「ふたつの 1を越える自然数 の積で表される自然数」としています。
 これは同値です（おまけを参照）。
-
-
-[a] https://ja.wikipedia.org/wiki/メルセンヌ数
  *)
 (**
 # 証明
 
 ## bigop の補題
+
+MathComp の ``bigop.v`` の補題のうちのいくつかを使いやすい命題にしておきます。
 *)
 Section BigOp.
   Lemma eq_sum m n a b : a =1 b ->
@@ -75,6 +86,7 @@ Section BigOp.
     move=> Hmn.
       by rewrite big_nat_recr.
   Qed.
+End BigOp.
 
 (**
 ## 補題
@@ -84,6 +96,8 @@ Section BigOp.
 
 $$ (2^{b} - 1) \sum_{i=0}^{a-1} 2^{i b} = 2^{a b} - 1, ただし 1 \le a $$
 *)
+Section Composite_Number.
+  
   Lemma l_e2_ab_1 a b :
     1 <= a ->
     (2^b - 1) * (\sum_(0 <= i < a) 2^(i * b)) = 2^(a * b) - 1.
@@ -95,9 +109,8 @@ $$ (2^{b} - 1) \sum_{i=0}^{a-1} 2^{i b} = 2^{a b} - 1, ただし 1 \le a $$
     
     (* 左辺、第1項 *)
     rewrite -sum_distrr //=.
-    have H : \sum_(0 <= i < a) 2 ^ b * 2 ^ (i * b) = \sum_(0 <= i < a) 2 ^ (i.+1 * b).
+    have -> : \sum_(0 <= i < a) 2 ^ b * 2 ^ (i * b) = \sum_(0 <= i < a) 2 ^ (i.+1 * b)
       by apply: eq_sum => i; rewrite -expnD mulnC -mulnS mulnC.
-    rewrite H.
     rewrite -(sum_add1 a (fun x => 2 ^ (x * b))).
     rewrite [\sum_(1 <= i < a.+1) 2 ^ (i * b)]sum_last //=.
     (* \sum_(1 <= i < a) 2 ^ (i * b) + 2 ^ (a * b) *)
@@ -116,17 +129,16 @@ $$ (2^{b} - 1) \sum_{i=0}^{a-1} 2^{i b} = 2^{a b} - 1, ただし 1 \le a $$
     (* 左辺と右辺が同じ。 *)
     done.
   Qed.
-End BigOp.
 
 (**
 ## 証明の概要
 
 証明したい命題
-「nが、2以上の任意の2自然数に積であるとき
+「nが、1を越える任意の2自然数に積であるとき
 （すなわち合成数であるとき）、$2^{n} - 1$ もふたつの自然数の積（合成数）である」
 
 これをもうすこし噛み砕くと、
-aとbが2以上の任意の自然数であるとき、適当な2以上の自然数xとyが存在して、
+aとbが1を越える任意の自然数であるとき、適当な1を越える自然数xとyが存在して、
 
 $$ 2^{a b} - 1 = x y $$
 
@@ -150,17 +162,15 @@ $$ (2^{b} - 1) \sum_{i=0}^{a-1}2^{i b} = 2^{a b} - 1 $$
 ## 値の範囲
 
 普通は、これで「証明終わり。」とするのですが、
-2以上のaとbに対して、xとyもまた2以上であることを示すことも忘れてはいけません。
+1を越えるaとbに対して、xとyもまた1を越えるであることを示すことも忘れてはいけません。
 それも、別の補題として証明しておきます。
 *)  
-Section Composite_Number.
-  
   (* 何か所かで使う補題。 *)
-  Lemma le2_le1 a : 2 <= a -> 1 <= a.       (* 1 < a -> 0 < a *)
-  Proof. move=> H. by rewrite ltnW. Qed.    (* ssromega でも解ける。 *)
+  Lemma lt1_le1 a : 1 < a -> 1 <= a.       (* 1 < a -> 1 <= a *)
+  Proof. move=> H. by rewrite ltnW. Qed.   (* ssromega でも解ける。 *)
   
-  (* 2 <= x を証明する補題： *)
-  Lemma e2b_1_ge2 b : 2 <= b -> 2 <= 2^b - 1. (* 1 < b -> 1 < 2^b - 1 *)
+  (* 1 < x を証明する補題： *)
+  Lemma e2b_1_ge2 b : 1 < b -> 1 < 2^b - 1. (* 1 < b -> 1 < 2^b - 1 *)
   Proof.
     move=> H.
     rewrite ltn_subRL addn1.
@@ -168,16 +178,16 @@ Section Composite_Number.
       by rewrite ltn_exp2l.
   Qed.
   
-  (* 2 <= y を証明する補題： *)  
+  (* 1 < y を証明する補題： *)  
   Lemma sum0_2_e2ib a b :
-    2 <= a -> 2 <= b -> 2 <= \sum_(0 <= i < a) 2 ^ (i * b).
+    1 < a -> 1 < b -> 1 < \sum_(0 <= i < a) 2 ^ (i * b).
   Proof.
     move=> Ha Hb.
-    rewrite sum_first; last by apply: le2_le1.
+    rewrite sum_first; last by apply: lt1_le1.
     rewrite sum_first; last done.
     have H1 : 1 <= 2 ^ (0 * b) by rewrite mul0n expn0.
     have H2 : 1 <= 2 ^ (1 * b) by rewrite mul1n expn_gt0 orb_idr.
-    have H3 : 0 <= \sum_(2 <= i < a) 2 ^ (i * b) by done. (* 0以上は自明。 *)
+    have H3 : 0 <= \sum_(1 <= i < a) 2 ^ (i * b) by done. (* 0以上は自明。 *)
       by ssromega.
   Qed.
   
@@ -185,15 +195,15 @@ Section Composite_Number.
 ## 証明したいもの
  *)
   Lemma mersenne_composite (a b : nat) :
-    2 <= a -> 2 <= b ->
-    exists (x y : nat), 2 <= x /\ 2 <= y /\ (x * y = 2^(a * b) - 1).
+    1 < a -> 1 < b ->
+    exists (x y : nat), 1 < x /\ 1 < y /\ (x * y = 2^(a * b) - 1).
   Proof.
     move=> Ha Hb.
     exists (2 ^ b - 1), (\sum_(0 <= i < a) 2 ^ (i * b)).
     split ; [| split].
-    - by apply: e2b_1_ge2.    (* 2 <= x を証明する。 *)
-    - by apply: sum0_2_e2ib.  (* 2 <= y を証明する。 *)
-    - move/le2_le1 in Ha.     (* 前提を 2 <= a から 1 <= a にする。 *)
+    - by apply: e2b_1_ge2.    (* 1 < x を証明する。 *)
+    - by apply: sum0_2_e2ib.  (* 1 < y を証明する。 *)
+    - move/lt1_le1 in Ha.     (* 前提を 1 < a から 1 <= a にする。 *)
         by apply: l_e2_ab_1.  (* x * y = ... を証明する。 *)
   Qed.
 
@@ -203,14 +213,6 @@ Section Composite_Number.
 「ある自然数が、より小さいふたつの自然数の積で表されるとき、
 そのふたつの自然数は1より大きい」ということを証明します。
  *)  
-
-(**
-「2以上」は、「1を越える」、と表記(Notation)だけが異なります。
-「1を越える」ほうが表記として複雑なので、Coqの清書系はそちらをつかいます。
-これに留意して、ここでは $1 \lt m$ の方を使います。
-*)
-  Check 2 <= 10.                            (* 1 < 10 *)
-  
   Lemma l_1m1n_mmn (m n : nat) : 1 < m -> 1 < n -> m < m * n.
   Proof.
     move=> Hm Hn.
@@ -238,7 +240,7 @@ Section Composite_Number.
   Qed.
 
 (**
-以上をまとめると、次のようになります。
+以上をまとめると、次のようになります。但し、不使用です。
 *)  
   Lemma l_composite_hypo (m n : nat) :
     ((m < m * n) && (n < m * n)) = ((1 < m) && (1 < n)).
@@ -262,17 +264,23 @@ Section Composite_Number.
     exists (2 ^ b - 1), (\sum_(0 <= i < a) 2 ^ (i * b)).
     split ; [| split].
     - apply/l_1m1n_mmn.
-      + by apply: e2b_1_ge2.                (* 2 <= x を証明する。 *)
-      + by apply: sum0_2_e2ib.              (* 2 <= y を証明する。 *)
+      + by apply: e2b_1_ge2.                (* 1 < x を証明する。 *)
+      + by apply: sum0_2_e2ib.              (* 1 < y を証明する。 *)
 
     - apply/l_1m1n_nmn.
-      + by apply: e2b_1_ge2.                (* 2 <= x を証明する。 *)
-      + by apply: sum0_2_e2ib.              (* 2 <= y を証明する。 *)
+      + by apply: e2b_1_ge2.                (* 1 < x を証明する。 *)
+      + by apply: sum0_2_e2ib.              (* 1 < y を証明する。 *)
         
-    - move/le2_le1 in Ha.     (* 前提を 2 <= a から 1 <= a にする。 *)
-        by apply: l_e2_ab_1.  (* x * y = ... を証明する。 *)
+    - move/lt1_le1 in Ha.      (* 前提を 1 < a から 1 <= a にする。 *)
+        by apply: l_e2_ab_1.   (* x * y = ... を証明する。 *)
   Qed.
   
 End Composite_Number.
+
+(**
+# 参考文献
+
+[a] https://ja.wikipedia.org/wiki/メルセンヌ数
+*)
 
 (* END *)
