ab03461b6ed4629e961c4014277e7b65004fe4ed
SUHARA Hiromichi <suhara@falla.vbox>
2020-10-03 23:53:41 +0900

    総和の式と等しいことを証明する。

--- math/ssr_sum_and_recrel.v
+++ math/ssr_sum_and_recrel.v
@@ -16,6 +16,7 @@ From mathcomp Require Import all_ssreflect.
 Require Import Recdef.
 Require Import Wf_nat.                      (* well_founded lt *)
 Require Import ssromega.
+Require Import ssrsumop.
 
 (**
 https://github.com/suharahiromichi/coq/blob/master/common/ssromega.v
@@ -33,7 +34,7 @@ Section Recrel.
   Variables alpha beta gamma : nat.
   
 (**
-漸化式
+漸化式 式(2.7)
 *)  
   Fixpoint R_rel (n : nat) : nat :=
     if n is n'.+1 then
@@ -49,10 +50,16 @@ Section Recrel.
   Proof. by rewrite {1}/R_rel. Qed.
   
 (**
-漸化式の解
+漸化式の解 式(2.8)
 *)  
   Definition R_sol (n : nat) : nat :=
     alpha + n * beta + (n^2 + n)./2 * gamma.
+
+(**
+総和の式
+ *)
+  Definition R_sum (n : nat) : nat :=
+    \sum_(1 <= i < n.+1) (beta + gamma * i) + alpha.
 
 (**
 2を掛けて2で割るともとにもどる。
@@ -86,38 +93,72 @@ Section Recrel.
       by rewrite odd_double.
   Qed.
   
-  Goal forall n, R_rel n = R_sol n.
+  (* 共通の数式の証明 *)
+  Lemma alpha_beta_gamma (n : nat) :
+    alpha + n * beta + (n ^ 2 + n)./2 * gamma + beta + gamma * n.+1 =
+    alpha + n.+1 * beta + (n.+1 ^ 2 + n.+1)./2 * gamma.
   Proof.
-    elim=> [| n IHn].
+    (* 同類項をまとめる。 *)
+    rewrite -[alpha + n * beta + (n ^ 2 + n)./2 * gamma + beta]addnAC.
+    rewrite -[alpha + n * beta + beta]addnA.
+    rewrite -[in LHS]addnA.
+    
+    (* 同類項をゴールに分ける。 *)
+    congr (_ + _ + _).
+    
+    (* beta の項 *)
+    + rewrite mulSn.
+        by ssromega.
+        
+    (* gamma の項 *)
+    + rewrite [gamma * n.+1]mulnC -[LHS]mulnDl.
+      congr (_ * gamma).
+      (* 通分の前処理をする。 *)
+      rewrite -[n.+1 in LHS]mul2K.
+      rewrite div_fract.
+      (* /2 を通分する。先立って odd の条件を追加する。 *)
+      rewrite -[LHS]add0n.
+      have {1}-> : 0 = false by done.
+      rewrite -{1}[false in LHS](odd_false n).
+      rewrite -[LHS]halfD.
+      
+      (* 左辺 = 右辺 *)
+      done.
+  Qed.
+  
+(**
+漸化式の解が正しいことの証明
+*)
+  Lemma R_rel_sol n : R_rel n = R_sol n.
+  Proof.
+    elim: n => [| n IHn].
     - rewrite /R_rel /R_sol /=.
         by ssromega.
     - rewrite R_rel_n IHn /R_sol.
-      (* 同類項をまとめる。 *)
-      rewrite -[alpha + n * beta + (n ^ 2 + n)./2 * gamma + beta]addnAC.
-      rewrite -[alpha + n * beta + beta]addnA.
-      rewrite -[in LHS]addnA.
-      
-      (* 同類項をゴールに分ける。 *)
-      congr (_ + _ + _).
-      
-      (* beta の項 *)
-      + rewrite mulSn.
-          by ssromega.
-          
-      (* gamma の項 *)
-      + rewrite [gamma * n.+1]mulnC -[LHS]mulnDl.
-        congr (_ * gamma).
-        (* 通分の前処理をする。 *)
-        rewrite -[n.+1 in LHS]mul2K.
-        rewrite div_fract.
-        (* /2 を通分する。先立って odd の条件を追加する。 *)
-        rewrite -[LHS]add0n.
-        have {1}-> : 0 = false by done.
-        rewrite -{1}[false in LHS](odd_false n).
-        rewrite -[LHS]halfD.
+        by apply: alpha_beta_gamma.         (* 共通の数式の証明 *)
+  Qed.
+  
+(**
+漸化式と総和の式が等しいことの証明
+*)
+  Lemma R_sum_sol (n : nat) : R_sum n = R_sol n.
+  Proof.
+    elim: n => [| n IHn].
+    - rewrite /R_sum /R_sol.
+      (* 右辺 *)
+      rewrite exp0n // addnn double0 -divn2 div0n 2!mul0n 2!addn0.
+      (* 左辺 *)
+      rewrite big_nil add0n.
+      done.
+    - rewrite /R_sum /R_sol.
+      rewrite /R_sum /R_sol in IHn.
+      rewrite sum_last //.
 
-        (* 左辺 = 右辺 *)
-        done.
+      (* 左辺 *)
+      rewrite -addnA [beta + gamma * n.+1 + alpha]addnC addnA.
+      rewrite IHn.
+      rewrite addnA.
+        by apply: alpha_beta_gamma.         (* 共通の数式の証明 *)
   Qed.
 End Recrel.
 
