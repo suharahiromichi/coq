41473b69a5557b2a0b76f0925bb2183c45a4d22a
SUHARA Hiromichi <suhara@falla.vbox>
2021-02-25 20:42:48 +0900

    Windowを水平分割するようにした。

--- tools/coq-windows.el
+++ tools/coq-windows.el
@@ -2,7 +2,8 @@
 ;; ProofGneral に適したウィンドウを開く。
 ;;
 (global-set-key [f4] 'coq-windows)
-(defun coq-windows ()
+
+(defun coq-windows-old ()
   "Setup Windows for Proof General"
   (interactive)
   (delete-other-windows)
@@ -12,4 +13,17 @@
   (switch-to-buffer "*goals*")
   (other-window 1)
   (switch-to-buffer "*response*"))
+
+(defun coq-windows ()
+  "Setup Windows for Proof General"
+  (interactive)
+  (toggle-frame-fullscreen)
+  (delete-other-windows)
+  (split-window-horizontally)
+  (other-window 1)
+  (switch-to-buffer "*goals*")
+  (split-window-vertically)
+  (other-window 1)
+  (switch-to-buffer "*response*")
+  (other-window 1))
 ;; end
