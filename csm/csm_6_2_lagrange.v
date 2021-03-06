a51b6bdd6a0bdc39c6f7e2b300c4bf1aee02ddee
SUHARA Hiromichi <suhara@falla.vbox>
2021-03-06 11:02:17 +0900

    説明を追加した。

--- csm/csm_6_2_lagrange.v
+++ csm/csm_6_2_lagrange.v
@@ -35,8 +35,7 @@ finType型クラスのインスタンス型 T を台とする。
   
   (* gT は finGroupType（有限群）型クラスの型インスタンスである。 *)
   Variable gT : finGroupType.
-  
-  Variable G H K : {group gT}.              (* G, H, K は有限群型の変数の *)
+  Variable G H K : {group gT}.              (* G, H, K は有限群型の変数 *)
   
   Goal 1 \in G. by rewrite group1. Qed.
   Goal 1 * G = G. by rewrite mul1g. Qed.
@@ -44,15 +43,22 @@ finType型クラスのインスタンス型 T を台とする。
   Goal H^-1^-1 = H. by rewrite invgK. Qed.
   Goal (G * H)^-1 = H^-1 * G^-1. by rewrite invMg. Qed.
   
+  Check groupM : forall (gT : finGroupType) (G : {group gT}) (x y : gT),
+      x \in G -> y \in G -> x * y \in G.
+  Check groupVr : forall (gT : finGroupType) (G : {group gT}) (x : gT),
+       x \in G -> x^-1 \in G.
+  
 (**
 ## coset と cosets
 
-$G$ を群、$H$ をその部分群として、
-後で定義する同値関係 $\sim$ について：
+$G$ を群、$H$ をその部分群とします。
 
-- coset 剰余類（$\sim$ による同値類） $H\ x$、 ``H :* x``
+- rcoset 右剰余類（$\sim$ による同値類） $H\ x$、 ``H :* x``
 
-- cosets 剰余類の集合（$\sim$ による商） $H \backslash G$
+- rcosets 右剰余類の集合（$\sim$ による商） $H \backslash G$
+
+後で定義する同値関係$sim$の定義から判るように、
+本節では、特に断りのない限り、右剰余類だけを考えます。
 *)
 
 (**
@@ -64,28 +70,34 @@ $G$ を群、$H$ をその部分群として、
 (**
 使用するのは、次のふたつの補題です：
 *)
+(**
+### ``rcosetsP``
+
+任意の集合Aが、
+適当なxについて$H x$と等しいこと（適当なxの剰余類であること）と、
+Aが$H \backslash G$の要素であることは、必要十分条件である：
+
+$$(\exists x, x \in G \land A = H x) \iff A \in H \backslash G$$
+*)
   Section Test.
     Variable A : {set gT}.
     Check rcosetsP
       : reflect (exists2 x, x \in G & A = H :* x) (A \in rcosets H G).
-(**
-任意の集合Aが、
-適当なxについて$H x$と等しいこと（適当なxの剰余類であること）と、
-Aが$H \backslash G$の要素であることは、必要十分条件である。
-
-$$(\exists x, x \in G \land A = H x) \iff A \in H \backslash G$$
-*)
   End Test.
-  
-  Check card_rcoset
-    : forall (gT : finGroupType) (A : {set gT}) (x : gT), #|A :* x| = #|A|.
+
 (**
-任意のxの剰余類の濃度は、もとの集合の濃度に等しい。
+### ``card_rcoset``
+
+任意のxの剰余類の濃度は、もとの集合の濃度に等しい：
 
 $$\forall x, |A x| = |A|$$
-*)
+*)  
+  Check card_rcoset
+    : forall (gT : finGroupType) (A : {set gT}) (x : gT), #|A :* x| = #|A|.
 
 (**
+### ``myCard_rcoset``
+
 証明したいのは、以下の命題です：
 
 剰余類の集合の任意の要素の濃度は、もとの集合の濃度に等しい。
@@ -124,12 +136,12 @@ Goal: $$ |H| = |H| $$
 (**
 ## 部分群
 
-$G$ を群、$H$ をその部分群とします。$ H \in G $
+$G$を群、$H$をその部分群とします。すなわち、$ H \in G $
 *)
   Hypothesis HG : H \subset G.
 
 (**
-## 同値関係
+## 同値関係の定義
 
 bool型の二項関係（$\sim$）を定義します。
 
@@ -137,16 +149,16 @@ $$ x \sim y \equiv x y^{-1} \in H $$
 *)  
   Definition R := [rel x y | x * y^-1 \in H].
   Check R : gT -> gT -> bool.               (* simpl_rel gT. *)
-  
-  Check groupM : forall (gT : finGroupType) (G : {group gT}) (x y : gT),
-      x \in G -> y \in G -> x * y \in G.
-  Check groupVr : forall (gT : finGroupType) (G : {group gT}) (x : gT),
-       x \in G -> x^-1 \in G.
+(**
+テキストにはありませんが、演算子``~``を定義しておきます。
+*)
+  Notation "x ~ y" := (R x y) (at level 40, left associativity).
   
 (**
-$\sim$が、同値関係であることを証明します。
+## 同値関係の証明
 
-equivalence_rel は ssrbool で定義されている。
+$\sim$が、同値関係であることを証明します。
+equivalence_rel は ssrbool で定義されています。
 *)
 (**
    fun (T : Type) (R : rel T) => forall x y z : T, R z z * (R x y -> R x z = R y z)
@@ -169,18 +181,19 @@ equivalence_rel は ssrbool で定義されている。
 and は Prod型のふたつの直積に対して、prod は Type型のふたつの直積です。
 ここではおなじことです。
 
-最後に、「=」はboolの「=」で必要十分条件を示します。
+また、``=``は、boolの``=``で、論理式としては必要十分条件を示します。
+以上を論理式として書くと、次の様になります：
  *)
 (**
-Goal: $ z \sim z \land (x \sim y\ \to (x\ \sim z \iff y \sim z)) $
+Goal: $$ z \sim z \land (x \sim y\ \to (x\ \sim z \iff y \sim z)) $$
 *)
     apply: pair => /=.
 (**
-Goal: $ z z^{-1} \in H $
+Goal: $$ z z^{-1} \in H $$
 *)
     - by rewrite mulgV group1.
 (**
-Goal: $ x y^{-1} \in H -> (x z^{-1} \in H \iff y z^{-1} \in H) $
+Goal: $$ x y^{-1} \in H -> (x z^{-1} \in H \iff y z^{-1} \in H) $$
 *)
     - move=> xRinvy.
       apply/idP/idP.
@@ -203,7 +216,7 @@ Goal: $ x y^{-1} \in H -> (x z^{-1} \in H \iff y z^{-1} \in H) $
 
 $$H x = \\{y \in G\ |\ x \sim y \\}$$
 *)
-  Lemma coset_equiv_class (x : gT) (xinG : x \in G) : H :* x = [set y in G | R x y].
+  Lemma coset_equiv_class (x : gT) (xinG : x \in G) : H :* x = [set y in G | x ~ y].
   Proof.
     apply/setP => /= y.
     rewrite inE.
@@ -234,7 +247,7 @@ $$H x = \\{y \in G\ |\ x \sim y \\}$$
 (**
 ## 補題：
 
-$H \backslash G$ は、G の$\sim$についての商と等しい。
+$H \backslash G$ は、$G$の$\sim$についての商と等しい。
 *)
   Lemma rcosets_equiv_part : rcosets H G = equivalence_partition R G.
   Proof.
@@ -265,7 +278,7 @@ $H \backslash G$ は、G の$\sim$についての商と等しい。
 (**
 ## 補題：
 
-$H \backslash G$ は、G の$sim$についての分割である。
+$H \backslash G$ は、$G$の$sim$についての分割である。
 
 商であるということと、分割になっているということは区別されます。
 前者と違って、後者では$\sim$が明示的に現れないことがポイントです。
@@ -279,7 +292,34 @@ $H \backslash G$ は、G の$sim$についての分割である。
 
 (**
 # 6.2.4 ラグランジュの定理
+ *)
+(**
+## 指数
+  
+テキストにもあるように、
+一般的な数学の記法では、$(G : H)$ は（$H$による$G$の）右剰余類の個数を表します。
+これを（$H$による$G$の）指数ともいいます。
+
+$$(G : H) = |H \ G|$$
+
+MathComp では、``#|_ : _|``というNotationで、次の意味です。
 *)
+  Goal #|G : H| = #|rcosets H G|.
+  Proof.
+    rewrite /indexg.
+    done.
+  Qed.
+
+(**
+右剰余類の個数も左剰余類の個数もおなじなので、
+fingroup.v にある補題``card_lcosets``で次が証明できます。
+*)  
+  Goal #|G : H| = #|lcosets H G|.
+  Proof.
+    rewrite card_lcosets.
+    done.
+  Qed.
+  
 (**
 ## 総和についての補題
 
@@ -288,7 +328,7 @@ $H \backslash G$ は、G の$sim$についての分割である。
 そのものずばりの補題があるので、それを使います。
 これは、定数の総和をもとめる $\sum_{0 <= i < n} c = c\ n$ の集合版です。
 
-$\sum_{i \in A} c = |A|\ c$
+$$\sum_{i \in A} c = |A|\ c$$
  *)
   Check sum_nat_const
     : forall (fT : finType) (A : pred fT) (c : nat), \sum_(i in A) c = (#|A| * c)%N.
@@ -330,30 +370,35 @@ $\sum_{i \in H \backslash G}\ |A| = \sum_{i \in H \backslash G}\ |H|$
   
 (**
 ## 定理の証明
+
+証明したいのは以下です。
+$G$を群、$H$をその部分群とするとき、
+$G$の濃度は、$H$の濃度と（$H$による$G$の）指数の積に等しい：
+
+$$|G| = |H|\ (G : H)$$
+
+指数の意味は冒頭を参照してください。
 *)  
   Theorem myLagrange : #|G| = (#|H| * #|G : H|)%nat.
   Proof.
 (**
-Goal: $|G| = |H|\ (G : H)$
+Goal: $$|G| = |H|\ (G : H)$$
 *)
     rewrite (card_partition partition_rcosets).
 (**
-Goal: $\sum_{A \in H \backslash G} |A| = |H|\ (G : H)$
+Goal: $$\sum_{A \in H \backslash G} |A| = |H|\ (G : H)$$
 *)
     rewrite ((eq_bigr (fun _ => #|H|)) myCard_rcoset).
 (**
-Goal: $\sum_{A \in H \backslash G} |H| = |H|\ (G : H)$
+Goal: $$\sum_{A \in H \backslash G} |H| = |H|\ (G : H)$$
 *)
     rewrite sum_nat_const.
 (**
-Goal: $|H  \backslash G|\ |H| = |H|\ |G : H|$
+Goal: $$|H \backslash G|\ |H| = |H|\ |G : H|$$
 *)
+    rewrite /indexg.
       by rewrite mulnC.
   Qed.
-  
-  (* (G : H) は、HによるGの右剰余類の個数であり、mathcomp の表記だと #|G : H| である。 *)
-  (* 剰余類(G : H) の個数(#| |) といういう意味ではないことに注意すること。 *)
-  (* 剰余類(rcosets H G) の 個数(#|_|) なら、 #|rcosets H G| = #|G : H| である。 *)
 End Lagrange.
 
 (* END *)
