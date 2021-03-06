fa9114bf450c2282d00f770ff0797f132067f9d7
SUHARA Hiromichi <suhara@falla.vbox>
2021-03-06 15:00:25 +0900

    説明を追加した。

--- csm/csm_6_2_lagrange.v
+++ csm/csm_6_2_lagrange.v
@@ -17,7 +17,6 @@ Unset Printing Implicit Defensive.
 Set Print All.
 
 Section Lagrange.
-
 (**
 # 6.2.1 有限群の定義 (see. fingroup.v)
 
@@ -35,29 +34,43 @@ finType型クラスのインスタンス型 T を台とする。
   
   (* gT は finGroupType（有限群）型クラスの型インスタンスである。 *)
   Variable gT : finGroupType.
-  Variable G H K : {group gT}.              (* G, H, K は有限群型の変数 *)
   
-  Goal 1 \in G. by rewrite group1. Qed.
-  Goal 1 * G = G. by rewrite mul1g. Qed.
-  Goal G * H * K = G * (H * K). by rewrite mulgA. Qed.
-  Goal H^-1^-1 = H. by rewrite invgK. Qed.
-  Goal (G * H)^-1 = H^-1 * G^-1. by rewrite invMg. Qed.
-  
-  Check groupM : forall (gT : finGroupType) (G : {group gT}) (x y : gT),
-      x \in G -> y \in G -> x * y \in G.
-  Check groupVr : forall (gT : finGroupType) (G : {group gT}) (x : gT),
-       x \in G -> x^-1 \in G.
+  Section Test.
+    Variable x y z : gT.
+    Variable G : {group gT}.
+    
+    (* 群の公理 *)
+    Goal 1 \in G. Proof. by rewrite group1. Qed.
+    Goal 1 * x = x. Proof. by rewrite mul1g. Qed.
+    Goal x * y * z = x * (y * z). Proof. by rewrite mulgA. Qed.
+    Goal x^-1^-1 = x. Proof. by rewrite invgK. Qed.
+    Goal (x * y)^-1 = y^-1 * x^-1. Proof. by rewrite invMg. Qed.
+    
+    (* 群の定理 *)
+    Goal x * 1 = x. Proof. by rewrite mulg1. Qed.
+    Goal x * x^-1 = 1. Proof. by rewrite mulgV. Qed.
+    Goal x^-1 * x = 1. Proof. by rewrite mulVg. Qed.
+    Goal x \in G -> x^-1 \in G. Proof. by move/groupVr. Qed.
+    Goal x \in G -> y \in G -> x * y \in G. Proof. apply: groupM. Qed.
+    
+    Check mulgV : forall T : finGroupType, forall x, x * x^-1 = 1.
+    Check groupM : forall (gT : finGroupType) (G : {group gT}) (x y : gT),
+        x \in G -> y \in G -> x * y \in G.
+    Check groupVr : forall (gT : finGroupType) (G : {group gT}) (x : gT),
+        x \in G -> x^-1 \in G.
+  End Test.
   
 (**
 ## coset と cosets
 
-$G$ を群、$H$ をその部分群とします。
 
 - rcoset 右剰余類（$\sim$ による同値類） $H\ x$、 ``H :* x``
 
 - rcosets 右剰余類の集合（$\sim$ による商） $H \backslash G$
 
-後で定義する同値関係$sim$の定義から判るように、
+
+
+後で定義する同値関係$\sim$の定義から判るように、
 本節では、特に断りのない限り、右剰余類だけを考えます。
 *)
 
@@ -71,7 +84,7 @@ $G$ を群、$H$ をその部分群とします。
 使用するのは、次のふたつの補題です：
 *)
 (**
-### ``rcosetsP``
+### 使用する補題 ``rcosetsP``
 
 任意の集合Aが、
 適当なxについて$H x$と等しいこと（適当なxの剰余類であること）と、
@@ -80,13 +93,14 @@ Aが$H \backslash G$の要素であることは、必要十分条件である：
 $$(\exists x, x \in G \land A = H x) \iff A \in H \backslash G$$
 *)
   Section Test.
-    Variable A : {set gT}.
+    Variable A H G : {set gT}.
+    
     Check rcosetsP
       : reflect (exists2 x, x \in G & A = H :* x) (A \in rcosets H G).
   End Test.
 
 (**
-### ``card_rcoset``
+### 使用する補題 ``card_rcoset``
 
 任意のxの剰余類の濃度は、もとの集合の濃度に等しい：
 
@@ -96,7 +110,7 @@ $$\forall x, |A x| = |A|$$
     : forall (gT : finGroupType) (A : {set gT}) (x : gT), #|A :* x| = #|A|.
 
 (**
-### ``myCard_rcoset``
+### 証明したい補題 ``myCard_rcoset``
 
 証明したいのは、以下の命題です：
 
@@ -108,7 +122,8 @@ Goal: $$ A \in H \backslash G \to |A| = |H| $$
 ここでは、任意の$H \backslash G$の要素で剰余類を選んでいます。
 多分、そこに違いがあるのだろうと思います。
 *)
-  Lemma myCard_rcoset (A : {set gT}) : A \in rcosets H G -> #|A| = #|H|.
+  Lemma myCard_rcoset (G H : {group gT}) (A : {set gT}) :
+    A \in rcosets H G -> #|A| = #|H|.
   Proof.
     move/rcosetsP.
 (**
@@ -136,8 +151,9 @@ Goal: $$ |H| = |H| $$
 (**
 ## 部分群
 
-$G$を群、$H$をその部分群とします。すなわち、$ H \in G $
+以下では、$G$を群、$H$をその部分群とします。すなわち、$ H \in G $
 *)
+  Variable G H : {group gT}.              (* G, H は有限群型の変数 *)
   Hypothesis HG : H \subset G.
 
 (**
@@ -176,16 +192,23 @@ equivalence_rel は ssrbool で定義されています。
   Proof.
     rewrite /equivalence_rel => x y z.
 (**
-左辺の * は直積(prod)の意味であるので、pair を適用することで、
-* の左と右のふたつのゴールに分けられます。
-and は Prod型のふたつの直積に対して、prod は Type型のふたつの直積です。
-ここではおなじことです。
+ゴールは、MathComp の表記で次のようになります：
 
-また、``=``は、boolの``=``で、論理式としては必要十分条件を示します。
+```z ~ z * (x ~ y -> x ~ z = y ~ z)```
+
+``*`` は、prod の意味で、Type型のふたつの直積です。
+（これに対して、お馴染みの ``/\`` は、andの意味で、Prod型のふたつの直積を示します。）
+ここではおなじことといってよいと思います。
+
+また、``=``は、boolの値がおなじことを示す``=``で、論理式としての必要十分条件を示します。
 以上を論理式として書くと、次の様になります：
  *)
 (**
 Goal: $$ z \sim z \land (x \sim y\ \to (x\ \sim z \iff y \sim z)) $$
+*)
+(**
+``*`` は直積(prod)の意味であるので、pair を適用することで、
+``*`` の左と右のふたつのゴールに分けられます。（``/\`` における split とおなじです。）
 *)
     apply: pair => /=.
 (**
@@ -200,10 +223,12 @@ Goal: $$ x y^{-1} \in H -> (x z^{-1} \in H \iff y z^{-1} \in H) $$
       + move/groupVr in xRinvy.
         (* [x in H -> y in H -> x * y in H] の [x in H] を指定する必要がある。 *)
         (* 直前の行とまとめて [move/(groupM (groupVr xRinvy))] とできる。 *)
-        Check groupM xRinvy.
+
+        Check groupM xRinvy : x * z^-1 \in H -> (x * y^-1)^-1 * (x * z^-1) \in H.
         move/(groupM xRinvy).
           by rewrite invMg invgK mulgA -(mulgA y) mulVg mulg1.
-      + Check (groupM xRinvy).
+
+      + Check (groupM xRinvy) : y * z^-1 \in H -> x * y^-1 * (y * z^-1) \in H.
         move/(groupM xRinvy).
           by rewrite mulgA -(mulgA x) mulVg mulg1.
   Qed.
@@ -278,10 +303,10 @@ $H \backslash G$ は、$G$の$\sim$についての商と等しい。
 (**
 ## 補題：
 
-$H \backslash G$ は、$G$の$sim$についての分割である。
+$H \backslash G$ は、$G$の$\sim$についての分割である。
 
 商であるということと、分割になっているということは区別されます。
-前者と違って、後者では$\sim$が明示的に現れないことがポイントです。
+ひとつまえの補題と違って、後者では$\sim$が明示的に現れないことがポイントです。
 *)
   Lemma partition_rcosets : partition (rcosets H G) G.
   Proof.
@@ -294,15 +319,15 @@ $H \backslash G$ は、$G$の$sim$についての分割である。
 # 6.2.4 ラグランジュの定理
  *)
 (**
-## 指数
+## 指数について
   
 テキストにもあるように、
 一般的な数学の記法では、$(G : H)$ は（$H$による$G$の）右剰余類の個数を表します。
 これを（$H$による$G$の）指数ともいいます。
 
-$$(G : H) = |H \ G|$$
+$$(G : H) = |H \backslash G|$$
 
-MathComp では、``#|_ : _|``というNotationで、次の意味です。
+MathComp では、``#|_ : _|``というNotationです。
 *)
   Goal #|G : H| = #|rcosets H G|.
   Proof.
@@ -311,8 +336,10 @@ MathComp では、``#|_ : _|``というNotationで、次の意味です。
   Qed.
 
 (**
-右剰余類の個数も左剰余類の個数もおなじなので、
-fingroup.v にある補題``card_lcosets``で次が証明できます。
+（参考）右剰余類の個数も左剰余類の個数もおなじなので、
+fingroup.v にある補題``card_lcosets``で、次が成り立ちます：
+
+$$(G : H) = |G / H| = |H \backslash G|$$
 *)  
   Goal #|G : H| = #|lcosets H G|.
   Proof.
@@ -321,20 +348,7 @@ fingroup.v にある補題``card_lcosets``で次が証明できます。
   Qed.
   
 (**
-## 総和についての補題
-
-総和についての補題を紹介します。
-テキストでは、補題``big_const``を使って``iter``の式に変換していますが、
-そのものずばりの補題があるので、それを使います。
-これは、定数の総和をもとめる $\sum_{0 <= i < n} c = c\ n$ の集合版です。
-
-$$\sum_{i \in A} c = |A|\ c$$
- *)
-  Check sum_nat_const
-    : forall (fT : finType) (A : pred fT) (c : nat), \sum_(i in A) c = (#|A| * c)%N.
-  
-(**
-## 使用する補題（その１）
+## 使用する補題 ``card_partition``
 
 finset.v で証明されている補題です。P（集合の集合）が D（集合）の分割になっているならば、
 Pの全ての要素の濃度の総和は、Dの濃度に等しい。
@@ -346,9 +360,10 @@ $$ |D| = \sum_{A \in P}\ |A|$$
       partition P D -> #|D| = \sum_(A in P) #|A|.
 
 (**
-直前に証明した補題です。
+これに直前に証明した補題を組み合わせます。
 *)
   Check partition_rcosets : partition (rcosets H G) G.
+
 (**
 組み合わせると、剰余類の集合の要素の濃度の総和は、群$G$の濃度に等しい、となります。
 これは、剰余類の集合の集合は、群$G$の分割であるためですね。
@@ -359,14 +374,27 @@ $|G| = \sum_(A \in H \backslash G)\ |A|$
     : #|G| = \sum_(A in rcosets H G) #|A|.
 
 (**
-## 使用する補題（その２）
+## 使用する補題 ``eq_bigr``
 
 myCard_rcoset の $|A| = |H|$ を Σの中に適用して書き換える補題です。
 
 $\sum_{i \in H \backslash G}\ |A| = \sum_{i \in H \backslash G}\ |H|$
 *)  
-  Check ((eq_bigr (fun _ => #|H|)) myCard_rcoset)
+  Check ((eq_bigr (fun _ => #|H|)) (@myCard_rcoset G H))
     : \sum_(i in rcosets H G) #|i| = \sum_(i in rcosets H G) #|H|.
+  
+(**
+## 総和についての補題 ``sum_nat_const``
+
+総和についての補題を紹介します。
+テキストでは、補題``big_const``を使って``iter``の式に変換していますが、
+そのものずばりの補題があるので、それを使います。
+これは、定数の総和をもとめる $\sum_{0 <= i < n} c = c\ n$ の集合版です。
+
+$$\sum_{i \in A} c = |A|\ c$$
+ *)
+  Check sum_nat_const
+    : forall (fT : finType) (A : pred fT) (c : nat), \sum_(i in A) c = (#|A| * c)%N.
   
 (**
 ## 定理の証明
@@ -388,7 +416,7 @@ Goal: $$|G| = |H|\ (G : H)$$
 (**
 Goal: $$\sum_{A \in H \backslash G} |A| = |H|\ (G : H)$$
 *)
-    rewrite ((eq_bigr (fun _ => #|H|)) myCard_rcoset).
+    rewrite ((eq_bigr (fun _ => #|H|)) (@myCard_rcoset G H)).
 (**
 Goal: $$\sum_{A \in H \backslash G} |H| = |H|\ (G : H)$$
 *)
@@ -399,6 +427,7 @@ Goal: $$|H \backslash G|\ |H| = |H|\ |G : H|$$
     rewrite /indexg.
       by rewrite mulnC.
   Qed.
+
 End Lagrange.
 
 (* END *)
