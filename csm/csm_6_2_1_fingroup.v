1f8140ff971eb9de25fd753c69334bffb7b4fbb3
SUHARA Hiromichi <suhara@falla.vbox>
2021-03-27 10:58:34 +0900

    説明を修正した。

--- csm/csm_6_2_1_fingroup.v
+++ csm/csm_6_2_1_fingroup.v
@@ -36,15 +36,15 @@ MathComp の型クラスとして定義されているのは、``finGroupType``
 ``finType``型クラスを継承して、公理として乗法の結合則や単位元の存在を仮定します。
 すなわち、``finType``型クラスのインスタンス型``T``を台として、以下が成り立ちます。
 
--二項演算 mul T -> T -> T が存在する。
--元 one : T が存在する。
--関数 inv : T -> T が存在する。
--mul は結合律を満たす。
--one は左単位元である。
--inv は対合である（involutive、2回適用するともとにもどる）。
-``inv (inv x) = x``
--inv と mul は逆自己同型（？）である（anti-mophism を満たす）。
-``inv (mul x y) = mul (inv y) (inv x)``
+- 二項演算 mulg : T -> T -> T が存在する。
+- 元 one : T が存在する。
+- 関数 invg : T -> T が存在する。
+- mulg は結合律を満たす。
+- one は左単位元である。
+- invg は対合である（involutive、2回適用するともとにもどる）。
+```invg (invg x) = x```
+- invg と mulg は逆自己同型（？）である（anti-mophism を満たす）。
+```invg (mulg x y) = mulg (invg y) (invg x)```
 
 （``anti-`` は x と y が逆になるということ）
  *)
@@ -86,6 +86,14 @@ Section Sect_1.
   Goal x^-1 * x = 1. Proof. by rewrite mulVg. Qed.
 
 (**
+参考： mulg において、``FinGroup.base gT`` が gT のカノニカル解です。
+（x や y が gT 型であることから、``FinGroup.base gT`` を見つけ出せるます）
+*)
+  Check @mulg : forall T : baseFinGroupType, T -> T -> T.
+  Check @mulg (FinGroup.base gT) x y : FinGroup.base gT.
+  Compute FinGroup.sort (FinGroup.base gT). (* gT *)
+
+(**
 別に定義した有限群 ``gT'`` と ``gT`` の要素の間の乗算はできません。
 *)
   Section Sect_1_2.
@@ -97,8 +105,12 @@ Section Sect_1.
   End Sect_1_2.
 
 (**
-## 集合としての群の定義
+## ``group_set_of_baseGroupType gT``
+*)
 
+(**
+### 集合としての群
+  
 テキストの6.2節でもそうですが、
 群そのもの（有限集合としての有限群）を扱いたい場合があります。
 
@@ -111,57 +123,75 @@ Section Sect_1.
 
 (**
 しかし、型と集合はCoqの対象としては別なものです。
+*)
+(**
+## 集合としての群の型（その１）
 
 集合としての群を扱うには次のようにします：
 
-まず、``finGroupType``型クラスのインスタンス型（例：A）の値を要素とする有限集合の型は、
-``{set A}``で表します。
-
-``{set A}``の実体は、``A``の要素の値が集合に含まれるか否か、
-を決定する``A -> bool``型の関数です(``finset`` の ``set_type``の定義を参照のこと)。
-
-``{set gT}``のインスタンスは、次のように使って定義します：
-*)
+まず、``finGroupType``型クラスのインスタンス型（例：gT）の値を要素とする
+有限集合の型は、``{set gT}``で表します。
+  
+``{set gT}``のインスタンス（この場合は集合） A, B, C は、次のように定義します：
+ *)
+  Check {set gT} : predArgType.
   Variable A B C : {set gT}.
 
 (**
-余談ですが、``gT``型の x は、``x ∈ A`` かどうかを決定できます。
-*)  
-  Check x : gT.          (* x は gT 型 *)
-  Check x \in A : bool.  (* true または false *)
+``{set gT}`` のインスタンスは、``[set _ : _ | _]`` のかたちでも定義できます。
+あとで出てきます。任意のbool述語を併用して集合を定義できます。
+*)
+    Check [set a : gT | _] : {set gT}.
+
 (**
-x は A型 でないことに注意してください。そもそも A は型ではありません。
+また、``{set gT}`` は、predArgType で ``\in`` による判定ができます。
+ *)
+  Check x : gT.                             (* x は gT 型 *)
+  Check x \in A : bool.                     (* true または false *)
+
+(**
+``mem A`` は、gT 型の変数が集合Aに含まれるか否かを決定する``gT -> bool``型の関数になります。
+(``finset`` の ``set_type``の定義を参照のこと)。
+*)
+  Check mem A : gT -> bool.
+  Check mem A x : bool. (* x \in A とだいたい同じ。。。ちょっと苦しい。 *)
+  
+(**
+しかし、x は A型 でないことに注意してください。そもそも A は型ではありません。
  *)
   Fail Check x : A.
+  Fail Check A : Type.
   
 (**
-余談終わり。
-
-すると、有限集合どうしの掛け算ができます。
+集合としての群の型として、group_set_of_baseGroupType が定義されています。
+ *)
+  Check group_set_of_baseGroupType : baseFinGroupType -> Type.
+  Check gT : baseFinGroupType.
+  Check group_set_of_baseGroupType gT : Type.
+  
+(**
+``group_set_of_baseGroupType gT`` は {set gT} のカノニカル解なので、
+（A や B が ``{set gT}``型であることから、``group_set_of_baseGroupType gT``
+を見つけ出すことができ）mulg (``*``) を使って集合としての群どうしの積が計算できます。
 *)  
-  Check A * B : {set gT}.
+  Check @mulg : forall T : baseFinGroupType, T -> T -> T.
+  Check @mulg (group_set_of_baseGroupType gT) A B : group_set_of_baseGroupType gT.
   
 (**
+## 集合としての群の型（その２）
+
 ついで、有限群は``{group A}``で表します。
 
 ``{group A}``の実体は、``{set A}``を台として、
 以下の条件を追加したものです(``fingroup`` の ``group_type``の定義を参照のこと)。
-
-- それに要素``1``を含こと。
-``1 \in A``
-
-- 積(mulg)が閉じていること。
-``A * A \subset A``
 *)
-  Check 1 \in A.
-  Check A * A \subset A.
   
 (**
 ``{group gT}``のインスタンスは、次のように使って定義します：
 *)
+  Check {group gT} : predArgType.
   Variable G H : {group gT}.
-  
-(**
+  (**
 有限群の公理や定理のうち、集合としての有限群がでてくるものが使えます。
 *)
   Goal 1 \in G. Proof. by rewrite group1. Qed.
@@ -172,7 +202,11 @@ x は A型 でないことに注意してください。そもそも A は型で
 ``{set T}`` と ``{group T}``の定義にはファントムタイプを使っていますが、
 これは、``T``の部分に、
 finType にカノニカルプロジェクションできる型だけを書けるように制限するためです。
-
+*)
+  Check {set bool}.                         (* bool型はよいが、 *)
+  Fail Check {set nat}.                     (* nat型はダメ。 *)
+  
+(**
 Math-Comp Book の 5.10.1 や次の文書も参照してください。
 
 [https://github.com/suharahiromichi/coq/blob/master/math-comp-book/suhara.ch7-phantom_types.v]
@@ -184,49 +218,12 @@ Math-Comp Book の 5.10.1 や次の文書も参照してください。
 
 MathComp には、関数``rcoset`` と 演算子``A :* x`` のふたつの定義があります。
 このふたつは別のものです。
- *)
-(**
-### ひとつめの定義
-
-関数``rcoset``の定義は、
-有限集合としての有限群Aのすべての要素に、右からxを掛けたものです。
-(A の ``mulg x = λy.(x * y)`` による像の集合といえます。)
-*)
-  Check rcoset A x                : {set gT}.
-  Check (fun a => mulg a x) @: A.           (* rcoset を展開する。 *)
-  Check mulg^~x @: A.                       (* ``^~`` は MathComp 風の表記 *)
-  Check [set a * x | a in A].               (* ``@:`` を展開する。 *)
-  (* ``@:`` は、finset で定義 *)
-  
-(**
-### ふたつめの定義
-
-演算子``A :* x``の定義は、有限集合としての有限群Aと``{x}``を掛けたものです。
-*)
-  Check A :* x                    : {set gT}.
-  Check A * [set x].                    (* ``:*`` を展開する。 *)
-  Check mulg A [set x].                 (* ``*`` を展開する。 *)
-  
-(**
-### ふたつの定義がおなじであることの証明
-*)
-  Check rcosetE
-    : forall (gT : finGroupType) (A : {set gT}) (x : gT), rcoset A x = A :* x.
-  Check rcosetP
-    : reflect (exists2 a, a \in G & y = a * x) (y \in G :* x).
-  
-(**
-## 剰余群 (cosets) の定義
-
-### ひとつめの定義
-
-MathComp には、関数``rcosets`` があります。
-
-関数``rcosets``の定義は、
 有限集合としての有限群Aのすべての要素に、
 有限集合としての有限群Bのすべての要素を右から掛けたものです。
 *)
-  Check rcosets A B               : {set {set gT}}.
+  Check rcosets : {set gT} -> {set gT} -> {set {set gT}}.
+  
+  Check rcosets A B.
   Check (rcoset A) @: B.                    (* rcosets を展開する。 *)
   Check [set (rcoset A x) | x in B].        (* ``@:`` を展開する。 *)
   Check [set [set a * x | a in A] | x in B]. (* rcoset を展開する。 *)
