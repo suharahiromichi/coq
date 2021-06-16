fc876cfe5d99aa00b42a2709ea4ba179f8357fe2
SUHARA Hiromichi <suhara@falla.vbox>
2021-06-16 21:09:20 +0900

    定義を一般的な一種類にした。

--- math/ssr_two_fib_eq.v
+++ math/ssr_two_fib_eq.v
@@ -16,29 +16,29 @@ Unset Strict Implicit.
 Unset Printing Implicit Defensive.
 
 (**
-ffibonacci
+二種類の ffibonacci 数列の定義が同じことを示す。
  *)
 Section Two_Fib_Eq.
 
 (**
-# keen さんの定義
+# fib の定義
 *)
-  Function fib'1 (n : nat) : nat :=
+  Function fib1 (n : nat) : nat :=
     match n with
-    | 0 => 1
+    | 0 => 0                                (* keen さんは 0 *)
     | 1 => 1
-    | (m.+1 as pn).+1 => fib'1 m + fib'1 pn (* fib' n.-2 + fib' n.-1 *)
+    | (m.+1 as pn).+1 => fib1 m + fib1 pn (* fib n.-2 + fib n.-1 *)
     end.
 
-  Fixpoint loop' m n k :=
+  Fixpoint loop m n k :=
     match k with
-    | 0 => n
-    | k.+1 => loop' n (n + m) k
+    | 0 => m                                (* keen さんは n *)
+    | k.+1 => loop n (n + m) k
     end.
-  Definition fib'2 n := loop' 0 1 n.
+  Definition fib2 n := loop 0 1 n.
 
-  Compute fib'1 5.                           (* 8 *)
-  Compute fib'2 5.                           (* 8 *)
+  Compute fib1 5.                           (* 5 *)
+  Compute fib2 5.                           (* 5 *)
   
 (**
 ## idris の証明に近いもの
@@ -62,38 +62,39 @@ twoFibEq (S (S k)) =
   rewrite twoFibEq (S k) in
   Refl
 *)
-  Lemma loop'_is_like_fib' i j n :
-    loop' (i + j) (i + j + j) n = loop' j (i + j) n + loop' i j n.
+  Lemma loop_is_like_fib' i j n :
+    loop (i + j) (i + j + j) n = loop j (i + j) n + loop i j n.
   Proof.
     elim: n i j.
     - move=> i j.
+      rewrite addnC.            (* 定義の修正によって必要になった。 *)
       reflexivity.
     - move=> //= n IHn i j.
       Check IHn j (j + i)
-        : loop' (j + (j + i)) (j + (j + i) + (j + i)) n =
-          loop' (j + i) (j + (j + i)) n + loop' j (j + i) n.
+        : loop (j + (j + i)) (j + (j + i) + (j + i)) n =
+          loop (j + i) (j + (j + i)) n + loop j (j + i) n.
       rewrite [i + j]addnC.
       rewrite -[j + i + j]addnA [i + j]addnC.
       rewrite (IHn j (j + i)).
       reflexivity.
   Qed.
   
-  Theorem two_fib'_eq' n : fib'2 n = fib'1 n.
+  Theorem two_fib_eq' n : fib2 n = fib1 n.
   Proof.
-    functional induction (fib'1 n).
-    (* fib'2 0 = 1 *)
+    functional induction (fib1 n).
+    (* fib2 0 = 1 *)
     - reflexivity.
-    (* fib'2 1 = 1 *)
+    (* fib2 1 = 1 *)
     - reflexivity.
-    (* fib'2 m.+2 = fib'1 m + fib'1 m.+1 *)
-    - rewrite /fib'2 [LHS]/=.
+    (* fib2 m.+2 = fib1 m + fib1 m.+1 *)
+    - rewrite /fib2 [LHS]/=.
       rewrite [1 + 0]addnC.
-      Check loop'_is_like_fib' 0 1 m
-        : loop' (0 + 1) (0 + 1 + 1) m = loop' 1 (0 + 1) m + loop' 0 1 m.
-      rewrite (loop'_is_like_fib' 0 1 m).
+      Check loop_is_like_fib' 0 1 m
+        : loop (0 + 1) (0 + 1 + 1) m = loop 1 (0 + 1) m + loop 0 1 m.
+      rewrite (loop_is_like_fib' 0 1 m).
       rewrite -IHn0.
       rewrite -IHn1.
-      rewrite /fib'2 [RHS]/=.
+      rewrite /fib2 [RHS]/=.
       rewrite add0n addn0 [RHS]addnC.
       reflexivity.
   Qed.
@@ -101,54 +102,11 @@ twoFibEq (S (S k)) =
 (**
 ## Coq風の証明
 *)  
-  Lemma loop'_is_like_fib i j n :
-    loop' (i + j) (i + j + j) n = loop' j (i + j) n + loop' i j n.
-  Proof.
-    elim: n i j => //= n IHn i j.
-    rewrite [i + j]addnC.
-    rewrite -[j + i + j]addnA [i + j]addnC.
-      by rewrite (IHn j (j + i)).
-  Qed.
-  
-  Theorem two_fib'_eq n : fib'2 n = fib'1 n.
-  Proof.
-    functional induction (fib'1 n) => //.
-    rewrite /fib'2 [LHS]/=.
-    rewrite (loop'_is_like_fib 0 1 m).
-    rewrite -IHn0.
-    rewrite -IHn1.
-    rewrite /fib'2 [RHS]/=.
-      by rewrite add0n addn0 [RHS]addnC.
-  Qed.
-
-(**
-# ふつうの fib の定義
- *)
-  Function fib1 (n : nat) : nat :=
-    match n with
-    | 0 => 0
-    | 1 => 1
-    | (m.+1 as pn).+1 => fib1 m + fib1 pn (* fib n.-2 + fib n.-1 *)
-    end.
-
-  Fixpoint loop m n k :=
-    match k with
-    | 0 => m
-    | k.+1 => loop n (n + m) k
-    end.
-  Definition fib2 n := loop 0 1 n.
-
-  Compute fib1 5.                           (* 5 *)
-  Compute fib1 10.                          (* 55 *)
-  Compute fib2 5.                           (* 5 *)
-  Compute fib2 10.                          (* 5 *)
-
-  
   Lemma loop_is_like_fib i j n :
     loop (i + j) (i + j + j) n = loop j (i + j) n + loop i j n.
   Proof.
     elim: n i j => //= [| n IHn] i j.
-    - by rewrite addnC.
+    - by rewrite addnC.         (* 定義の修正によって必要になった。 *)
     - rewrite [i + j]addnC.
       rewrite -[j + i + j]addnA [i + j]addnC.
       by rewrite (IHn j (j + i)).
