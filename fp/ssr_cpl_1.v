2ec1e3ddb1b5d364baf946cd13a41f239b7f4852
SUHARA Hiromichi <suhara@falla.vbox>
2021-08-20 22:00:56 +0900

    prodを追加した。

--- fp/ssr_cpl_1.v
+++ fp/ssr_cpl_1.v
@@ -12,12 +12,14 @@ Section CPL.
   Inductive object :=
   | I
   | dot of object & object
+  | prod of (object * object)
   | pair of (object * object)
   | pi1
   | pi2
   | pr of (object * object)
   | O
   | S
+  | exp of (object * object)
   | cur of object
   | ev
   .
@@ -27,14 +29,6 @@ Section CPL.
   Check pi1 \; (pi1 \; pi1).
   Check (pi1 \o pi1) \o pi1.
   
-  Definition add :=
-    ev \o pair (pr (cur pi2,
-                    cur (S \o ev)) \o pi1,
-                pi2).
-  
-  Definition one := S \o O.
-  Definition two := S \o S \o O.
-  
   Inductive step : object -> object -> Prop :=
   | id_l   {a}     : step (I \o a) a
   | id_r   {a}     : step (a \o I) a
@@ -42,6 +36,8 @@ Section CPL.
   | s_r    {a b c} : step b c -> step (a \o b) (a \o c) *)
   | dot_al {a b c} : step ((a \o b) \o c) (a \o (b \o c))
   | dot_ar {a b c} : step (a \o (b \o c)) ((a \o b) \o c)
+  | prod_c {a b}   : step (prod (a, b))
+                          (pair (a \o pi1, b \o pi2))
   | pair_c {a b c} : step (pair (a, b) \o c)
                           (pair (a \o c, b \o c))
   | pair_l {a b}   : step (pi1 \o pair (a, b)) a
@@ -49,10 +45,13 @@ Section CPL.
   | pr_l   {a b}   : step (     pr (a, b) \o S)
                           (b \o pr (a, b))
   | pr_r   {a b}   : step (     pr (a, b) \o O) a
-  | ev_a   {a b c} : step (ev \o pair (cur(a) \o c, b))
+  | ev_d   {a}     : step (ev \o prod (cur a, I)) a
+  | ev_a   {a b c} : step (ev \o pair (cur a \o c, b))
                           (a  \o pair (c, b))
+  | exp_e  {a b}   : step (exp (a, b))
+                          (cur b \o ev \o prod (I, a))
   .
-
+  
   Axiom AX : forall {a b}, step a b -> a = b.
   
   Lemma ida1 a : a \o I = a.
@@ -70,6 +69,11 @@ Section CPL.
       by apply: (AX dot_al).
   Qed.
   
+  Lemma prodC a b : prod (a, b) = pair (a \o pi1, b \o pi2).
+  Proof.
+      by apply: (AX prod_c).
+  Qed.
+
   Lemma pairC a b c : pair (a, b) \o c = pair (a \o c, b \o c).
   Proof.
       by apply: (AX pair_c).
@@ -107,6 +111,34 @@ Section CPL.
       by apply: eval.
   Qed.
   
+(**
+sample
+*)  
+  
+  Definition one := S \o O.
+  Definition two := S \o S \o O.
+
+  Definition double := pr (O, S \o S).
+
+  Goal double \o one = two.
+  Proof.
+    rewrite /double /one.
+    rewrite -!dotA.                         (* 左結合 *)
+    rewrite prL.
+    rewrite !dotA.                          (* 右結合 *)
+    rewrite prR.
+    rewrite /two.
+    rewrite !dotA.                          (* 右結合 *)
+    done.
+  Qed.
+  
+(**
+add
+*)
+  Definition add := ev \o pair (pr (cur pi2,
+                                    cur (S \o ev)) \o pi1,
+                                pi2).
+  
   Goal add \o pair (one, one) = two.
   Proof.
     rewrite /add /one.
@@ -127,6 +159,52 @@ Section CPL.
     done.
   Qed.
 
+  Definition add_alt := ev \o prod (pr (cur pi2, cur (S \o ev)), I).
+  
+  Goal add_alt \o pair (one, one) = two.
+  Proof.
+    rewrite /add /one.
+    rewrite !dotA.                          (* 右結合 *)
+    rewrite prodC.
+    rewrite pairC.
+    rewrite !dotA.                          (* 右結合 *)
+    rewrite pairL.
+    rewrite -!dotA.                         (* 左結合 *)
+    rewrite prL.
+    rewrite !dotA.                          (* 右結合 *)
+    rewrite prR.
+    rewrite eval.
+    rewrite !dotA.                          (* 右結合 *)
+    rewrite eval1.
+    rewrite !pairR.
+    rewrite /two.
+    rewrite !dotA.                          (* 右結合 *)
+    rewrite id1a.
+    done.
+  Qed.
+  
+(**
+mult
+*)  
+  Definition mult := ev \o prod (pr (cur (O \o I),
+                                     cur (add \o pair (ev, pi2))),
+                                 I).
+
+(**
+fact
+*)
+  Definition fact := pi1 \o pr (pair (S \o O, O),
+                                pair (mult \o pair (S \o pi2,
+                                                    pi1),
+                                      S \o pi2)).
+
 End CPL.
+
+
+id)
+mult = ev.prod(pr(curry(zero.!),
+curry(add.pair(ev, pi2))), id)
+fact = pi1.pr(pair(s.zero, zero),
+pair(mult.pair(s.pi2, pi1), s.pi2))
 
 (* END *)
