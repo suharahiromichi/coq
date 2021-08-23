cc4177ec87ea6bb68cdc0abd25054e5101a18345
SUHARA Hiromichi <suhara@falla.vbox>
2021-08-23 23:43:23 +0900

    説明を追加した。

--- math/ssr_ackermann.v
+++ math/ssr_ackermann.v
@@ -1,14 +1,24 @@
+(**
+アッカーマン Ackermann 関数は、
+高階原始帰納的関数（primitive recursiveな汎関数）として定義できる。
+*)
+
 From mathcomp Require Import all_ssreflect.
+(*
 Require Import ssromega.
 Require Import FunInd.                      (* Functional Scheme *)
 Require Import Recdef.                      (* Function *)
 Require Import Wf_nat.                      (* wf *)
+*)
 
 Set Implicit Arguments.
 Unset Strict Implicit.
 Unset Printing Implicit Defensive.
 (* Set Print All. *)
 
+(**
+# Coq での定義
+*)
 Module Ack1.
 
   Fail Fixpoint ack_bad (n m : nat) : nat :=
@@ -43,6 +53,11 @@ Compute Ack1.ack 1 2.                       (* 4 *)
 Compute Ack1.ack 2 3.                       (* 9 *)
 Compute Ack1.ack 3 4.                       (* 125 *)
 
+(**
+# 原始帰納法
+
+pr2のほうが使いやすいが、prのほうが圏論的には正しい？
+*)
 Section Pr.
 
   Variable T : Type.
@@ -52,57 +67,62 @@ Section Pr.
     | 0 => f
     | n'.+1 => g \o (pr n' f g)
     end.
-
-(*
+(**
   (pr 0 f g) x = f x
   (pr 1 f g) x = g (f x)
   (pr 2 f g) x = g (g (f x)).
 *)
 
+  Fixpoint pr2 (n : nat) (x : T) (g : T -> T) : T :=
+    match n with
+    | 0 => x
+    | n'.+1 => g (pr2 n' x g)
+    end.
+(**
+  (pr2 0 x g) x = x
+  (pr2 1 x g) x = g x
+  (pr2 2 x g) x = g (g x).
+*)
 End Pr.
 
+(**
+# 高階原始帰納法による Ackermann 関数の定義
+
+高階原始帰納的関数（primitive recursiveな汎関数）として定義する。
+*)
 Section Ack2.
 (**
 ack' f 0 = f 1
 ack' f 1 = f (f 1)
 ack' f 2 = f (f (f 1))
-*)
-  
-  Definition ack' (f : nat -> nat) (m : nat) : nat :=
-    (pr m f f) 1.
 
+ack' は pr でも pr2 でも定義できるが。
+*)
+  Definition ack' (f : nat -> nat) (m : nat) : nat := (pr m f f) 1.
+  Definition ack'' (f : nat -> nat) (m : nat) : nat := pr2 m (f 1) f.
+  
   Compute ack' S 10.                        (* 12 *)
+  Compute ack'' S 10.                       (* 12 *)
+  
 
 (**
-ここまでは正しそう。。。。。
- *)
-
-  Section Test.
-    Variable m : nat.
-
-    Check fun m => m.+1.                            (* n = 0 *)
-    Check ack' (fun m => m.+1) m.                   (* n = 1 *)
-    Check ack' (fun m => ack' (fun m => m.+1) m) m. (* n = 2 *)
-  End Test.
-  Compute (ack' (fun m => m.+1) 2).         (* n = 1, m = 2, 4 *)
-  Compute (ack' (fun m => ack' (fun m => m.+1) m)) 3. (* n = 2, m = 3, 9 *)
-  
-  Check ack' (ack' (fun m => m.+1)).
-  Compute (ack' (ack' (fun m => m.+1))) 3.  (* 9 *)
-
-
-  Fixpoint pr2 (n : nat) (f : nat -> nat) (g : (nat -> nat) -> nat -> nat) :=
-    match n with
-    | 0 => f
-    | n'.+1 => g (pr2 n' f g)
-    end.
-
-  Definition ack n m :=
-    (pr2 n (fun m => m.+1) ack') m.
+ack は pr2 でないと難しい。
+*)
+  Definition ack n m := (pr2 n (fun m => m.+1) ack'') m.
 
   Compute ack 1 2.                          (* 4 *)
   Compute ack 2 3.                          (* 9 *)
-
+  Compute ack 3 4.                          (* 125 *)
+  
 End Ack2.
+
+Section Th.
+
+  Lemma test n m : Ack1.ack n m = ack n m.
+  Proof.
+    elim: n m => //= n IHn m.
+  Admitted.
+
+End Th.
 
 (* END *)
