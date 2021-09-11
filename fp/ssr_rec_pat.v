ef353f83ef6aae5169a02d67367e4148666e632d
SUHARA Hiromichi <suhara@falla.vbox>
2021-09-11 19:24:13 +0900

    型情報を追加した。

--- fp/ssr_rec_pat.v
+++ fp/ssr_rec_pat.v
@@ -40,57 +40,63 @@ End Homomorphism.
 
 Section Function.
 
-  Variable T S : Type.
-  Variable d : T.                           (* ダミー値 *)
+  Variable A B T : Type.
+  Variable _a : A.                          (* ダミー値 *)
+  Variable _b : B.                          (* ダミー値 *)
   
-  Definition cataLength : seq _ -> nat :=
-    cata 0 (fun (_ n : nat) => n.+1).
+  Definition cataLength : seq A -> nat :=
+    cata 0 (fun (_ : A) (n : nat) => n.+1).
   
-  Definition cataFilter (p : _ -> bool) : seq nat -> seq nat :=
-    cata [::] (fun (a : nat) (a' : seq nat) => if (p a) then (a :: a') else a').
-
-  Definition unsp (s : (seq nat * seq nat)) : (nat * nat) * (seq nat * seq nat) :=
+  Definition cataFilter (p : A -> bool) : seq A -> seq A :=
+    cata [::] (fun (a : A) (a' : seq A) => if (p a) then (a :: a') else a').
+  
+  Definition unsp (s : (seq A * seq B)) : (A * B) * (seq A * seq B) :=
     match s with
     | ((a :: a'), (b :: b')) => ((a, b), (a', b'))
-    | _ => ((0, 0), ([::], [::]))
+    | _ => ((_a, _b), ([::], [::]))
     end.
   
-  Definition fin (s : (seq nat * seq nat)) : bool :=
+  Definition fin (s : (seq A * seq B)) : bool :=
     match s with
     | ([::], _) => true
     | (_, [::]) => true
     | _ => false
     end.
 
-  Definition anaZip := ana 10 unsp fin.
-
-  Definition anaIterate (f : T -> T) :=
+  Definition anaZip : seq A * seq B -> seq (A * B) := ana 10 unsp fin.
+  
+  Definition anaIterate (f : A -> A) : A -> seq A :=
     ana 10 (fun a => (a, f a)) xpred0.      (* (fun _ => false) *)
   
-  Definition cataMap (f : T -> S) :=
+  Definition cataMap (f : A -> B) : seq A -> seq B :=
     cata [::] (fun a a' => (f a) :: a').
   
-  Definition anaMap (f : T -> T) :=
+  Definition anaMap (f : A -> A) : seq A -> seq A :=
     ana 10
         (fun s => match s with
                   | a :: a' => (f a, a')
-                  | _ => (d, [::])
+                  | _ => (_a, [::])
                   end)
         (* fun s => s == [::] ではエラーになる。  *)
         (fun s => match s with
                   | [::] => true
                   | _ => false
                   end).
+  
 End Function.
 
 Section Examples.
   
   Compute cataLength [:: 1; 2; 3; 4].       (* 4 *)
+  
   Compute cataFilter odd [:: 1; 2; 3; 4].   (* [:: 1; 3] *)
+  
   Compute anaZip ([:: 1; 3; 5], [:: 2; 4; 6]). (* [:: (1, 2); (3, 4); (5, 6)] *)
+  
   Compute anaIterate succn 0.  (* [:: 0; 1; 2; 3; 4; 5; 6; 7; 8; 9] *)
+  
   Compute cataMap succn [:: 0; 1; 2; 3; 4]. (* [:: 1; 2; 3; 4; 5] *)
-
+  
 End Examples.
 
 (* END *)
