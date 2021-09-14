1cade544383880afe491aad9cc423bcc322a97c9
SUHARA Hiromichi <suhara@falla.vbox>
2021-09-14 19:02:33 +0900

    ssr_rec_pat_2.v を分離した。

--- fp/ssr_rec_pat.v
+++ fp/ssr_rec_pat.v
@@ -1,10 +1,15 @@
 (* -*- coding: utf-8 -*- *)
 (**
+有限リストで表現するが、停止条件を外から与える再帰は、停止性が証明できないため、
+Coqでは扱いにくい。
+
+無限リストによる unfold の定義は、 ``ssr_rec_pat_2.v`` を参照のこと。
+*)
+
+(**
 # 再帰のパターン
 
-maoe のブログ
-
-- 再帰のパターン
+- 再帰のパターン maoe のブログ
 
 https://maoe.hatenadiary.jp/entry/20090820/1250782646
 
@@ -24,7 +29,7 @@ Section Homomorphism.
   Variable _a : A.                          (* ダミー値 *)
   
 (**
-Catamorphism
+## Catamorphism
 *)
   Fixpoint cata (b : B) (f : A -> B -> B) (s : seq A) : B :=
     match s with
@@ -33,7 +38,7 @@ Catamorphism
     end.
   
 (**
-Anamorphism - ダミー変数を使って定義する。
+## Anamorphism - ダミー変数を使って定義する。
 *)
   Fixpoint ana (h : nat) (g : B -> A * B) (p : pred B) (b : B) : seq A :=
     match h with
@@ -43,7 +48,7 @@ Anamorphism - ダミー変数を使って定義する。
     end.
 
 (**
-Hylomorphism - ダミー変数を使って定義する。
+## Hylomorphism - ダミー変数を使って定義する。
 *)
   Fixpoint hylo (h : nat) (c : A) (f : B -> A -> A)
            (g : A -> B * A) (p : pred A) (a : A) : A :=
@@ -55,7 +60,7 @@ Hylomorphism - ダミー変数を使って定義する。
     end.
   
 (**
-Paramorphism
+## Paramorphism
 *)
   Fixpoint numPara (b : B) (f : nat -> B -> B) (n : nat) : B := (* 数 *)
     match n with
@@ -179,278 +184,8 @@ Section TH.
   
 End TH.
 
-(**
-# 余帰納法による Anamorphism
-
-Ryusei’s Notes (a.k.a. M59のブログ)
-
-- 無限リストと証明じゃないCoqの話
-
-https://mandel59.hateblo.jp/entry/2013/02/07/213230
-
-- Coq 帰納法と余帰納法
-
-https://mandel59.hateblo.jp/entry/2013/02/09/151347  
- *)
-Require Import Coq.Lists.Streams.
 (*
-Section Streams.
-
-  Variable A : Type.
-
-  CoInductive Stream : Type := Cons : A -> Stream -> Stream.
-
-  Definition hd (x : Stream) :=
-    match x with
-    | Cons a _ => a
-    end.
-
-  Definition tl (x : Stream) :=
-    match x with
-    | Cons _ xs =>xs
-    end.
-  
-(**
-Streamどうしが等しいことを余帰納的に定義する述語
-*)
-  CoInductive EqSt (s1 s2 : Stream) : Prop :=
-  | eqst : hd s1 = hd s2 -> EqSt (tl s1) (tl s2) -> EqSt s1 s2.
-  
-  CoFixpoint EqSt_reflex (s : Stream) : EqSt s s :=
-    eqst (erefl (hd s)) (EqSt_reflex (tl s)).
-
-End Streams.
-*)
-Section MyStreams.
-  
-  Variable A B : Type.
-
-  CoFixpoint iterate (f : A -> A) (x : A) : Stream A :=
-    Cons x (iterate f (f x)).
-
-  Fixpoint takeStream (n : nat) (s : Stream A) : list A :=
-    match n with
-    | O => nil
-    | S n1 => (hd s) :: takeStream n1 (tl s)
-    end.
-  
-(**
-Anamorphism - coFix で定義した。
- *)
-  CoFixpoint unfold (f : B -> A * B) (b : B) : Stream A :=
-    let (a, b') := f b in Cons a (unfold f b').
-  
-  CoFixpoint unfold' (f : A * B -> A * B) (b : A * B) : Stream A :=
-    let (a, b') := f b in Cons a (unfold' f (a, b')).
-  
-  (* npca2012年部誌KOF号_データ構造の畳み込みと展開 *)
-  (* unfold' と同じだが、自由度が少し高い版 *)
-  CoFixpoint unfold'' (f : B -> A) (g : B -> B) (b : B) : Stream A :=
-    Cons (f b) (unfold'' f g (g b)).
-  
-(**
-Streamの先頭にlistを連結するapp。
-*)
-  Fixpoint appStream (l : list A) (s : Stream A) : Stream A :=
-    match l with
-    | [::] => s
-    | a :: l1 => Cons a (appStream l1 s)
-    end.
-
-(**
-Streamの先頭n要素を捨てるdrop。
-*)
-  Fixpoint dropStream (n : nat) (s : Stream A) : Stream A :=
-    match n with
-      | O => s
-      | S n1 =>
-        match s with
-        | Cons _ s1 => dropStream n1 s1
-        end
-    end.
-
-(**  
-Streamからtakeしてから、dropした残りのStreamにappすると元のストリームに戻ることの証明。
-*)
-  Theorem app_take : forall (n : nat) (s : Stream A),
-      EqSt (appStream (takeStream n s) (dropStream n s)) s.
-  Proof.
-    move=> n.
-    induction n as [|n' IH].
-    - by apply EqSt_reflex.
-    - case=> a s' /=.
-        by apply: eqst => /=.
-  Qed.
-
-(**  
-listをStreamにappしてからtakeすると元のlistが得られることの証明。
-*)
-  Theorem take_app : forall (l : list A) (s : Stream A),
-      (takeStream (length l) (appStream l s)) = l.
-  Proof.
-    induction l as [|a l' IH].
-    - done.
-    - case=> a0 s0 /=.
-        by apply f_equal.
-  Qed.
-
-(**  
-iterate id a が const a に等しいことの証明。
-
-これは余帰納法を使わないといけない。
-Stream A型を返す関数で、余再帰的に定義されているconstやiterateを簡約するには、
-simplを使う前に補題 unfold_Stream を使って書き換えておく必要があった。
-*)
-  Theorem iterate_id (a : A) :
-    EqSt (iterate id a) (const a).
-  Proof.
-    cofix iterate_id.
-    rewrite (unfold_Stream (iterate id a)).
-    rewrite (unfold_Stream (const a)).
-    simpl.
-    apply eqst.
-    - done.
-    - by apply iterate_id.
-  Qed.
-
-End MyStreams.  
-
-(**
-zip を unfold で定義する。suhara
-*)
-Section Function2.
-
-  Compute unsp 0 0  ([:: 1; 3; 5], [:: 2; 4; 6]). (* 0 はダミー値 _a, _b *)
-  
-  Definition zipStream s t := takeStream (length s) (unfold (unsp 0 0) (s, t)).
-  Check zipStream [:: 1; 3; 5] [:: 2; 4; 6].
-  Compute zipStream [:: 1; 3; 5] [:: 2; 4; 6]. (* = [:: (1, 2); (3, 4); (5, 6)] *)
-  
-  Function myZip (s t : seq nat) : seq (nat * nat):=
-    match s with
-    | [::] => [::]
-    | x :: s' => match t with
-                 | [::] => [::]
-                 | y :: t' => (x, y) :: myZip s' t'
-                 end
-    end.
-    Compute myZip [:: 1; 3; 5] [:: 2; 4; 6].  (* = [:: (1, 2); (3, 4); (5, 6)] *)
-    
-    Lemma zip_ok (s t : seq nat) : zipStream s t = myZip s t.
-    Proof.
-      functional induction (myZip s t).
-      - admit.
-      - admit.
-      - rewrite /zipStream.
-    Admitted.
-
-End Function2.
-
-(**
-Hylomorphism - coFix で定義する。suhara
-*)
-Section MyStreams2.
-
-  Variable A B : Type.
-  
-  Fixpoint fold (h : nat) (b : B) (f : A -> B -> B) (s : Stream A) : B :=
-    match h with
-    | 0 => b
-    | h'.+1 => f (hd s) (fold h' b f (tl s))
-    end.
-  
-  Definition foldunfold h (b1 b2 : B) g f := fold h b1 g (unfold f b2).
-  
-End MyStreams2.
-
-Section Summation.
-
-  Definition gen := unfold (fun n => (n, n.+1)).
-  Compute takeStream 10 (gen 1). (* = [:: 1; 2; 3; 4; 5; 6; 7; 8; 9; 10] *)
-
-(**
-## Summuation
-*)
-  Definition sum h := fold h 0 addn.
-  Definition sumi m n := sum n (gen m).
-  Compute sumi 1 3.                         (* 6 *)
-  Compute sumi 1 4.                         (* 10 *)
-  Compute sumi 1 5.                         (* 15 *)
-  Compute sumi 1 10.                        (* 55 *)
-  
-(**
-## Factorial
-*)
-  Definition mul h := fold h 1 muln.
-  Definition fact m n := mul n (gen m).
-  Compute fact 1 3.                         (* 6 *)
-  Compute fact 1 4.                         (* 24 *)
-  Compute fact 1 5.                         (* 120 *)
-  
-End Summation.
-
-(**
-*)
-Section Fibonacci.
-
-(*
-  CoFixpoint unfold' (f : nat * nat -> nat * nat) (b : nat * nat) : Stream nat :=
-    let (a, b') := f b in Cons a (unfold' f (a, b')).
-*)
-  Definition psi (a : nat * nat) := let (x, y) := a in (y, x + y).
-
-  Definition fibgen := unfold' psi (0, 1).
-  Compute takeStream 10 fibgen. (* = [:: 1; 1; 2; 3; 5; 8; 13; 21; 34; 55] *)
-
-  Definition fibgen' := unfold'' fst psi (0, 1).
-  Compute takeStream 10 fibgen'. (* = [:: 0; 1; 1; 2; 3; 5; 8; 13; 21; 34] *)
-  
-End Fibonacci.
-
-Section Collatz.
-
-  Definition collatz :=
-    unfold'' id (fun x => if odd x then (x * 3).+1 else x %/2).
-  Compute takeStream 20 (collatz 11).
-  (* = [:: 11; 34; 17; 52; 26; 13; 40; 20; 10; 5; 16; 8; 4; 2; 1; 4; 2; 1; 4; 2] *)
-
-  Definition collatz' :=
-    unfold'' id (fun x => match x with
-                          | None => None
-                          | Some 1 => None
-                          | Some x' => Some (if odd x' then (x' * 3).+1 else x' %/2)
-                          end).
-  Compute takeStream 20 (collatz' (Some 11)).
-  (* = [:: Some 11; Some 34; Some 17; Some 52; Some 26; Some 13; 
-     Some 40; Some 20; Some 10; Some 5; Some 16; Some 8; 
-     Some 4; Some 2; Some 1; None; None; None; None; None]
-   *)
-  
-End Collatz.
-
-Section Sort.
-  (*
-    ssort :: Ord a => [a] -> [a]
-    ssort = unfoldr' (==[]) minimum (\xs->delete (minimum xs) xs)
-    *Main> ssort [1,8,3,5,2]
-    [1,2,3,5,8]
-   *)
-End Sort.
-
-(**
-# Catamorphism ふたたび
-
-- Simon Robillard, "Catamorphism Generation and Fusion Using Coq"
-*)
-
-(* 
-T.B.D.
- *)
-
-(* **************************************************** *)
-(* **************************************************** *)
-(*
-ダミー変数のためにうまくいかないzipの証明
+# ダミー変数のためにうまくいかないzipの証明
 *)
 Section NG.
 
@@ -458,7 +193,6 @@ Section NG.
   Variable _a : A.
   Variable _b : B.
   
-(*
   Function myZip (s : seq A) (t : seq B) : seq (A * B):=
     match s with
     | [::] => [::]
@@ -467,7 +201,7 @@ Section NG.
                  | y :: t' => (x, y) :: myZip s' t'
                  end
     end.
-*)  
+  
   Lemma l_zip_nil_l (h : nat) (t : seq B) :
     0 < h  ->
     ana h (unsp _a _b) (fin (B:=B)) ([::], t) = [::].
@@ -507,7 +241,8 @@ Section NG.
       + admit.
   Admitted.
 
-
 End NG.
+
+Compute myZip [:: 1; 3; 5] [:: 2; 4; 6].  (* = [:: (1, 2); (3, 4); (5, 6)] *)
 
 (* END *)
