c71fc084b7fc4ef457434ddd98992628e58bbe33
SUHARA Hiromichi <suhara@falla.vbox>
2021-09-23 14:05:26 +0900

    説明を追加した。

--- fp/ssr_eqchain_test.v
+++ fp/ssr_eqchain_test.v
@@ -1,5 +1,16 @@
 (**
-# EqChain
+連鎖記法の導入と使用例
+========================
+
+@suharahiromichi
+
+2020/09/23
+ *)
+
+(**
+# EqChain について
+
+等号「=」についての連鎖記法を導入します。
 
 - Julien Tesson, Hideki Hashimoto, Zhenjiang Hu, Frederic Loulergue,
 and Masato Takeichi, "Program Calculation in Coq"
@@ -11,6 +22,9 @@ and Masato Takeichi, "Program Calculation in Coq"
 eqchain.v
  *)
 
+(* ******************* *)
+(** eqchan.v ここから。 *)
+(* ******************* *)
 Require Import Coq.Program.Basics.
 
 Module EqChain.
@@ -105,15 +119,20 @@ Tactic Notation (at level 2) "=" "Right" "{" tactic(t) "}" :=
   | [_ := EqChain.direction EqChain.Rightwards : Prop |- _ _ ?r] =>
     EqChain.eq_rewrite_l r t; reflexivity
   end.
+(* ******************* *)
+(** eqchan.v ここまで *)
+(* ******************* *)
 
 (**
-# テスト
+# 例
 *)
 From mathcomp Require Import all_ssreflect.
 From mathcomp Require Import all_algebra.
-Require String.
 
 Section Test.
+(**
+## 簡単な例
+*)
   
   Goal 1 = 1.
   Proof.
@@ -128,7 +147,12 @@ Section Test.
     = (1)   {rewrite addn0}.
     = Right {done}.
   Qed.
-  
+
+(**
+## すこし大きな例
+
+普通の証明
+*)
   Lemma square_eq' a b : (a + b) * (a + b) = a * a + 2 * a * b + b * b.
   Proof.
     rewrite [LHS]mulnDr.
@@ -143,7 +167,38 @@ Section Test.
     rewrite [LHS]addnA.
     done.
   Qed.
-  
+
+(**
+have の連鎖で証明する例。左辺の書き換えを追えるが、転記が必要である。
+*)
+  Lemma square_eq'' a b : (a + b) * (a + b) = a * a + 2 * a * b + b * b.
+  Proof.
+    have -> : (a + b) * (a + b) = (a + b) * a + (a + b) * b
+      by rewrite [LHS]mulnDr.
+    have -> : (a + b) * a + (a + b) * b = a * a + b * a + (a * b + b * b)
+      by rewrite 2![in LHS]mulnDl.
+    have -> : a * a + b * a + (a * b + b * b) = a * a + b * a + a * b + b * b
+      by rewrite [LHS]addnA.
+    have -> : a * a + b * a + a * b + b * b = a * a + a * b + a * b + b * b
+      by rewrite [in LHS]mulnC.
+    have -> : a * a + a * b + a * b + b * b = a * a + (a * b + (a * b + b * b))
+      by rewrite -2![LHS]addnA.
+    have -> : a * a + (a * b + (a * b + b * b)) = a * a + (a * b + a * b + b * b)
+      by rewrite [(a * b + (a * b + b * b))]addnA.
+    have -> : a * a + (a * b + a * b + b * b) = a * a + ((a * b).*2 + b * b)
+      by rewrite addnn.
+    have -> : a * a + ((a * b).*2 + b * b) = a * a + (a.*2 * b + b * b)
+      by rewrite doubleMl.
+    have -> : a * a + (a.*2 * b + b * b) = a * a + (2 * a * b + b * b)
+      by rewrite -mul2n.
+    have -> : a * a + (2 * a * b + b * b) = a * a + 2 * a * b + b * b
+      by rewrite addnA.
+    done.
+  Qed.
+
+(**
+等号による連鎖記法の例
+*)  
   Lemma square_eq a b : (a + b) * (a + b) = a * a + 2 * a * b + b * b.
   Proof.
     Left
@@ -162,6 +217,9 @@ Section Test.
   
 End Test.
 
+(**
+## 整数の例
+*)
 Section Test2.
 
   Import GRing.Theory.        (* mulrA などを使えるようにする。 *)
@@ -185,79 +243,10 @@ Section Test2.
     Left
     = (q * p / p)   {rewrite [p * q]mulrC}.
     = (q * (p / p)) {rewrite -mulrA}.
-    = (q * 1)       {rewrite divrr; last by rewrite unitf_gt0}.
+    = (q * 1)       {rewrite divrr; last by rewrite unitf_gt0}. (* サブゴールは作らぬ *)
     = Right         {by rewrite mulr1}.
   Qed.
 
 End Test2.
 
-(**
-ゴールそのものを転記できてもよいのではないか。
-*)
-
-Section Test3.
-  
-  Definition var := 'I_3.                     (* nat *)
-  Notation symbol := String.string.
-  Definition union (vl1 vl2 : {set var}) := vl1 :|: vl2.
-  
-  Inductive tree (* : Set *) :=
-  | Var : var -> tree
-  | Sym : symbol -> tree
-  | Fork : tree -> tree -> tree.
-  
-  Fixpoint subs (x : var) (t t' : tree) : tree :=
-    match t' with
-    | Var v => if v == x then t else t'
-    | Sym b => Sym b
-    | Fork t1 t2 => Fork (subs x t t1) (subs x t t2)
-    end.
-
-  Fixpoint vars (t : tree) : {set var} :=     (* tree -> {set var} *)
-    match t with
-    | Var x => [set x]
-    | Sym _ => set0
-    | Fork t1 t2 => union (vars t1) (vars t2)
-  end.
-  
-  Fixpoint vars_pairs (l : list (tree * tree)) : {set var} :=
-    match l with
-    | nil => set0                     (* 集合を後部から作るようにする *)
-    | (t1, t2) :: r => union (union (vars t1) (vars t2)) (vars_pairs r)
-    end.
-
-  Definition subs_pair x t (p : tree * tree) := (subs x t p.1, subs x t p.2).
-
-Lemma subs_pairs_sub x t l :                (* set *)
-  {subset vars_pairs (map (subs_pair x t) l) <= union (vars t) (vars_pairs l)}.
-(* *** *)
-Admitted.
-
-  Lemma l_vars_pairs_subset x t l :         (* set *)
-    x \notin (vars t) ->
-    x |: vars_pairs (map (subs_pair x t) l) \subset vars_pairs ((Var x, t) :: l).
-  Proof.
-    move=> Hx.
-    apply/subsetP.
-    move=> /= y /setU1P.
-    case.
-    + move=> ->.
-      rewrite /union.
-      rewrite -setUA.
-      apply/setU1P.
-        by left.
-    + move/subs_pairs_sub.
-      rewrite /union.
-      move/setUP => [H1 | H2].
-      * rewrite -setUA.
-        apply/setU1P.
-        right.
-        apply/setUP.
-          by left.
-      *  rewrite -setUA.
-         apply/setU1P.
-         right.
-         apply/setUP.
-           by right.
-  Qed.
-  
+(* END *)
