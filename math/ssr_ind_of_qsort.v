8da682670812d12eea5d0755073f62e9a5262533
SUHARA Hiromichi <suhara@falla.vbox>
2021-10-17 11:57:39 +0900

    説明を追加した。

--- math/ssr_ind_of_qsort.v
+++ math/ssr_ind_of_qsort.v
@@ -20,14 +20,41 @@ Unset Printing Implicit Defensive.
 
 Require Import Recdef.
 
+Section filter.
+  Variable T : Type.
+
+(**
+## filer関数についての補題
+*)
+(*
+  Lemma filter_size {x : T} {s : seq T} (R' : rel T) :
+    size [seq y <- s | R' y x] <= size s.
+  Proof.
+    elim: s => //= y s IHs.
+    case H : (R' y x) => //=.
+      by ssromega.
+  Qed.
+*)
+  Lemma filter_size {x : T} {s : seq T} (R : rel T) :
+    size [seq y <- s | R y x] <= size s.
+  Proof.
+      by rewrite size_filter count_size.
+  Qed.
+
+End filter.
+
 Section qsort.
   Variable T : Type.
   Variable R : rel T.
 
+(**
+## Quick Sort 関数
+*)
   Function qsort (s : seq T) {measure size s} :=
-    if s is x :: s'
-    then qsort [seq y <- s' | R y x] ++ x :: qsort [seq y <- s' | ~~ R y x]
-    else [::].
+    if s is x :: s' then
+      qsort [seq y <- s' | R y x] ++ x :: qsort [seq y <- s' | ~~ R y x]
+    else
+      [::].
   Proof.
     - move => _ x s _ /=. apply /ltP.
         by rewrite ltnS size_filter count_size.
@@ -35,41 +62,44 @@ Section qsort.
         by rewrite ltnS size_filter count_size.
   Defined.
 
-  Lemma ubnP m : {n | m < n}.               (* ssrnat.v *)
-  Proof. by exists m.+1. Qed.
-  
-  Lemma filter_size (x : T) (s : seq T) : size [seq y <- s | R y x] <= size s.
-  Proof.
-    elim: s => //= y s IHs.
-    case H : (R y x) => //=.
-      by ssromega.
-  Qed.
-  
-  Lemma filter_size' (x : T) (s : seq T) : size [seq y <- s | ~~ R y x] <= size s.
-  Proof.
-    elim: s => //= y s IHs.
-    case H : (R y x) => //=.
-      by ssromega.
-  Qed.
-  
 (**
 ## Function コマンドのつくる qsort_ind は複雑なので、自作する。
 *)
+  Lemma ubnP m : {n | m < n}.     (* 最新の ssrnat.v で導入された。 *)
+  Proof. by exists m.+1. Qed.
+  
   Lemma my_qsort_ind (P : seq T -> Prop) :
     P [::] ->
     (forall x s, P [seq y <- s | R y x] ->
                  P [seq y <- s | ~~ R y x] -> P (x :: s)) ->
     forall s, P s.
   Proof.
-    move => Hnil Hcons s.
+    move=> Hnil Hcons s.
+(*
+  シノニムを使う例（避けるべき）：
+    elim : s {-2}s (leqnn (size s)) => [|xs s IHs] [] //= xl l Hsize.
+    Undo 1.
+    elim : (size s) {-2}s (leqnn (size s)) => [|n IHn] [] //= xl l Hsize.
+    Undo 1.
+*)
+    (* リストの長さに対する帰納法をおこなう。 *)
     have [n] := ubnP (size s).
-    elim: n s =>[| n IHn] [| xs s] //= Hsize.
+    elim: n s => [| n IHn] [| xs s] //= Hsize.
     apply: Hcons.
-    Check leq_ltn_trans (filter_size xs s).
-    - exact: IHn (leq_ltn_trans (filter_size _ _) Hsize).
-    - exact: IHn (leq_ltn_trans (filter_size' _ _) Hsize).
+    Check leq_ltn_trans (filter_size R).
+    - exact: IHn (leq_ltn_trans (filter_size R) Hsize).
+    - exact: IHn (leq_ltn_trans (filter_size (fun x y => ~~ R x y)) Hsize).
   Qed.
   
+(**
+## ソートされている状態かどうかを判定する述語
+*)
+  Fixpoint mysorted (s : seq T) :=
+    if s is x :: s' then
+      all (fun y => ~~ R y x || R x y) s' && mysorted s'
+    else
+      true.
+
 (**
 ## 補題
 *)
@@ -83,15 +113,6 @@ Section qsort.
     exact: andbF.
   Qed.
 
-(**
-## ソートされている状態
-*)
-  Fixpoint mysorted (s : seq T) :=
-    if s is x :: s' then
-      all (fun y => ~~ R y x || R x y) s' && mysorted s'
-    else
-      true.
-
   Lemma mysorted_cat s1 s2 :
     mysorted s1 -> mysorted s2 ->
     all (fun x => all (fun y => ~~ R y x || R x y) s2) s1 ->
@@ -103,7 +124,7 @@ Section qsort.
   Qed.
 
 (**
-## ソートであることの証明
+## qsort関数によって、ソートされることの証明
  *)
   Hypothesis (Htrans : transitive R).
 
