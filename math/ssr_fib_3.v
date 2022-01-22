d3f79240156f9b0c1aca0ad9158355f4aba16b8a
SUHARA Hiromichi <suhara@falla.vbox>
2022-01-22 09:44:54 +0900

    説明を追加した。

--- math/ssr_fib_3.v
+++ math/ssr_fib_3.v
@@ -1,3 +1,8 @@
+(**
+フィボナッチ数の最大公約数
+
+GCD of Fibonacci Numbers
+*)
 From mathcomp Require Import all_ssreflect.
 Require Import ssromega.
 Require Import FunInd.                      (* Functional Scheme *)
@@ -11,37 +16,15 @@ Unset Printing Implicit Defensive.
 
 Section Fib3.
 
+(**  
+# フィボナッチ数の定義と定理
+*)
   Function fib (n : nat) : nat :=
     match n with
     | 0 => 0
     | 1 => 1
     | (m.+1 as pn).+1 => fib m + fib pn (* fib n.-2 + fib n.-1 *)
     end.
-  
-  Fail Functional Scheme gcdn_ind := Induction for gcdn Sort Prop.
-  (* Error: A fixpoint needs at least one parameter. *)
-  
-  Function gcd (m n : nat) {wf lt m} : nat :=
-    match m with
-    | 0 => n
-    | _ => gcd (n %% m) m
-    end.
-  Proof.
-    - move=> m n m0 _.
-      apply/ltP.
-        by rewrite ltn_mod.
-    - by apply: lt_wf.
-  Qed.
-  Check gcd_ind
-    : forall P : nat -> nat -> nat -> Prop,
-      (forall m n : nat, m = 0 -> P 0 n n) ->
-      (forall m n _x : nat,
-          m = _x ->
-          match _x with
-          | 0 => False
-          | _.+1 => True
-          end -> P (n %% m) m (gcd (n %% m) m) -> P _x n (gcd (n %% m) m)) ->
-      forall m n : nat, P m n (gcd m n).
   
   Lemma fib_n n : fib n.+2 = fib n + fib n.+1.
   Proof.
@@ -56,6 +39,39 @@ Section Fib3.
       by rewrite gcdnDr gcdnC.
   Qed.
   
+(**
+フィボナッチ数列の加法定理
+*)
+  Lemma fib_addition n m :
+    1 <= m -> fib (n + m) = fib m * fib n.+1 + fib m.-1 * fib n.
+  Proof.
+    (* see. ssr_fib_2.v *)
+  Admitted.                                 (* OK *)
+
+(**
+# GCDの定義と定理
+*)  
+  Fail Functional Scheme gcdn_ind := Induction for gcdn Sort Prop.
+  (* Error: A fixpoint needs at least one parameter. *)
+  
+(**
+仕方ないので Function を使って自分で定義する。
+*)
+  Function gcd (m n : nat) {wf lt m} : nat :=
+    match m with
+    | 0 => n
+    | _ => gcd (n %% m) m
+    end.
+  Proof.
+    - move=> m n m0 _.
+      apply/ltP.
+        by rewrite ltn_mod.
+    - by apply: lt_wf.
+  Qed.
+
+(**
+MathComp の gcdn を同じであることを証明する。
+*)  
   Lemma gcdE m n : gcdn m n = gcd m n.
   Proof.
     functional induction (gcd m n).
@@ -103,14 +119,8 @@ Section Fib3.
   Qed.
   
 (**
-フィボナッチ数列の加法定理
+# フィボナッチ数のGCD
 *)
-  Lemma fib_addition n m :
-    1 <= m -> fib (n + m) = fib m * fib n.+1 + fib m.-1 * fib n.
-  Proof.
-    (* see. ssr_fib_2.v *)
-  Admitted.                                 (* OK *)
-
   Lemma lemma5' m n :
     1 <= m -> gcd (fib (n + m)) (fib n) = gcd (fib m) (fib n).
   Proof.
@@ -151,6 +161,23 @@ Section Fib3.
     rewrite -divn_eq.
     done.
   Qed.
+
+(**
+# 証明したいもの
+*)
+  Compute gcdn 0 0.                         (* 0 *)
+  Compute gcdn 1 0.                         (* 1 *)
+  Compute gcdn 0 1.                         (* 1 *)
+  Compute gcdn 1 1.                         (* 1 *)
+  
+  Goal gcd (fib 1) (fib 0) = fib (gcd 1 0).
+  Proof.
+    rewrite gcd_equation.
+    simpl.
+    rewrite gcd_equation.
+    simpl.
+    done.
+  Qed.
   
   Lemma lemma9' (m n : nat) :
     (gcd (fib m) (fib n) = fib (gcd m n)).
@@ -169,20 +196,40 @@ Section Fib3.
       done.
   Qed.
   
-  Compute gcdn 0 0.                         (* 0 *)
-  Compute gcdn 1 0.                         (* 1 *)
-  Compute gcdn 0 1.                         (* 1 *)
-  Compute gcdn 1 1.                         (* 1 *)
-  
-  Goal gcd (fib 1) (fib 0) = fib (gcd 1 0).
-  Proof.
-    rewrite gcd_equation.
-    simpl.
-    rewrite gcd_equation.
-    simpl.
-    done.
-  Qed.
-  
 End Fib3.
+
+(**
+GCDの帰納法の証明
+*)
+Section Fib31.
+
+  Lemma gcdn_ind : forall P : nat -> nat -> nat -> Prop,
+    (forall m n : nat, m = 0 -> P 0 n n) ->
+    (forall m n : nat,
+          match m with
+          | 0 => False
+          | _.+1 => True
+          end -> P (n %% m) m (gcdn (n %% m) m) -> P m n (gcdn (n %% m) m)) ->
+    forall m n : nat, P m n (gcdn m n).
+  Proof.
+    move=> P H1 H2 m n.
+    rewrite gcdnE /=.
+    elim: n.
+    - case: m => //=.
+      + by apply: H1.
+      + move=> n.
+        rewrite mod0n gcd0n.
+        admit.
+    - case: m.
+      + move=> n //= H.
+          by apply: H1.
+      + move=> n n'.
+        case H : n.+1 => //=.
+        rewrite -H.
+        move=> Hc.
+        apply: H2 => //.
+  Admitted.
+  
+End Fib31.
 
 (* END *)
