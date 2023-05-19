fc2a643499b13de7d5c2cd98589ea96ec91f0b13
SUHARA Hiromichi <suhara@mesa.local>
2023-05-20 08:32:10 +0900

    ssr1_int.v と比較できるようにした。

--- ssr2/ssr2_int.v
+++ ssr2/ssr2_int.v
@@ -7,13 +7,16 @@ https://perso.crans.org/cohen/JFLA23/lesson2.v
 From elpi Require Import elpi.
 From HB Require Import structures.
 From mathcomp Require Import all_ssreflect.
-From mathcomp Require ssralg ssrnum zmodp. (* all_algebra *)
+From mathcomp Require all_algebra. (* 使うところは個別に Import する必要がある。 *)
+From mathcomp Require all_field.   (* 使うところは個別に Import する必要がある。 *)
+Require Import Psatz.                       (* lia *)
 
 Set Implicit Arguments.
 Unset Strict Implicit.
 Unset Printing Implicit Defensive.
 
-Import ssralg GRing.Theory.
+Import ssralg.
+Import GRing.Theory.
 Local Open Scope ring_scope.
 
 (**
@@ -21,7 +24,11 @@ Local Open Scope ring_scope.
 *)
 Module InstantiationInteger.
 
-  Variant int : Set := Posz of nat | Negz of nat.
+(* 自然数から整数を作るコンストラクタ Posz と Negz を定義する。
+   Posz は自然数から整数へのコアーションにするとともに、``%:Z``という演算子を用意しておく。*)
+  Variant int : Set :=
+    | Posz of nat
+    | Negz of nat.
   Local Coercion Posz : nat >-> int.
   Notation "n %:Z" := (Posz n)(at level 2, left associativity, format "n %:Z", only parsing).
   
@@ -45,11 +52,10 @@ Module InstantiationInteger.
   HB.instance Definition _ : hasChoice int := CanChoiceMixin natsum_of_intK.
   HB.instance Definition _ : isCountable int := CanCountMixin natsum_of_intK.
 *)  
-  (* こちらのほうが、MathComp2的である。 *)
-  (* Advanced way of doing it one go: *)
-  HB.instance Definition _ := Equality.copy int (can_type natsum_of_intK).
-  HB.instance Definition _ := Choice.copy int (can_type natsum_of_intK).
-  HB.instance Definition _ := Countable.copy int (can_type natsum_of_intK).
+  (* こちらのほうが、よりMathComp2的である。 *)
+  HB.instance Definition _ := Equality.copy int (can_type natsum_of_intK). (* MathComp2 *)
+  HB.instance Definition _ := Choice.copy int (can_type natsum_of_intK). (* MathComp2 *)
+  HB.instance Definition _ := Countable.copy int (can_type natsum_of_intK). (* MathComp2 *)
   
 (**
 ## Z加群
@@ -75,6 +81,13 @@ Module InstantiationInteger.
         - addNr : left_inverse 0 -%R +%R
         *)
       
+      (* Negz 0 で -1 を表すようにする。すると、intの表現の一意性が保たれる。 *)
+      Check Posz 1.                         (* +1 *)
+      Check Posz 0.                         (* 0 *)
+      Check Negz 0.                         (* -1 *)
+      Check Negz 1.                         (* -2 *)
+      (* この定義に基づいて、整数の加算を定義すると、以下 addz と oppz のようになる。 *)
+      
       Definition addz (m n : int) :=
         match m, n with
         | Posz m', Posz n' => Posz (m' + n')
@@ -96,22 +109,25 @@ Module InstantiationInteger.
       Lemma addzA : associative addz. Admitted.
       Lemma addNz : left_inverse (Posz 0) oppz addz. Admitted.
 
-      Definition Mixin := GRing.isZmodule.Build int addzA addzC add0z addNz.
+      Definition Mixin := GRing.isZmodule.Build int addzA addzC add0z addNz. (* MathComp2 *)
     End intZmod.
   End intZmod.
 
-  HB.instance Definition _ := intZmod.Mixin.
+  HB.instance Definition _ := intZmod.Mixin. (* MathComp2 *)
   Check (int : zmodType).
 
 (*
 PoszD は、Posz (正整数) の加算 (D) について、
 自然数を加算して正整数にすることと、自然数を正整数にして加算することは等しい。
  *)
-  Lemma PoszD : {morph Posz : n m / (n + m)%N >-> n + m}.
-  Proof. by []. Qed.
-  Check PoszD : forall x y : nat, Posz (x + y) = Posz x + Posz y.
-  Check PoszD : forall x y : nat, (x + y)%:Z = x%:Z + y%:Z.
-  
+  Section intZmoduleTheory.
+    
+    Lemma PoszD : {morph Posz : n m / (n + m)%N >-> n + m}.
+    Proof. by []. Qed.
+    Check PoszD : forall x y : nat, Posz (x + y) = Posz x + Posz y.
+    Check PoszD : forall x y : nat, (x + y)%:Z = x%:Z + y%:Z.
+    
+  End intZmoduleTheory.  
 (**
 ## 環
  *)
@@ -148,13 +164,15 @@ PoszD は、Posz (正整数) の加算 (D) について、
       Lemma mulzC : commutative mulz. Admitted.
       Lemma mul1z : left_id (Posz 1) mulz. Admitted.
       Lemma mulz_addl : left_distributive mulz (+%R). Admitted.
+      
       Lemma onez_neq0 : (Posz 1) != 0. Proof. by []. Qed.
-
       Definition comMixin := GRing.Zmodule_isComRing.Build int mulzA mulzC mul1z mulz_addl onez_neq0.
+      (* MathComp2 *)
+      
     End intRing.
   End intRing.
 
-  HB.instance Definition _ := intRing.comMixin.
+  HB.instance Definition _ := intRing.comMixin. (* MathComp2 *)
   Check (int : ringType).
   Check (fun x : int => x + x).
   Check (@addr0 int).
@@ -169,11 +187,12 @@ Section GaussianIntegers.
 (**
 ## 代数的定義
 *)
-  From mathcomp Require Import algC ssrint ssrnum.
+  Import algC.
+  Import ssrint ssrnum fingroup.
   Import Num.Theory.
 
   Definition gaussInteger := [pred x : algC | ('Re x \in Cint) && ('Im x \in Cint)].
-  HB.about GRing.isSubringClosed.
+  HB.about GRing.isSubringClosed.           (* MathComp2 *)
   Print GRing.subring_closed.
 
   Lemma GI_subring : GRing.isSubringClosed _ gaussInteger.
@@ -183,7 +202,7 @@ Section GaussianIntegers.
     - by rewrite inE !raddfB /= ?rpredB.
       by rewrite inE ReM ImM rpredB ?rpredD // rpredM.
   Qed.
-  HB.instance Definition _ := GI_subring.
+  HB.instance Definition _ := GI_subring.   (* MathComp2 *)
 
 (**
 ## sigma type
@@ -194,14 +213,23 @@ Section GaussianIntegers.
                  }.
   Hint Resolve algGIP.
 
-  HB.instance Definition _ := [isSub for algGI].
-  HB.instance Definition _ := [Choice of GI by <:].
+  HB.instance Definition _ := [isSub for algGI].    (* MathComp2 *)
+  HB.instance Definition _ := [Choice of GI by <:]. (* MathComp2 *)
 
   Fail Check (forall x y : GI, x + y = y + x).
-  HB.instance Definition _ := [SubChoice_isSubComRing of GI by <:].
+  HB.instance Definition _ := [SubChoice_isSubComRing of GI by <:]. (* MathComp2 *)
   Check (GI : comRingType).
   Check (forall x y : GI, x + y = y + x).   (* 可換性が成り立つ。 *)
 
 End GaussianIntegers.
+
+(**
+# subType
+ *)
+From mathcomp Require Import perm.
+
+Check tval       : forall (n : nat) (T : Type), n.-tuple T -> seq T.
+Check nat_of_ord : forall n : nat, 'I_n -> nat.
+Check pval       : forall T : finType, perm_type T -> {ffun T -> T}.
 
 (* END *)
