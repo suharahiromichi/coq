9e2734fb4eda6aed11872c1b70198edc53a79c17
SUHARA Hiromichi <suhara@mesa.local>
2023-09-19 23:02:32 +0900

    homo mono を追加した。

--- alg/ssr_morph.v
+++ alg/ssr_morph.v
@@ -44,4 +44,63 @@ Check a3 : forall (f : U -> V) (F : U -> U -> U) (G : V -> V -> V) (x y : U), f 
 Axiom a4 : forall (f : V -> V) (G : V -> V -> V), {morph f : x y / G x y}. (* G x y >-> G x y *)
 Check a4 : forall (f : V -> V) (G : V -> V -> V) (x y : V), f (G x y) = G (f x) (f y).
 
+(**
+     {homo f : x / a >-> r} <-> f is a homomorphism with respect to the
+                               predicates (fun x => a) and (fun x => r);
+                               if r == R#[#x#]#, this states that a -> R#[#f x#]#
+                               for all x.
+ *)
+Axiom h1 : forall (f : U -> V) (F : U -> Prop) (G : V -> Prop), {homo f : x / F x >-> G x}.
+Check h1 : forall (f : U -> V) (F : U -> Prop) (G : V -> Prop) (x : U), F x -> G (f x).
+
+(**
+           {homo f : x / a} <-> f is a homomorphism with respect to the
+                               predicate expression (fun x => a).
+*)
+Axiom h2 : forall (f : U -> U) (F : U -> Prop), {homo f : x / F x}.
+Check h2 : forall (f : U -> U) (F : U -> Prop) (x : U), F x -> F (f x).
+
+(**
+   {homo f : x y / a >-> r} <-> f is a homomorphism with respect to the
+                               relations (fun x y => a) and (fun x y => r).
+ *)
+Axiom h3 : forall (f : U -> V) (F : U -> U -> Prop) (G : V -> V -> Prop), {homo f : x y / F x y >-> G x y}.
+Check h3 : forall (f : U -> V) (F : U -> U -> Prop) (G : V -> V -> Prop) (x y : U), F x y -> G (f x) (f y).
+
+(**
+         {homo f : x y / a} <-> f is a homomorphism with respect to the
+                               relation expression (fun x y => a).
+*)
+Axiom h4 : forall (f : U -> U) (F : U -> U -> Prop), {homo f : x y / F x y}.
+Check h4 : forall (f : U -> U) (F : U -> U -> Prop) (x y : U), F x y -> F (f x) (f y).
+
+(**
+     {mono f : x / a >-> r} <-> f is monotone with respect to projectors
+                               (fun x => a) and (fun x => r); if r == R#[#x#]#,
+                               this states that R#[#f x#]# = a for all x.
+ *)
+Axiom m1 : forall (f : U -> V) (F : U -> Prop) (G : V -> Prop), {mono f : x / F x >-> G x}.
+Check m1 : forall (f : U -> V) (F : U -> Prop) (G : V -> Prop) (x : U), G (f x) = F x.
+
+(**
+           {mono f : x / a} <-> f is monotone with respect to the projector
+                               expression (fun x => a).
+ *)
+Axiom m2 : forall (f : U -> U) (F : U -> Prop), {mono f : x / F x}.
+Check m2 : forall (f : U -> U) (F : U -> Prop) (x : U), F (f x) = F x.
+
+(**
+   {mono f : x y / a >-> r} <-> f is monotone with respect to relators
+                               (fun x y => a) and (fun x y => r).
+ *)
+Axiom m3 : forall (f : U -> V) (F : U -> U -> Prop) (G : V -> V -> Prop), {mono f : x y / F x y >-> G x y}.
+Check m3 : forall (f : U -> V) (F : U -> U -> Prop) (G : V -> V -> Prop) (x y : U), G (f x) (f y) = F x y.
+
+(**
+         {mono f : x y / a} <-> f is monotone with respect to the relator
+                               expression (fun x y => a).
+*)
+Axiom m4 : forall (f : U -> U) (F : U -> U -> Prop), {mono f : x y / F x y}.
+Check m4 : forall (f : U -> U) (F : U -> U -> Prop) (x y : U), F (f x) (f y) = F x y.
+
 (* END *)
