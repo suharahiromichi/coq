448bddce86ee0a35cea343efe7a0e28504b30c22
SUHARA Hiromichi <suhara@mesa.local>
2023-09-25 22:57:03 +0900

    修正した。

--- alg/int_sample.v
+++ alg/int_sample.v
@@ -33,6 +33,96 @@ Fail Check int : rcfType.                   (* A Real Field with the real closed
 
 
 (*
+int のつくりかた
+ *)
+Check intZmod.Mixin : GRing.isZmodule.axioms_ int _ _.
+Import intZmod.
+Check GRing.isZmodule.Build int addzA addzC add0z addNz.
+Check zmodType.
+
+Check intRing.comMixin : GRing.Zmodule_isComRing.axioms_ int _ _ _ _.
+Import intRing.
+Check GRing.Zmodule_isComRing.Build int mulzA mulzC mul1z mulz_addl nonzero1z.
+Check comRingType.
+
+Check intUnitRing.comMixin : GRing.ComRing_hasMulInverse.axioms_ int _ _ _ _ _ _.
+Check comUnitRingType.
+
+Check intUnitRing.idomain_axiomz : forall m n : int, (m * n)%R = 0%R -> (m == 0%R) || (n == 0%R).
+Check GRing.ComUnitRing_isIntegral.Build int intUnitRing.idomain_axiomz.
+Check idomainType.
+
+Check intOrdered.Mixin : Num.IntegralDomain_isLeReal.axioms_ int _ _ _ _ _ _ _ _.
+Import intOrdered.
+Check Num.IntegralDomain_isLeReal.Build int
+  lez_add lez_mul lez_anti subz_ge0 (lez_total 0) normzN gez0_norm ltz_def.
+Check realDomainType.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+Check intZmod.Mixin : GRing.isZmodule.axioms_ int _ _.
+Check zmodType.
+Check idomainType.
+
+Check intRing.comMixin : GRing.Zmodule_isComRing.axioms_ int _ _ _ _.
+Check comRingType.
+
+Check intUnitRing.comMixin : GRing.ComRing_hasMulInverse.axioms_ int _ _ _ _ _ _.
+Check comUnitRingType.
+
+Check intUnitRing.idomain_axiomz : forall m n : int, (m * n)%R = 0%R -> (m == 0%R) || (n == 0%R).
+Check GRing.ComUnitRing_isIntegral.Build int intUnitRing.idomain_axiomz.
+Check idomainType.
+
+Check intOrdered.Mixin : Num.IntegralDomain_isLeReal.axioms_ int _ _ _ _ _ _ _ _.
+Check realDomainType.
+
+
+
+
+Check HB_unnamed_mixin_4.
+Check HB_unnamed_mixin_5.
+Check HB_unnamed_mixin_6.
+Check HB_unnamed_mixin_7.
+Check HB_unnamed_mixin_8.
+Check HB_unnamed_mixin_9.
+Check HB_unnamed_mixin_10.
+Check HB_unnamed_mixin_11.
+Check HB_unnamed_mixin_12.
+Check HB_unnamed_mixin_14.
+Check HB_unnamed_mixin_15.
+Check HB_unnamed_mixin_16.
+Check HB_unnamed_mixin_17.
+Check HB_unnamed_mixin_20.
+Check HB_unnamed_mixin_21.
+Check HB_unnamed_mixin_22.
+Check HB_unnamed_mixin_23.
+Check HB_unnamed_mixin_24.
+Check HB_unnamed_mixin_26.
+Check HB_unnamed_mixin_27.
+Check HB_unnamed_mixin_28.
+Check HB_unnamed_mixin_29.
+Check HB_unnamed_mixin_30.
+Check HB_unnamed_mixin_31.
+Check HB_unnamed_mixin_33.
+
+
+
+(*
      Check int : lmodType R.
      Check int : lalgType R.
      Check int : algType R.
@@ -42,3 +132,26 @@ Fail Check int : rcfType.                   (* A Real Field with the real closed
      Check int : unitAlgType R.
      Check int : comUnitAlgType R.
 *)                 
+
+(*
+ssrint.v
+
+HB.instance Definition _ := intZmod.Mixin.
+HB.instance Definition _ := intRing.comMixin.
+HB.instance Definition _ := intUnitRing.comMixin.
+HB.instance Definition _ := intOrdered.Mixin.
+
+Definition Mixin := GRing.isZmodule.Build int addzA addzC add0z addNz.
+Definition comMixin := GRing.Zmodule_isComRing.Build int
+Definition comMixin := GRing.ComRing_hasMulInverse.Build int
+Definition Mixin := Num.IntegralDomain_isLeReal.Build int
+ *)
+
+
+  (*
+ssralg.v:HB.factory Record isZmodule V of Choice V := {
+ssralg.v:HB.factory Record Zmodule_isComRing R of Zmodule R := {
+ssralg.v:HB.factory Record ComRing_hasMulInverse R of ComRing R := {
+ssrnum.v:HB.factory Record IntegralDomain_isLeReal R of GRing.IntegralDomain R := {
+
+*)
--- alg/rat_sample.v
+++ alg/rat_sample.v
@@ -27,3 +27,29 @@ Fail Check rat : numClosedFieldType.        (* Partially ordered Closed Field wi
      Check rat : realFieldType.             (* Num Field where all elements are positive or negative *)
      Check rat : archiFieldType.            (* A Real Field with the archimedean axiom *)
 Fail Check rat : rcfType.                   (* A Real Field with the real closed axiom *)
+
+(* rat の作り方 *)
+Check GRing.isZmodule.Build rat addqA addqC add0q addNq.
+Check zmodType.
+
+Check GRing.Zmodule_isComRing.Build rat mulqA mulqC mul1q mulq_addl nonzero1q.
+Check comRingType.
+
+Check GRing.ComRing_isField.Build rat mulVq invq0.
+Check fieldType.
+
+Check Num.IntegralDomain_isLeReal.Build rat le_rat0D le_rat0M le_rat0_anti
+  subq_ge0 (@le_rat_total 0%R) norm_ratN ge_rat0_norm lt_rat_def.
+Check realFieldType.
+
+Check Num.RealField_isArchimedean.Build rat rat_archimedean.
+Check archiFieldType.
+
+
+
+
+
+
+
+Check GRing.isSubringClosed.Build rat Qint_pred Qint_subring_closed.
+Check GRing.isSemiringClosed.Build rat Qnat_pred Qnat_semiring_closed.
