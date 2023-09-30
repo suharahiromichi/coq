67606b71221b2fae13c64254ddbaa14dcceaabe9
SUHARA Hiromichi <suhara@mesa.local>
2023-09-30 10:27:31 +0900

    k-coq Exercise 3.5.1.

--- alg/num_sample.v
+++ alg/num_sample.v
@@ -26,12 +26,43 @@ realDomainType
 ```
 *)
 
-
+Check Num.sqrt : (_ : rcfType) -> (_ : rcfType).
+Import GRing.Theory.
+Import Num.
 Import Num.Theory.
 Import Num.Def.
 
-Local Open Scope order_scope.
 Local Open Scope ring_scope.
 
-Locate "'Re".
 
+(* k-coq Exercise 3.5.1. *)
+
+Variable R : rcfType.
+
+Lemma l1 : (@sqrt R (4 + @sqrt R 3 *+ 2)) ^+ 2 = 4 + @sqrt R 3 *+ 2.
+Proof.
+  rewrite sqr_sqrtr //.
+  apply: Internals.addr_ge0 => //.
+  rewrite mulrn_wge0 //.
+  by rewrite sqrtr_ge0.
+Qed.
+
+Lemma l2 : (@sqrt R 3 + 1) ^+ 2 = 4 + @sqrt R 3 *+ 2.
+Proof.
+  rewrite sqrrD1 sqr_sqrtr //.
+  rewrite addrAC.
+  have -> : 3 + 1 = 4 by move=> R; rewrite natr1.
+  done.
+Qed.
+
+Goal @sqrt R (4 + @sqrt R 3 *+ 2) = @sqrt R 3 + 1.
+Proof.
+  apply/eqP.
+  Check (@eqrXn2 R 2 _ _).
+  rewrite -(@eqrXn2 R 2 _ _) //.
+  - by rewrite l1 l2.
+  - by rewrite sqrtr_ge0.
+  - by rewrite addr_ge0 // sqrtr_ge0.
+Qed.
+
+(* END *)
