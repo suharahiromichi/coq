d4be31d383c2b565cd5e9154e0a7559220792e51
SUHARA Hiromichi <suhara@mesa.local>
2024-08-02 23:20:02 +0900

    修正した。

--- alg/matrix_sample.v
+++ alg/matrix_sample.v
@@ -20,35 +20,36 @@ Variables m n : nat.
 R型で 寸法m, nのマトリクス型。寸法は自然数で指定する。
  *)
 Locate "'M[ _ ]_ ( _ , _ )".           (* := (matrix R m n) : type_scope *)
-Check matrix R m n : predArgType. (* m n は本当に自然数 *)
+Check matrix R m n : predArgType.      (* m n は本当に自然数 *)
+Variable A : 'M[R]_(2, 3).
+
 
 (**
-``i < 'I_m`` と ``j < 'I_n`` の直積``(i, j)``から R を返す関数
-``(i, j)`` はfinTypeなので、この関数は finfun である。
+Ordinal型 ``i < 'I_m`` と ``j < 'I_n`` の直積``(i, j)``から R を返す関数
+``(i, j)`` は finType なので、この関数は finfun型 である。
  *)
 Check {ffun 'I_m * 'I_n -> R}.
 
 (**
-マトリクス型の実体は、この finfun である。これをマトリクス型にする表記
+マトリクス型の実体は、この finfun ``(fun i j => E)`` である。これをマトリクス型にする。
 *)
 Locate "\matrix[ k ]_ ( i , j ) E". (* := (matrix_of_fun.body k (fun i j => E)) *)
 Check matrix_of_fun : forall (R : Type) (m n : nat), unit -> ('I_m -> 'I_n -> R) -> 'M_(m, n).
 
-
 (**
 # マトリクス型の作り方
 *)
-
-(**
-コンストラクタ Matrix は、finfun型をとって、マトリクス型を返す。
-*)
-Check Matrix : forall (R : Type) (m n : nat), (* R型と寸法m, nは与える。 *)
-    {ffun 'I_m * 'I_n -> R} ->
-    matrix R m n.
 (**
 Variant matrix (R : Type) (m n : nat) : predArgType :=
         Matrix : {ffun 'I_m * 'I_n -> R} -> 'M_(m, n).
 *)
+
+(**
+コンストラクタ Matrix は、finfun をとって、マトリクス型を返す。
+*)
+Check Matrix : forall (R : Type) (m n : nat), (* R型と寸法m, nは与える。 *)
+    {ffun 'I_m * 'I_n -> R} ->
+    matrix R m n.
 
 (**
 マトリクス型からfinfunを返す関数
@@ -59,10 +60,13 @@ Check mx_val : forall (R : Type) (m n : nat),
 
 (**
 マトリクス型を funfun をカリー化した関数に変換する関数
-コアーションとして使われる。
+これはコアーションとして働くので、マトリクス型にインデックスとしてordinal型の引数を与えることができる。
 *)
 Check fun_of_matrix : forall (R : Type) (m n : nat),
     matrix R m n -> ('I_m -> 'I_n -> R).
+
+Check A (Ordinal (_ : 1 < 2)) (Ordinal (_ : 1 < 3)).
+Check (fun_of_matrix A) (Ordinal (_ : 1 < 2)) (Ordinal (_ : 1 < 3)).
 
 (**
  funfun をカリー化した関数をマトリクス型に変換する関数
@@ -82,13 +86,19 @@ Definition matrix_of_fun R (m n : nat) (k : unit) (F : 'I_m -> 'I_n -> R) :=
 # mxE
  *)
 Check mxE : forall (R : Type) (m n : nat) (k : unit) (F : 'I_m -> 'I_n -> R),
+    (\matrix[k]_(i, j) F i j)%R =2 F.
+Check mxE : forall (R : Type) (m n : nat) (k : unit) (F : 'I_m -> 'I_n -> R),
     fun_of_matrix (\matrix[k]_(i, j) F i j) =2 F.
+Check mxE : forall (R : Type) (m n : nat) (k : unit) (F : 'I_m -> 'I_n -> R),
+    (@fun_of_matrix R m n (@matrix_of_fun.body R m n k F)) =2 F.
 (* 両辺は、``'I_m -> 'I_n -> R`` *)
 
 (**
 # matrixP.
  *)
-Check matrixP : forall (R : Type) (m n : nat) (A B : 'M_(m, n)), A =2 B <-> A = B.
+Check matrixP : forall (R : Type) (m n : nat) (A B : 'M_(m, n)),
+    A =2 B <-> A = B.                       (* 左は、fun_of_matrix のコアーション *)
+(* 右はマトリクス型どうし *)
 
 (**
 # eq_mx
