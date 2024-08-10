8abd37716914dcfbab11ab6f069c29dc7a0e0fe9
SUHARA Hiromichi <suhara@mesa.local>
2024-08-10 10:40:15 +0900

    行空間

--- alg/mxalgebra_sample.v
+++ alg/mxalgebra_sample.v
@@ -13,7 +13,7 @@ Open Scope ring_scope.
 
 Variables m n p : nat.                      (* 行列の寸法 *)
 Variable F : fieldType.
-Variable A B C : 'M[F]_(m, n).
+Variable A : 'M[F]_(m, n).
 
 (**
 # Rank 階数
@@ -41,16 +41,6 @@ Print col_base.
 Print row_base.
 
 (**
-包含関係など
-*)
-Check submx A B : bool.
-Check ltmx A B : bool.
-Check eqmx A B : Prop.
-Check genmx A : 'M_n.
-Check addsmx A B : 'M_n.
-Check capmx A B : 'M_n.
-
-(**
 ## rank に関連する補題
 *)
 (* 逆行列が存在すること（正則であること）と、row_freeは同値 *)
@@ -59,13 +49,67 @@ Check @row_freeP F m n A : reflect (exists B : 'M_(n, m), A *m B = 1%:M) (row_fr
 (* 逆行列が存在すること（正則であること）と、row_fullは同値 *)
 Check @row_fullP F m n A : reflect (exists B : 'M_(n, m), B *m A = 1%:M) (row_full A).
 
-Check @submx1 F m n A : (A <= 1%:M)%MS.
-Check @sub1mx F m n A : (1%:M <= A)%MS = row_full A.
-Check @submx0 F m n A : (A <= 0)%MS = (A == 0).
-Check @sub0mx F m _ n A : (0 <= A)%MS. (* _ は 0 の行数、A と 0 の行数は違ってよい。 *)
 
+(**
+# マトリクススペース(%MS)
+ *)
+Section MS.
+  Variable m2 : nat.
+  Variable B : 'M[F]_(m2, n).
+  
+(**
+## マトリクススペース(%MS)の関数
+*)
+(* 小行列、部分行列ではない。 *)
+(* Aの行空間がBの行空間に含まれる。 *)
+Check submx A B : bool.                     (* <= *)
+(* A <= B かつ ~~(B <= A) *)
+Check ltmx A B : bool.                      (* < *)
+(* A と B の階数が同じで、任意の行列Cに対して、
+   ((A <= C) = (B <= C)) * ((C <= A) = (C <= B)) *)
+Check eqmx A B : Prop.                      (* :=: *)
+
+(* m✖️n行列の行空間の基底。
+   n次の正方行列で1%:Mまたは、その余計なところを0にしたもの。 *)
+Check genmx A : 'M_n.                       (* << A >> *)
+
+(* 行列の行空間どうしのの和空間 << col_mx A B>>
+   連結した行列に対して、行空間の基底を求める *)
+(* A Bの列数は同じ n であること。値はnの正方行列 *)
+Check addsmx A B : 'M_n.                    (* + *)
+
+(* 行空間（行ベクトルの張る空間）どうしの交空間 *)
+(* A Bの列数は同じ n であること。値はnの正方行列 *)
+Check capmx A B : 'M_n.                     (* :&: *)
+
+(* 行空間どうしの差空間 *)
+Check diffmx A B : 'M_n.                    (* :\: *)
+
+(**
+## マトリクススペース(%MS)の補題
+*)
+(* 自明なもの *)
+Check @submx_refl F m n A : (A <= A)%MS.
+Check @addsmxSl F m m2 n A B : (A <= A + B)%MS.
+Check @addsmxSr F m m2 n A B : (B <= A + B)%MS.
+Check @capmxSr F m m2 n A B : (A :&: B <= B)%MS.
+Check @capmxSl F m m2 n A B : (A :&: B <= A)%MS.
+Check @diffmxSl F m m2 n A B : (A :\: B <= A)%MS.
+
+(* 任意の行列の行空間は、単位行列の行空間に含まれる。 *)
+Check @submx1 F m n A : (A <= 1%:M)%MS.
+(* 行空間が単位行列の行空間と同じであることと、正則行列であることは同値 *)
+Check @sub1mx F m n A : (1%:M <= A)%MS = row_full A.
+(* 零行列の行空間と同じであることと、零行列であることは同値 *)
+Check @submx0 F m n A : (A <= 0)%MS = (A == 0).
+(* 零行列の行空間は、任意の行列区間の行空間を含む。 *)
+Check @sub0mx F m _ n A : (0 <= A)%MS.
+
+(* 行列Aの行空間が、行列BとCの行空間の交空間を含むことと、
+   行列Aの行空間が行列Cの行空間を含み、かつ、行列Bの行空間が行列Cの行空間を含むことと同じ。 *)
 Check @sub_capmx F : forall (m m1 m2 n : nat) (A : 'M_(m, n)) (B : 'M_(m1, n)) (C : 'M_(m2, n)),
     (A <= B :&: C)%MS = (A <= B)%MS && (A <= C)%MS.
+
 
 (**
 # Kernel 核
@@ -108,6 +152,8 @@ Check @sub_kermxP F : forall (p m n : nat) (A : 'M_(m, n)) (B : 'M_(p, m)),
 
 (**
 ## %MS - scope MS (matrix set space)
+
+マトリクススペース
 *)
 
 Locate "A <= B".                            (* := (submx A B) *)
@@ -121,7 +167,7 @@ Locate "A + B".                             (* := (addsmx A B) *)
 Locate "A :&: B".                           (* := (capmx A B) *)
 Locate "\bigcap_ i B".
 Locate "A ^C".                              (* := (complmx A) *)
-Locate "A :\: B".                           (* := (diffmx.body A B) *)
+Locate "A :\: B".                           (* := (diffmx A B) *)
 Locate "A \in R".                           (* := (submx (mxvec A) R) *)
 Locate "A * R".                             (* := (mulsmx R S) *)
 
