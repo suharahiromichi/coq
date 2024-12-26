961236cf2f3f9bd17ae87c786bf0f846532052ea
SUHARA Hiromichi <suhara@mesa.local>
2024-12-26 23:38:36 +0900

    修正した。

--- math4/ssr_math2001_functions.v
+++ math4/ssr_math2001_functions.v
@@ -19,20 +19,38 @@ Import Order.Theory.
 
 Open Scope ring_scope.
 
-Import intZmod.                             (* addz など *)
-Import intRing.                             (* mulz など *)
-
 Section Sample.
 
   Variable R : realDomainType.
   Variable x y z : R.
   
+  (* order *)
+  Check @lt_eqF : forall (disp : unit) (T : porderType disp) (x y : T), (x < y)%O -> (x == y) = false.
+  Check @gt_eqF : forall (disp : unit) (T : porderType disp) (x y : T), (y < x)%O -> (x == y) = false.
+
+  (* ssrnum *)
   Check lerN10 R : -1 <= 0.
   Check @gtrN R x : 0 < x -> - x < x.
   Check lerN2 x y : (- x <= - y) = (y <= x).
   Check ltrN2 x y : (- x < - y) = (y < x).
   Check lerNr x y : (x <= - y) = (y <= - x).
-  Check @real_neqr_lt R x y.
+  
+  Check @real_ltNge R : {in Num.real &, forall x y : R, (x < y) = ~~ (y <= x)}.
+  Check @real_leNgt R : {in Num.real &, forall x y : R, (x <= y) = ~~ (y < x)}.
+  Check @real_neqr_lt R : {in Num.real &, forall x y : R, (x != y) = (x < y) || (y < x)}.
+  
+(**
+使用例：
+*)
+  Lemma ne_of_gt (a b : R) : b < a -> a != b.
+  Proof.
+    move=> H.
+    rewrite real_neqr_lt //=.
+    by apply/orP/or_intror.
+    Undo 2.
+    apply/negbT.
+    by rewrite gt_eqF.
+  Qed.
 
 End Sample.
 
@@ -41,6 +59,9 @@ Section Functions.
 
   Variable R : realDomainType.
   
+(**
+## 単射
+*)
   Goal injective (fun x : R => x + 3).
   Proof.
     rewrite /injective => x y.
@@ -54,6 +75,7 @@ Section Functions.
 
   Lemma neq1N1 : (- 1) <> 1 :> R.
   Proof.
+    Search ((- _) = _).
     apply/eqP.
     rewrite real_neqr_lt.
     - by apply/orP/or_introl/gtrN.
@@ -70,6 +92,9 @@ Section Functions.
     by move/neq1N1.
   Qed.
   
+(**
+## 全射
+*)
   Definition surjective (rT aT : Type) (f : aT -> rT) := forall y, exists x, f x = y.
   
   Goal surjective (fun (a : rat) => 3 * a + 2).
@@ -81,19 +106,13 @@ Section Functions.
     ring.
   Qed.
   
-  Lemma ne_of_gt (a b : R) : b < a -> a != b.
-  Proof.
-    move=> H.
-    rewrite real_neqr_lt //=.
-    by apply/orP/or_intror.
-  Qed.
-
   Goal ~ surjective  (fun x : R => x^+2).
   Proof.
     rewrite /surjective.
     move/(_ (- 1)).
     case=> x.
-    apply/eqP/ne_of_gt.
+    apply/eqP/negbT.
+    rewrite gt_eqF //=.
     Check sqr_ge0 x : 0  <= x ^+ 2.
     Check ltrN10 R : -1 < 0.
     apply: lt_le_trans.
