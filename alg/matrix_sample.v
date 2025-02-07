9ac8418a7b6c1039c58e47c196de4c231d2abef4
SUHARA Hiromichi <suhara@mesa.local>
2025-02-07 23:10:20 +0900

    追記した。

--- alg/matrix_sample.v
+++ alg/matrix_sample.v
@@ -30,7 +30,7 @@ Open Scope ring_scope.
 - 列（行）の入れ替え、列（行）の取り出し
   - 関数
   - 補題
-- block matrix ブロック行列 区分行列 小行列を連結する。
+- block matrix ブロック行列 区分行列 小行列を繋ぎ合わせる。
   - 関数
   - 補題
 - submatrix 部分行列 小行列を取り出す。
@@ -62,10 +62,12 @@ Check matrix R m n : predArgType.      (* m n は本当に自然数 *)
 Ordinal型 ``i < 'I_m`` と ``j < 'I_n`` の直積``(i, j)``から R を返す関数
 ``(i, j)`` は finType なので、この関数は finfun型 である。
  *)
-Check {ffun 'I_m * 'I_n -> R}.
+Check {ffun 'I_m * 'I_n -> R} : predArgType.
 
 (**
 マトリクス型の実体は、この finfun ``(fun i j => E)`` である。これをマトリクス型にする。
+
+k についてはよくわからない。
 *)
 Locate "\matrix_ ( i  < m , j < n ) E". (* := (matrix_of_fun matrix_key (fun i j => E)) *)
 Locate "\matrix[ k ]_ ( i , j ) E".     (* := (matrix_of_fun k          (fun i j => E)) *)
@@ -93,8 +95,12 @@ Check mx_val : forall R m n,
     matrix R m n ->
     {ffun 'I_m * 'I_n -> R}. (* ``i < 'I_m`` と ``j < 'I_n`` から R を返す関数(funfun)を返す。 *)
 
+(* isNew でサブタイプになっているので、\var が使える。 *)
+Check val : matrix R m n -> {ffun 'I_m * 'I_n -> R}.
+Goal @mx_val R m n = val. Proof. done. Qed.
+
 (**
-マトリクス型を funfun をカリー化した関数に変換する関数
+マトリクス型を、funfunをカリー化した関数に変換する関数
 
 これはコアーションとして働くので、マトリクス型にインデックスとしてordinal型の引数を与えることができる。
 *)
@@ -106,7 +112,7 @@ Variable A : 'M[R]_(2, 3).
 (* Check (fun_of_matrix A) (Ordinal (_ : 1 < 2)) (Ordinal (_ : 1 < 3)). *)
 
 (**
-## funfun をカリー化した関数をマトリクス型に変換する関数
+## funfunをカリー化した関数を、マトリクス型に変換する関数
 *)
 Check matrix_of_fun : forall R m n,
     unit -> ('I_m -> 'I_n -> R) -> matrix R m n.
@@ -116,14 +122,17 @@ Definition matrix_of_fun R (m n : nat) (k : unit) (F : 'I_m -> 'I_n -> R) :=
 *)
 
 (**
-# 重要な補題
+# 三つの重要な補題
 *)
 Goal 1%:M = 1%:M :> 'M[nat]_(n, n).
 Proof.
   (* 行列の``=``を関数の``=``として、関数値の``=``にする。 *)
   apply/matrixP => i j.
+  Check 1%:M i j = 1%:M i j.
+  
   (* 行列の要素どうしの``=``にする。 *)
   rewrite mxE.
+  Check (i == j)%:R = (i == j)%:R.
   Restart.
   
   (* 上記を同時に行う。 *)
@@ -136,8 +145,10 @@ Qed.
  *)
 Check mxE : forall R m n (k : unit) (F : 'I_m -> 'I_n -> R),
     (\matrix[k]_(i, j) F i j)%R =2 F.
+
 Check mxE : forall R m n (k : unit) (F : 'I_m -> 'I_n -> R),
     fun_of_matrix (\matrix[k]_(i, j) F i j) =2 F.
+
 Check mxE : forall R m n (k : unit) (F : 'I_m -> 'I_n -> R),
     (@fun_of_matrix R m n (@matrix_of_fun.body R m n k F)) =2 F.
 (* 両辺は、``'I_m -> 'I_n -> R`` *)
@@ -153,11 +164,12 @@ Check matrixP : forall R m n (A B : 'M_(m, n)),
 
 (**
 ## eq_mx
+
+\matrix[k]_(i, j)(F i j) の F を書き換える。bigop の eq_big と同じ。
  *)
 Check @eq_mx : forall R m n (k : unit) (F1 F2 : 'I_m -> 'I_n -> R),
     F1 =2 F2 ->
     (\matrix[k]_(i, j) F1 i j)%R = (\matrix[k]_(i, j) F2 i j)%R.
-
 
 (**
 # マトリクス型の応用
@@ -289,16 +301,21 @@ Check @block_mx : forall R m1 m2 n1 n2,
 Check rowK : forall R m n (u_ : 'I_m -> 'rV_n) (i0 : 'I_m),
     row i0 (\matrix_i u_ i) = u_ i0 :> 'rV_n.
 
-(* 行列の積をperm する。 *)
+(* 行列の積をperm する。``+`` について可換であること。 *)
+(* 証明は、acs_lesson7_proofcafe.v *)
 Check mul_col_perm : forall R m n p (s : 'S_n) (A : 'M_(m, n)) (B : 'M_(n, p)),
     (col_perm s A *m B)%R = (A *m row_perm s^-1 B)%R.
 Check mul_row_perm : forall R m n p (s : 'S_n) (A : 'M_(m, n)) (B : 'M_(n, p)),
     (A *m row_perm s B)%R = (col_perm s^-1 A *m B)%R.
 
-(* i列目を列ベクトルとして取り出す関数は、(i, 0)だけが1の行列との積に等しい。 *)
-Check colE : forall R m n (i : 'I_n) (A : 'M_(m, n)), col i A = (A *m delta_mx i 0)%R.
-(* j行目を行ベクトルとして取り出す関数は、(0, j)だけが1の行列との積に等しい。 *)
-Check rowE : forall R m n (j : 'I_m) (A : 'M_(m, n)), row j A = (delta_mx 0 j *m A)%R.
+
+(* 指定要素だけ1の行列。対角ではない。 *)
+Check delta_mx.
+
+(* i列目を列ベクトルとして取り出す関数は、(i, 0)だけが1の行列（単位列ベクトル）との積に等しい。 *)
+Check colE : forall R m n (i : 'I_n) (A : 'M_(m, n)), col i A = (A *m delta_mx i 0)%R :> 'cV_m.
+(* j行目を行ベクトルとして取り出す関数は、(0, j)だけが1の行列（単位行ベクトル）との積に等しい。 *)
+Check rowE : forall R m n (j : 'I_m) (A : 'M_(m, n)), row j A = (delta_mx 0 j *m A)%R :> 'rV_n.
 
 (* xcol でj1列とj2列を入れ替えた行列は、j1列とj2列を入れ替えた単位行列との積に等しい。 *)
 Check xcolE : forall R m n (j1 j2 : 'I_n) (A : 'M_(m, n)), xcol j1 j2 A = (A *m tperm_mx j1 j2)%R.
@@ -306,6 +323,7 @@ Check xcolE : forall R m n (j1 j2 : 'I_n) (A : 'M_(m, n)), xcol j1 j2 A = (A *m 
 Check xrowE : forall R m n (i1 i2 : 'I_m) (A : 'M_(m, n)), xrow i1 i2 A = (tperm_mx i1 i2 *m A)%R.
 
 (* 行列の任意の列を取り出した列ベクトルが一致であることと、行列が一致であることは、同値。 *)
+(* acs_lesson7_proofcafe.v *)
 Lemma col_matrixP (A B : 'M_(m, n)) : (forall j : 'I_n, @col R m n j A = col j B) <-> A = B.
 Proof.
   split=> [eqAB | -> //]; apply/matrixP=> i j.
@@ -340,7 +358,7 @@ Check eq_col_mx : forall R m1 m2 n
 
 End MatrixStructural.
 (**
-# block matrix ブロック行列 区分行列 （4つの小行列を組み合わせる）
+# block matrix ブロック行列 区分行列 （4つの小行列を繋ぎ合わせる）
  *)
 (**
 ## 関数
@@ -396,6 +414,10 @@ Check colsub _.                             (* g のみ *)
 (**
 ## 補題
 *)
+(* 取り出した小行列をつなぎ合わせる *)
+Check submxK : forall (R : Type) (m1 m2 n1 n2 : nat) (A : 'M_(m1 + m2, n1 + n2)),
+    block_mx (ulsubmx A) (ursubmx A) (dlsubmx A) (drsubmx A) = A.
+
 (* row_mx で連結したものを、lsubmx で取り出す。lsubmx にn1,n2の指定は不要であることがわかる。 *)
 Check row_mxKl : forall (R : Type) (m n1 n2 : nat) (A1 : 'M_(m, n1)) (A2 : 'M_(m, n2)),
     lsubmx (row_mx A1 A2) = A1.
