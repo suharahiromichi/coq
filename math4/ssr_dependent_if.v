c60316533a4ecb4822a8596c77b11ec34e56ae76
SUHARA Hiromichi <suhara@mesa.local>
2025-03-13 23:01:13 +0900

    first commit

--- math4/ssr_dependent_if.v
+++ math4/ssr_dependent_if.v
@@ -8,7 +8,7 @@ Unset Strict Implicit.
 Unset Printing Implicit Defensive.
 
 (**
-Dependent if-then-else
+Dependent if-then-else の定義
  *)
 Definition dite {a : Type} (c : Prop) (t : c -> a) (e : ~ c -> a) : a.
 Proof.
@@ -19,31 +19,39 @@ Defined.
 Arguments dite {a} c t e.
 Notation "'if' h '|' c 'then' t 'else' e" := (dite c (fun h => t) (fun h => e)) (at level 200).
 
-Definition linv {A B : Type} {hnonempty : inhabited A} (b : B) (f : A -> B) :=
-  dite (exists a, f a = b)
-    (fun h => projT1 (cid h))    (* lean の Classical.choose_spec h *)
-    (fun h => inhabited_witness hnonempty). (* default *)
-Arguments linv {A B hnonempty} b f.
-
-Definition linv' {A B : Type} {hnonempty : inhabited A} (b : B) (f : A -> B) :=
-  if h | exists a, f a = b then projT1 (cid h) else inhabited_witness hnonempty.
-
-Section d.
-  Variable A B : Type.
-  Variable hnonempty : inhabited A.
-  Variable f : A -> B.
-  Variable y : B.
-
-  Check @linv A B  hnonempty y f.
-  Check linv y f.
-End d.
-
-Section c.
+(**
+使用例
+*)
+Section a.
   Variable A B : Type.
   Variable hnonempty : inhabited A.
   
-  (*                                                         ここを linv y f と書きたい。 *)
-  Lemma linv_spec (y : B) (f : A -> B) : (exists x, f x = y) -> f (@linv A B hnonempty y f) = y.
+(**
+左逆関数の定義
+*)
+  Definition linv (b : B) (f : A -> B) :=
+    dite (exists a, f a = b)
+      (fun h => projT1 (cid h))    (* lean の Classical.choose_spec h *)
+      (fun h => inhabited_witness hnonempty). (* default *)
+  
+(**
+Notationのif-then-elseを使った例
+*)
+  Definition linv' (b : B) (f : A -> B) :=
+    if h | exists a, f a = b then projT1 (cid h) else inhabited_witness hnonempty.
+
+  Section d.
+    Variable f : A -> B.
+    Variable y : B.
+    
+    Check linv y f.
+    Check linv' y f.
+  End d.
+  
+(**
+左逆関数が仕様を満たすことの証明
+*)
+  Lemma linv_spec (y : B) (f : A -> B) : (exists x, f x = y) -> f (linv y f) = y.
   Proof.
     case=> x fx_y.
     rewrite /linv /dite.
@@ -55,6 +63,6 @@ Section c.
       by exists x.
   Qed.
 
-End c.
+End a.
 
 (* END *)
