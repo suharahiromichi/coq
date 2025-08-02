5f716036954903246c26e77fee357b8d840a49b9
SUHARA Hiromichi <suhara@mesa.local>
2025-08-02 21:22:19 +0900

    first commit

--- math4/ssr_ruler_diff_nat.v
+++ math4/ssr_ruler_diff_nat.v
@@ -22,21 +22,51 @@ Section a.
   Notation "a ^^ b" := (xorb a b) (at level 50).
 
 (**
-# ldiff を使って p を定義する。
-*)
-  Definition px (n : nat) := n .- n.-1.
+# lxor を使って p を定義する。
+ *)
+  Definition px (n : nat) := n .^ n.-1.
 
-  (* ``x & -x`` 同様に変化した桁が1になる。 *)
+  (* ``x & -x`` と違い、桁より右の桁がすべて1になる。 *)
   (* 自然数の範囲で計算できる。 *)
   Compute px 0.                             (* = 0x0000 *)
   Compute px 1.                             (* = 0x0001 *)
-  Compute px 2.                             (* = 0x0010 *)
+  Compute px 2.                             (* = 0x0011 *)
   Compute px 3.                             (* = 0x0001 *)
-  Compute px 4.                             (* = 0x0100 *)
+  Compute px 4.                             (* = 0x0111 *)
   Compute px 5.                             (* = 0x0001 *)
-  Compute px 6.                             (* = 0x0010 *)
+  Compute px 6.                             (* = 0x0011 *)
   Compute px 7.                             (* = 0x0001 *)
-  Compute px 8.                             (* = 0x1000 *)
+  Compute px 8.                             (* = 0x1111 *)
+  
+  Definition rulerx (n : nat) := Nat.log2 (px n).
+
+  (* log2 をとると同じになる。 *)
+  Compute rulerx 0.                         (* = 0 *)
+  Compute rulerx 1.                         (* = 0 *)
+  Compute rulerx 2.                         (* = 1 *)
+  Compute rulerx 3.                         (* = 0 *)
+  Compute rulerx 4.                         (* = 2 *)
+  Compute rulerx 5.                         (* = 0 *)
+  Compute rulerx 6.                         (* = 1 *)
+  Compute rulerx 7.                         (* = 0 *)
+  Compute rulerx 8.                         (* = 3 *)
+
+(**
+# ldiff を使って p を定義する。
+*)
+  Definition pd (n : nat) := n .- n.-1.
+
+  (* ``x & -x`` 同様に変化した桁が1になる。 *)
+  (* 自然数の範囲で計算できる。 *)
+  Compute pd 0.                             (* = 0x0000 *)
+  Compute pd 1.                             (* = 0x0001 *)
+  Compute pd 2.                             (* = 0x0010 *)
+  Compute pd 3.                             (* = 0x0001 *)
+  Compute pd 4.                             (* = 0x0100 *)
+  Compute pd 5.                             (* = 0x0001 *)
+  Compute pd 6.                             (* = 0x0010 *)
+  Compute pd 7.                             (* = 0x0001 *)
+  Compute pd 8.                             (* = 0x1000 *)
 
   Lemma nat_ind2 :
     forall P : nat -> Prop,
@@ -85,28 +115,41 @@ Section a.
     done.
   Qed.
 
-  (* px 関数の引数が 0 以外の偶数の場合、testbit_div2 のようなことになる。 *)
+  (* pd 関数の引数が 0 以外の偶数の場合、testbit_div2 のようなことになる。 *)
   Check Nat.testbit_div2 : forall a n : nat, (Nat.div2 a).[n] = a.[n.+1].
   
   Lemma doubleDiff (m n : nat) : (m .- n).*2 = m.*2 .- n.*2.
   Proof.
-  Admitted.
+    have H x : x.*2 = (x * 2 ^ 1)%coq_nat by rewrite Nat.pow_1_r -muln2.
+    rewrite 3!H.
+    rewrite -3!Nat.shiftl_mul_pow2.
+    by rewrite Nat.shiftl_ldiff.
+  Qed.
   
+  Lemma shiftr1_div2 (n : nat) : n./2 = Nat.shiftr n 1.
+  Proof.
+  Admitted.
+
   Lemma halfDiff (m n : nat) : (m .- n)./2 = m./2 .- n./2.
   Proof.
+(*
     rewrite /Nat.ldiff.
-    Search (_ .- _).
-    Search "bitwise".
     rewrite -!coq_divn2.
     rewrite -Nat.div2_bitwise.
     congr (_ (Nat.bitwise _ _ _ _)).
     Check m = (Nat.div2 m).+1. (* bitwise から余計な引数を渡しているため。 *)
-  Admitted.
+    Restart.
+*)
+    have H x : x./2 = (x / 2 ^ 1)%coq_nat by rewrite Nat.pow_1_r -Nat.div2_div coq_divn2.
+    rewrite 3!H.
+    rewrite -3!Nat.shiftr_div_pow2.
+    by rewrite Nat.shiftr_ldiff.
+  Qed.
   
-  Lemma px_even (n i : nat) : (0 < n)%N -> ~~ odd n -> (px n).[i.+1] = (px n./2).[i].
+  Lemma pd_even (n i : nat) : (0 < n)%N -> ~~ odd n -> (pd n).[i.+1] = (pd n./2).[i].
   Proof.
     case: n => //= n _ Ho.
-    rewrite /px.
+    rewrite /pd.
     rewrite -pred_Sn.
     rewrite negbK in Ho.
     (* rewrite Nat.testbit_div2. *)
@@ -115,10 +158,10 @@ Section a.
     lia.
   Qed.
   
-  Lemma px_even' (n : nat) : (0 < n)%N -> ~~ odd n -> (px n) = (px n./2).*2.
+  Lemma pd_even' (n : nat) : (0 < n)%N -> ~~ odd n -> (pd n) = (pd n./2).*2.
   Proof.
     case: n => //= n _ Ho.
-    rewrite /px.
+    rewrite /pd.
     rewrite -pred_Sn.
     rewrite negbK in Ho.
     (* rewrite Nat.testbit_div2. *)
@@ -130,54 +173,54 @@ Section a.
       Check n = n./2.*2.                    (* n は奇数なので成り立たない。 *)
   Admitted.
   
-  (* px 関数の引数が奇数の場合、値は 1 である。 *)
-  Lemma px_odd__1 (n : nat) (i : nat) : odd n -> px n = 1.
+  (* pd 関数の引数が奇数の場合、値は 1 である。 *)
+  Lemma pd_odd__1 (n : nat) (i : nat) : odd n -> pd n = 1.
   Proof.
     case: n => //= n Hno.
-    rewrite /px -pred_Sn.
+    rewrite /pd -pred_Sn.
     by rewrite ldiff_even_n_n1_diff_n__1 //.
   Qed.
   
 (**
 # ルーラー関数
 *) 
-  Definition rulerx (n : nat) := Nat.log2 (px n).
+  Definition rulerd (n : nat) := Nat.log2 (pd n).
 
   (* log2 をとっても、同じである。 *)
-  Compute rulerx 0.                         (* = 0 *)
-  Compute rulerx 1.                         (* = 0 *)
-  Compute rulerx 2.                         (* = 1 *)
-  Compute rulerx 3.                         (* = 0 *)
-  Compute rulerx 4.                         (* = 2 *)
-  Compute rulerx 5.                         (* = 0 *)
-  Compute rulerx 6.                         (* = 1 *)
-  Compute rulerx 7.                         (* = 0 *)
-  Compute rulerx 8.                         (* = 3 *)
+  Compute rulerd 0.                         (* = 0 *)
+  Compute rulerd 1.                         (* = 0 *)
+  Compute rulerd 2.                         (* = 1 *)
+  Compute rulerd 3.                         (* = 0 *)
+  Compute rulerd 4.                         (* = 2 *)
+  Compute rulerd 5.                         (* = 0 *)
+  Compute rulerd 6.                         (* = 1 *)
+  Compute rulerd 7.                         (* = 0 *)
+  Compute rulerd 8.                         (* = 3 *)
 
 (**
 ## ルーラー関数の性質
  *)
   
-  Lemma rulerx_0 : rulerx 0 = 0.
+  Lemma rulerd_0 : rulerd 0 = 0.
   Proof.
     done.
   Qed.
 
-  Lemma rulerx_odd (n : nat) : odd n -> rulerx n = 0.
+  Lemma rulerd_odd (n : nat) : odd n -> rulerd n = 0.
   Proof.
     move=> Ho.
-    by rewrite /rulerx px_odd__1.
+    by rewrite /rulerd pd_odd__1.
   Qed.
   
-  Lemma rulerx_even (n : nat) : (0 < n)%N -> ~~ odd n -> rulerx n = (rulerx n./2).+1.
+  Lemma rulerd_even (n : nat) : (0 < n)%N -> ~~ odd n -> rulerd n = (rulerd n./2).+1.
   Proof.
     move=> Hn He.
-    rewrite /rulerx.
+    rewrite /rulerd.
     rewrite -Nat.log2_double.
-    - rewrite px_even' => //.
+    - rewrite pd_even' => //.
       f_equal.
       lia.
-    - rewrite /px.
+    - rewrite /pd.
       Check 0 < n./2 .- n./2.-1.
       (* n は2以上だが、2のとき1になるので、成り立つはずである。 *)
   Admitted.
