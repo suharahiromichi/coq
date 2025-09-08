4234d46d7e388deb611d6128cebf2b4e76e5bd9e
SUHARA Hiromichi <suharahiromichi@gmail.com>
2025-09-08 19:06:04 +0900

    Scopeを宣言し、証明を綺麗にした。

--- math4/ssr_root2_contrarian_ssralg.v
+++ math4/ssr_root2_contrarian_ssralg.v
@@ -20,85 +20,103 @@ Unset Strict Implicit.
 Unset Printing Implicit Defensive.
 
 Section Root2.
-
-  (**
-## 補題
-   *)
-  Lemma evenP n : reflect (exists m, n = 2 * m) (~~ odd n).
-  Proof.
-    have -> : ~~ odd n = (2 %| n) by lia.
-    apply: (iffP eqP) => [md0 | [k ->]]; last by rewrite modnMr.
-    exists (n %/ 2).
-    lia.
-  Qed.
-
-  (* これだと done がうまくいかないのは、なぜだろう。 *)
-  Lemma evenP' n : reflect (exists m, n = m * 2) (~~ odd n).
-  Proof.
-    have -> : ~~ odd n = (2 %| n) by lia.
-    by apply: dvdnP.
-  Qed.
   
-  Lemma even_not_coprime p q : ~~ odd p -> ~~ odd q -> ~~ coprime p q.
-  Proof.
-    move/evenP => [p' ->].
-    move/evenP => [q' ->].
-    rewrite coprimeMl 2!coprimeMr.
-    Check ~~ [&& coprime 2 2 && coprime 2 q', coprime p' 2 & coprime p' q'].
-    done.
-  Qed.
-  
-  Lemma two_p2_implies_not_coprime (p q : nat) :
-    2 * q ^ 2 = p ^ 2 -> ~~ coprime p q.
-  Proof.
-    move=> H.
-    have Hq_even : ~~ odd p by lia.           (* q が偶数である。 *)
-    case: q H => [| q] H.
-    (* p = 0 の場合 *)
-    - rewrite /coprime gcdn0.
+  Section Nat.
+    (**
+## 自然数の補題
+     *)
+    Lemma evenP n : reflect (exists m, n = 2 * m) (~~ odd n).
+    Proof.
+      have -> : ~~ odd n = (2 %| n) by lia.
+      apply: (iffP eqP) => [md0 | [k ->]]; last by rewrite modnMr.
+      exists (n %/ 2).
       lia.
-    (* p != 0 の場合 *)
-    - have Hp_even : ~~ odd q.+1 by lia.      (* p が偶数である。 *)
-      by rewrite even_not_coprime.
-  Qed.
-
+    Qed.
+    
+    (* これだと done がうまくいかないのは、なぜだろう。 *)
+    Lemma evenP' n : reflect (exists m, n = m * 2) (~~ odd n).
+    Proof.
+      have -> : ~~ odd n = (2 %| n) by lia.
+      by apply: dvdnP.
+    Qed.
+    
+    Lemma even_not_coprime p q : ~~ odd p -> ~~ odd q -> ~~ coprime p q.
+    Proof.
+      move/evenP => [p' ->].
+      move/evenP => [q' ->].
+      rewrite coprimeMl 2!coprimeMr.
+      Check ~~ [&& coprime 2 2 && coprime 2 q', coprime p' 2 & coprime p' q'].
+      done.
+    Qed.
+    
+    Lemma two_p2_implies_not_coprime (p q : nat) :
+      2 * q ^ 2 = p ^ 2 -> ~~ coprime p q.
+    Proof.
+      move=> H.
+      have Hq_even : ~~ odd p by lia.           (* q が偶数である。 *)
+      case: q H => [| q] H.
+      (* p = 0 の場合 *)
+      - rewrite /coprime gcdn0.
+        lia.
+      (* p != 0 の場合 *)
+      - have Hp_even : ~~ odd q.+1 by lia.      (* p が偶数である。 *)
+        by rewrite even_not_coprime.
+    Qed.
+  End Nat.
+  
   Section Real.
-    Variable R : rcfType.
+    Open Scope ring_scope.
+    Import GRing.                        (* natrM *)
+    Import Num.Theory.                   (* sqr_sqrtr, eqr_nat など *)
+    
+    Variable R : rcfType.                   (* sqrt が計算できる型 *)
     
     Search "sqrt".
-    Check Num.Theory.sqr_sqrtr : forall (R : rcfType) (a : R), (0 <= a)%R -> (Num.sqrt a ^+ 2)%R = a.
+    Check Num.Theory.sqr_sqrtr : forall (R : rcfType) (a : R), 0 <= a -> Num.sqrt a ^+ 2 = a.
+    
+    (* R と nat の相互変換 *)
+    Check eqr_nat R : forall m n : nat, (m%:R == n%:R) = (m == n).
+    Check ler_nat R : forall m n : nat, (m%:R <= n%:R) = (m <= n)%N.
+    Check ltr_nat R : forall m n : nat, (m%:R < n%:R) = (m < n)%N.
     
   (**
-## 無理数である。
+## x は無理数である。
    *)
     Definition irrational (x : R) : Prop := forall (p q : nat),
-        q <> 0 -> coprime p q -> x <> (p%:R / q%:R)%R.
-
+        q <> 0 -> coprime p q -> x <> p%:R / q%:R.
+    
   (**
 ## 証明したいもの ``sqrt 2`` は無理数である。
    *)
-    Theorem irrational_sqrt_2 : irrational (Num.sqrt 2%R).
+    Theorem irrational_sqrt_2 : irrational (Num.sqrt 2).
     Proof.
       move=> p q Hq Hco Hrt.
       move/negPn/negP in Hco.
       apply/Hco/two_p2_implies_not_coprime.
 
-      have H2 : (0 <= 2)%R by done.
       apply/eqP.
-      rewrite -(Num.Theory.eqr_nat R).
+      rewrite -(eqr_nat R) natrM.
       apply/eqP.
-      rewrite GRing.natrM.
-      Check @Num.Theory.sqr_sqrtr R 2 (H2 _).
-      rewrite -(@Num.Theory.sqr_sqrtr R 2 (H2 _)).
-      - rewrite ?Hrt.
+      
+      Check @sqr_sqrtr R 2 : 0 <= 2 -> Num.sqrt 2 ^+ 2 = 2.
+      rewrite -(@sqr_sqrtr R 2).
+      
+      Check Num.sqrt 2 ^+ 2 * (q ^ 2)%:R = (p ^ 2)%:R.
+      - rewrite Hrt.
         field.
-      - apply/negP.
-        move/eqP in Hq.
-        rewrite -(Num.Theory.eqr_nat R) in Hq.
+        rewrite (eqr_nat R q 0).
+        lia.
+        
+      Check 0 <= 2.
+      - auto.
+      (*
+      - move/eqP in Hq.
+        rewrite -(eqr_nat R) in Hq.
         apply/negP.
         done.
+       *)
     Qed.
-
+    
   End Real.
 End Root2.
 
