ae1e9c04ec6c98ca16009b9fc2b58bb5e8847617
SUHARA Hiromichi <suharahiromichi@gmail.com>
2025-09-09 18:41:19 +0900

    first commit

---
+++ math4/ssr_root2_div0_ssralg.v
@@ -0,0 +1,290 @@
+(**
+2020_AW 6. 数の証明
+
+``https://www.math.nagoya-u.ac.jp/~garrigue/lecture/2020_AW/ssrcoq6.pdf``
+
+3. √2 が無理数
+
+======
+
+2021_04_29 @suharahiromichi
+ *)
+From mathcomp Require Import all_ssreflect.
+From mathcomp Require Import all_algebra.
+From mathcomp Require Import ssrZ zify ring lra.
+
+(* for test *)
+(* lt_wf_nat, gt_wf_ind, lt_wf_double_ind *)
+Require Import Wf_nat.          (* 整礎帰納法 *)
+
+Set Implicit Arguments.
+Unset Strict Implicit.
+Unset Printing Implicit Defensive.
+
+Section Root2.
+  
+  Section Nat.
+    
+    Definition even x := ~~ odd x.
+    
+    Lemma odd_square n : odd n = odd (n * n).
+    Proof.
+      rewrite oddM.
+      by rewrite andbb.
+    Qed.
+    
+    Lemma even_square n : even n = even (n * n).
+    Proof.
+      congr (~~ _).
+      by rewrite odd_square.
+    Qed.
+    
+    Lemma l_even_d2 x : even x = (2 %| x).
+    Proof.
+      by rewrite dvdn2.
+    Qed.
+  
+(**
+## 簡単な算数 ``4 \ 2x = 2 \ x``
+*)
+    Lemma l_dd4_d2 x : (4 %| x.*2) = (2 %| x).
+    Proof.
+      rewrite -muln2.
+      have -> : 4 = 2 * 2 by done.
+      by rewrite dvdn_pmul2r.
+    Qed.
+
+(**
+## n と p が偶数であることの証明
+*)
+    Lemma l_d4s x : 2 %| x -> 4 %| (x * x).
+    Proof.
+      move=> H.
+      have -> : 4 = 2 * 2 by done.
+      by apply: dvdn_mul.
+    Qed.
+
+(**
+## nは2の倍数
+*)  
+    Lemma l_d2n n p : n * n = (p * p).*2 -> 2 %| n.
+    Proof.
+      move=> H.
+      rewrite -l_even_d2.
+      rewrite even_square H.
+      by rewrite /even odd_double.
+    Qed.
+
+(**
+## pは2の倍数
+*)  
+    Lemma l_d2p (n p : nat) : n * n = (p * p).*2 -> 2 %| p.
+    Proof.
+      move=> H.
+      rewrite -l_even_d2 even_square l_even_d2.
+      rewrite -l_dd4_d2 -H l_d4s => //.
+      by apply: (l_d2n H).
+    Qed.
+
+(**
+## ``p/2 = 0 -> p = 0`` の証明
+*)  
+    Lemma l_h0_0 p : 2 %| p -> p./2 = 0 -> p = 0.
+    Proof.
+      move=> Hp.
+      move/eqP.
+      rewrite -divn2 -eqn_mul //=.
+      by rewrite mul0n => /eqP.
+    Qed.
+    
+(**
+## 直接使う補題（その１）
+*)
+    Lemma l_hh_sq x : 2 %| x -> x./2 * x./2 = (x * x) %/ 4.
+    Proof.
+      move=> H.
+      have -> : 4 = 2 * 2 by done.
+      rewrite divnMA.
+      rewrite -muln_divA //.
+      rewrite -divn_mulAC //.
+      rewrite divn2.
+      done.
+    Qed.
+
+(**
+## 直接使う補題（その２）
+*)
+    Lemma l_dq_qd x : 4 %| x -> x.*2 %/ 4 = (x %/ 4).*2.
+    Proof.
+      move=> H.
+      by rewrite -2!muln2 divn_mulAC.
+    Qed.
+    
+
+
+    Lemma ll_main_ba n : 0 < n -> n./2 < n.
+    Proof.
+      move=> H.
+      rewrite -divn2.
+      by apply: ltn_Pdiv.
+    Qed.
+    
+
+
+    Lemma ll_main_ba' n : 0 < n -> n./2 <= n.
+    Proof.
+      move=> H.
+      lia.
+    Qed.
+    
+    Lemma ll_main_ih (n p : nat) :
+      n * n = (p * p).*2 -> n./2 * n./2 = (p./2 * p./2).*2.
+    Proof.
+      move=> Hnp.
+(*
+Goal に直接 apply IH せず、一旦 p/2 = 0 に変換してからおこなう。
+ *)
+      - rewrite l_hh_sq;
+          last apply: (l_d2n Hnp).            (* nが2の倍数なら。 *)
+        rewrite Hnp.
+        rewrite l_hh_sq;
+          last apply: (l_d2p Hnp).            (* pが2の倍数なら。 *)
+        rewrite l_dq_qd;
+          last (apply: l_d4s; apply: (l_d2p Hnp)). (* p*p が4の倍数なら。 *)
+        done.
+    Qed.
+  
+(**
+## 定理1
+
+任意の自然数 n と p について，n · n = 2(p · p) ならば p = 0。
+
+非形式的な証明：
+
+- 右辺は偶数なので、左辺も偶数なので、n は 偶数である。
+- nが偶数なら、両辺は 4 の倍数なので、p も 偶数である。
+- n = 2n' と p = 2p'を代入すると、再び n'*n' = 2*p'*p' , n'<n が得られる。
+- 帰納法の仮定から p' = 0 である。
+- すなわち p = 0 である。
+
+形式的な証明：
+- Hn : 0 < n
+- Hnp : n * n = (p * p).*2
+の元で、帰納法の仮定
+
+IH : forall m, (m < n -> forall p, (m * m = (p * p).*2 -> p = 0))
+
+から p = 0 を証明する。具体的には、
+
+n/2 < n なので、
+
+- n ← n/2
+- p ← p/2
+
+という代入のもとで p/2 = 0 を証明する。p/2 = 0 ならば p = 0 といえる。
+*)
+    Lemma test m n : m <= n -> m./2 < n.
+    Proof.
+      case: m => //=.
+    Admitted.
+
+
+
+    Theorem main_thm (n p : nat) : n * n = (p * p).*2 -> p = 0.
+    Proof.
+      elim/lt_wf_ind: n p => n.               (* 清礎帰納法 *)
+      case: (posnP n) => [-> _ [] // | Hn IH p Hnp].
+      (*
+  n : nat
+  Hn : 0 < n
+  IH : forall m : nat, (m < n)%coq_nat -> forall p : nat, m * m = (p * p).*2 -> p = 0
+  p : nat
+  Hnp : n * n = (p * p).*2
+  ============================
+  p = 0
+*)
+      apply: l_h0_0.
+      Check 2 %| p.
+      - by apply: (l_d2p Hnp).                (* pは2の倍数。 *)
+      Check p./2 = 0.
+      - apply: (IH n./2).
+        Check (n./2 < n)%coq_nat.
+        + apply/ltP.
+          lia.
+        + by apply: ll_main_ih.
+          
+      Restart.
+      
+      elim: n {-2}n (leqnn n) p; try lia.
+      move=> n IH m Hn p Hnp.
+      apply: l_h0_0.
+      Check 2 %| p.
+      - by apply: (l_d2p Hnp).                (* pは2の倍数。 *)
+      Check p./2 = 0.
+      Check (IH m./2).
+      - apply: (IH m./2).
+        + lia.
+        + by apply: ll_main_ih.
+(*      
+      Restart.
+
+      move: n p => m.
+      have [n] := ubnP m.
+      elim: n m => // n IH m => /ltnSE Hn p Hmp.
+      apply: l_h0_0.
+      - by apply: (l_d2p Hmp).                (* pは2の倍数。 *)
+        
+      Check IH m./2.
+      - apply: (IH m./2).
+        + by rewrite test.
+        + by apply: ll_main_ih.
+*)
+    Qed.
+  End Nat.
+
+(*
+# 無理数
+*)
+  Section Real.
+    Open Scope ring_scope.
+    Import GRing.                        (* natrM *)
+    Import Num.Theory.                   (* sqr_sqrtr, eqr_nat など *)
+    
+    Variable R : rcfType.                   (* sqrt が計算できる型 *)
+    
+    Search "sqrt".
+    Check sqr_sqrtr : forall (R : rcfType) (a : R), 0 <= a -> Num.sqrt a ^+ 2 = a.
+    
+    (* R と nat の相互変換 *)
+    Check eqr_nat R : forall m n : nat, (m%:R == n%:R) = (m == n).
+    Check ler_nat R : forall m n : nat, (m%:R <= n%:R) = (m <= n)%N.
+    Check ltr_nat R : forall m n : nat, (m%:R < n%:R) = (m < n)%N.
+    
+(**
+## x は無理数である。
+*)
+    Definition irrational (x : R) : Prop := forall (p q : nat),
+        q <> 0 -> x <> p%:R / q%:R.
+    
+(*
+## 証明したいもの ``sqrt 2`` は無理数である。
+*)
+    Theorem irrational_sqrt_2 : irrational (Num.sqrt 2).
+    Proof.
+      move=> p q Hq Hrt.
+      apply/Hq/(@main_thm p).
+      
+      apply/eqP.
+      rewrite -(eqr_nat R) natrM -mul2n natrM.
+      rewrite -(@sqr_sqrtr R 2) //= Hrt.
+      apply/eqP.
+      field.
+      apply/negP.
+      rewrite (eqr_nat R q 0).
+      lia.
+    Qed.
+
+  End Real.
+End Root2.
+
+(* END *)
